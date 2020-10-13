754c81e5ee9a6fc6f8716d0eabc56771b0ef01d8
Alex Bradley <alex.robert.bradley@gmail.com>
2020-10-13 10:17:42 -0400

    Updated load_data.sh to check for tty
    Updated load_data.sh to see if terminal running the command is tty. If
    it is it will continue regulary with running the docker script to load
    data. If it is not tty assuming that the person running load_data is
    using Git Bash it will prepend winpty to the docker command.

--- load_data.sh
+++ load_data.sh
@@ -5,4 +5,13 @@ mkdir -p output
 # --rm removes the container after execution
 # -it makes the container interactive (particularly useful with --debug)
 # -v mounts volumes for cache, output, and copies in the local settings
-docker run --rm -it -e REGS_GOV_KEY='$1' --network host -v eregs-cache:/app/cache -v $PWD/output:/app/output -v $PWD/config/regulations-parser/local_settings.py:/app/src/local_settings.py eregs_parser_kaitlin ${@:2}
+
+LOAD_DATA="docker run --rm -it -e REGS_GOV_KEY='$1' --network host -v eregs-cache:/app/cache -v $PWD/output:/app/output -v $PWD/config/regulations-parser/local_settings.py:/app/src/local_settings.py eregs_parser_kaitlin ${@:2}"
+
+# Check to see if terminal is tty. If not do not run the -it command in docker.
+if tty -s;
+then
+  $LOAD_DATA
+else
+  winpty $LOAD_DATA
+fi
