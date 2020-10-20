f7effc0e4c1b21e3733fa390ffda2ab65d60bc64
Alex Bradley <alex.robert.bradley@gmail.com>
2020-10-20 11:12:10 -0400

    [DRAFT] EREGCSC-59
    This is a draft of the serverless setup for eRegs.

---
+++ config/regulations-site/handler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def reg_site(event, context):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "regulations.settings.dev") 
+    try:
+        from django.core.management import excecute_from_command_line
+    except ImportError:
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django.
+            )
+execute_from_command_line(sys.argv)
