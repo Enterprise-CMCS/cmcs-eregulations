896fcd42a0547d8030de1d4c48528e05d4090fcb
David Gage <whytheplatypus@gmail.com>
2020-10-20 14:07:23 -0400

    EREGCSC-58 Create a "guidance" sidebar of hardcoded html (#1)
    Creates a "guidance" sidebar of hardcoded html
    
    Guidance can now be authored for a a specific regulations part.
    
    e.g.
    Add guidance fro part 435-905, by adding a html file at `guidance/content/guidance/435-900.html`.

--- config/regulations-site/local_settings.py
+++ config/regulations-site/local_settings.py
@@ -1,5 +1,49 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'regulations.apps.RegulationsConfig',
     'notice_comment',
+    # TODO: can django be convinced it can live without the following?
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
 )
+
+SIDEBARS = (
+    'regulations.generator.sidebar.analyses.Analyses',
+    'regulations.generator.sidebar.help.Help',
+    'regulations.generator.sidebar.print_part.PrintPart',
+    'cmcs.regulations.sidebar.guidance.Guidance',
+)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": (
+                # "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
+                "regulations.context.eregs_globals",
+            ),
+            # List of callables that know how to import templates from various
+            # sources.
+            "loaders": [
+                ('django.template.loaders.cached.Loader', (
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader'))
+            ],
+        },
+        "DIRS": [
+            "%s/templates" % BASE_DIR,
+            "/var/lib/eregs/cmcs/regulations/sidebar/content",
+        ],
+    },
+]
