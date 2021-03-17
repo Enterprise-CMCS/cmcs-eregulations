48d6e45f4ac7993fd2e840561b3e462df599d7cc
Alex Bradley <alex.robert.bradley@gmail.com>
2021-03-17 10:56:17 -0400

    EREGCSC-336 guidance download (#46)
    * Added the ability to read from a directory
    
    Now added the ability to read a single file with the -f [FILE] flag
    and reading a directory with the -d [DIRECTORY] flag
    
    * Read text file and extract urls from them
    
    * Download regs from the google sheets
    
    * Extract the header from the mime type in the download
    
    * moved process files and urls into their own functions
    
    * Changed test file to test against two files
    
    * Check to see if header is already in use
    
    * fixup
    
    * fixup
    
    * Add logging for invalid URLs
    
    Logs invalid URLs from the csv files and outputs it to logs.txt
    
    * Invalid URL errors out
    
    * Explicitly check for flags
    
    Explicitly check for directory, file, or url flag before running
    
    * log errors for invalidurls continue loop

--- guidance_pipeline/main.go
+++ guidance_pipeline/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/url"
 	"os"
+	"path/filepath"
 )
 
 type Guidance struct {
@@ -13,13 +17,81 @@ type Guidance struct {
 	Regs []string `json:"regs"`
 }
 
+var file = flag.String("f", "", "supply a file of URLs to download or a csv file")
+var directory = flag.String("d", "", "give a directory of csv files")
+var csvURL = flag.String("u", "", "give a url to a csv file")
+var outputDirectory = flag.String("o", "guidance", "give the directory with which you want to output json")
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage:", "./guidance", "FILE")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] [file]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	flags := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) { flags[f.Name] = true })
+	if !(flags["f"] || flags["d"] || flags["u"]) {
+		flag.Usage()
 		return
 	}
 
-	file := os.Args[1]
+	logFile, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+	mw := io.MultiWriter(os.Stderr, logFile)
+	log.SetOutput(mw)
+
+	if len(*directory) > 0 {
+		processDirectory(*directory)
+	} else if filepath.Ext(*file) == ".txt" {
+		processURLsFromFile(*file)
+	} else if len(*csvURL) > 0 {
+		processURL(*csvURL)
+	} else {
+		processFile(*file)
+	}
+}
+
+func processDirectory(directory string) {
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".csv" {
+			path := filepath.Join(directory, file.Name())
+			processFile(path)
+		}
+	}
+}
+
+func processURLsFromFile(file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal("An error has occured :: ", err)
+	}
+	urls, err := readFile(f)
+	if err != nil {
+		log.Fatal("An error has occured :: ", err)
+	}
+	for _, u := range urls {
+		processURL(u)
+	}
+}
+
+func processURL(csvURL string) {
+	header, body, err := downloadCSV(csvURL)
+	if err != nil {
+		log.Fatal("An error has occured :: ", err)
+	}
+	defer body.Close()
+	processData(formatHeader(header), body)
+}
+
+func processFile(file string) {
 	header := formatHeader(file)
 
 	f, err := os.Open(file)
@@ -27,8 +99,11 @@ func main() {
 		log.Fatal("An error has occured :: ", err)
 	}
 	defer f.Close()
+	processData(header, f)
+}
 
-	records, err := readData(f)
+func processData(header string, data io.Reader) {
+	records, err := readCSV(data)
 
 	if err != nil {
 		log.Fatal("An error has occured :: ", err)
@@ -42,17 +117,31 @@ func main() {
 	}
 }
 
+func validURL(header string, link string) (string, error) {
+	_, err := url.ParseRequestURI(link)
+	if err != nil {
+		err := fmt.Errorf("%s %s", header, err)
+		return "", err
+	}
+	return link, nil
+}
+
 func makeMapOfRegs(header string, records [][]string) map[string][]Guidance {
 	regMap := make(map[string][]Guidance)
 
 	for _, record := range records {
 		if len(record[0]) > 0 {
+			link, err := validURL(header, record[1])
+			if err != nil {
+				log.Println(err)
+				continue
+			}
 			regs := formatRegs(record[2:])
 
 			for _, reg := range regs {
 				guidance := Guidance{
 					Name: record[0],
-					Link: record[1],
+					Link: link,
 					Regs: regs,
 				}
 
@@ -65,7 +154,7 @@ func makeMapOfRegs(header string, records [][]string) map[string][]Guidance {
 
 func writeRegsToFile(header string, regs map[string][]Guidance) error {
 	for key, reg := range regs {
-		filename := formatFilename(key)
+		filename := formatFilename(*outputDirectory, key)
 		f, err := ioutil.ReadFile(filename)
 		dataJSON, err := toJSON(f, header, reg)
 		if err != nil {
@@ -73,10 +162,12 @@ func writeRegsToFile(header string, regs map[string][]Guidance) error {
 		}
 		file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
+			err := fmt.Errorf("%s %s", filename, err)
 			return err
 		}
 		defer file.Close()
 		if err := writeData(file, dataJSON); err != nil {
+			err := fmt.Errorf("%s %s", filename, err)
 			return err
 		}
 	}
