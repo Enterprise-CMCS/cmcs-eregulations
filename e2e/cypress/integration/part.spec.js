ac1009eb31cee68f0e1620033fa4f24d632a68d1
Alex Bradley <alex.robert.bradley@gmail.com>
2021-03-29 11:29:24 -0400

    Cypress tests for section, part, and subpart view

---
+++ e2e/cypress/integration/part.spec.js
@@ -0,0 +1,40 @@
+describe("Part View", () => {
+  it("loads part 433", () => {
+    cy.visit("/433/");
+    cy.contains("State Fiscal Administration").should("be.visible");
+  });
+
+  it("loads a section view", () => {
+    cy.visit("/433/");
+    cy.get(".toc-section-number").contains("433.50").click()
+
+    cy.url().should("include", Cypress.config().baseUrl + "/433/50");
+    cy.get("h1.section-title")
+      .contains("433.50 Basis, scope, and applicability.")
+      .should("be.visible");
+  });
+
+  it("loads a subpart view", () => {
+    cy.visit("/433/");
+    cy.contains("433.51").click()
+
+    cy.contains("Subpart View").click();
+
+    cy.url().should("include", "Subpart-B");
+    cy.get("h1.section-title")
+      .contains("General Administrative Requirements State Financial Participation")
+      .should("be.visible");
+  });
+
+  it("loads a part view", () => {
+    cy.visit("/433/");
+    cy.contains("433.51").click()
+
+    cy.get("a").contains("Part View").click();
+
+    cy.url().should("include", "#433-51");
+    cy.get("#433").contains("PART 433—STATE FISCAL ADMINISTRATION").should("be.visible");
+    // cy.contains("433.10 Rates of FFP for program services.");
+    // cy.contains("433.50 Basis, scope, and applicibility.");
+  });
+});
