c8e619a05ba8145526db12d89473ac0571b8f994
Alex Bradley <alex.robert.bradley@gmail.com>
2021-03-30 10:44:28 -0400

    EREGCSC-353 Cypress (#55)
    * Initial configuration of cypress in Docker
    
    * Added skeleton of cypress folder
    * Added Dockerfile for cypress
    * Added cypress to the docker-compose.yml
    * Added make test to run the e2e test suite
    
    * Homepage spec tests.
    
    * Added tests for guidances in section view
    
    * Cypress tests for section, part, and subpart view
    
    * Created docker-compose.test.yml for tests
    
    * added more tests for part
    
    * change e2e to host network

---
+++ e2e/cypress/integration/homepage.spec.js
@@ -0,0 +1,21 @@
+describe("Homepage", () => {
+  it("loads the homepage", () => {
+    cy.visit("/");
+    cy.contains("Medicaid & CHIP Regulations");
+  });
+
+  it("clicks on part 400 and loads the page", () => {
+    cy.visit("/");
+    cy.contains("400").click()
+
+    cy.url().should('eq', Cypress.config().baseUrl + '/400/')
+    cy.contains("This is the default landing view content to be displayed if no overrides exist.")
+  });
+
+  it("allows a user to go back to the homepage by clicking the top left link", () => {
+    cy.visit("/400/");
+    cy.contains("Medicaid & CHIP Regulations").click();
+
+    cy.url().should("eq", Cypress.config().baseUrl + "/");
+  });
+});
