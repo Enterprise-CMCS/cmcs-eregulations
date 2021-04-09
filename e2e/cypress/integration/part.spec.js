425665b3c45b221dce3ca4dee959a02246623f4c
Caleb Godwin <cgodwin@fearless.tech>
2021-04-09 13:17:42 -0400

    Switched indentation from 2 to 4 spaces, except YAML

--- e2e/cypress/integration/part.spec.js
+++ e2e/cypress/integration/part.spec.js
@@ -1,40 +1,40 @@
 describe("Part View", () => {
-  it("loads part 433", () => {
-    cy.visit("/433/");
-    cy.contains("State Fiscal Administration").should("be.visible");
-  });
+    it("loads part 433", () => {
+        cy.visit("/433/");
+        cy.contains("State Fiscal Administration").should("be.visible");
+    });
 
-  it("loads a section view", () => {
-    cy.visit("/433/");
-    cy.get(".toc-section-number").contains("433.50").click()
+    it("loads a section view", () => {
+        cy.visit("/433/");
+        cy.get(".toc-section-number").contains("433.50").click()
 
-    cy.url().should("include", Cypress.config().baseUrl + "/433/50");
-    cy.get("h1.section-title")
-      .contains("433.50 Basis, scope, and applicability.")
-      .should("be.visible");
-  });
+        cy.url().should("include", Cypress.config().baseUrl + "/433/50");
+        cy.get("h1.section-title")
+            .contains("433.50 Basis, scope, and applicability.")
+            .should("be.visible");
+    });
 
-  it("loads a subpart view", () => {
-    cy.visit("/433/");
-    cy.contains("433.51").click()
+    it("loads a subpart view", () => {
+        cy.visit("/433/");
+        cy.contains("433.51").click()
 
-    cy.contains("Subpart View").click();
+        cy.contains("Subpart View").click();
 
-    cy.url().should("include", "Subpart-B");
-    cy.get("h1.section-title")
-      .contains("General Administrative Requirements State Financial Participation")
-      .should("be.visible");
-  });
+        cy.url().should("include", "Subpart-B");
+        cy.get("h1.section-title")
+            .contains("General Administrative Requirements State Financial Participation")
+            .should("be.visible");
+    });
 
-  it("loads a part view", () => {
-    cy.visit("/433/");
-    cy.contains("433.51").click()
+    it("loads a part view", () => {
+        cy.visit("/433/");
+        cy.contains("433.51").click()
 
-    cy.get("a").contains("Part View").click();
+        cy.get("a").contains("Part View").click();
 
-    cy.url().should("include", "#433-51");
-    cy.get("#433").contains("PART 433—STATE FISCAL ADMINISTRATION").should("be.visible");
-    cy.get("#433-10").contains("433.10 Rates of FFP for program services.").should("be.visible");
-    cy.get("#433-50").contains("433.50 Basis, scope, and applicability.").should("be.visible");
-  });
+        cy.url().should("include", "#433-51");
+        cy.get("#433").contains("PART 433—STATE FISCAL ADMINISTRATION").should("be.visible");
+        cy.get("#433-10").contains("433.10 Rates of FFP for program services.").should("be.visible");
+        cy.get("#433-50").contains("433.50 Basis, scope, and applicability.").should("be.visible");
+    });
 });
