5248f5b04b80d90b4e8bd77cf4e5cc1611adc01a
Alex Bradley <alex.robert.bradley@gmail.com>
2021-04-16 14:24:40 -0400

    Added types to each field added subjgrp

--- ecfr-parser/main.go
+++ ecfr-parser/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -27,28 +28,37 @@ type Subpart struct {
 	Children SubpartChildren `xml:",any"`
 }
 
-// subpart > sections & subjgrps
+type SubjectGroup struct {
+	Header string `xml:"HEAD"`
+	Type   string          `xml:"TYPE,attr"`
+	Children SubjectGroupChildren `xml:",any"`
+}
 
 type Section struct {
+	Type string `xml:"TYPE,attr"`
 	Header   string          `xml:"HEAD"`
 	Children SectionChildren `xml:",any"`
 }
 
 type Paragraph struct {
+	Type string
 	Content string `xml:",innerxml"`
 }
 
 type Extract struct {
+	Type string
 	Content string `xml:",innerxml"`
 }
 
-type Citation string
-
-// section > paragraphs
+type Citation struct {
+	Type string
+	Content string `xml:",innerxml"`
+}
 
 type PartChildren []interface{}
 type SubpartChildren []interface{}
 type SectionChildren []interface{}
+type SubjectGroupChildren []interface{}
 
 func (c *PartChildren) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	switch start.Name.Local {
@@ -66,6 +76,7 @@ func (c *PartChildren) UnmarshalXML(d *xml.Decoder, start xml.StartElement) erro
 		*c = append(*c, child)
 	default:
 		//return fmt.Errorf("Unknown XML type in Part: %+v", start)
+		fmt.Printf("Unknown XML type in Part: %+v\n", start)
 		d.Skip()
 	}
 
@@ -80,8 +91,31 @@ func (c *SubpartChildren) UnmarshalXML(d *xml.Decoder, start xml.StartElement) e
 			return err
 		}
 		*c = append(*c, child)
+	case "DIV7":
+		child := &SubjectGroup{}
+		if err := d.DecodeElement(child, &start); err != nil {
+			return err
+		}
+		*c = append(*c, child)
 	default:
 		//return fmt.Errorf("Unknown XML type in Subpart: %+v", start)
+		fmt.Printf("Unknown XML type in Subpart: %+v\n", start)
+		d.Skip()
+	}
+	return nil
+}
+
+func (c *SubjectGroupChildren) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	switch start.Name.Local {
+	case "DIV8":
+		child := &Section{}
+		if err := d.DecodeElement(child, &start); err != nil {
+			return err
+		}
+		*c = append(*c, child)
+	default:
+		//return fmt.Errorf("Unknown XML type in Subpart: %+v", start)
+		fmt.Printf("Unknown XML type in Subject Group: %+v\n", start)
 		d.Skip()
 	}
 	return nil
@@ -90,25 +124,26 @@ func (c *SubpartChildren) UnmarshalXML(d *xml.Decoder, start xml.StartElement) e
 func (c *SectionChildren) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	switch start.Name.Local {
 	case "P":
-		child := &Paragraph{}
+		child := &Paragraph{ Type: "Paragraph"}
 		if err := d.DecodeElement(child, &start); err != nil {
 			return err
 		}
 		*c = append(*c, child)
 	case "EXTRACT":
-		child := &Extract{}
+		child := &Extract{ Type: "Extract" }
 		if err := d.DecodeElement(child, &start); err != nil {
 			return err
 		}
 		*c = append(*c, child)
 	case "CITA":
-		var child Citation
-		if err := d.DecodeElement(&child, &start); err != nil {
+		child := &Citation{ Type: "Citation" }
+		if err := d.DecodeElement(child, &start); err != nil {
 			return err
 		}
 		*c = append(*c, child)
 	default:
 		//return fmt.Errorf("Unknown XML type in Section: %+v", start)
+		fmt.Printf("Unknown XML type in Section: %+v\n", start)
 		d.Skip()
 	}
 
