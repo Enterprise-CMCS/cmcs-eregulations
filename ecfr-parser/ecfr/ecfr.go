879a0823dadb15bf7125dd3f49a380a76d5ae3fb
David Gage <whytheplatypus@gmail.com>
2021-04-29 15:33:14 -0400

    EREGCSC-395 eCFR XML Parser (#63)
    * Pull part from ECFR and get top level info
    
    Decided to proceed with a strongly typed structure for the different
    elements of the documents.
    
    * Initial pattern for ordered children in XML
    
    * Move ecfr interactions into a module
    
    * Create ParsePart function to enforce interface
    
    * Created Section Children and Subpart Children
    
    * Added types to each field added subjgrp
    
    * Parse all xml in part 433
    
    * Move parseXML into own package
    
    * Initial post-processing to create paragraph labels
    
    * Outline steps for paragraph label generation
    
    * Generate labels for paragraph elements
    
    Fails at the depth where paragraphs are labeled with capital A-Z.
    
    Does not handle (a)(1) case yet nor (a) _italics_ (1)
    
    * Working paragraph name generation
    
    Adding tests for paragraph marker extraction
    
    * Create PostProcessor interface
    
    Looking at where we can move tward the smallest interface principle
    
    * Parse P identifiers with italics in between
    
    * Split out paragraph marker logic
    
    * Handle edge cases for PostProcessing P
    
    at the level of the post processing function
    
    * Consider the current P's level it's first Marker
    
    e.g. (a)(1) is level 0
    
    * Generate paragraph citation using induction
    
    Base case: no previous paragraph and label is (a) TODO: assert (a)
    
    n+1 case: use previous paragraphs label up to the level of the current
    paragraph
    
    * Output formatted json
    
    * Updated js output to render well in html
    
    * Successfully parse all history of 42 subchapter c (#64)
    
    * Successfully parse all history of 42 subchapter c
    
    * Update ecfr API to encapsulte some behavior
    
    Patch issue with regex for labels matching anything in ()
    
    * Separate UnmarshalXML and PostProcessing of regs
    
    * Add structure to the Part document
    
    * Revisions based on PR feedback
    
    * Allow user input to specify the reg parts to load
    
    * Use a context to control the lifecycle of the op
    
    * Update parsing flow
    
    * Configure eregs url with a flag
    
    * Update flow structure of eregs part construction
    
    Removed concurrency until flow is more stable
    
    * Remove extra comments
    
    * Update identifier and range in structure
    
    Co-authored-by: Alex Bradley <alex.robert.bradley@gmail.com>

---
+++ ecfr-parser/ecfr/ecfr.go
@@ -0,0 +1,142 @@
+package ecfr
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const dateFormat = "2006-01-02"
+const timeout = 10 * time.Second
+
+var (
+	ecfrSite         = urlMustParse("https://ecfr.federalregister.gov/api/versioner/v1/")
+	ecfrFullXML      = "full/%s/title-%d.xml"
+	ecfrVersionsXML  = "versions/title-%d.json"
+	ecfrStructureXML = "structure/%s/title-%d.json"
+)
+
+var client = &http.Client{
+	Transport: &http.Transport{},
+}
+
+func urlMustParse(s string) *url.URL {
+	u, err := url.Parse(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return u
+}
+
+func buildQuery(opts []FetchOption) string {
+	q := url.Values{}
+	for _, opt := range opts {
+		v := opt.Values()
+		for key, _ := range v {
+			q.Set(key, v.Get(key))
+		}
+	}
+	return q.Encode()
+}
+
+func fetch(ctx context.Context, path *url.URL, opts []FetchOption) (io.Reader, error) {
+	path.RawQuery = buildQuery(opts)
+
+	u := ecfrSite.ResolveReference(path)
+
+	c, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(c, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		if resp.StatusCode == http.StatusTooManyRequests || resp.StatusCode == http.StatusBadGateway {
+			time.Sleep(2 * time.Second)
+			return fetch(ctx, path, opts)
+		}
+		return nil, fmt.Errorf("%d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	body := bytes.NewBuffer(b)
+
+	return body, nil
+}
+
+func FetchFull(ctx context.Context, date string, title int, opts ...FetchOption) (io.Reader, error) {
+	path, err := url.Parse(fmt.Sprintf(ecfrFullXML, date, title))
+	if err != nil {
+		return nil, err
+	}
+	return fetch(ctx, path, opts)
+}
+
+func FetchStructure(ctx context.Context, date string, title int, opts ...FetchOption) (io.Reader, error) {
+	path, err := url.Parse(fmt.Sprintf(ecfrStructureXML, date, title))
+	if err != nil {
+		return nil, err
+	}
+	return fetch(ctx, path, opts)
+}
+
+func FetchVersions(ctx context.Context, title int, opts ...FetchOption) (io.Reader, error) {
+	path, err := url.Parse(fmt.Sprintf(ecfrVersionsXML, title))
+	if err != nil {
+		return nil, err
+	}
+	return fetch(ctx, path, opts)
+}
+
+type FetchOption interface {
+	Values() url.Values
+}
+
+type partOption struct {
+	part string
+}
+
+func (p *partOption) Values() url.Values {
+	v := url.Values{}
+	v.Set("part", p.part)
+	return v
+}
+
+func PartOption(part string) *partOption {
+	return &partOption{
+		part: part,
+	}
+}
+
+type subchapterOption struct {
+	chapter    string
+	subchapter string
+}
+
+func (p *subchapterOption) Values() url.Values {
+	v := url.Values{}
+	v.Set("chapter", p.chapter)
+	v.Set("subchapter", p.subchapter)
+	return v
+}
+
+func Subchapter(chapter string, subchapter string) *subchapterOption {
+	return &subchapterOption{
+		chapter:    chapter,
+		subchapter: subchapter,
+	}
+}
---
+++ ecfr-parser/eregs/eregs.go
@@ -0,0 +1,51 @@
+package eregs
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/cmsgov/cmcs-eregulations/ecfr-parser/ecfr"
+	"github.com/cmsgov/cmcs-eregulations/ecfr-parser/parseXML"
+)
+
+var BaseURL string
+
+var client = &http.Client{
+	Transport: &http.Transport{},
+}
+
+type Part struct {
+	Title     int             `json:"title,string" xml:"-"`
+	Name      string          `json:"name" xml:"-"`
+	Date      string          `json:"date" xml:"-"`
+	Structure *ecfr.Structure `json:"structure" xml:"-"`
+	Document  *parseXML.Part  `json:"document"`
+}
+
+func PostPart(ctx context.Context, p *Part) (*http.Response, error) {
+	buff := bytes.NewBuffer([]byte{})
+	enc := json.NewEncoder(buff)
+	enc.SetEscapeHTML(false)
+
+	if err := enc.Encode(p); err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, BaseURL, buff)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+
+	if resp.StatusCode >= 400 {
+		return resp, fmt.Errorf("%d", resp.StatusCode)
+	}
+	return resp, nil
+}
