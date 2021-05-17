17b397a05f283e8ba498253da0833c8abdb1a680
David Gage <whytheplatypus@gmail.com>
2021-05-17 15:00:36 -0400

    upgrade to eregs v2 (#67)
    * Getting ready to upgrade to eregs v2
    
    * Begin deploy for version 2 API
    
    * Modified Makefile for new parser
    
    * Removed old regulations-parser submodule
    
    * Added load-regulations GitHub action
    
    * Updated tests to match new data
    
    * v2 suffix in makefile for envs
    
    * Update serverless reqs for regulations-core
    
    * Add reqs for lambda django back
    
    * Remove node_modules after deploy
    
    There was too much code for the lambda
    
    * Update local dev eregs creds
    
    Co-authored-by: Caleb Godwin <cgodwin@fearless.tech>
    Co-authored-by: Alex Bradley <abradley@fearless.tech>

--- config/regulations-parser/local_settings.py
+++
@@ -1,4 +0,0 @@
-import os
-REGS_GOV_KEY = os.environ.get("REGS_GOV_KEY")
-REGS_GOV_API = 'https://api.regulations.gov/v4/'
-XML_REPO_PREFIX = 'https://raw.githubusercontent.com/kaitlin/fr-notices/master/'
