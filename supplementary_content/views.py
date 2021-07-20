57bde743a63e891358b05cac1c47ac26ff227377
Caleb Godwin <cgodwin@fearless.tech>
2021-07-20 14:28:40 -0400

    Added function to turn model data into a tree of categories

--- supplementary_content/views.py
+++ supplementary_content/views.py
@@ -2,6 +2,10 @@ import supplementary_content
 from rest_framework import serializers, generics
 from rest_framework.response import Response
 
+import logging
+logger = logging.getLogger(__name__)
+
+
 from .models import (
     Category,
     SupplementaryContent,
@@ -25,7 +29,7 @@ class SupplementaryContentSerializer(serializers.ModelSerializer):
 class ParentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ("parent", "title", "description",)
+        fields = ("id", "parent", "title", "description",)
 
     def get_fields(self):
         fields = super(ParentSerializer, self).get_fields()
@@ -39,7 +43,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ("parent", "title", "description", "supplementary_content")
+        fields = ("id", "parent", "title", "description", "supplementary_content")
 
 
 class SupplementaryContentView(generics.ListAPIView):
@@ -66,6 +70,43 @@ class SupplementaryContentView(generics.ListAPIView):
 
         serializer = self.get_serializer(queryset, many=True)
 
-        categories = Category.objects.all()
-        return Response(serializer.data)
+        data = serializer.data
 
+        tree = []
+
+
+
+        #return Response(serializer.data)
+        return Response(make_category_tree(serializer.data))
+
+
+def add_category(category):
+    return {
+        'title': category['title'],
+        'description': category['description'],
+        'supplementary_content': [],
+        'sub_categories': {},
+    }
+
+
+def make_category_tree(data):
+    tree = {}
+    for category in data:
+        stack = []
+        current = category
+        logger.warn("Processing {}".format(current['id']))
+        while current['parent'] is not None:
+            logger.warn("Added {} to the stack".format(current['parent']['id']))
+            stack.append(current['parent'])
+            current = current['parent']
+        node = tree
+        while len(stack) > 0:
+            current = stack.pop()
+            logger.warn("Removed {} from the stack".format(current['id']))
+            if current['id'] not in node:
+                node[current['id']] = add_category(current)
+            node = node[current['id']]['sub_categories']
+        if category['id'] not in node:
+            node[category['id']] = add_category(category)
+        node[category['id']]['supplementary_content'].append(category['supplementary_content'])
+    return tree
