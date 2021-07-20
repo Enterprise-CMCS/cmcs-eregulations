7098706cd48ca40928ac22aa77e3f1624dc07970
Caleb Godwin <cgodwin@fearless.tech>
2021-07-20 14:45:51 -0400

    Linting cleanup

--- supplementary_content/views.py
+++ supplementary_content/views.py
@@ -17,7 +17,7 @@ class RegulationSectionSerializer(serializers.ModelSerializer):
 
 class SupplementaryContentSerializer(serializers.ModelSerializer):
     sections = RegulationSectionSerializer(many=True)
-    
+
     class Meta:
         model = SupplementaryContent
         fields = ("url", "title", "description", "date", "created_at", "updated_at", "category", "sections")
