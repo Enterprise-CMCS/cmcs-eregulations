d7f45b036b370e9b83f2ebcb3dd5e34c9a44a029
Alex Bradley <alex.robert.bradley@gmail.com>
2021-07-26 12:00:20 -0400

    EREGCSC-743 Supplementary Content load (#100)
    * Changed some of the names of the JSON fields
    
    * Changed section to be split up into an array
    
    * fixup formatting

--- tools/guidance_pipeline/format.go
+++ tools/guidance_pipeline/format.go
@@ -5,6 +5,12 @@ import (
 	"strings"
 )
 
+type Section struct {
+	Title   string `json:"title"`
+	Part    string `json:"part"`
+	Section string `json:"section"`
+}
+
 func formatHeader(file string) string {
 	header := filepath.Base(file)
 	if strings.Contains(header, "-") {
@@ -14,15 +20,21 @@ func formatHeader(file string) string {
 	return newHeader
 }
 
-func formatRegs(regs []string) []string {
-	newRegs := make([]string, 0)
-	for _, reg := range regs {
-		if len(reg) > 0 && len(reg) < 10 { //Look into related final rule field in NPRMs
-			removeSpace := strings.ReplaceAll(reg, " ", "")
-			newRegs = append(newRegs, strings.ReplaceAll(removeSpace, ".", "-"))
+func formatSections(sections []string) []Section {
+	sectionSlice := make([]Section, 0)
+	for _, section := range sections {
+		if len(section) > 0 && len(section) < 10 { //Look into related final rule field in NPRMs
+			removeSpace := strings.ReplaceAll(section, " ", "")
+			partSection := strings.Split(removeSpace, ".")
+			sec := Section{
+				Title:   "42",
+				Part:    partSection[0],
+				Section: partSection[1],
+			}
+			sectionSlice = append(sectionSlice, sec)
 		}
 	}
-	return newRegs
+	return sectionSlice
 }
 
 func formatFilename(outputDirectory string, reg string) string {
