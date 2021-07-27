589198ff0335f282d6c3d54872d72b3538ae701e
Caleb Godwin <cgodwin@fearless.tech>
2021-07-27 14:57:47 -0400

    Added vue component to display list of supplementary content

---
+++ regulations/static/components/SupplementaryContentList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="supplementary-content-list">
+        <supplementary-content v-for="(content, index) in supplementary_content" :key="index"
+            :title="content.title"
+            :description="content.description"
+            :date="content.date"
+            :url="content.url">
+        </supplementary-content>
+    </div>
+</template>
+
+<script>
+import SupplementaryContent from './SupplementaryContent.vue'
+export default {
+    name: 'supplementary-content-list',
+
+    components: {
+        SupplementaryContent,
+    },
+
+    props: {
+        supplementary_content: Array,
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+
+    filters: {
+
+    },
+};
+</script>
