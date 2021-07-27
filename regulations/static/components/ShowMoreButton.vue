952000e950efb912d17f3590d95cfe6d24d6e6b1
Alex Bradley <alex.robert.bradley@gmail.com>
2021-07-27 13:53:37 -0400

    Added show more button with props

---
+++ regulations/static/components/ShowMoreButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="show-more-button">
+    <b>+ Show More</b> ({{count}})
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'show-more-button',
+  props: {
+    count: {
+      type: Number,
+      default: 1
+    }
+  } 
+};
+</script>
