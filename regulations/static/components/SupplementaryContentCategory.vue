85c3976da3c9d9ce6c686555a20a102a2af0e3db
Caleb Godwin <cgodwin@fearless.tech>
2021-08-02 14:58:54 -0400

    Moved dropdown arrow to left side of category info

--- regulations/static/components/SupplementaryContentCategory.vue
+++ regulations/static/components/SupplementaryContentCategory.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="supplementary-content-category">
-        <h3>{{ title }}</h3>
-        <collapse-button :name="title" state="collapsed">
+        <collapse-button :name="title" state="collapsed" class="category-toggle">
             <template v-slot:expanded><i class="fa fa-chevron-up"></i></template>
             <template v-slot:collapsed><i class="fa fa-chevron-down"></i></template>
         </collapse-button>
-        <collapsible :name="title" state="collapsed" direction="vertical"> 
-            <supplementary-content-category v-for="(category, index) in sub_categories" :key="index"
-                :title="category.title"
-                :description="category.description"
-                :supplementary_content="category.supplementary_content"
-                :sub_categories="category.sub_categories">
-            </supplementary-content-category>
-            <supplementary-content-list :supplementary_content="supplementary_content" />
-        </collapsible>
+        <div class="category">
+            <h3 class="category-title">{{ title }}</h3>
+            <span class="category-description">{{ description }}</span>
+            <collapsible :name="title" state="collapsed" direction="vertical">
+                <supplementary-content-category v-for="(category, index) in sub_categories" :key="index"
+                    :title="category.title"
+                    :description="category.description"
+                    :supplementary_content="category.supplementary_content"
+                    :sub_categories="category.sub_categories">
+                </supplementary-content-category>
+                <supplementary-content-list :supplementary_content="supplementary_content" />
+            </collapsible>
+        </div>
     </div>
 </template>
 
