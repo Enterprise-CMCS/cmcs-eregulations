a6c68de7e7d823280b6f6478c3b6631c99be6d27
Caleb Godwin <cgodwin@fearless.tech>
2021-08-02 16:29:23 -0400

    Added show more button to supplementary content list

--- regulations/static/components/SupplementaryContentList.vue
+++ regulations/static/components/SupplementaryContentList.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="supplementary-content-list">
-        <supplementary-content v-for="(content, index) in supplementary_content" :key="index"
+        <supplementary-content v-for="(content, index) in limitedContent" :key="index"
             :title="content.title"
             :description="content.description"
             :date="content.date"
             :url="content.url">
         </supplementary-content>
+        <show-more-button v-if="showMoreNeeded" :showMore="showMore" :count="contentCount"></show-more-button>
     </div>
 </template>
 
 <script>
 import SupplementaryContent from './SupplementaryContent.vue'
+import ShowMoreButton from './ShowMoreButton.vue'
+
 export default {
     name: 'supplementary-content-list',
 
     components: {
         SupplementaryContent,
+        ShowMoreButton,
     },
 
     props: {
@@ -23,6 +27,38 @@ export default {
             type: Array,
             required: true,
         },
+        limit: {
+            type: Number,
+            required: false,
+            default: 5,
+        },
+    },
+
+    data() {
+        return {
+            limitedList: true,
+        }
+    },
+
+    computed: {
+        limitedContent() {
+            if(this.limitedList) {
+                return this.supplementary_content.slice(0, this.limit);
+            }
+            return this.supplementary_content;
+        },
+        contentCount() {
+            return this.supplementary_content.length;
+        },
+        showMoreNeeded() {
+            return this.contentCount > this.limit;
+        },
+    },
+
+    methods: {
+        showMore() {
+            this.limitedList = !this.limitedList;
+        }
     },
 };
 </script>
