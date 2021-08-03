1df77862fdea5d6d217fe8daec22109a02443bc4
Alex Bradley <alex.robert.bradley@gmail.com>
2021-08-03 11:12:43 -0400

    fixup

--- serverless/createsu.py
+++ serverless/createsu.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
-from django.contrib.auth.models import User
 
 
 def handler(self, *args, **options):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmcs_regulations.settings")
+    from django.contrib.auth.models import User
+
     if not User.objects.filter(username=os.environ.get('DJANGO_ADMIN_USERNAME')).exists():
         User.objects.create_superuser(os.environ.get('DJANGO_ADMIN_USERNAME'),
                                       'user@email.com',
