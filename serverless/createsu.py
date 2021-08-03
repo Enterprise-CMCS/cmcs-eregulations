f8094b1383ea38528d87d4bf85392e7466566db5
Alex Bradley <alex.robert.bradley@gmail.com>
2021-08-03 11:38:08 -0400

    fixup

--- serverless/createsu.py
+++ serverless/createsu.py
@@ -4,6 +4,9 @@ import os
 
 def handler(self, *args, **options):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmcs_regulations.settings")
+    import django
+    django.setup()
+
     from django.contrib.auth.models import User
 
     if not User.objects.filter(username=os.environ.get('DJANGO_ADMIN_USERNAME')).exists():
