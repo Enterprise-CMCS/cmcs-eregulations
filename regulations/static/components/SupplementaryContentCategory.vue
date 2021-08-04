a42a4fd750c3e7eced7c6b6da205a2d7f77bbfdf
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2021-08-04 13:03:19 -0400

    EREGCSC-805 Create a Vue component to display supplemental content (#112)
    * Added supplementary content vue component
    
    * Added vue component to display list of supplementary content
    
    * Added unstyled supplementary content category vue component
    
    * Recursively displays sub-categories
    * Displays supplementary content list below
    * Elements are fully collapsible
    
    * Moved dropdown arrow to left side of category info
    
    * Styling for vue component
    
    * Added show more button to supplementary content list
    
    * Renamed supplementary-content vue component to supplementary-content-object
    
    * Top-level component will be supplementary-content
    
    * Added SupplementaryContent loader component
    
    * Fixed collapse 'invisible' class not working outside of left sidebar
    
    * Added supplementary content to right sidebar
    
    * Improved styling
    
    * Added clickable title
    
    * Made related rules look "nice" next to our supplementary content
    
    * Changed padding to margin to improve animation
    
    * Added get_sections method to ReaderView.
    
    * Supplementary content vue component now has list of sections
    
    * Added API base path as property for supplementary content component
    
    * Moved final rules out of subpart resources, changed display limit to 2
    
    * More than 2 clutters the right sidebar and makes subpart resources less visible
    
    * fixup

---
+++ regulations/static/components/SupplementaryContentCategory.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="supplementary-content-category">
+        <div class="category-toggle-container">
+            <collapse-button :name="title" state="collapsed" class="category-toggle">
+                <template v-slot:expanded><i class="fa fa-chevron-up"></i></template>
+                <template v-slot:collapsed><i class="fa fa-chevron-down"></i></template>
+            </collapse-button>
+        </div>
+        <div class="category">
+            <collapse-button v-bind:class="{ subcategory: subcategory }" :name="title" state="collapsed" class="category-title">
+                <template v-slot:expanded>{{ title }}</template>
+                <template v-slot:collapsed>{{ title }}</template>
+            </collapse-button>
+            <span class="category-description">{{ description }}</span>
+            <collapsible :name="title" state="collapsed" direction="vertical" class="category-content">
+                <supplementary-content-category v-for="(category, index) in sub_categories" :key="index"
+                    :subcategory="true"
+                    :title="category.title"
+                    :description="category.description"
+                    :supplementary_content="category.supplementary_content"
+                    :sub_categories="category.sub_categories">
+                </supplementary-content-category>
+                <supplementary-content-list :supplementary_content="supplementary_content" />
+            </collapsible>
+        </div>
+    </div>
+</template>
+
+<script>
+import SupplementaryContentList from './SupplementaryContentList.vue'
+import CollapseButton from './CollapseButton.vue'
+import Collapsible from './Collapsible.vue'
+
+export default {
+    name: 'supplementary-content-category',
+
+    components: {
+        SupplementaryContentList,
+        CollapseButton,
+        Collapsible,
+    },
+
+    props: {
+        subcategory: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        supplementary_content: {
+            type: Array,
+            required: false,
+        },
+        sub_categories: {
+            type: Array,
+            required: false,
+        },
+    },
+};
+</script>
