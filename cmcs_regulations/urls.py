b0840a8dff428ef9fe69fe67bac9213e1ada1e93
Phil Reese <preese@fearless.tech>
2021-08-11 15:01:51 -0400

    feat: django favicon redirect
    redirect root favicon.ico location to static images favicon.ico location in urls.py

--- cmcs_regulations/urls.py
+++ cmcs_regulations/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", include('regcore.urls')),
     path("", include('regulations.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon/favicon.ico')),
     path('admin/', admin.site.urls),
 ]
