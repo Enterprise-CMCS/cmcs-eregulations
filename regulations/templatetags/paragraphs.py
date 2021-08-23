0e1e9a307b019d6b3aecdaafb7a8f2b94e6a7b60
Caleb Godwin <cgodwin@fearless.tech>
2021-08-23 14:38:25 -0400

    Further imprvoed logic clarity on pdepth filter

--- regulations/templatetags/paragraphs.py
+++ regulations/templatetags/paragraphs.py
@@ -9,11 +9,11 @@ def pdepth(value):
     parent_type = value.get("parent_type", "")
     label_len = len(value.get("label", []))
     marker_len = len(value.get("marker", []) or [])
+    depth = label_len - section_depth
 
     if parent_type == "appendix" or label_len == 0:
         return 1
+    elif marker_len > 1:
+        return depth - (marker_len - 1)
     else:
-        depth = label_len - section_depth
-        if marker_len > 1:
-            depth = depth - (marker_len - 1)
         return depth
