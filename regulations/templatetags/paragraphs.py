724d01edc43142bef1ce9d5eaa5073c7a2bde870
Caleb Godwin <cgodwin@fearless.tech>
2021-08-23 15:24:59 -0400

    Add check for none or empty labels when generating paragraph links

--- regulations/templatetags/paragraphs.py
+++ regulations/templatetags/paragraphs.py
@@ -6,12 +6,11 @@ section_depth = 2
 
 @register.filter(name='pdepth')
 def pdepth(value):
-    parent_type = value.get("parent_type", "")
     label_len = len(value.get("label", []) or [])
     marker_len = len(value.get("marker", []) or [])
     depth = label_len - section_depth
 
-    if parent_type == "appendix" or label_len == 0:
+    if label_len == 0:
         return 1
     elif marker_len > 1:
         return depth - (marker_len - 1)
