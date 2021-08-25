cb91b8fbe6283c1e5b2407651beb1d11cfb38ce0
David Gage <whytheplatypus@gmail.com>
2021-08-25 10:49:53 -0400

    Refactor supp content use  content vs categories
    Fetches applicable content rather than categories.
    Uses serializer logic to build the finished list of categories.

--- supplementary_content/views.py
+++ supplementary_content/views.py
@@ -10,85 +10,67 @@ from .models import (
 )
 
 
-class RegulationSectionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RegulationSection
-        fields = ("title", "part", "subpart", "section")
+class RegulationSectionSerializer(serializers.Serializer):
+    title = serializers.IntegerField()
+    part = serializers.IntegerField()
+    section = serializers.IntegerField()
 
 
-class SupplementaryContentSerializer(serializers.ModelSerializer):
-    sections = RegulationSectionSerializer(many=True)
-
-    class Meta:
-        model = SupplementaryContent
-        fields = ("url", "title", "description", "date", "created_at", "updated_at", "category", "sections")
-
-
-class ParentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ("id", "parent", "title", "description",)
+class CategorySerializer(serializers.Serializer):
+    description = serializers.CharField()
+    title = serializers.CharField()
+    id = serializers.IntegerField()
 
     def get_fields(self):
-        fields = super(ParentSerializer, self).get_fields()
-        fields['parent'] = ParentSerializer()
+        fields = super().get_fields()
+        fields['parent'] = CategorySerializer()
         return fields
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    supplementary_content = SupplementaryContentSerializer(many=True)
-    parent = ParentSerializer()
+class ApplicableSupplementaryContentSerializer(serializers.ListSerializer):
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        return _make_category_tree(data)
+
+
+class SupplementaryContentSerializer(serializers.Serializer):
+    sections = RegulationSectionSerializer(many=True)
+    category = CategorySerializer()
+    url = serializers.URLField()
+    description = serializers.CharField()
+    title = serializers.CharField()
+    date = serializers.DateField()
 
     class Meta:
-        model = Category
-        fields = ("id", "parent", "title", "description", "supplementary_content")
+        list_serializer_class = ApplicableSupplementaryContentSerializer
 
 
 class SupplementaryContentView(generics.ListAPIView):
-    serializer_class = CategorySerializer
+    serializer_class = SupplementaryContentSerializer
 
     def get_queryset(self):
         section_list = self.request.GET.getlist("sections")
         title = self.kwargs.get("title")
         part = self.kwargs.get("part")
-        query = Category.objects.prefetch_related(
-            Prefetch(
-                'supplementary_content',
-                queryset=SupplementaryContent.objects.filter(approved=True).prefetch_related(
-                    Prefetch(
-                        'sections',
-                        queryset=RegulationSection.objects.filter(
-                            title=title,
-                            part=part,
-                            section__in=section_list,
-                        )
+        query = SupplementaryContent.objects \
+            .filter(
+                approved=True,
+                sections__title=title,
+                sections__part=part,
+                sections__section__in=section_list,
+            )\
+            .prefetch_related(
+                Prefetch(
+                    'sections',
+                    queryset=RegulationSection.objects.filter(
+                        title=title,
+                        part=part,
+                        section__in=section_list,
                     )
-                ).distinct(),
-            )
-        ).annotate(
-            content_count=Count(
-                'supplementary_content',
-                distinct=True,
-                filter=Q(
-                    supplementary_content__approved=True,
-                    supplementary_content__sections__title=title,
-                    supplementary_content__sections__part=part,
-                    supplementary_content__sections__section__in=section_list,
                 )
-            )
-        ).filter(content_count__gt=0)
+           ).distinct()
         return query
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(_make_category_tree(serializer.data))
 
 
 # The following functions are related to generating a JSON structure for SupplementaryContentView.
@@ -97,44 +79,39 @@ class SupplementaryContentView(generics.ListAPIView):
 # supplementary content from the resulting tree that does not match the requested sections.
 
 
-def _add_category(category):
-    return {
-        'id': category['id'],
-        'title': category['title'],
-        'description': category['description'],
-        'supplementary_content': category.get('supplementary_content', []),
-        'sub_categories': [],
-    }
+def _get_parents(category, memo):
+    if category is None:
+        return memo
 
+    memo.append(category)
+    return _get_parents(category.pop('parent', None), memo)
 
-def _make_category_stack(category):
-    stack = [category]
-    current = category
-    while current['parent'] is not None:
-        stack.append(current['parent'])
-        current = current['parent']
-    return stack
+def _make_parent_tree(parents, tree):
+    if len(parents) < 1:
+        return
 
+    parent = parents.pop()
 
-def _get_category(tree, id):
-    for category in tree:
-        if category['id'] == id:
-            return category
-    return None
+    if parent['id'] not in tree.keys():
+        tree[parent['id']] = parent
+        tree[parent['id']]['sub_categories'] = {}
+        tree[parent['id']]['supplementary_content'] = []
 
+    if len(parents) < 1:
+        return tree[parent['id']]
+
+    return _make_parent_tree(parents, tree[parent['id']]['sub_categories'])
+
+def _category_arrays(tree):
+    t = tree.values()
+    for category in t:
+       category['sub_categories'] = _category_arrays(category['sub_categories'])
+    return t
 
 def _make_category_tree(data):
-    tree = []
-    for category in data:
-        stack = _make_category_stack(category)
-        node = tree
-        while len(stack) > 0:
-            current = stack.pop()
-            sub_node = _get_category(node, current['id'])
-            if sub_node is None:
-                sub_node = _add_category(current)
-                node.append(sub_node)
-            elif 'supplementary_content' in current:
-                sub_node['supplementary_content'] = current['supplementary_content']
-            node = sub_node['sub_categories']
-    return tree
+    tree = {}
+    for content in data:
+        parents = _get_parents(content.pop('category'), [])
+        parent = _make_parent_tree(parents, tree)
+        parent['supplementary_content'].append(content)
+    return _category_arrays(tree)
