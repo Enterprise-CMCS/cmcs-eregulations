be2b7ed8aae2beeeba4014620ca9bda269c1de0f
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2021-08-26 15:11:37 -0400

    Remove supplementary content display from category view (#158)

--- supplementary_content/admin.py
+++ supplementary_content/admin.py
@@ -20,18 +20,6 @@ class SupplementaryContentAdmin(admin.ModelAdmin):
     ]
 
 
-class ContentInline(admin.StackedInline):
-    model = SupplementaryContent
-    extra = 1
-    readonly_fields = ("reg_sections",)
-    show_change_link = True
-
-    @admin.display(description="Regulation Sections")
-    def reg_sections(self, instance):
-        section_citations = [f"{s.title} {s.part}.{s.section}" for s in instance.sections.all()]
-        return ", ".join(section_citations)
-
-
 class ChildCategory(admin.StackedInline):
     model = Category
     fk_name = "parent"
@@ -42,7 +30,6 @@ class ChildCategory(admin.StackedInline):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [
-        ContentInline,
         ChildCategory,
     ]
     list_display = ("title", "parent")
