cdcca7c865b1f796d3c1aa47a1d969170597c5e9
Phil Reese <preese@fearless.tech>
2021-08-31 10:04:10 -0400

    feat: add line-clamp; move external link icon to bolded heading
    use CSS line-clamp to limit line text to nine lines to match mockup; set overflow to be ellipses; move external link icon to be at end of bolded link heading so it is not hidden by the line clamp

--- regulations/static/components/RelatedRule.vue
+++ regulations/static/components/RelatedRule.vue
@@ -1,59 +1,73 @@
 <template>
-  <div class="related-rule recent-change">
-    <a class="related-rule-title" :href="html_url" target="_blank" rel="noopener noreferrer">
-      <span class="recent-flag indicator">{{ expandedType }}</span>
-      <span class="recent-date" v-if="publication_date">{{ publication_date|formatDate }}</span> | <span class="recent-fr">{{ citation }}</span>
-      <div class="recent-title">{{ title }}</div>
-    </a>
-  </div>
+    <div class="related-rule recent-change">
+        <a
+            class="related-rule-title"
+            :href="html_url"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <span class="link-heading">
+                <span class="recent-flag indicator">{{ expandedType }}</span>
+                <span class="recent-date" v-if="publication_date">{{
+                    publication_date | formatDate
+                }}</span>
+                | <span class="recent-fr">{{ citation }}</span>
+            </span>
+            <div class="recent-title">{{ title }}</div>
+        </a>
+    </div>
 </template>
 
 <script>
-
 export default {
-  name: 'related-rule',
+    name: "related-rule",
 
-  props: {
-    title: {
-      type: String,
-      required: true,
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        citation: {
+            type: String,
+            required: true,
+        },
+        publication_date: String,
+        document_number: {
+            type: String,
+            required: true,
+        },
+        html_url: {
+            type: String,
+            required: true,
+        },
     },
-    type: {
-      type: String,
-      required: true,
-    },
-    citation: {
-      type: String,
-      required: true,
-    },
-    publication_date: String,
-    document_number: {
-      type: String,
-      required: true,
-    },
-    html_url: {
-      type: String,
-      required: true,
-    },
-  },
 
-  computed: {
-    expandedType: function() {
-      if(this.type === "Rule") {
-        return "Final";
-      }
-      return "Unknown";
+    computed: {
+        expandedType: function () {
+            if (this.type === "Rule") {
+                return "Final";
+            }
+            return "Unknown";
+        },
     },
-  },
 
-  methods: {},
-  filters: {
-    formatDate: function(value) {
-      const date = new Date(value);
-      const options = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
-      const format = new Intl.DateTimeFormat("en-US", options);
-      return format.format(date);
-    }
-  }
+    methods: {},
+    filters: {
+        formatDate: function (value) {
+            const date = new Date(value);
+            const options = {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                timeZone: "UTC",
+            };
+            const format = new Intl.DateTimeFormat("en-US", options);
+            return format.format(date);
+        },
+    },
 };
 </script>
