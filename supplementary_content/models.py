18d068f90118607d8332a918fe6a74f9a5ef72d3
Caleb Godwin <cgodwin@fearless.tech>
2021-08-31 12:34:38 -0400

    Migrated date field to year, month, and day fields
    * Allows for flexibility in date inputs, i.e.:
    	- YYYY-MM-DD
    	- YYYY-MM
    	- YYYY
    	- No date

--- supplementary_content/models.py
+++ supplementary_content/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -21,7 +22,11 @@ class SupplementaryContent(models.Model):
     url = models.URLField(unique=True, max_length=512)
     title = models.CharField(max_length=512, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    date = models.DateField(null=True, blank=True)
+
+    year = models.IntegerField(null=True, blank=True)
+    month = models.IntegerField(null=True, blank=True)
+    day = models.IntegerField(null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL, related_name="supplementary_content")
@@ -33,6 +38,13 @@ class SupplementaryContent(models.Model):
     @property
     def truncated_description(self):
         return (self.description or [])[:50]
+
+    def clean(self):
+        super().clean()
+        if self.day is not None and (self.month is None or self.year is None):
+            raise ValidationError("When day is defined, month and year must also be defined!")
+        elif self.month is not None and self.year is None:
+            raise ValidationError("When month is defined, year must also be defined!")
 
 
 class RegulationSection(models.Model):
