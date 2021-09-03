95953403c057781f40eddadbf06d6af94ed97e22
Phil Reese <preese@fearless.tech>
2021-09-03 14:09:49 -0400

    EREGCSC-855 truncate long titles (#168)
    * feat: add line-clamp; move external link icon to bolded heading
    
    use CSS line-clamp to limit line text to nine lines to match mockup; set overflow to be ellipses; move external link icon to be at end of bolded link heading so it is not hidden by the line clamp
    
    * feat: make exteranal link icon change color with hover state
    
    * chore: remove unused visited style rule

--- regulations/static/components/RelatedRules.vue
+++ regulations/static/components/RelatedRules.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <related-rule-list :rules="limitedRules"></related-rule-list>
-        <show-more-button :showMore="showMore" :count="rulesCount"></show-more-button>
+        <show-more-button
+            :showMore="showMore"
+            :count="rulesCount"
+        ></show-more-button>
     </div>
 </template>
 
 <script>
-import RelatedRuleList from './RelatedRuleList.vue'
-import ShowMoreButton from './ShowMoreButton.vue'
+import RelatedRuleList from "./RelatedRuleList.vue";
+import ShowMoreButton from "./ShowMoreButton.vue";
 export default {
     components: {
         RelatedRuleList,
@@ -33,19 +36,19 @@ export default {
         return {
             rules: [],
             limitedList: true,
-        }
+        };
     },
 
     computed: {
-      limitedRules() {
-        if(this.limitedList) {
-          return this.rules.slice(0, this.limit);
-        }
-        return this.rules;
-      },
-      rulesCount() {
-        return this.rules.length;
-      }
+        limitedRules() {
+            if (this.limitedList) {
+                return this.rules.slice(0, this.limit);
+            }
+            return this.rules;
+        },
+        rulesCount() {
+            return this.rules.length;
+        },
     },
 
     async created() {
@@ -54,13 +57,15 @@ export default {
 
     methods: {
         async fetch_rules(title, part) {
-            const response = await fetch(`https://www.federalregister.gov/api/v1/documents.json?fields[]=type&fields[]=abstract&fields[]=citation&fields[]=correction_of&fields[]=dates&fields[]=docket_id&fields[]=docket_ids&fields[]=document_number&fields[]=effective_on&fields[]=html_url&fields[]=publication_date&fields[]=regulation_id_number_info&fields[]=regulation_id_numbers&fields[]=title&order=newest&conditions[type][]=RULE&conditions[cfr][title]=${title}&conditions[cfr][part]=${part}`);
+            const response = await fetch(
+                `https://www.federalregister.gov/api/v1/documents.json?fields[]=type&fields[]=abstract&fields[]=citation&fields[]=correction_of&fields[]=dates&fields[]=docket_id&fields[]=docket_ids&fields[]=document_number&fields[]=effective_on&fields[]=html_url&fields[]=publication_date&fields[]=regulation_id_number_info&fields[]=regulation_id_numbers&fields[]=title&order=newest&conditions[type][]=RULE&conditions[cfr][title]=${title}&conditions[cfr][part]=${part}`
+            );
             const rules = await response.json();
             return rules.results;
         },
         showMore() {
-          this.limitedList = !this.limitedList
-        }
-    }
+            this.limitedList = !this.limitedList;
+        },
+    },
 };
 </script>
