f8f47fcd6ecf411abbd48d71b06739c9fb3c8445
Alex Bradley <alex.robert.bradley@gmail.com>
2021-09-28 11:01:09 -0400

    moved date column back added category

--- supplementary_content/admin.py
+++ supplementary_content/admin.py
@@ -31,7 +31,7 @@ class SectionsInline(admin.TabularInline):
 
 @admin.register(SupplementaryContent)
 class SupplementaryContentAdmin(admin.ModelAdmin):
-    list_display = ("date", "title", "description", "created_at", "updated_at")
+    list_display = ("date", "title", "description", "category", "created_at", "updated_at")
     inlines = [
         SectionsInline,
     ]
