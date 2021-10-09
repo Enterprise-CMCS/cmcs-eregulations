0f53b6854429a89e0741acbe816fb6df66dc73f1
Phil Reese <preese@fearless.tech>
2021-10-08 21:08:57 -0400

    feat: format right sidebar citation

--- regulations/templatetags/string_formatters.py
+++ regulations/templatetags/string_formatters.py
@@ -10,6 +10,12 @@ def surround(str):
 
 @register.simple_tag
 @stringfilter
+def sidebar_formatter(title, part, subpart):
+    return title + " CFR Part " + part + ", Subpart " + subpart
+
+
+@register.simple_tag
+@stringfilter
 def subpart_formatter(title, citation, node_label):
     part = citation[0]
     subpart = citation[1] if len(citation) > 1 else node_label[0]
@@ -33,9 +39,9 @@ def paragraph_formatter(title, node_label):
     section = node_label[1]
     subsection_list = node_label[2:]
 
-    title_part_section = title + " CFR § " + part + "." + section 
+    title_part_section = title + " CFR § " + part + "." + section
     return_string = title_part_section \
-            if len(subsection_list[0]) > 3 \
-            else title_part_section + "".join(map(surround, subsection_list))
+        if len(subsection_list[0]) > 3 \
+        else title_part_section + "".join(map(surround, subsection_list))
 
     return return_string
