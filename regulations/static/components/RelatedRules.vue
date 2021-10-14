f0eaeab728aae69e6e03014168261a255b8875e8
knollfear <mknoll@fearless.tech>
2021-10-14 13:04:31 -0400

    fixing some styling and text issues with the rules list implementation.

--- regulations/static/components/RelatedRules.vue
+++ regulations/static/components/RelatedRules.vue
@@ -85,7 +85,7 @@ export default {
                     }
                 )
               },
-              title: "Request for Information"
+              title: "Requests for Information"
             }
           }
         },
