0346a2deebed7eb0ca04c5005e791d1a3f9a8843
Phil Reese <preese@fearless.tech>
2021-10-18 11:48:34 -0400

    fix: use f strings to concatenate strings and vars

--- regulations/templatetags/string_formatters.py
+++ regulations/templatetags/string_formatters.py
@@ -12,7 +12,7 @@ def surround(str):
 @register.simple_tag
 @stringfilter
 def sidebar_formatter(title, part, subpart):
-    return strip_tags(title + " CFR Part " + part + ", Subpart " + subpart)
+    return strip_tags(f"{title} CFR Part {part}, Subpart {subpart}")
 
 
 @register.simple_tag
@@ -21,7 +21,7 @@ def subpart_formatter(title, citation, node_label):
     part = citation[0]
     subpart = citation[1] if len(citation) > 1 else node_label[0]
 
-    return strip_tags(title + " CFR Part " + part + ", Subpart " + subpart)
+    return strip_tags(f"{title} CFR Part {part}, Subpart {subpart}")
 
 
 @register.simple_tag
@@ -30,7 +30,7 @@ def section_formatter(title, node_label):
     part = node_label[0]
     subpart = node_label[1]
 
-    return strip_tags(title + " CFR § " + part + "." + subpart)
+    return strip_tags(f"{title} CFR § {part}.{subpart}")
 
 
 @register.simple_tag
@@ -40,7 +40,7 @@ def paragraph_formatter(title, node_label):
     section = node_label[1]
     subsection_list = node_label[2:]
 
-    title_part_section = title + " CFR § " + part + "." + section
+    title_part_section = f"{title} CFR § {part}.{section}"
     return_string = title_part_section \
         if len(subsection_list[0]) > 3 \
         else title_part_section + "".join(map(surround, subsection_list))
