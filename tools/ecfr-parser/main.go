03703bd77ede9f75b9c0ef68a732d412b4197a30
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2021-10-29 17:59:35 -0400

    Add detailed logging to parser (#216)
    * Add detailed logging for debugging purposes
    
    * Add timeout location details to ecfr.go

--- tools/ecfr-parser/main.go
+++ tools/ecfr-parser/main.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"strings"
 	"sync"
 	"time"
@@ -15,6 +14,8 @@ import (
 	"github.com/cmsgov/cmcs-eregulations/ecfr-parser/ecfr"
 	"github.com/cmsgov/cmcs-eregulations/ecfr-parser/eregs"
 	"github.com/cmsgov/cmcs-eregulations/ecfr-parser/parseXML"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const TIMELIMIT = 5000 * time.Second
@@ -24,6 +25,7 @@ var (
 	title           int
 	subchapter      SubchapterArg
 	individualParts PartsArg
+	loglevel        string
 )
 
 type SubchapterArg []string
@@ -58,23 +60,38 @@ func init() {
 	flag.Var(&individualParts, "parts", "A comma-separated list of parts to load, e.g. 457,460")
 	flag.StringVar(&eregs.BaseURL, "eregs-url", "http://localhost:8080/v2/", "A url specifying where to send eregs parts")
 	flag.IntVar(&attempts, "attempts", 1, "The number of times to attempt regulation loading")
+	flag.StringVar(&loglevel, "loglevel", "warn", "Logging severity level. One of: fatal, error, warn, info, debug, trace.")
+	flag.BoolVar(&parseXML.LogParseErrors, "log-parse-errors", true, "Output errors encountered while parsing.")
 	flag.Parse()
 
 	if title < 0 {
-		log.Fatal("Title flag is required and must be greater than 0.")
+		log.Fatal("[MAIN] Title flag is required and must be greater than 0.")
 	}
+
+	level := log.WarnLevel
+	switch loglevel {
+	case "fatal":
+		level = log.FatalLevel
+	case "error":
+		level = log.ErrorLevel
+	case "info":
+		level = log.InfoLevel
+	case "debug":
+		level = log.DebugLevel
+	case "trace":
+		level = log.TraceLevel
+	}
+	log.SetLevel(level)
 }
 
 func main() {
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
-
 	for i := 0; i < attempts; i++ {
 		if err := run(); err == nil {
 			break
 		} else if i == attempts - 1 {
-			log.Fatal("Failed to load regulations ", attempts, " times. Error: ", err)
+			log.Fatal("[MAIN] Failed to load regulations ", attempts, " times. Error: ", err)
 		} else {
-			log.Println("[ERROR] Failed to load regulations. Retrying", attempts - i - 1, "more times. Error: ", err)
+			log.Error("[MAIN] Failed to load regulations. Retrying", attempts - i - 1, "more times. Error: ", err)
 		}
 	}
 }
@@ -85,15 +102,16 @@ func run() error {
 
 	start := time.Now()
 	defer func() {
-		log.Println("[DEBUG] run time:", time.Since(start))
+		log.Debug("[MAIN] Run time:", time.Since(start))
 	}()
 
 	today := time.Now()
 
-	log.Println("[DEBUG] fetching Parts")
+	log.Info("[MAIN] Fetching parts list...")
 
 	var parts []string
 	if subchapter != nil {
+		log.Debug("[MAIN] Fetching subchapter ", subchapter, " parts list...")
 		var err error
 		parts, err = ecfr.ExtractSubchapterParts(ctx, today, title, ecfr.Subchapter(subchapter[0], subchapter[1]))
 		if err != nil {
@@ -103,17 +121,20 @@ func run() error {
 	parts = append(parts, individualParts...)
 
 	if len(parts) < 1 {
-		log.Fatal("Some number of parts must be specified")
+		log.Fatal("[MAIN] Some number of parts must be specified")
 	}
 
+	log.Debug("[MAIN] Extracting versions...")
 	versions, err := ecfr.ExtractVersions(ctx, title)
 	if err != nil {
 		return err
 	}
 
+	log.Info("[MAIN] Fetching and processing parts...")
 	var wg sync.WaitGroup
 	for _, part := range parts {
 		for date := range versions[part] {
+			log.Debug("[MAIN] Processing part ", part, " version ", date, "...")
 			reg := &eregs.Part{
 				Title:     title,
 				Name:      part,
@@ -125,53 +146,62 @@ func run() error {
 			go func(ctx context.Context, reg *eregs.Part) {
 				defer wg.Done()
 				if err := handlePart(ctx, today, reg); err != nil {
-					log.Println("[ERROR]", err)
+					log.Error(err)
 				}
 			}(ctx, reg)
 		}
 	}
+	log.Debug("[MAIN] Waiting until all parts are finished processing.")
 	wg.Wait()
+	log.Info("[MAIN] All parts finished processing!")
 
 	return nil
 }
 
 func handlePart(ctx context.Context, date time.Time, reg *eregs.Part) error {
+	log.Debug("[MAIN] Fetching structure for part ", reg.Name)
 	sbody, err := ecfr.FetchStructure(ctx, date.Format("2006-01-02"), reg.Title, ecfr.PartOption(reg.Name))
 	if err != nil {
 		return err
 	}
 
+	log.Trace("[MAIN] Decoding structure for part ", reg.Name)
 	sd := json.NewDecoder(sbody)
 	if err := sd.Decode(reg.Structure); err != nil {
 		return err
 	}
 
+	log.Debug("[MAIN] Fetching full document for part ", reg.Name)
 	body, err := ecfr.FetchFull(ctx, reg.Date, reg.Title, ecfr.PartOption(reg.Name))
 	if err != nil {
 		return err
 	}
 
+	log.Trace("[MAIN] Decoding full structure for part ", reg.Name)
 	d := xml.NewDecoder(body)
-
 	if err := d.Decode(reg.Document); err != nil {
 		return err
 	}
 
+	log.Trace("[MAIN] Running post process on structure for part ", reg.Name)
 	if err := reg.Document.PostProcess(); err != nil {
 		return err
 	}
 
+	log.Debug("[MAIN] Posting part ", reg.Name, " to eRegs")
 	resp, err := eregs.PostPart(ctx, reg)
 	if err != nil {
 		if resp != nil {
 			defer resp.Body.Close()
 			response, e := io.ReadAll(resp.Body)
 			if e != nil {
-				log.Println("[ERROR]", e)
+				log.Error(e)
 			}
 			return fmt.Errorf("%s | %s", err.Error(), string(response))
 		}
 		return err
 	}
+
+	log.Debug("[MAIN] Finished processing part ", reg.Name)
 	return nil
 }
