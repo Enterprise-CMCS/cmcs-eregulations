a4719b0877664f776a6dd7a40d5f5b5d4842ade2
Michael Knoll <42681520+knollfear@users.noreply.github.com>
2021-11-04 12:06:43 -0400

    Knollfear/csv export (#203)
    * fixing some styling and text issues with the rules list implementation.
    
    * fixing lint issues.
    
    * adding in JSON export and adding in related models into a pipe separated list.
    
    * Removed unused import that broke the linting.
    
    * fixing up json export to now pull from the model's parent class. for completeness.  These files seem similar to those generated by manage.py dumpdata.
    
    * recursively parsing the inheritance tree.

--- supplemental_content/admin.py
+++ supplemental_content/admin.py
@@ -39,6 +39,7 @@ class BaseAdmin(admin.ModelAdmin, ExportCsvMixin):
         urls = super().get_urls()
         my_urls = [
             path('export_all_csv/', self.export_all_as_csv),
+            path('export_all_json/', self.export_all_as_json),
         ]
         return my_urls + urls
 
