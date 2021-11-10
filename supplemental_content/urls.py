be269dc4d9f5775106f79bb9695ed734983005af
Alex Bradley <alex.robert.bradley@gmail.com>
2021-11-10 11:29:25 -0500

    add create route for posting sections and subparts

--- supplemental_content/urls.py
+++ supplemental_content/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import SupplementalContentView
+from .views import SupplementalContentView, SupplementalContentSectionsView
 
-urlpatterns = [path("title/<title>/part/<part>/supplemental_content", SupplementalContentView.as_view())]
+urlpatterns = [
+        path("title/<title>/part/<part>/supplemental_content", SupplementalContentView.as_view()),
+        path("supplemental_content", SupplementalContentSectionsView.as_view()),
+]
