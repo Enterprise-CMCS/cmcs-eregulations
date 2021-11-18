9b54dc8b691345b16ea0334d10fb78357c5b9855
Alex Bradley <alex.robert.bradley@gmail.com>
2021-11-18 11:05:55 -0500

    fixup

--- supplemental_content/views.py
+++ supplemental_content/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.views import exception_handler
 from django.db.models import Prefetch, Q
-from django.core import serializers
 
 from .models import (
     AbstractSupplementalContent,
