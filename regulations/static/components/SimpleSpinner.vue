2ae8fd2c1f42c8c801a32803cdd3aeb9ca2acfd6
Phil Reese <preese@fearless.tech>
2021-12-02 15:21:34 -0500

    EREGCSC-1028 - add loading spinner to Related Rules/Supp Content while fetching (#250)
    * feat: add cms loading spinner for supp content
    
    * feat: break spinner out into own component
    
    * feat: add props to spinner component
    
    * feat: add spinner to related rules and tweak styles
    
    * chore: add try/catch/finally block around async calls
    
    * chore: be consistent in using console.error msgs in catch blocks

---
+++ regulations/static/components/SimpleSpinner.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+        class="ds-u-display--flex ds-u-justify-content--center ds-u-align-items--center"
+    >
+        <span
+            class="ds-c-spinner"
+            :class="spinnerClasses"
+            :style="spinnerStyles"
+            role="status"
+        >
+            <span class="ds-u-visibility--screen-reader">Loading</span>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "simple-spinner",
+
+    props: {
+        size: {
+            type: String,
+            default: "medium",
+        },
+        filled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    computed: {
+        spinnerClasses() {
+            return {
+                "ds-c-spinner--filled": this.filled,
+                "ds-c-spinner--small": this.size === "small",
+                "ds-c-spinner--big": this.size === "large",
+            };
+        },
+
+        spinnerStyles() {
+            return {
+                margin: this.size === "small" ? "4px" : "8px",
+            };
+        },
+    },
+};
+</script>
