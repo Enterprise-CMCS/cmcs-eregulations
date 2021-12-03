b28e1e35364b324433176c74a64294233695cd91
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-03 08:31:44 +0800

    Delete regulations directory

--- regulations/views/about.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class AboutView(TemplateView):
-
-    template_name = 'regulations/about.html'
--- regulations/views/errors.py
+++
@@ -1,3 +0,0 @@
-
-class NotInSubpart(Exception):
-    pass
