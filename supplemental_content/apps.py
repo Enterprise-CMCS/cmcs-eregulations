682ef0e7350c4dff6381ccf1ecf40a552f3d62c9
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-03 08:35:19 +0800

    Delete supplemental_content directory

--- supplemental_content/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SupplementalContentConfig(AppConfig):
-    name = "supplemental_content"
-    verbose_name = "Supplemental content for regulations"
--- supplemental_content/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-from .views import SupplementalContentView
-
-urlpatterns = [path("title/<title>/part/<part>/supplemental_content", SupplementalContentView.as_view())]
