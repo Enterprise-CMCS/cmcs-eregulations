c61d16a56459708dfa4ca15d12ca61aa3a53ac72
Caleb Godwin <cgodwin@fearless.tech>
2021-12-14 13:29:38 -0500

    Middleware to render errors as json if "json_errors" get param present

---
+++ regulations/middleware/json_errors.py
@@ -0,0 +1,22 @@
+import json, traceback
+
+from django.http.response import HttpResponse
+
+class JsonErrors:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+    
+    def process_exception(self, request, exception):
+        if "json_errors" in request.GET:
+            error = {
+                "status": "error",
+                "exception": str(exception),
+                "traceback": traceback.extract_tb(exception.__traceback__).format(),
+            }
+
+            return HttpResponse(json.dumps(error), content_type="application/json", status=500)
+        else:
+            return None
