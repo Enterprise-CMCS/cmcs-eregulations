10428b95c6cb4b1967888e844a8c59eba9e2dcac
Phil Reese <preese@fearless.tech>
2021-12-17 09:55:42 -0500

    test: add better test

--- e2e/cypress/integration/homepage.spec.js
+++ e2e/cypress/integration/homepage.spec.js
@@ -1,3 +1,5 @@
+const mainContentId = "#main-content";
+
 describe("Homepage", { scrollBehavior: "center" }, () => {
     beforeEach(() => {
         cy.intercept("/**", (req) => {
@@ -13,7 +15,7 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
         cy.checkAccessibility();
     });
 
-    it("has a hidden Skip to main content button", () => {
+    it("has a hidden Skip to main content link", () => {
         cy.viewport("macbook-15");
         cy.visit("/");
         cy.get(".ds-c-skip-nav").then(($el) => {
@@ -22,7 +24,18 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
         });
     });
 
-    it("focuses the Skip to main content button after tab is pressed", () => {
+    it("should have a div id on the page that matches the href of the skip to main content link", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/");
+        cy.get(".ds-c-skip-nav").should(
+            "have.attr",
+            "href",
+            mainContentId
+        );
+        cy.get(mainContentId).should("exist");
+    });
+
+    it("focuses and displays the Skip to main content link after tab is pressed one time", () => {
         cy.viewport("macbook-15");
         cy.visit("/");
         cy.get("body").tab();
