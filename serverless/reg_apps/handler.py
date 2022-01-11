62d982749d15a2470625e786e57c0d0878f5abd9
knollfear <mknoll@fearless.tech>
2022-01-11 14:34:12 -0500

    better path for the settings module

--- serverless/reg_apps/handler.py
+++ serverless/reg_apps/handler.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmcs_regulations.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reg_apps.cmcs_regulations.settings")
 
 application = get_wsgi_application()
