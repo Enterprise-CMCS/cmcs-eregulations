d1edc0ab32ca8c3d69457f06966aa184e357dac5
Phil Reese <preese@fearless.tech>
2022-01-11 13:23:35 -0500

    test: add jest unit test plugins and one passing test

--- regulations/static/prototype/src/main.js
+++ regulations/static/prototype/src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+
 import App from "./App.vue";
-import vuetify from './plugins/vuetify'
-import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
     vuetify,
     router,
-    render: (h) => h(App)
+    render: (h) => h(App),
 }).$mount("#app");
