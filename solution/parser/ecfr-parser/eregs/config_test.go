f3947e8bff0e667878a744030839d5e4e88cd983
Caleb Godwin <cgodwin@fearless.tech>
2022-01-24 16:24:44 -0500

    90% coverage for eregs package

--- solution/parser/ecfr-parser/eregs/config_test.go
+++ solution/parser/ecfr-parser/eregs/config_test.go
@@ -2,6 +2,9 @@ package eregs
 
 import (
 	"testing"
+	"reflect"
+	"net/http/httptest"
+	"net/http"
 )
 
 func TestSubchapterArgString(t *testing.T) {
@@ -29,7 +32,7 @@ func TestSubchapterArgSet(t *testing.T) {
 		},
 		{
 			Name: "test-bad-args",
-			Input: "onetwo*three/-",
+			Input: "one-",
 			Error: true,
 		},
 	}
@@ -49,14 +52,249 @@ func TestSubchapterArgSet(t *testing.T) {
 	}
 }
 
-func TestSubchapterList(t *testing.T) {
-	
+func TestSubchapterListUnmarshalText(t *testing.T) {
+	testTable := []struct {
+		Name string
+		Input []byte
+		Output *SubchapterList
+		Error bool
+	}{
+		{
+			Name: "test-single-good",
+			Input: []byte("IV-C"),
+			Output: &SubchapterList{
+				SubchapterArg{"IV", "C"},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-multi-good",
+			Input: []byte("IV-C, AB-C, EF-G"),
+			Output: &SubchapterList{
+				SubchapterArg{"IV", "C"},
+				SubchapterArg{"AB", "C"},
+				SubchapterArg{"EF", "G"},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-inconsistent-spacing",
+			Input: []byte("IV-C, AB-C,EF-G"),
+			Output: &SubchapterList{
+				SubchapterArg{"IV", "C"},
+				SubchapterArg{"AB", "C"},
+				SubchapterArg{"EF", "G"},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-first-arg-bad",
+			Input: []byte("IV"),
+			Output: nil,
+			Error: true,
+		},
+		{
+			Name: "test-second-arg-bad",
+			Input: []byte("-C"),
+			Output: nil,
+			Error: true,
+		},
+		{
+			Name: "test-empty-args",
+			Input: []byte("-"),
+			Output: nil,
+			Error: true,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			var sl SubchapterList
+			err := sl.UnmarshalText(tc.Input)
+
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received (%+v)", sl)
+			} else if err == nil && !reflect.DeepEqual(&sl, tc.Output) {
+				t.Errorf("expected (%+v), received (%+v)", tc.Output, sl)
+			}
+		})
+	}
 }
 
-func TestPartList(t *testing.T) {
-	
+func TestPartListUnmarshalText(t *testing.T) {
+	testTable := []struct {
+		Name string
+		Input []byte
+		Output *PartList
+	}{
+		{
+			Name: "test-single-good",
+			Input: []byte("123"),
+			Output: &PartList{"123"},
+		},
+		{
+			Name: "test-multi-good",
+			Input: []byte("123, 456, 789"),
+			Output: &PartList{"123", "456", "789"},
+		},
+		{
+			Name: "test-inconsistent-spacing",
+			Input: []byte("123, 456,789"),
+			Output: &PartList{"123", "456", "789"},
+		},
+		{
+			Name: "test-invalid-middle-number",
+			Input: []byte("123, a, 456"),
+			Output: &PartList{"123", "456"},
+		},
+		{
+			Name: "test-single-letter",
+			Input: []byte("a"),
+			Output: &PartList{},
+		},
+		{
+			Name: "test-bad-number",
+			Input: []byte("12a3"),
+			Output: &PartList{},
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			var pl PartList
+			pl.UnmarshalText(tc.Input)
+			
+			if !reflect.DeepEqual(&pl, tc.Output) {
+				t.Errorf("expected (%+v), received (%+v)", tc.Output, pl)
+			}
+		})
+	}
 }
+
+//good config, good results
+//bad config, error fails to decode
+//fetch fails due to bad response, error
+//bad url
 
 func TestRetrieveConfig(t *testing.T) {
-	
+	testTable := []struct {
+		Name string
+		Server *httptest.Server
+		Output *ParserConfig
+		Error bool
+	}{
+		{
+			Name: "test-good-config",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path != "/parser_config" {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte(`{ "exception": "Bad URL ` + r.URL.Path + `!" }`))
+				} else {
+					w.WriteHeader(http.StatusOK)
+					w.Write([]byte(`{
+						"workers": 3,
+						"attempts": 1,
+						"loglevel": "info",
+						"upload_supplemental_locations": false,
+						"log_parse_errors": true,
+						"skip_versions": false,
+						"titles": [
+							{
+								"title": 4,
+								"subchapters": "IV-C, IX-D",
+								"parts": "123, 456"
+							},
+							{
+								"title": 5,
+								"subchapters": "AB-C, DE-F",
+								"parts": "789, 101"
+							}
+						]
+					}`))
+				}
+			})),
+			Output: &ParserConfig{
+				Workers: 3,
+				Attempts: 1,
+				LogLevel: "info",
+				UploadSupplemental: false,
+				LogParseErrors: true,
+				SkipVersions: false,
+				Titles: []*TitleConfig{
+					&TitleConfig{
+						Title: 4,
+						Subchapters: SubchapterList{
+							SubchapterArg{"IV", "C"},
+							SubchapterArg{"IX", "D"},
+						},
+						Parts: PartList{"123", "456"},
+					},
+					&TitleConfig{
+						Title: 5,
+						Subchapters: SubchapterList{
+							SubchapterArg{"AB", "C"},
+							SubchapterArg{"DE", "F"},
+						},
+						Parts: PartList{"789", "101"},
+					},
+				},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-bad-response",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{
+					"asdf": 3
+					"attempts": 1,
+					"loglevel": "info",
+					"upload_supplemental_locations": false,
+					"log_parse_errors" true,
+					"skip_versions": false,
+					"titles": [
+						{
+							"title": 4,
+							"subchapters": "IV-C, IX-D",
+							"parts": "123, 456"
+						}
+						{
+							"title": 5,
+							"subchapters": "AB-C, DE-F",
+							"parts": "789, 101"
+						}
+					]
+				}`))
+			})),
+			Output: nil,
+			Error: true,
+		},
+		{
+			Name: "test-bad-fetch",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("oops"))
+			})),
+			Output: nil,
+			Error: true,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			defer tc.Server.Close()
+			BaseURL = tc.Server.URL
+			config, err := RetrieveConfig()
+
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received (%+v)", config)
+			} else if err == nil && !reflect.DeepEqual(config, tc.Output) {
+				t.Errorf("expected (%+v), received (%+v)", tc.Output, config)
+			}
+		})
+	}
 }
