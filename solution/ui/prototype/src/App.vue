f495ab8db22e1ecc4e871b0ae6158c4edb531070
Phil Reese <preese@fearless.tech>
2022-01-24 16:17:09 -0500

    refactor: get legacy assets and styles working with prototype

--- solution/ui/prototype/src/App.vue
+++ solution/ui/prototype/src/App.vue
@@ -8,7 +8,16 @@
     </div>
 </template>
 
+<script></script>
+
 <style lang="scss">
+$font-path: "~@cmsgov/design-system/dist/fonts/"; // cmsgov font path
+$image-path: "~@cmsgov/design-system/dist/images/"; // cmsgov image path
+$fa-font-path: "~@fortawesome/fontawesome-free/webfonts";
+$eregs-image-path: "~legacy-static/images";
+
+@import "legacy/css/scss/main.scss";
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
--- solution/ui/prototype/src/views/Home.vue
+++ solution/ui/prototype/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    components: {
+        HelloWorld,
+    },
+};
 </script>
+
+<style>
+</style>
