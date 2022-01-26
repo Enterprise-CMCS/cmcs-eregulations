d980441c8dbe9c3a55b1ce9777d38425f5b88f07
Caleb Godwin <cgodwin@fearless.tech>
2022-01-26 16:38:30 -0500

    97% coverage for eCFR package

--- solution/parser/ecfr-parser/ecfr/versions_test.go
+++ solution/parser/ecfr-parser/ecfr/versions_test.go
@@ -3,6 +3,10 @@ package ecfr
 import (
 	"testing"
 	"reflect"
+	"net/http/httptest"
+	"net/http"
+	"context"
+	"time"
 )
 
 func TestPartVersions(t *testing.T) {
@@ -54,9 +58,256 @@ func TestPartVersions(t *testing.T) {
 }
 
 func TestExtractVersions(t *testing.T) {
-	
+	testTable := []struct {
+		Name string
+		Server *httptest.Server
+		Expected map[string]map[string]struct{}
+		Error bool
+	}{
+		{
+			Name: "test-valid-response",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{
+					"content_versions": [
+						{
+							"date": "2016-12-19",
+							"identifier": "10.1",
+							"name": "§ 10.1   Purpose.",
+							"part": "10",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2018-12-19",
+							"identifier": "10.10",
+							"name": "§ 10.10   Entities eligible to participate in the 340B Drug Pricing Program.",
+							"part": "10",
+							"removed": false,
+							"subpart": "B",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2018-12-19",
+							"identifier": "10.2",
+							"name": "§ 10.2   Summary of 340B Drug Pricing Program.",
+							"part": "10",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2016-01-18",
+							"identifier": "11.10",
+							"name": "§ 11.10   What definitions apply to this part?",
+							"part": "11",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2018-01-18",
+							"identifier": "11.2",
+							"name": "§ 11.2   What is the purpose of this part?",
+							"part": "11",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2019-01-18",
+							"identifier": "11.20",
+							"name": "§ 11.20   Who must submit clinical trial registration information?",
+							"part": "11",
+							"removed": false,
+							"subpart": "B",
+							"title": "42",
+							"type": "section"
+						}
+					]
+				}`))				
+			})),
+			Expected: map[string]map[string]struct{}{
+				"10": map[string]struct{}{
+					"2017-01-01": struct{}{},
+					"2018-12-19": struct{}{},
+				},
+				"11": map[string]struct{}{
+					"2017-01-01": struct{}{},
+					"2018-01-18": struct{}{},
+					"2019-01-18": struct{}{},
+				},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-server-error",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(`{ "exception": "this is expected" }`))
+			})),
+			Expected: map[string]map[string]struct{}{},
+			Error: true,
+		},
+		{
+			Name: "test-bad-json",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{ "exception" "this is bad JSON" `))
+			})),
+			Expected: map[string]map[string]struct{}{},
+			Error: true,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			defer tc.Server.Close()
+			ecfrSite = tc.Server.URL
+			ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+			defer cancel()
+
+			out, err := ExtractVersions(ctx, 42)
+
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received (%+v)", out)
+			} else if err == nil && !reflect.DeepEqual(out, tc.Expected) {
+				t.Errorf("expected (%+v), received (%+v)", tc.Expected, out)
+			}
+		})
+	}
 }
 
 func TestExtractPartVersions(t *testing.T) {
-	
+	testTable := []struct {
+		Name string
+		Server *httptest.Server
+		Expected map[string]struct{}
+		Error bool
+	}{
+		{
+			Name: "test-valid-response",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{
+					"content_versions": [
+						{
+							"date": "2016-12-19",
+							"identifier": "10.1",
+							"name": "§ 10.1   Purpose.",
+							"part": "10",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2018-12-19",
+							"identifier": "10.10",
+							"name": "§ 10.10   Entities eligible to participate in the 340B Drug Pricing Program.",
+							"part": "10",
+							"removed": false,
+							"subpart": "B",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2018-12-19",
+							"identifier": "10.2",
+							"name": "§ 10.2   Summary of 340B Drug Pricing Program.",
+							"part": "10",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2016-01-18",
+							"identifier": "11.10",
+							"name": "§ 11.10   What definitions apply to this part?",
+							"part": "11",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2018-01-18",
+							"identifier": "11.2",
+							"name": "§ 11.2   What is the purpose of this part?",
+							"part": "11",
+							"removed": false,
+							"subpart": "A",
+							"title": "42",
+							"type": "section"
+						},
+						{
+							"date": "2019-01-18",
+							"identifier": "11.20",
+							"name": "§ 11.20   Who must submit clinical trial registration information?",
+							"part": "11",
+							"removed": false,
+							"subpart": "B",
+							"title": "42",
+							"type": "section"
+						}
+					]
+				}`))				
+			})),
+			Expected: map[string]struct{}{
+				"2017-01-01": struct{}{},
+				"2018-12-19": struct{}{},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-server-error",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(`{ "exception": "this is expected" }`))
+			})),
+			Expected: map[string]struct{}{},
+			Error: true,
+		},
+		{
+			Name: "test-bad-json",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{ "exception" "this is bad JSON" `))
+			})),
+			Expected: map[string]struct{}{},
+			Error: true,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			defer tc.Server.Close()
+			ecfrSite = tc.Server.URL
+			ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+			defer cancel()
+
+			part := PartOption{
+				Part: "10",
+			}
+
+			out, err := ExtractPartVersions(ctx, 42, &part)
+
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received (%+v)", out)
+			} else if err == nil && !reflect.DeepEqual(out, tc.Expected) {
+				t.Errorf("expected (%+v), received (%+v)", tc.Expected, out)
+			}
+		})
+	}
 }
