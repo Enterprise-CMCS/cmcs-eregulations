00c4e1d622c870deb2ea8a209d4a289b1c65581e
Phil Reese <preese@fearless.tech>
2022-01-27 15:52:12 -0500

    feat: add Header to Home view

--- solution/ui/prototype/src/views/Home.vue
+++ solution/ui/prototype/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="home">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="app">
+        <Header />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 export default {
     name: "Home",
+
     components: {
-        HelloWorld,
+        Header
     },
 };
 </script>
