74e4fa4bd1a6283ce17334be89a7ce0bd019d973
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2022-01-31 11:58:30 -0500

    EREGCSC-1096 Create unit tests for eCFR parser (#317)
    * Move tests to new eRegs project structure
    
    * 90% coverage for eregs package
    
    * Add eCFR fetch function tests
    
    * fixup
    
    * JSON exceptions for clarity
    
    * Add PartOptions.Values test
    
    * Add SubchapterOption.Values test
    
    * Add eCFR extractSection test
    
    * Add eCFR ExtractStructure test
    
    * fixup
    
    * Add RangeString unmarshal test
    
    * Add IdentifierString and HTMLString unmarshal test
    
    * Add SubchapterParts test
    
    * Add ExtractSubchapterParts test
    
    * PartVersions test
    
    * 97% coverage for eCFR package
    
    * generateParagraphCitation test
    
    * Tests for unmarshaling XML in parsexml package
    
    * Test ParsePart function
    
    * Initial Section PostProcess test
    
    * Add second case to section PostProcess test
    
    * Other PostProcess tests
    
    * Paragraph.Level test
    
    * Remove unused error from parsexml PostProcess
    
    * Remove Section children PostProcess
    
    * Sections should never have children
    
    * Add logParseError test
    
    * Remove length < 2 error case for extractMarker
    
    * It can't happen
    
    * 90% coverage for parsexml package
    
    * Test init function
    
    * Test get LogLevel from config params
    
    * Test parseConfig
    
    * Test contains func
    
    * Add initial handlePartVersion test
    
    * Switch from reflect.DeepEqual to deep.Equal
    
    * Provides human-readable output
    
    * Complete handlePartVersion test
    
    * startHandlePartVersionWorker test
    
    * Test parseTitle
    
    * 91% coverage on main package
    
    * Linting fixup

---
+++ solution/parser/ecfr-parser/ecfr/structure_test.go
@@ -0,0 +1,329 @@
+package ecfr
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"context"
+	"time"
+
+	"github.com/go-test/deep"
+)
+
+func TestRangeStringUnmarshal(t *testing.T) {
+	input := []byte("432.1 – 432.200")
+	expected := RangeString{"432.1", "432.200"}
+	var rs RangeString
+	rs.UnmarshalText(input)
+	if diff := deep.Equal(rs, expected); diff != nil {
+		t.Errorf("output not as expected: %+v", diff)
+	}
+}
+
+func TestHTMLStringUnmarshal(t *testing.T) {
+	input := []byte("&quot;Hello world&quot; &lt;&amp; &#39;")
+	expected := HTMLString("\"Hello world\" <& '")
+	var hs HTMLString
+	hs.UnmarshalText(input)
+	if hs != expected {
+		t.Errorf("expected (%s), received (%s)", expected, hs)
+	}
+}
+
+func TestIdentifierStringUnmarshal(t *testing.T) {
+	testTable := []struct {
+		Name string
+		Input []byte
+		Expected IdentifierString
+	}{
+		{
+			Name: "test-title-identifier",
+			Input: []byte("42"),
+			Expected: IdentifierString{"42"},
+		},
+		{
+			Name: "test-chapter-identifier",
+			Input: []byte("IV"),
+			Expected: IdentifierString{"IV"},
+		},
+		{
+			Name: "test-subchapter-identifier",
+			Input: []byte("C"),
+			Expected: IdentifierString{"C"},
+		},
+		{
+			Name: "test-part-identifier",
+			Input: []byte("430"),
+			Expected: IdentifierString{"430"},
+		},
+		{
+			Name: "test-subpart-identifier",
+			Input: []byte("A"),
+			Expected: IdentifierString{"A"},
+		},
+		{
+			Name: "test-subjectgroup-identifier",
+			Input: []byte("ECFR370de681c5a0a70"),
+			Expected: IdentifierString{"ECFR370de681c5a0a70"},
+		},
+		{
+			Name: "test-section-identifier",
+			Input: []byte("430.1"),
+			Expected: IdentifierString{"430", "1"},
+		},
+		{
+			Name: "test-paragraph-identifier",
+			Input: []byte("430.1 a 1"),
+			Expected: IdentifierString{"430", "1", "a", "1"},
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			var is IdentifierString
+			is.UnmarshalText(tc.Input)
+			if diff := deep.Equal(is, tc.Expected); diff != nil {
+				t.Errorf("output not as expected: %+v", diff)
+			}
+		})
+	}
+}
+
+func TestSubchapterParts(t *testing.T) {
+	testTable := []struct {
+		Name string
+		Input Structure
+		Expected []*Structure
+		Error bool
+	}{
+		{
+			Name: "test-one-level",
+			Input: Structure{
+				Children: []*Structure{},
+			},
+			Expected: []*Structure{},
+			Error: true,
+		},
+		{
+			Name: "test-two-levels",
+			Input: Structure{
+				Children: []*Structure{
+					&Structure{
+						Children: []*Structure{},
+					},
+				},
+			},
+			Expected: []*Structure{},
+			Error: true,
+		},
+		{
+			Name: "test-three-levels",
+			Input: Structure{
+				Children: []*Structure{
+					&Structure{
+						Children: []*Structure{
+							&Structure{
+								Children: []*Structure{
+									&Structure{
+										LabelDescription: "test 1",
+									},
+									&Structure{
+										LabelDescription: "test 2",
+									},
+									&Structure{
+										LabelDescription: "test 3",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			Expected: []*Structure{
+				&Structure{
+					LabelDescription: "test 1",
+				},
+				&Structure{
+					LabelDescription: "test 2",
+				},
+				&Structure{
+					LabelDescription: "test 3",
+				},
+			},
+			Error: false,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			out, err := SubchapterParts(&tc.Input)
+			diff := deep.Equal(out, tc.Expected)
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received (%+v)", out)
+			} else if err == nil && diff != nil {
+				t.Errorf("output not as expected: %+v", diff)
+			}
+		})
+	}
+}
+
+func TestExtractSubchapterParts(t *testing.T) {
+	testTable := []struct {
+		Name string
+		Server *httptest.Server
+		Expected []string
+		Error bool
+	}{
+		{
+			Name: "test-valid-response",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{
+					"identifier": "42",
+					"label": "Title 42 - Public Health",
+					"label_level": "Title 42",
+					"label_description": "Public Health",
+					"reserved": false,
+					"type": "title",
+					"children": [
+						{
+							"identifier": "IV",
+							"label": " Chapter IV - Centers for Medicare &amp; Medicaid Services, Department of Health and Human Services",
+							"label_level": " Chapter IV",
+							"label_description": "Centers for Medicare &amp; Medicaid Services, Department of Health and Human Services",
+							"reserved": false,
+							"type": "chapter",
+							"children": [
+								{
+									"identifier": "C",
+									"label": "Subchapter C - Medical Assistance Programs",
+									"label_level": "Subchapter C",
+									"label_description": "Medical Assistance Programs",
+									"reserved": false,
+									"type": "subchapter",
+									"children": [
+										{
+											"identifier": "430",
+											"label": "Part 430 - Grants to States for Medical Assistance Programs",
+											"label_level": "Part 430",
+											"label_description": "Grants to States for Medical Assistance Programs",
+											"reserved": false,
+											"type": "part",
+											"volumes": [
+												"4"
+											],
+											"children": [],
+											"descendant_range": "430.0 – 430.104"
+										},
+										{
+											"identifier": "431",
+											"label": "Part 431 - State Organization and General Administration",
+											"label_level": "Part 431",
+											"label_description": "State Organization and General Administration",
+											"reserved": false,
+											"type": "part",
+											"volumes": [
+												"4"
+											],
+											"children": [],
+											"descendant_range": "431.1 – 431.1010"
+										},
+										{
+											"identifier": "432",
+											"label": "Part 432 - State Personnel Administration",
+											"label_level": "Part 432",
+											"label_description": "State Personnel Administration",
+											"reserved": false,
+											"type": "part",
+											"volumes": [
+												"4"
+											],
+											"children": [],
+											"descendant_range": "432.1 – 432.55"
+										}
+									],
+									"descendant_range": "430 – 456"
+								}
+							],
+							"descendant_range": "400 – 699"
+						}
+					]
+				}`))
+			})),
+			Expected: []string{"430", "431", "432"},
+			Error: false,
+		},
+		{
+			Name: "test-server-error",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(`{ "exception": "All is well" }`))
+			})),
+			Expected: []string{},
+			Error: true,
+		},
+		{
+			Name: "test-bad-json",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{ "what" "this json won't decode properly"`))
+			})),
+			Expected: []string{},
+			Error: true,
+		},
+		{
+			Name: "test-bad-depth",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{
+					"identifier": "42",
+					"label": "Title 42 - Public Health",
+					"label_level": "Title 42",
+					"label_description": "Public Health",
+					"reserved": false,
+					"type": "title",
+					"children": [
+						{
+							"identifier": "IV",
+							"label": " Chapter IV - Centers for Medicare &amp; Medicaid Services, Department of Health and Human Services",
+							"label_level": " Chapter IV",
+							"label_description": "Centers for Medicare &amp; Medicaid Services, Department of Health and Human Services",
+							"reserved": false,
+							"type": "chapter",
+							"children": [],
+							"descendant_range": "400 – 699"
+						}
+					]
+				}`))
+			})),
+			Expected: []string{},
+			Error: true,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.Name, func(t *testing.T) {
+			defer tc.Server.Close()
+			EcfrSite = tc.Server.URL
+			ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+			defer cancel()
+			subchapter := SubchapterOption{
+				Chapter: "IV",
+				Subchapter: "C",
+			}
+
+			out, err := ExtractSubchapterParts(ctx, time.Now(), 42, &subchapter)
+
+			diff := deep.Equal(out, tc.Expected)
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received (%+v)", out)
+			} else if err == nil && diff != nil {
+				t.Errorf("output not as expected: %+v", diff)
+			}
+		})
+	}
+}
---
+++ solution/parser/ecfr-parser/eregs/eregs_test.go
@@ -0,0 +1,138 @@
+package eregs
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"context"
+	"time"
+	"fmt"
+
+	"github.com/cmsgov/cmcs-eregulations/ecfr-parser/ecfr"
+
+	"github.com/go-test/deep"
+)
+
+func TestPostPart(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{ "exception": "Bad URL ` + r.URL.Path + `!" }`))
+		}
+	}))
+	defer server.Close()
+	BaseURL = server.URL
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	defer cancel()
+
+	part := &Part{
+		Title: 1,
+		Name: "test",
+		Date: "2022-01-01",
+		Structure: nil,
+		Document: nil,
+	}
+
+	if err := PostPart(ctx, part); err != nil {
+		t.Errorf("received error (%+v)", err)
+	}
+}
+
+func TestPostSupplementalPart(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/supplemental_content" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{ "exception": "Bad URL ` + r.URL.Path + `!" }`))
+		}
+	}))
+	defer server.Close()
+	BaseURL = server.URL
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	defer cancel()
+
+	part := ecfr.Part{
+		Name: "test",
+		Title: "1",
+		Sections: []ecfr.Section{
+			ecfr.Section{
+				Title: "1",
+				Part: "2",
+				Section: "3",
+			},
+		},
+	}
+
+	if err := PostSupplementalPart(ctx, part); err != nil {
+		t.Errorf("received error (%+v)", err)
+	}
+}
+
+func TestGetExistingParts(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != fmt.Sprintf(partURL, 42) {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{ "exception": "Bad URL ` + r.URL.Path + `!" }`))
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`[
+				{
+					"date": "2017-08-04",
+					"partName": [
+						"431",
+						"457"
+					]
+				},
+				{
+					"date": "2021-08-13",
+					"partName": [
+						"455"
+					]
+				},
+				{
+					"date": "2019-08-02",
+					"partName": [
+						"460"
+					]
+				},
+				{
+					"date": "2020-12-31",
+					"partName": [
+						"438",
+						"433",
+						"447",
+						"456"
+					]
+				}
+			]`))
+		}
+	}))
+	defer server.Close()
+	BaseURL = server.URL
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	defer cancel()
+
+	results, err := GetExistingParts(ctx, 42)
+	if err != nil {
+		t.Errorf("received error (%+v)", err)
+	}
+
+	expected := map[string][]string{
+		"2017-08-04": []string{"431", "457"},
+		"2019-08-02": []string{"460"},
+		"2020-12-31": []string{"438", "433", "447", "456"},
+		"2021-08-13": []string{"455"},
+	}
+
+	if diff := deep.Equal(results, expected); diff != nil {
+		t.Errorf("output not as expected: %+v", diff)
+	}
+}
