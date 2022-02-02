3bef7ad4ec1d6ecf89b12256c6b1bd351147b112
Phil Reese <preese@fearless.tech>
2022-02-02 12:25:48 -0500

    EREGCSC-1117 Duplicate eRegs landing page in Prototype (#321)
    * chore: formatting
    
    * chore: use named routes
    
    * chore: fix prototype.clean Make recipe
    
    * refactor: get legacy assets and styles working with prototype
    
    * chore: tweak prototype.clean docker command
    
    * refactor: reorg directories in docker container to mount legacy assets
    
    * style: update eslint rules
    
    * feat: first pass at About page
    
    * feat: tidy up about; first pass at flash banner/header
    
    * feat: header mobile search input open/close
    
    * feat: add Header to Home view
    
    * feat: add hero and jump-to
    
    * feat: bring in existing RelatedRules vue component
    
    * chore: lint related-rules vue component
    
    * chore: ignore cypress test artifacts
    
    * feat: add basic footer
    
    * feat: import favicons
    
    * fix: header box sizing issue
    
    * chore: add footer component to about page
    
    * chore: remove django template artifacts

---
+++ solution/ui/prototype/src/components/FlashBanner.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="flash-banner"
+        role="banner"
+    >
+        <p>
+            This is a CMCS project with draft content for CMCS staff.
+            <router-link :to="{ name: 'about' }">
+                About this tool.
+            </router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
