773e5b1ce191514b8d8bc0dd39fb5f95f185977a
Michael Knoll <42681520+knollfear@users.noreply.github.com>
2022-02-23 15:58:05 -0500

    Eregcmsc 1184 resources modal (#343)
    * chore: push up starting work
    
    * CORS setup for cross origin calls
    
    * new line at the end of the file was missing
    
    * feat: mock lastUpdated call
    
    * feat: handle loading state
    
    * feat: fetch from local API
    
    * chore: remove mock data from dal
    
    * chore: clean up legacy component to clear vue warnings
    
    * chore: tweak eslintrc
    
    * chore: path and name cleanup
    
    * feat: load and transform homepage toc structure data
    
    * feat: build out TOC on homepage
    
    * feat: very basic dynamic routing to part
    
    * feat: import common components to part view
    
    * Add simple endpoint to return a sorted list of parts
    
    * feat: get parts and enable JumpTo
    
    * feat: new endpoint and update config
    
    * chore: update apiPath
    
    * feat: add cors regex for deployed prototype
    
    * chore: add comma
    
    * chore: update static-assets location that broke make local
    
    * SUpplemental content modal
    
    * full screen looking good, need to figue out collapse option.
    
    * collapse working.  Need to figure out how to start in collapsed mode
    
    * going to explore flexpane system to see if that works.
    
    * refactoring to be not a dialog, but use a fixed position element over the bottom of the page.
    
    * removing "selected Section helper"
    
    * installed localforage
    
    Co-authored-by: Phil Reese <preese@fearless.tech>
    Co-authored-by: Caleb Godwin <cgodwin@fearless.tech>

---
+++ solution/ui/prototype/src/components/SectionResources.vue
@@ -0,0 +1,250 @@
+<template>
+        <div
+            class="footer-panel"
+            :class="{fullHeight:!collapsed, halfHeight:collapsed}"
+        >
+            <div
+                class="centered-container"
+                style="margin-bottom: 25px"
+            >
+                <b style="font-size:30px">§{{ part }}.{{ section }} Resources </b>
+                <a style="font-size:14px; margin-left:15px"> Show All Resources</a>
+                <v-btn-toggle
+                    style="float:right"
+                >
+                    <v-btn
+                        @click="collapse"
+                        v-if="collapsed"
+                    >
+                        <v-icon>mdi-chevron-up</v-icon>
+                        Expand
+                    </v-btn>
+                    <v-btn
+                        @click="collapse"
+                        v-else
+                    >
+                        <v-icon>mdi-chevron-down</v-icon>
+                        Collapse
+                    </v-btn>
+                  
+                    <v-btn
+                        @click="$emit('close')"
+                    >
+                        <v-icon>mdi-close</v-icon>
+                        Close
+                    </v-btn>
+                </v-btn-toggle>
+            </div>
+            <div class="wrapper centered-container">
+                <div class="one">
+                    <v-text-field
+                        solo
+                        placeholder="Search Resources"
+                        append-icon="mdi-magnify"
+                    />
+                </div>
+                <div style="grid-column: 3; text-align:right">
+                    <label>Filter By:</label>
+                </div>
+                <div style="grid-column: 4 / 6;">
+                    <v-select
+                        v-model="selectedCategory"
+                        solo
+                        :items="availableCategories"
+                        multiple
+                    />
+                </div>
+            </div>
+            <v-divider />
+
+            <v-btn-toggle
+                v-model="cardView"
+                style="float:right"
+            >
+                <v-btn>
+                    <v-icon>mdi-format-list-bulleted</v-icon>
+                    List
+                </v-btn>
+      
+                <v-btn>
+                    <v-icon>mdi-view-grid</v-icon>
+                    Grid
+                </v-btn>
+            </v-btn-toggle>
+            <div
+                v-for="category in availableContent"
+                :key="category.name"
+                class="centered-container"
+            >
+                <div class="supplemental-content-category-title">
+                    {{ category.name }}
+                </div>
+                <div
+                    v-if="cardView"
+                    class="flex-row-container"
+                >
+                    <SupplementalContentCard
+                        v-for="c in category.supplemental_content"
+                        :key="c.url"
+                        :supplemental-content="c"
+                    />
+                </div>
+                <div v-else>
+                    <supplemental-content-list
+                        v-for="c in category.supplemental_content"
+                        :key="c.url"
+                        :supplemental-content="c"
+                    />
+                </div>
+                <div
+                    v-for="subcategory in category.sub_categories"
+                    :key="subcategory.name"
+                >
+                    <div class="supplemental-content-subcategory-title">
+                        {{ subcategory.name }}
+                    </div>
+                    <div
+                        v-if="cardView"
+                        class="flex-row-container"
+                    >
+                        <SupplementalContentCard
+                            v-for="c in subcategory.supplemental_content"
+                            :key="c.url"
+                            :supplemental-content="c"
+                        />
+                    </div>
+                    <div v-else>
+                        <supplemental-content-list
+                            v-for="c in subcategory.supplemental_content"
+                            :key="c.url"
+                            :supplemental-content="c"
+                        />
+                    </div>
+                </div>
+                <v-divider />
+            </div>
+        </div>
+
+</template>
+
+<script>
+  import {getSupplementalContent} from "../utilities/api";
+  import SupplementalContentCard from "./SupplementalContentCard";
+  import SupplementalContentList from "./SupplementalContentList";
+
+  export default {
+    name: "SectionResources",
+    components: {SupplementalContentList, SupplementalContentCard},
+    props:{
+      title: String,
+      part: String,
+      section: String,
+    },
+    watch: {
+      // whenever section changes, this function will run
+      async section(newSection) {
+        try {
+            this.content = await getSupplementalContent(this.title, this.part, [newSection], null );
+        } catch (error) {
+            console.error(error);
+        }
+      }
+    },
+    data () {
+      return {
+        dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
+        selectedCategory: [],
+        content:[],
+        cardView: 1,
+        collapsed: true,
+      }
+    },
+    computed:{
+      availableCategories: function(){
+        return this.content.map(category => category.name)
+      },
+      availableContent: function (){
+        if (this.selectedCategory.length > 0){
+          return this.content.filter(category => this.selectedCategory.indexOf(category.name) >= 0)
+        }
+        else{
+          return this.content
+        }
+      }
+    },
+    async created() {
+        try {
+            this.content = await getSupplementalContent(this.title, this.part, [this.section], null );
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    methods:{
+      collapse: function (){
+        this.collapsed = !this.collapsed
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+.footer-panel{
+  background-color: #f3f3f3;
+  position:fixed;
+  bottom:0;
+  width:100%;
+  z-index:202;
+  overflow: scroll;
+}
+
+.fullHeight{
+  height:100vh;
+}
+
+.halfHeight{
+  height:50vh;
+}
+
+.supplemental-content-category-title{
+  font-size:22px;
+  font-weight:bold;
+}
+
+.supplemental-content-subcategory-title{
+  font-size:18px;
+
+}
+.centered-container{
+  width:90%;
+  margin:auto;
+}
+
+.flex-row-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+    justify-content: left;
+    margin-bottom: 40px;
+}
+
+.flex-row-container > .flex-row-item {
+    flex: 1 1 30%; /*grow | shrink | basis */
+    margin: 10px;
+    max-width:30%;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  gap: 10px;
+}
+.one {
+  grid-column: 1 / 3;
+  grid-row: 1;
+}
+</style>
