58e96705e26f93ee5360a8ba8616d24cb9314476
knollfear <mknoll@fearless.tech>
2022-02-23 12:48:16 -0500

    lint fixes

--- solution/backend/cmcs_regulations/urls.py
+++ solution/backend/cmcs_regulations/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path('report_builder/', include('report_builder.urls')),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-     name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
--- solution/backend/regulations/sitemap.py
+++ solution/backend/regulations/sitemap.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from django.urls import reverse
 from regcore.models import Part
 
-class PartSitemap(Sitemap):
 
+class PartSitemap(Sitemap):
 
     changefreq = "daily"
     priority = 0.5
@@ -13,9 +13,14 @@ class PartSitemap(Sitemap):
         results = []
         for child in children:
             if child.get('type', '') == 'subpart':
-                results.append({'title': title, 'part': part, 'subpart': child['identifier'][0], 'last_updated':last_updated})
+                results.append({
+                    'title': title,
+                    'part': part,
+                    'subpart': child['identifier'][0],
+                    'last_updated': last_updated
+                })
             # This just ends up forwarding to the subchapter page so removing for now, but we can add back if needed
-            #if child.get('type', '') == 'section':
+            # if child.get('type', '') == 'section':
             #    results.append({'title': title, 'part': part, 'section': child['identifier'][1]})
             if child['children']:
                 childResults = self.processChildren(child['children'], title, part, last_updated)
@@ -29,7 +34,11 @@ class PartSitemap(Sitemap):
         query = Part.objects.filter(title=title).filter(date__lte=date).order_by("name", "-date").distinct("name")
         results = []
         for p in query:
-            results.append({'title':p.title, 'part': p.name, 'last_updated': p.last_updated})
+            results.append({
+                'title': p.title,
+                'part': p.name,
+                'last_updated': p.last_updated
+            })
             children = p.structure['children']
             results = results + self.processChildren(children, p.title, p.name, p.last_updated)
 
@@ -44,5 +53,3 @@ class PartSitemap(Sitemap):
             if item.get(key):
                 kwargs[key] = item[key]
         return reverse("reader_view", kwargs=kwargs)
-
-
