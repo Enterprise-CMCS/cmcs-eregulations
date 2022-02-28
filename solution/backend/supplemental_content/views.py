4805a87544bebd7513b57044959715eaf1c2c3c0
Thomas Walker <twalker@fearless.tech>
2022-02-28 11:34:57 -0500

    indents

--- solution/backend/supplemental_content/views.py
+++ solution/backend/supplemental_content/views.py
@@ -42,7 +42,7 @@ class SupplementalContentView(generics.ListAPIView):
                                OpenApiParameter(name='subparts', description='What subparts would you like to filter by.',
                                                 required=False, type=arrayStrings),
                                OpenApiParameter(name='subjectgroups', description='Subject groups to filter by.',
-                                    required=False, type=arrayStrings)])
+                                                required=False, type=arrayStrings)])
     @extend_schema(description='Get a list of supplmental content')
     def get(self, *args, **kwargs):
         title = kwargs.get("title")
