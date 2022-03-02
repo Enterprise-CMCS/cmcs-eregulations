47554b754509c027119024839c6d724c1eb04096
Thomas Walker <twalker@fearless.tech>
2022-03-02 15:18:26 -0500

    Tab changes

--- solution/ui/prototype/src/components/PDPart/RightColumn.vue
+++ solution/ui/prototype/src/components/PDPart/RightColumn.vue
@@ -1,27 +1,44 @@
 <template>
-    <div>
+    <div style="width: 100%; margin: 20px">
         <div>
-            <h2 style="display: inline">
-                Part {{ part }} resources
-            </h2>
-            <span style="float:right">
+            <h2 style="display: inline">Part {{ part }} resources</h2>
+            <span style="float: right">
                 <a>Filter and sort resources</a>
             </span>
         </div>
-        Right Side
+        <div>
+            <v-text-field clearable outlined class="exdample"> </v-text-field>
+        </div>
+<TabFilters/>
     </div>
 </template>
 
 <script>
+import TabFilters from "./TabFilters.vue"
 export default {
-  name: "RightColumn",
-  props:{
-    title: {type:Number},
-    part: {type: Number}
-  }
-}
+    components: {
+        TabFilters
+    },
+    name: "RightColumn",
+    props: {
+        title: { type: Number },
+        part: { type: Number },
+    },
+    data(){
+        return{
+            tabs: null,
+            text: 'djfdhkf'
+        }
+    }
+};
 </script>
 
-<style scoped>
-
+<style>
+.v-text-field.v-text-field--enclosed .v-input__slot {
+    width: 100% !important;
+    max-width: 100% !important;
+}
+.v-slide-group__content{
+    border-bottom:1px black solid;
+}
 </style>
