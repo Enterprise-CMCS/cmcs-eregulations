caccffc687f372f0e56d09d4e1db14ce3773d973
Phil Reese <preese@fearless.tech>
2022-03-04 11:10:25 -0500

    feat: reactive sidebar title

--- solution/ui/prototype/src/components/SectionResourcesSidebar.vue
+++ solution/ui/prototype/src/components/SectionResourcesSidebar.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="sidebar-container">
-        <h3>Resources</h3>
-        <p class="empty-state-text">
+        <div class="title-container">
+            <span v-if="selectedIdentifier" class="subsection">§</span>
+            <h3 class="sidebar-title">
+                {{ sidebarTitle }} Resources
+            </h3>
+        </div>
+        <p class="empty-state-text" v-if="!selectedIdentifier">
             Resource links are curated and updated by a Medicaid subject matter
             expert on our team. Select a subpart or section on the left to view
             associated resources.
@@ -35,7 +40,7 @@ export default {
 
     data() {
         return {
-            apiPath: `${process.env.VUE_APP_API_URL}/v2/`
+            apiPath: `${process.env.VUE_APP_API_URL}/v2/`,
         };
     },
 
@@ -51,14 +56,35 @@ export default {
                 ? [this.selectedIdentifier]
                 : [];
         },
+        sidebarTitle() {
+            return this.selectedScope === "subpart"
+                ? `${this.part} Subpart ${this.selectedIdentifier}`
+                : this.selectedScope === "section"
+                ? `${this.part}.${this.selectedIdentifier}`
+                : "";
+        },
     },
 };
 </script>
 
-<style>
-.sidebar-container h3 {
-    margin-top: 0px;
-    font-size: 24px;
+<style lang="scss">
+.sidebar-container {
+    .title-container {
+        display: flex;
+        align-items: center;
+        margin-bottom: 24px;
+
+        .subsection {
+            font-size: 20px;
+            font-weight: 700;
+            margin: -5px 5px 0 0;
+        }
+
+        h3 {
+            margin: 0px;
+            font-size: 24px;
+        }
+    }
 }
 
 .empty-state-text {
