200124cd4fbf8cd482f0fcf5a3d5e23550dabf0e
Caleb Godwin <cgodwin@fearless.tech>
2022-03-07 16:14:34 -0500

    Add toc get/post endpoint

--- solution/backend/regcore/urls.py
+++ solution/backend/regcore/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from regcore.views import (
     EffectivePartView,
     EffectiveTitlesView,
@@ -10,6 +12,13 @@ from regcore.views import (
     PartListView,
 )
 
+from regcore.v3views import (
+    ContentsViewSet,
+)
+
+
+router = DefaultRouter()
+router.register("toc", ContentsViewSet)
 
 urlpatterns = [
     path("v2/", include([
@@ -24,5 +33,12 @@ urlpatterns = [
         path("<date>/title/<title>/part/<name>", EffectivePartView.as_view()),
         path("<date>/title/<title>/part/<name>/toc", EffectivePartTocView.as_view()),
         path("title/<title>/existing", ExistingPartsView.as_view()),
-    ]))
+    ])),
+    path("v3/", include([
+        path("toc", ContentsViewSet.as_view({
+            "get": "list",
+            "post": "create",
+            "delete": "destroy",
+        })),
+    ])),
 ]
--- solution/backend/regcore/v3views.py
+++ solution/backend/regcore/v3views.py
@@ -2,8 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from regcore.models import Title
+from regcore.serializers import ContentsSerializer
+from regcore.views import SettingsAuthentication
 
 class ContentsViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = ContentsSerializer
+    
+    authentication_classes = [SettingsAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
