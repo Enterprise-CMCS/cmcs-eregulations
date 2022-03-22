e09017b25798e4717c0e87ab2692ac7a22bf5bb7
Thomas W <twalker@fearless.tech>
2022-03-22 10:27:10 -0400

    subpart and section list

--- solution/ui/prototype/src/components/ResourcesPage/ResourceFilters.vue
+++ solution/ui/prototype/src/components/ResourcesPage/ResourceFilters.vue
@@ -2,7 +2,7 @@
     <div class="resourcefilters">
         <h2>Filters</h2>
         <h3>Resource Type</h3>
-     <treeselect v-model="value" :multiple="true" :options="this.catOptions" />
+     <treeselect v-model="resourcesValues" :multiple="true" :options="this.catOptions" />
 
         <h3>Title</h3>
   
@@ -17,6 +17,7 @@
             multiple
             v-model="selectedParts"
             :items="parts"
+            item-text='name'
             outlined
         ></v-select>
         <h3>Section</h3>
@@ -24,13 +25,14 @@
             multiple
             v-model="selectedSections"
             :items="sections"
+            item-text="label"
             outlined
         ></v-select>
     </div>
 </template>
 <script>
 
-import { getSupplementalContentNew, getCategories, getAllParts } from "@/utilities/api";
+import { getSupplementalContentNew, getCategories,getPartsDetails, getSubPartsandSections } from "@/utilities/api";
 import { getCategoryTree } from "@/utilities/utils";
 import Treeselect from '@riophae/vue-treeselect'
 
@@ -43,7 +45,7 @@ export default {
         titles: ["42"],
         parts: [],
         sections: ["sections"],
-        resources: ["resources"],
+        resourcesValues: [],
         selectedResources: [],
         selectedParts: [],
         selectedSections: [],
@@ -51,16 +53,16 @@ export default {
         supList: [],
         categories: [],
         catOptions:[],
-        value: null,
-
+        partandsubparts: []
     }),
  
     async created() {
         try {
             this.supList = await getSupplementalContentNew(42, 441, [], ["B"]);
-            this.parts = await getAllParts();
+            this.parts = await getPartsDetails();
             this.categories = await getCategories();
             this.catOptions = getCategoryTree(this.categories);
+            this.sections = await getSubPartsandSections();
         } catch (error) {
             console.error(error);
         } finally {
