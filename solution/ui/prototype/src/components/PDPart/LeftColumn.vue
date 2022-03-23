e5434bac6880c9e873add36a7070c33e1db00b02
knollfear <mknoll@fearless.tech>
2022-03-23 16:36:17 -0400

    fixed size of breadcrumbs and navigation

--- solution/ui/prototype/src/components/PDPart/LeftColumn.vue
+++ solution/ui/prototype/src/components/PDPart/LeftColumn.vue
@@ -9,7 +9,7 @@
                     :section="section"
                 />
             </h2>
-            <span style="float: right">
+            <span style="float: right" class="breadcrumbs">
                 <router-link
                     v-if="navigation.previous"
                     :to="{
@@ -92,4 +92,13 @@ export default {
 </script>
 
 <style scoped>
+  .breadcrumbs{
+      font-family: Open Sans;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 30px;
+      letter-spacing: 0em;
+      text-align: left;
+  }
 </style>
