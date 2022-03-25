35677f24fa888a248415acbe2fb1786f24c2abe6
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2022-03-25 08:52:59 -0400

    Adjust homepage hero header (#381)

--- solution/ui/prototype/src/components/homepage/Hero.vue
+++ solution/ui/prototype/src/components/homepage/Hero.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="hero-banner">
         <div class="site-container hero-content">
-            <p class="hero-text">
+            <h3 class="hero-header">eRegulations organizes together regulations, subregulatory guidance, and other related policy materials.</h3>
+            <p class="hero-text">    
                 Explore this site as a supplement to existing policy tools.
                 <router-link :to="{ name: 'about', hash: '#automated-updates' }">
                     How this tool is updated.
@@ -21,4 +22,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+    .hero-header {
+        width: 50%;
+    }
+</style>
