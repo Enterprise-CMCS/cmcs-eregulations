6ea60bec77e0e956164ec1ff59cd41606d2d8382
Caleb Godwin <cgodwin@fearless.tech>
2022-03-31 16:13:19 -0400

    Fix content-type checking by moving logic to __call__ func

--- solution/backend/regcore/middleware/html_api.py
+++ solution/backend/regcore/middleware/html_api.py
@@ -8,14 +8,8 @@ class HtmlApi:
         self.get_response = get_response
 
     def __call__(self, request):
-        return self.get_response(request)
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        if "html_api" in request.GET:
-            try:
-                response = view_func(request, *view_args, **view_kwargs)
-                data = json.dumps(response.data, indent=4)
-                return render(request, "response.html", context={'data': data})
-            except:
-                pass
-        return None
+        response = self.get_response(request)
+        if "html_api" in request.GET and response.get("Content-Type", None) == "application/json":
+            data = json.dumps(json.loads(response.content.decode("utf-8")), indent=4)
+            return render(request, "response.html", context={'data': data})
+        return response
