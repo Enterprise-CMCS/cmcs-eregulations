1f69aba254a1f11b501ddf38ceb76f125162538a
Phil Reese <preese@fearless.tech>
2022-04-11 11:38:17 -0400

    EREGCSC-444 - Print styles (#389)
    * feat: very basic print stylesheet
    
    * feat: more print styles; nail down title header stripe
    
    * feat: section text and indents
    
    * feat: current version MVP
    
    * feat: prev version styling
    
    * feat: tweaks
    
    * feat: add right sidebar print btn
    
    * feat: all special cases except tables
    
    * feat: add bold italic OpenSans font for FF printing
    
    * chore: add new scss variable to prototype components
    
    but there's gotta be a better way to reference these paths instead of including them on a per-component basis
    
    * feat: table style tweaks
    
    * feat: TOC print styles
    
    * chore: remove stray console.log
    
    * test: print button calls window.print
    
    * test: some visual tests for print styles
    
    * feat: globalize part-meta font size (source)
    
    * feat: bump up subpart paragraph font size

---
+++ solution/ui/e2e/cypress/integration/print.spec.js
@@ -0,0 +1,60 @@
+describe("Print Styles", () => {
+    const destination = "/42/433/Subpart-A/2021-03-01/";
+    const previousVersion = "/42/433/Subpart-A/2020-12-31/";
+
+    beforeEach(() => {
+        cy.intercept("/**", (req) => {
+            req.headers["x-automated-test"] = Cypress.env("DEPLOYING");
+        }).as("headers");
+
+        cy.setCssMedia("screen");
+    });
+
+    it("has a print button in subpart view", () => {
+        cy.viewport("macbook-15");
+        cy.visit(destination);
+        cy.window().then((win) => {
+            cy.stub(win, "print");
+            cy.get(".print-btn").contains("Print").click().then(() => {
+                expect(win.print).to.be.called;
+            })
+        });
+    });
+
+    it("has proper print styles for latest version", () => {
+        cy.viewport("macbook-15");
+        cy.visit(destination);
+
+        cy.get(".right-sidebar").should("be.visible");
+
+        cy.get("footer .print-footer").should("have.css", "display", "none");
+
+        cy.setCssMedia("print");
+
+        cy.get(".left-sidebar").should("have.css", "display", "none");
+        cy.get(".right-sidebar").should("have.css", "display", "none");
+        cy.get("footer .site-footer").should("have.css", "display", "none");
+        cy.get(".left-sidebar").should("have.css", "display", "none");
+
+        cy.get("footer .print-footer").should("have.css", "display", "block");
+
+        cy.get("header").should("have.css", "height", "48px");
+        cy.get("header").should("have.css", "border-top-color", "rgb(2, 102, 102)");
+        cy.get("header").should("have.css", "border-bottom-color", "rgb(2, 102, 102)");
+    })
+
+    it("has proper print styles for a previous version", () => {
+        cy.viewport("macbook-15");
+        cy.visit(previousVersion);
+
+        cy.get(".right-sidebar").should("be.visible");
+
+        cy.get(".print-view-container").should("have.css", "display", "none");
+        cy.get(".view-container").should("have.css", "display", "flex");
+
+        cy.setCssMedia("print");
+
+        cy.get(".print-view-container").should("have.css", "display", "block");
+        cy.get(".view-container").should("have.css", "display", "none");
+    })
+});
