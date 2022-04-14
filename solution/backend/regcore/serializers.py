89a3987ce1e23232a69991177530809bb626dd6b
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2022-04-14 11:59:19 -0400

    EREGCSC-1284 Make Swagger API documentation work again (#401)
    * Use values_list in viewset, serializer json fields manually
    
    * Rewrite VersionsViewSet
    
    * Fix part TOC endpoint
    
    * Fix subpart contents endpoint
    
    * Fix section and subpart list endpoints
    
    * Move all serializers to Serializer type
    
    * linting
    
    * Fix versions endpoint swagger response
    
    * adding in some annotations for v2 views
    
    * Python lint fixes
    
    * Fix random strings in parser_config swagger response
    
    * Cypress test for swagger page.
    
    * Add endpoint and parameter descriptions for V3 endpoints
    
    * Change OpenAPI parameter types where necessary
    
    Co-authored-by: knollfear <mknoll@fearless.tech>

--- solution/backend/regcore/serializers.py
+++ solution/backend/regcore/serializers.py
@@ -1,71 +1,47 @@
 from rest_framework import serializers
 
-from regcore.models import Title, Part
+
+class FlatContentsSerializer(serializers.Serializer):
+    type = serializers.CharField()
+    label = serializers.CharField()
+    parent = serializers.ListField(child=serializers.CharField())
+    reserved = serializers.BooleanField()
+    identifier = serializers.ListField(child=serializers.CharField())
+    label_level = serializers.CharField()
+    parent_type = serializers.CharField()
+    descendant_range = serializers.ListField(child=serializers.CharField())
+    label_description = serializers.CharField()
 
 
-class ContentsSerializer(serializers.BaseSerializer):
+class ContentsSerializer(FlatContentsSerializer):
+    def get_fields(self):
+        fields = super(ContentsSerializer, self).get_fields()
+        fields['children'] = serializers.ListField(child=ContentsSerializer())
+        return fields
+
+
+class TitlesSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    last_updated = serializers.CharField()
+
+
+class TitleSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    last_updated = serializers.CharField()
+    toc = ContentsSerializer()
+
+
+class PartsSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    date = serializers.CharField()
+    last_updated = serializers.CharField()
+    depth = serializers.IntegerField()
+    title_object = serializers.IntegerField()
+
+
+class VersionsSerializer(serializers.Serializer):
     def to_representation(self, instance):
-        return instance.toc
-
-
-class TitlesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Title
-        fields = ("id", "name", "last_updated")
-
-
-class TitleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Title
-        fields = "__all__"
-
-
-class PartsSerialier(serializers.ModelSerializer):
-    class Meta:
-        model = Part
-        fields = ("id", "name", "date", "last_updated", "depth", "title_object")
-
-
-class VersionsSerializer(serializers.BaseSerializer):
-    def to_representation(self, instance):
-        return instance.date
-
-
-# Inherit from this class to return a flat list of specific types of nodes within the part
-# You must specify a node_type
-class NodeTypeSerializer(serializers.BaseSerializer):
-    remove_fields = []
-
-    def find_nodes(self, structure):
-        nodes = []
-        for child in structure["children"]:
-            if child["type"] == self.node_type:
-                nodes.append(child)
-            if child["children"]:
-                nodes = nodes + self.find_nodes(child)
-        return nodes
-
-    def to_representation(self, instance):
-        nodes = self.find_nodes(instance.toc)
-        for node in nodes:
-            for field in self.remove_fields + ["children", "type"]:
-                del node[field]
-        return nodes
-
-
-class PartSectionsSerializer(NodeTypeSerializer):
-    node_type = "section"
-    remove_fields = ["descendant_range"]
-
-
-class PartSubpartsSerializer(NodeTypeSerializer):
-    node_type = "subpart"
-
-
-class SubpartContentsSerializer(serializers.BaseSerializer):
-    def to_representation(self, instance):
-        toc = instance.toc
-        for node in toc["children"]:
-            if node["type"] == "subpart" and len(node["identifier"]) and node["identifier"][0] == self.context["subpart"]:
-                return node["children"]
-        return []
+        return instance
