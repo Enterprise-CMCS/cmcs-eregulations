263c84faa6ed43f8a554975ec9b188cb84f57a49
Caleb Godwin <cgodwin@fearless.tech>
2022-04-25 12:53:20 -0400

    Add eregs tests

--- solution/parser/fr-parser/eregs/eregs_test.go
+++ solution/parser/fr-parser/eregs/eregs_test.go
@@ -2,12 +2,110 @@ package eregs
 
 import (
 	"testing"
+	"net/http/httptest"
+	"net/http"
+	"context"
+	"time"
+	"encoding/json"
 
 	"github.com/go-test/deep"
 )
 
 func TestSendDocument(t *testing.T) {
+	testTable := []struct {
+		Name string
+		Server *httptest.Server
+		Input FRDoc
+		Error bool
+	}{
+		{
+			Name: "test-valid",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				var fr FRDoc
+				d := json.NewDecoder(r.Body)
+				err := d.Decode(&fr)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					w.Write([]byte(`{ "exception": "` + err.Error() + `" }`))
+				} else {
+					w.WriteHeader(http.StatusOK)
+					w.Write([]byte(`OK`))
+				}
+			})),
+			Input: FRDoc{
+				Name: "this is a name",
+				Description: "this is a description",
+				Category: "this is a category",
+				URL: "https://test.gov/test",
+				Date: "2021-01-31",
+				DocketNumber: "CMS-0000-F2",
+				DocumentNumber: "2021-12345",
+				Locations: []*Section{
+					&Section{
+						Title: "42",
+						Part: "433",
+						Section: "1",
+					},
+					&Section{
+						Title: "45",
+						Part: "450",
+						Section: "10",
+					},
+				},
+			},
+			Error: false,
+		},
+		{
+			Name: "test-server-error",
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(`{ "exception": "This is expected" }`))
+			})),
+			Input: FRDoc{},
+			Error: true,
+		},
+		{
+			Name: "test-no-connection",
+			Server: nil,
+			Input: FRDoc{},
+			Error: true,
+		},
+	}
 
+	for _, tc := range testTable {
+		t.Run(tc.Name, func (t *testing.T) {
+			if tc.Server != nil {
+				defer tc.Server.Close()
+				BaseURL = tc.Server.URL
+			} else {
+				server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte(`{ "exception": "This should never be seen" }`))
+				}))
+				BaseURL = server.URL
+				server.Close()
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+			defer cancel()
+
+			err := SendDocument(ctx, &tc.Input)
+
+			if err != nil && !tc.Error {
+				t.Errorf("expected no error, received (%+v)", err)
+			} else if err == nil && tc.Error {
+				t.Errorf("expected error, received none")
+			}
+		})
+	}
+
+	eregsServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	}))
+	defer eregsServer.Close()
+	BaseURL = eregsServer.URL
+
+	
 }
 
 func TestCreateSections(t *testing.T) {
