0788fb57cea8923c50cd79e6d34d322c4657e643
Phil Reese <preese@fearless.tech>
2022-04-26 12:21:30 -0400

    EREGCSC-1296 - Add Supplemental Content search to JS1 Resources prototype page (#407)
    * feat: attempt to add q to supp content fetch
    
    * fix: correctly name q param
    
    * feat: working vue search input
    
    * feat: allow all for title and part in suppcontent endpoint
    
    * feat: update request in js api
    
    * fix: clear vue-router warning
    
    * feat: search on enter/submit; polish
    
    * refactor: DRY-ish
    
    * feat: fix resource cat chips w query
    
    * feat: update serializer to contain option headline fields
    
    * feat: enable related sections
    
    * chore: remove unused vue methods
    
    * feat: highlightall=True so entire description is shown in search result
    
    * fix: better handle part names with dashes in name
    
    * chore: clean up vue sfc boilerplate
    
    * fix: update SubparkList descriptionOnly filter to handle dashes
    
    * feat: tweak max-height for fancy dropdown container
    
    to hint at further content below

--- solution/ui/prototype/src/components/custom_elements/SubpartList.vue
+++ solution/ui/prototype/src/components/custom_elements/SubpartList.vue
@@ -30,8 +30,6 @@ import _isEmpty from "lodash/isEmpty";
 export default {
     name: "SubpartList",
 
-    components: {},
-
     props: {
         filterEmitter: {
             type: Function,
@@ -40,34 +38,6 @@ export default {
         listItems: {
             type: Array,
             required: true,
-        },
-    },
-
-    beforeCreate() {},
-
-    created() {},
-
-    beforeMount() {},
-
-    mounted() {},
-
-    beforeUpdate() {},
-
-    updated() {},
-
-    beforeDestroy() {},
-
-    destroyed() {},
-
-    data() {
-        return {
-            dataProp: "value",
-        };
-    },
-
-    computed: {
-        computedProp() {
-            return this.dataProp.toUpperCase();
         },
     },
 
@@ -82,7 +52,7 @@ export default {
 
     filters: {
         descriptionOnly(value) {
-            return value.split("-")[1];
+            return value.substring(value.indexOf("-") + 1);
         },
         formatRange(array) {
             if (_isEmpty(array)) return "";
