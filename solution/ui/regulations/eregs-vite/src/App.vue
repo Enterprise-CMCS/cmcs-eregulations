49939240ab51e541d1ecb2c6c5f35288a8800685
Phil Reese <preese@fearless.tech>
2022-04-28 15:01:21 -0400

    feat: get vite working with vue2

---
+++ solution/ui/regulations/eregs-vite/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+    import HelloWorld from "./components/HelloWorld.vue";
+
+    export default {
+        name: "App",
+
+        components: {
+            HelloWorld,
+        }
+    }
+</script>
+
+<template>
+    <div>
+        Test
+        <HelloWorld msg="Hello Vue 3 + Vite" />
+    </div>
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ solution/ui/regulations/eregs-vite/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+
+        <p>
+            Recommended IDE setup:
+            <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+            +
+            <a href="https://github.com/johnsoncodehk/volar" target="_blank"
+                >Volar</a
+            >
+        </p>
+
+        <p>
+            <a href="https://vitejs.dev/guide/features.html" target="_blank">
+                Vite Documentation
+            </a>
+            |
+            <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+        </p>
+
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test hot module replacement.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HelloWorld",
+
+        props: {
+            msg: {
+                type: String,
+                default: "",
+            }
+        }
+    }
+</script>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
