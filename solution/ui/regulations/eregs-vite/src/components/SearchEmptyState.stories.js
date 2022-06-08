6e0e39ccf4aa6da89992b449693f77e9826acdb6
Phil Reese <preese@fearless.tech>
2022-06-08 10:30:43 -0400

    test: tweak storybook controls

--- solution/ui/regulations/eregs-vite/src/components/SearchEmptyState.stories.js
+++ solution/ui/regulations/eregs-vite/src/components/SearchEmptyState.stories.js
@@ -3,6 +3,35 @@ import SearchEmptyState from "./SearchEmptyState.vue";
 export default {
     title: "Regulations/Search/Empty State",
     component: SearchEmptyState,
+    argTypes: {
+        eregs_url: {
+            name: "eRegs URL",
+            description: "URL for link to eRegs Search Page",
+            defaultValue: "https://regulations-pilot.cms.gov/resources/",
+            options: [
+                "https://regulations-pilot.cms.gov/resources/",
+                "https://regulations-pilot.cms.gov/search/",
+            ],
+            control: {
+                type: "radio",
+            },
+        },
+        eregs_url_label: {
+            name: "eRegs Search Link Text",
+            description: "Text for the link",
+            defaultValue: "eRegulations resource links",
+        },
+        eregs_sublabel: {
+            name: "eRegs Search Link parenthetical text",
+            description: "additional label",
+            defaultValue: "subregulatory guidance and implementation resources",
+        },
+        query: {
+            name: "Search term",
+            description: "word or phrase that will be used to perform search",
+            defaultValue: "test",
+        },
+    },
 };
 
 const Template = (args, { argTypes }) => ({
@@ -12,10 +41,3 @@ const Template = (args, { argTypes }) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = {
-    eregs_url: "https://www.google.com",
-    eregs_url_label: "eRegulations resource links",
-    eregs_sublabel: "subregulatory guidance and implementation resources",
-    query: "test",
-};
-
