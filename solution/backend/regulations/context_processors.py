f1be6c651b1da5bfa157bd7001a66f96515d6e32
Caleb Godwin <cgodwin@fearless.tech>
2022-06-22 13:23:21 -0400

    Add context_processor for site config

---
+++ solution/backend/regulations/context_processors.py
@@ -0,0 +1,7 @@
+from .models import SiteConfiguration
+
+def site_config(request):
+    config = SiteConfiguration.objects.first()
+    return {
+        "allow_indexing": config.allow_indexing,
+    }
