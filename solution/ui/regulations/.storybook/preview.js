7609ba06e2adcbd04926e3823040e7942094290c
Phil Reese <preese@fearless.tech>
2022-06-29 15:46:52 -0400

    test: add expanded control info to storybook controls

--- solution/ui/regulations/.storybook/preview.js
+++ solution/ui/regulations/.storybook/preview.js
@@ -2,9 +2,15 @@ import "!style-loader!raw-loader!../../../static-assets/regulations/css/main.css
 
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: { expanded: true },
     options: {
         storySort: {
-            order: ["Regulations", "Prototype", "Supplemental Resources", "Components"],
-        }
-    }
+            order: [
+                "Regulations",
+                "Prototype",
+                "Supplemental Resources",
+                "Components",
+            ],
+        },
+    },
 };
