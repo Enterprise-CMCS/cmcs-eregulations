1ec16166a00f432ca069c07b7e9f7f46cfcade7f
Phil Reese <preese@fearless.tech>
2022-06-29 10:29:29 -0400

    test: enable scss imports and vars in storybook

--- solution/ui/regulations/.storybook/main.js
+++ solution/ui/regulations/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
     stories: [
         "../stories/**/*.stories.mdx",
@@ -6,10 +8,14 @@ module.exports = {
         {
             directory: "../../prototype",
             titlePrefix: "Prototype",
-            files: "**/*.stories.*"
+            files: "**/*.stories.*",
         },
     ],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-scss"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-scss",
+    ],
     core: {
         builder: "webpack5",
     },
@@ -17,4 +23,16 @@ module.exports = {
         { from: "../../../static-assets/regulations/images", to: "images" },
         { from: "../../../static-assets/regulations/fonts", to: "fonts" },
     ],
+    webpackFinal: async (config) => {
+        config.resolve.alias = {
+            vue: "vue/dist/vue.js",
+            legacy: path.resolve(__dirname, "../../regulations"),
+            "legacy-static": path.resolve(
+                __dirname,
+                "../../../static-assets/regulations"
+            ),
+        };
+
+        return config;
+    },
 };
