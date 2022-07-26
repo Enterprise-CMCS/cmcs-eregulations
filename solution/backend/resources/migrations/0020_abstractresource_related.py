b87c3c977f1c92c997b17837ed63e2e9154964b5
Caleb Godwin <cgodwin@fearless.tech>
2022-07-26 15:18:52 -0400

    Group docs in migration

--- solution/backend/resources/migrations/0020_abstractresource_related.py
+++ solution/backend/resources/migrations/0020_abstractresource_related.py
@@ -3,6 +3,16 @@
 from django.db import migrations, models
 
 
+def initial_set_related_resources(apps, schema_editor):
+    FederalRegisterDocument = apps.get_model("resources", "FederalRegisterDocument")
+    FederalRegisterDocumentGroup = apps.get_model("resources", "FederalRegisterDocumentGroup")
+
+    for group in FederalRegisterDocumentGroup.objects.all():
+        for doc in FederalRegisterDocument.objects.filter(group=group.id):
+            doc.related_resources.set(group.documents.exclude(id=doc.id).order_by("-date"))
+            doc.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,4 +25,5 @@ class Migration(migrations.Migration):
             name='related_resources',
             field=models.ManyToManyField(blank=True, to='resources.AbstractResource'),
         ),
+        migrations.RunPython(initial_set_related_resources),
     ]
