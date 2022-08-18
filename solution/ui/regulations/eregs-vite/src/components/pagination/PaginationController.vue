a3f15d26356d5d9d32355e0a84a218eb2cc0f1ce
Phil Reese <preese@fearless.tech>
2022-08-18 15:25:43 -0400

    chore: initial progress

---
+++ solution/ui/regulations/eregs-vite/src/components/pagination/PaginationController.vue
@@ -0,0 +1,58 @@
+<template>
+    <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+    name: "PaginationController",
+
+    components: {},
+
+    props: {
+        propName: {
+            type: String,
+            required: true,
+        },
+    },
+
+    beforeCreate() {},
+
+    created() {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    beforeUpdate() {},
+
+    updated() {},
+
+    beforeDestroy() {},
+
+    destroyed() {},
+
+    data() {
+        return {
+            dataProp: "value",
+        }
+    },
+
+    computed: {
+        computedProp() {
+            return this.dataProp.toUpperCase();
+        },
+    },
+
+    methods: {
+        methodName() {
+            console.log("method has been invoked");
+        },
+    },
+
+}
+</script>
+
+<style>
+
+</style>
+
