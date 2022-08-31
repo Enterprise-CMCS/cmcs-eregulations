aab114b65af69946ab3475bd6facab05024ec906
Phil Reese <preese@fearless.tech>
2022-08-31 09:35:16 -0400

    EREGCSC-1338 pagination controls (#522)
    * chore: initial progress
    
    * fix: set rsrcs loading flag to true
    
    * fix: remove multiWordQuery so rsrcs page will load
    
    * chore: light formatting
    
    * feat: skeleton of basic pagination control
    
    * feat: promote floating nav btns from prototype for pagination back/fwd controls'
    
    * feat: get content count from field in server response; temp remove pagination
    
    * feat: add page param from url to control response page number
    
    * refactor: change folder name for navigation components
    
    * feat: basic pagination number links
    
    * feat: hook up prev/nxt btns
    
    * feat: disable prev/nxt buttons when appropriate
    
    * feat: add disabled style to prev/next btns
    
    * feat: style out default desktop pagination controls
    
    * fix: handle undefined page query param
    
    * chore: skip navigation storybook stories for now
    
    * chore: move router link logic into pagenumber component; created pageslist
    
    * feat: work out ellipses logic for results with more than seven pages
    
    * fix: tweak ul padding, add comments, refactor list logic
    
    * feat: mobile pagination
    
    * feat: aria-label, better prv/nxt btn styles
    
    * feat: enable pagination links when first visiting w no queryparams
    
    * feat: tweak results count at top of results to better commnicate paginated results
    
    * fix: handle clearSearchQuery and doQuoteSerach to include page/sort/etc params
    
    * feat: strip out page when updating filters
    
    * feat: strip out page when changing search query and clearing chips

---
+++ solution/ui/regulations/eregs-vite/src/components/pagination/PagesList.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="list-container">
+        <ul class="pages desktop-list">
+            <PageNumber
+                v-if="pagesArray.length > 0"
+                :current-page="currentPage"
+                :number="firstPage"
+            />
+            <span
+                v-if="pagesArray.length > 7 && currentPage > 4"
+                class="ellipses"
+            >
+                …
+            </span>
+            <PageNumber
+                v-for="pageNum in iterablePages"
+                :key="pageNum"
+                :current-page="currentPage"
+                :number="pageNum"
+            />
+            <span
+                v-if="
+                    pagesArray.length > 7 && pagesArray.length > currentPage + 3
+                "
+                class="ellipses"
+            >
+                …
+            </span>
+            <PageNumber
+                v-if="pagesArray.length > 1"
+                :current-page="currentPage"
+                :number="lastPage"
+            />
+        </ul>
+        <div class="mobile-list">
+            Page <span class="mobile-page-number">{{ currentPage }}</span> of
+            <span class="mobile-page-number">{{ lastPage }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import PageNumber from "@/components/pagination/PageNumber.vue";
+
+export default {
+    name: "PagesList",
+
+    components: {},
+
+    props: {
+        currentPage: {
+            type: Number,
+            required: false,
+            default: 1,
+        },
+        pagesArray: {
+            type: Array,
+            required: false,
+            default: () => [],
+        },
+    },
+
+    computed: {
+        firstPage() {
+            return this.pagesArray[0];
+        },
+        frontFour() {
+            // slice of pages 2 through 5
+            return this.pagesArray.slice(1, 5);
+        },
+        shortMiddlePages() {
+            // three-page slice of page before current page,
+            // current page, and page after current page
+            return this.pagesArray.slice(
+                this.currentPage - 2,
+                this.currentPage + 1
+            );
+        },
+        allMiddlePages() {
+            // slice of all pages between first and last page
+            return this.pagesArray.slice(1, -1);
+        },
+        backFour() {
+            // slice of n - 5 to n - 1
+            return this.pagesArray.slice(-5, -1);
+        },
+        lastPage() {
+            return this.pagesArray[this.pagesArray.length - 1];
+        },
+        iterablePages() {
+            if (this.pagesArray.length > 7) {
+                if (this.currentPage < 5) {
+                    return this.frontFour;
+                }
+
+                if (this.pagesArray.length - 3 <= this.currentPage) {
+                    return this.backFour;
+                }
+
+                if (this.pagesArray.length > this.currentPage) {
+                    return this.shortMiddlePages;
+                }
+            }
+
+            return this.allMiddlePages;
+        },
+    },
+
+    methods: {
+        methodName() {
+            console.log("method has been invoked");
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.list-container ul.pages {
+    padding-left: 0;
+}
+
+.desktop-list {
+    @include custom-max($mobile-max / 1px) {
+        display: none;
+    }
+}
+
+.mobile-list {
+    @include screen-md {
+        display: none;
+    }
+
+    .mobile-page-number {
+        font-weight: bold;
+    }
+}
+</style>
