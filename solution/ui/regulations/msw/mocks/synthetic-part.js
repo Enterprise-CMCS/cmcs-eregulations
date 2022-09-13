6dfcb98a64e4ef76fa0dce6a6104774515c85a3c
Phil Reese <preese@fearless.tech>
2022-09-13 09:24:00 -0400

    feat: progress on JSON version of synthetic part

---
+++ solution/ui/regulations/msw/mocks/synthetic-part.js
@@ -0,0 +1,316 @@
+const fullPart = {
+    node_type: "PART",
+    label: ["499"],
+    title: "PART 499 - SYNTHETIC PART",
+    authority: {
+        node_type: "",
+        header: "Authority:",
+        content: "42 U.S.C. 1302. ",
+    },
+    source: {
+        node_type: "",
+        header: "Source:",
+        content: "43 FR 45201, Sept. 29, 1978, unless otherwise noted. ",
+    },
+    editorial_note: {
+        node_type: "",
+        header: "",
+        content: "",
+    },
+    children: [
+        {
+            node_type: "SECTION",
+            title: "§ 499.1 Section with No Subpart.",
+            label: ["499", "1"],
+            children: [
+                {
+                    node_type: "Paragraph",
+                    text:
+                        "We call this an orphan section in our everyday team language. This part prescribes State plan requirements, FFP limitations and procedures concerning payments made by State Medicaid agencies for Medicaid services.",
+                    label: ["499", "1", "d78835ad878d59bddbcde2a31249107c"],
+                    marker: null,
+                },
+            ],
+        },
+        {
+            node_type: "SECTION",
+            title: "§ 499.2 Second Section with No Subpart.",
+            label: ["499", "2"],
+            children: [
+                {
+                    node_type: "Paragraph",
+                    text:
+                        "Since some parts have multiple orphan sections. The agency must pay 99 percent of all clean claims from practitioners within 90 days of the date of receipt.",
+                    label: ["499", "2", "e8097e171215490dbe0b3206107a5049"],
+                    marker: null,
+                },
+            ],
+        },
+        {
+            node_type: "SUBPART",
+            title: "Subpart A - Payments: General Provisions",
+            label: ["A"],
+            children: [
+                {
+                    node_type: "SECTION",
+                    title: "§ 499.3 Basis and Applicability",
+                    label: ["499", "3"],
+                    children: [
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "This paragraph is followed by a list of special text. Implements the following sections of the Act and public law that state requirements and standards for payment to states:",
+                            label: ["499", "3", "e8097e171215490dbe0b3206107a504a"],
+                            marker: null,
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "1885(a) Withholding of Payment /Cross-reference to a less commonly cited part of the Social Security Act/",
+                            label: ["499", "3", "a"],
+                            marker: ["a"],
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "1916(a)(1) Use of Enrollment Fees, /This is a cross-reference to the Social Security Act too/",
+                            label: ["499", "3", "a", "1"],
+                            marker: [1"],
+                        },
+                    ],
+                },
+                {
+                    node_type: "SECTION",
+                    title: "§ 499.21 Reduction of payments to providers.",
+                    label: ["499", "21"],
+                    children: [
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "If a provider seeks to collect from an individual an amount that exceeds an amount specified under 499.30(a) /example of cross-reference to a paragraph in another section in the same subpart/ -",
+                            label: ["499", "21", "e8097e171215490dbe0b3206107a504b"],
+                            marker: null,
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "(a) The /formula example/ to use to calculate the reduction in payments to the provider is:",
+                            label: ["499", "21", "a"],
+                            marker: ["a"],
+                        },
+                        {
+                            node_type: "Extract",
+                            content:
+                                "\n    <FP-2>Reduced provider payment = P - (Px3) + Q, where</FP-2>\n    <FP-2>P = payment requested by the provider</FP-2>\n    <FP-2>Q = additional monetary penalty imposed by the state</FP-2>\n  ",
+                        },
+                        {
+                            node_type: "Citation",
+                            content: "[55 FR 1433, Jan. 16, 1990] ",
+                        },
+                    ],
+                },
+                {
+                    node_type: "SECTION",
+                    title:
+                        "§ 499.30 Withholding the Federal share of payments to Medicaid /this is an example of a really long section name/.",
+                    label: ["499", "30"],
+                    children: [
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "<I>/This is an example of a bolded phrase/.</I> This section implements section 1914 of the Act. /this sentence has a footnote/.<SU>1</SU>",
+                            label: ["499", "30", "e8097e171215490dbe0b3206107a504c"],
+                            marker: null,
+                        },
+                        {
+                            node_type: "Citation",
+                            content:
+                                "[50 FR 19688, May 10, 1985; 50 FR 23307, June 3, 1985] ",
+                        },
+                        {
+                            node_type: "FootNote",
+                            content:
+                                "\n    <P><SU>1</SU>  /Text of footnote/ Center for Medicare and Medicaid Services (CMS ) will review claims to determine the appropriate Medicare payment to apply to the calculation to determine the level of overpayment that should have been withheld.</P>\n  ",
+                        },
+                    ],
+                },
+                {
+                    node_type: "SECTION",
+                    title:
+                        "§ 499.31 Withholding the Federal share of payments to Medicaid /this is an example of a really long section name/.",
+                    label: ["499", "31"],
+                    children: [
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "(a) <I>Agency notice to providers.</I> (1) /This first element of this list is part of the same paragraph as the bolded mini-heading for the list, which creates a weird indenting situation that happens./ Before the agency requests recovery of a Medicaid overpayment through Medicare, the agency must send either or both of the following notices to the provider.",
+                            label: ["499", "31", "a", "1"],
+                            marker: ["1"],
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "(i) Notice that /example of nested paragraphs/ -",
+                            label: ["499", "31", "a", "1", "i"],
+                            marker: ["i"],
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text: "(A) There has been an overpayment;",
+                            label: ["499", "31", "a", "1", "i", "A"],
+                            marker: ["A"],
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text: "(B) Repayment is required; and",
+                            label: ["499", "31", "a", "1", "i", "B"],
+                            marker: ["B"],
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "(2) Notice that, 30 days or later from the date of the notice, the agency intends to refer the case to CMS for withholding of Medicare payments.",
+                            label: ["499", "31", "a", "2"],
+                            marker: ["2"],
+                        },
+                        {
+                            node_type: "Citation",
+                            content:
+                                "[50 FR 19689, May 10, 1985, as amended at 61 FR 63749, Dec. 2, 1996] ",
+                        },
+                    ],
+                },
+                {
+                    node_type: "SECTION",
+                    title: "§§ 499.32-499.38 [Reserved /multiple sections/]",
+                    label: ["499", "32", "499", "38"],
+                    children: null,
+                },
+                {
+                    node_type: "SECTION",
+                    title: "§ 499.40 Timely /short section title/.",
+                    label: ["499", "40"],
+                    children: [
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "(a) /This is a short section/ The Medicaid agency may make payments to reserve a bed, if -",
+                            label: ["499", "40", "a"],
+                            marker: ["a"],
+                        },
+                        {
+                            node_type: "Paragraph",
+                            text:
+                                "(1) The State plan provides for such payments and specifies any limitations on the policy.",
+                            label: ["499", "31", "a", "1"],
+                            marker: ["1"],
+                        },
+                        {
+                            node_type: "Citation",
+                            content:
+                                "[43 FR 45253, Sept. 29, 1978, as amended at 51 FR 24991, July 3, 1986] ",
+                        },
+                    ],
+                },
+                {
+                    node_type: "SECTION",
+                    title: "§§ 499.42 [Reserved /single section/]",
+                    label: ["499", "32"],
+                    children: null,
+                },
+                {
+                    node_type: "SUBJGRP",
+                    title:
+                        "Medicaid /subject group with footnote/ Sharing<SU>1</SU>",
+                    label: ["ECFR92bbc42ac8acd78"],
+                    children: [
+                        {
+                            node_type: "FootNote",
+                            content:
+                                "\n                <P><SU>1</SU> The /See real example right after 456.113/ Department was enjoined in 1975 in the case of American Medical Assn. et al. v. Weinberger, 395 F. Supp. 515 (N.D. Ill., 1975), aff'd., 522 F2d 921 (7th cir., 1975) from implementing the admission review requirements contained in §§ 456.121-456.127. </P>\n              ",
+                        },
+                        {
+                            node_type: "SECTION",
+                            title: "§ 499.51 Definitions.",
+                            label: ["499", "51"],
+                            children: [
+                                {
+                                    node_type: "Paragraph",
+                                    text:
+                                        " As used in this part /Example list of definitions./ -",
+                                    label: [
+                                        "499",
+                                        "51",
+                                        "aee66b0bc0cf3eddf5a5e86d88f7b6da",
+                                    ],
+                                    marker: null,
+                                },
+                                {
+                                    node_type: "Paragraph",
+                                    text:
+                                        "<I>Alternative non-emergency services provider</I> means a Medicaid provider, such as a physician's office, clinic, hospital outpatient department, or similar provider.",
+                                    label: [
+                                        "499",
+                                        "51",
+                                        "aee66b0bc0cf3eddf5a5e86d88f7b6db",
+                                    ],
+                                    marker: null,
+                                },
+                                {
+                                    node_type: "Paragraph",
+                                    text:
+                                        "<I>Emergency services</I> has the same meaning as in § 438.114 of this chapter.",
+                                    label: [
+                                        "499",
+                                        "51",
+                                        "aee66b0bc0cf3eddf5a5e86d88f7b6dc",
+                                    ],
+                                    marker: null,
+                                },
+                                {
+                                    node_type: "Paragraph",
+                                    text:
+                                        "<I>Non-emergency services</I> means any care or services that are not considered emergency services as defined in this section.",
+                                    label: [
+                                        "499",
+                                        "51",
+                                        "aee66b0bc0cf3eddf5a5e86d88f7b6dc",
+                                    ],
+                                    marker: null,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    node_type: "SUBJGRP",
+                    title: "Cost Sharing /subject group with footnote/",
+                    label: ["ECFR92bbc42ac8acd7a"],
+                    children: [
+                        {
+                            node_type: "SECTION",
+                            title: "§ 499.52 Cost sharing.",
+                            label: ["499", "52"],
+                            children: [
+                                {
+                                    node_type: "Paragraph",
+                                    text:
+                                        "(a) <I>Maximum Allowable Cost Sharing.</I> (1) At State option, cost sharing imposed for any service, may be established at or below the amounts shown in the following /example of a table with several columns/:",
+                                    label: ["499", "52", "a", "1"],
+                                    marker: [1"],
+                                },
+                                {
+                                    node_type: "Paragraph",
+                                    text:
+                                        "(2) States with cost sharing for an inpatient stay that exceeds $75, as of July 15, 2013, must submit a plan to CMS that provides for reducing inpatient cost sharing to $75 on or before July 1, 2017. /Sentence after a table/",
+                                    label: ["499", "52", "a", "2"],
+                                    marker: ["2"],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
