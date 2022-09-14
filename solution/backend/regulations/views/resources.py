69ce9ecdfef6b36fe5b106569065a2f1bbb59e54
Thomas W <twalker@fearless.tech>
2022-09-14 14:01:59 -0400

    Add view resource button to pages

--- solution/backend/regulations/views/resources.py
+++ solution/backend/regulations/views/resources.py
@@ -4,3 +4,11 @@ from django.views.generic.base import TemplateView
 class ResourcesView(TemplateView):
 
     template_name = 'regulations/resources.html'
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+
+            c = {
+                'hide_resource_btn': True,
+            }
+
+            return {**context, **c}
