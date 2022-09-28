bb6c6ec64540a8ff998f74dfaf821990ef5730df
Phil Reese <preese@fearless.tech>
2022-09-28 06:38:43 -0400

    EREGCSC-1499 Put Rule Groupings onto Homepage (#547)
    * feat: basic front page templating
    
    * feat: get correct fr cat name for homepage link
    
    * feat: add recent changes container component, fetch three recent rules
    
    * fix: please python linter
    
    * feat: add line clamp styles, reorg scss files
    
    * feat: add line clamp styles to vue with variabel line count
    
    * feat: user provide/inject instead of prop drilling itemTitleLineLimit
    
    * test: add cypress tests

--- solution/ui/regulations/js/src/components/SupplementalContent.vue
+++ solution/ui/regulations/js/src/components/SupplementalContent.vue
@@ -252,6 +252,7 @@ export default {
                         this.resourceCount = subpart_response.length;
                     }
 
+
                     this.categories = formatResourceCategories(response);
                 }
             } catch (error) {
