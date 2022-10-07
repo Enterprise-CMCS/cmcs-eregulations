e4ab0793bce6b5c661427485913e0a9d0b1dbe66
Phil Reese <preese@fearless.tech>
2022-10-07 12:54:10 -0400

    fix: add kb/a11y to synonym/quoted search links

--- solution/ui/regulations/eregs-vite/src/views/Resources.vue
+++ solution/ui/regulations/eregs-vite/src/views/Resources.vue
@@ -14,6 +14,7 @@
                         solo
                         clearable
                         label="Search resources using keywords or citations."
+                        aria-label="Search resources using keywords or citations."
                         type="text"
                         class="search-field"
                         append-icon="mdi-magnify"
@@ -25,12 +26,20 @@
                     <div v-if="synonyms.length > 0 || multiWordQuery" class="search-suggestion">
                         <div v-if="multiWordQuery">
                             Didn't find what you were looking for? Try searching for
-                            <a @click="doQuoteSearch">"{{this.searchQuery}}"</a>
+                            <a
+                                tabindex="0"
+                                @click="doQuoteSearch"
+                                @keydown.enter.space.prevent="doQuoteSearch"
+                            >"{{ searchQuery }}"</a>
                         </div>
-                        <div class="synonyms" v-if="synonyms.length > 0"> 
+                        <div v-if="synonyms.length > 0" class="synonyms">
                             <span v-if="multiWordQuery">Or s</span><span v-else>S</span>earch for similar terms:
-                            <span v-bind:key=a v-for="a in synonyms">
-                                <a @click="synonymLinks(a)">{{a}}</a>
+                            <span v-for="a in synonyms" :key=a>
+                                <a
+                                    tabindex=0
+                                    @click="synonymLinks(a)"
+                                    @keydown.enter.space.prevent="synonymLinks(a)"
+                                >{{ a }}</a>
                                 <span v-if="synonyms[synonyms.length-1] != a">, </span>
                             </span>
                         </div>
