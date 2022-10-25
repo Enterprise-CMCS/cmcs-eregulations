fb6548a08ff2df5c1a32d2beb18c9d879e2d35d5
Phil Reese <preese@fearless.tech>
2022-10-25 13:52:26 -0400

    feat: style and format download csv buttons

--- solution/ui/regulations/eregs-vite/src/components/resources/ResourceExportBtn.vue
+++ solution/ui/regulations/eregs-vite/src/components/resources/ResourceExportBtn.vue
@@ -1,36 +1,42 @@
 <template>
-    <div class="resource_btn_container" style="padding-left:5px;">
-        <button id="export-csv" class="default-btn action-btn search_resource_btn desktop-btn-label`"
-            @click="createCSV">
-            <span v-if="!downloadingCSV">
-                <span class="desktop-btn-label">
-                    Download Spreadsheet (CSV)
-                </span>
-                <span class="mobile-btn-label">CSV</span>
-                <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"
-                    v-if="!downloadingCSV">
+    <div class="resource-btn-container">
+        <button
+            id="exportCSV"
+            class="default-btn action-btn search_resource_btn desktop-btn-label"
+            @click="createCSV"
+        >
+            <template v-if="!downloadingCSV">
+                <span class="desktop-btn-label">Download Spreadsheet (CSV)</span
+                ><span class="mobile-btn-label">CSV</span>
+                <svg
+                    width="18"
+                    height="17"
+                    viewBox="0 0 18 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
                     <path
                         d="M1.78361 16.5C1.36371 16.5 0.996298 16.35 0.681372 16.05C0.366447 15.75 0.208984 15.4 0.208984 15V11.425H1.78361V15H15.4304V11.425H17.005V15C17.005 15.4 16.8475 15.75 16.5326 16.05C16.2177 16.35 15.8503 16.5 15.4304 16.5H1.78361ZM8.60699 12.675L3.54194 7.85L4.67043 6.775L7.81968 9.775V0.5H9.39431V9.775L12.5436 6.775L13.672 7.85L8.60699 12.675Z"
-                        fill="white" />
-                </svg></span>
-            <span class="desktop-btn-label" style="width:100%" v-else>
-                <simple-spinner spinnerType="button" :size="'small'"></simple-spinner>
-            </span>
+                        fill="white"
+                    />
+                </svg>
+            </template>
+            <template v-else>
+                <simple-spinner
+                    spinner-type="button"
+                    size="small"
+                ></simple-spinner>
+            </template>
         </button>
     </div>
-
 </template>
 
 <script>
+import exportFromJSON from "export-from-json";
+import SimpleSpinner from "legacy/js/src/components/SimpleSpinner.vue";
+import { getSupplementalContentV3 } from "@/utilities/api";
 
-import exportFromJSON from 'export-from-json'
-import SimpleSpinner from "../../../../../regulations/js/src/components/SimpleSpinner.vue";
-import {
-    getSupplementalContentV3,
-
-} from "../../utilities/api";
 export default {
-
     name: "ResourceExportBtn",
     components: {
         SimpleSpinner,
@@ -38,53 +44,52 @@ export default {
     props: {
         query: {
             type: String,
-            default: ""
+            default: "",
         },
         partDict: {
             type: Object,
-            default: () => { }
+            default: () => {},
         },
         categories: {
             type: Array,
-            default: () => []
+            default: () => [],
         },
         supCount: {
             type: Number,
-            default: 0
-        }
+            default: 0,
+        },
     },
 
     data() {
         return {
             supplementalContent: [],
             downloadingCSV: false,
-        }
+        };
     },
 
-    computed: {
-
-    },
+    computed: {},
 
     methods: {
         async createCSV() {
-            this.downloadingCSV = true
-            this.supplementalContent = await this.getSupplementalContent()
-            const supSort = this.supplementalContent.map(cont => {
-                const content = {}
-                content.category = cont.category.name
-                content.date = cont.date
-                content.description = cont.description
-                content.document_number = cont.document_number
-                content.name = cont.name
-                content.url = cont.url
-                content.Related_Regulations = this.formatLocations(cont.locations)
-                return content
-            }
-            )
-            this.downloadingCSV = false
-            const fileName = 'Resources'
+            this.downloadingCSV = true;
+            this.supplementalContent = await this.getSupplementalContent();
+            const supSort = this.supplementalContent.map((cont) => {
+                const content = {};
+                content.category = cont.category.name;
+                content.date = cont.date;
+                content.description = cont.description;
+                content.document_number = cont.document_number;
+                content.name = cont.name;
+                content.url = cont.url;
+                content.Related_Regulations = this.formatLocations(
+                    cont.locations
+                );
+                return content;
+            });
+            this.downloadingCSV = false;
+            const fileName = "Resources";
             const exportType = exportFromJSON.types.csv;
-            exportFromJSON({ data: supSort, fileName, exportType })
+            exportFromJSON({ data: supSort, fileName, exportType });
         },
 
         formatLocations(locations) {
@@ -103,44 +108,57 @@ export default {
         async getSupplementalContent() {
             let supNum = 0;
             let page = 1;
-            let responseContent = []
-            const content = []
+            let responseContent = [];
+            const content = [];
             while (supNum < this.supCount) {
-                content.push(getSupplementalContentV3({
-                    page,
-                    partDict: Object.keys(this.partDict).length > 0 ? this.partDict : "all",
-                    categories: this.categories,
-                    q: this.query,
-                    fr_grouping: false
-                }));
-                page += 1
+                content.push(
+                    getSupplementalContentV3({
+                        page,
+                        partDict:
+                            Object.keys(this.partDict).length > 0
+                                ? this.partDict
+                                : "all",
+                        categories: this.categories,
+                        q: this.query,
+                        fr_grouping: false,
+                    })
+                );
+                page += 1;
                 supNum += 100;
             }
-            responseContent = await Promise.all(content)
-            const ret = responseContent.map(x => x.results).flat()
-            return ret
+            responseContent = await Promise.all(content);
+            const ret = responseContent.map((x) => x.results).flat();
+            return ret;
         },
     },
-}
+};
 </script>
-<style lang="scss">
-#export-csv {
-    padding-top: 6px;
-    padding-bottom: 6px;
-    width: 246px;
-}
+<style lang="scss" scoped>
+.resource-btn-container {
+    padding-left: 5px;
 
-.desktop-btn-label {
-    @include custom-max($mobile-max / 1px) {
-        // 767px
-        display: none;
-    }
-}
+    #exportCSV {
+        padding-top: 8px;
+        padding-bottom: 8px;
+        min-height: 18px;
+        align-items: center;
+        justify-content: space-around;
 
-.mobile-btn-label {
-    @include screen-md {
-        // 768px
-        display: none;
+        @include screen-md {
+            min-width: 225px;
+        }
+
+        .desktop-btn-label {
+            @include custom-max($mobile-max / 1px) {
+                display: none;
+            }
+        }
+
+        .mobile-btn-label {
+            @include screen-md {
+                display: none;
+            }
+        }
     }
 }
 </style>
