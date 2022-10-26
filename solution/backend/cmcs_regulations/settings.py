726a06a686e6bc2f5af56cbb8de6c2b64a1c5ff8
thwalker6 <thomas.e.walker88@gmail.com>
2022-10-26 07:05:09 -0400

    Eregcsc 1520 fix - CSP re Swagger (#585)
    * EREGCSC-1541
    
    * EREGCSC-1360 Fix for swagger
    
    * undo wront updated
    
    Co-authored-by: Thomas W <twalker@fearless.tech>

--- solution/backend/cmcs_regulations/settings.py
+++ solution/backend/cmcs_regulations/settings.py
@@ -215,6 +215,8 @@ LOGIN_URL = "/admin"
 CSP_IMG_SRC = [
     "'self'",
     STATIC_URL,
+    "https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/favicon-32x32.png",
+    'data:',
     "https://www.googletagmanager.com",
     "https://www.google-analytics.com",
     "https://images.federalregister.gov/",
@@ -224,6 +226,7 @@ CSP_STYLE_SRC = [
     "'unsafe-inline'",
     STATIC_URL,
     "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/",
+    "https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/swagger-ui.css"
 ]
 CSP_FONT_SRC = [
     "'self'",
@@ -240,6 +243,8 @@ CSP_SCRIPT_SRC = [
     "'unsafe-eval'",
     STATIC_URL,
     "https://www.googletagmanager.com",
+    "https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/swagger-ui-bundle.js",
+    "https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/swagger-ui-standalone-preset.js"
 ]
 CSP_INCLUDE_NONCE_IN = ["script-src"]
 
