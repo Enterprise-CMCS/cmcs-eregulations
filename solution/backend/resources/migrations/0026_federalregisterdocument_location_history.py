547727d125e43349451a52b74c04e86548955883
Caleb Godwin <cgodwin@fearless.tech>
2022-10-31 14:36:06 -0400

    Fix migration

--- solution/backend/resources/migrations/0026_federalregisterdocument_location_history.py
+++ solution/backend/resources/migrations/0026_federalregisterdocument_location_history.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('resources', '0024_alter_federalregisterdocumentgroup_options'),
+        ('resources', '0025_auto_20221027_0816'),
     ]
 
     operations = [
