fe4378e158d199ef1958d63c9b36b6743e09ca3c
Phil Reese <preese@fearless.tech>
2022-11-08 13:22:06 -0500

    EREGCSC-1599 -- Most recent successful ECFR parse date in footer from API (#597)
    * test: check date format
    
    * feat: create lastParserSuccessDate vue component
    
    * chore: JSDoc comments
    
    * feat: add loading spinner when loading date
    
    * test: add print styling test; tweak homepage test
    
    * test: edit test name

--- solution/ui/regulations/js/utils.js
+++ solution/ui/regulations/js/utils.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 import _delay from "lodash/delay";
 import _get from "lodash/get";
+import _isEmpty from "lodash/isEmpty";
+import _isNil from "lodash/isNil";
+import _isString from "lodash/isString";
 
 // a promise friendly delay function
 function delay(seconds) {
@@ -201,13 +204,31 @@ const highlightText = (location, paramKey) => {
     }
 };
 
+/**
+ * Converts date from YYYY-MM-DD to MMM DD, YYYY
+ *
+ * @param {string} kebabDate - date in `YYYY-MM-DD` format
+ *
+ * @returns {string} - date in `MMM DD, YYYY` format
+ */
+const niceDate = (kebabDate) => {
+    if (_isNil(kebabDate)) return "N/A";
+    if (_isString(kebabDate) && _isEmpty(kebabDate)) return "N/A";
+    const date = new Date(`${kebabDate}T12:00:00.000-05:00`);
+    const month = date.toLocaleString("default", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+};
+
 export {
     delay,
     EventCodes,
+    highlightText,
     flattenSubpart,
     formatDate,
     formatResourceCategories,
     getQueryParam,
-    highlightText,
+    niceDate,
     parseError,
 };
