02cc3517659b36d9cef52629b0c4ad1e618c342b
Phil Reese <preese@fearless.tech>
2022-11-14 16:44:40 -0500

    feat: begin search spa

--- solution/ui/regulations/eregs-vite/src/views/Search.vue
+++ solution/ui/regulations/eregs-vite/src/views/Search.vue
@@ -1,14 +1,44 @@
 <template>
-    <div class="content">
-        <h2>In Vite</h2>
-    </div>
+    <body class="ds-base">
+        <div id="searchApp" class="search-view">
+            <Banner title="Search Results">
+                <template #description>
+                    <p>This site searches Title 42, Parts 400 and 430-460</p>
+                </template>
+                <template #input>
+                    <form
+                        class="search-form"
+                    >
+                        <v-text-field
+                            id="main-content"
+                            outlined
+                            flat
+                            solo
+                            clearable
+                            label="Search Regulations"
+                            aria-label="Search Regulations"
+                            type="text"
+                            class="search-field shrink"
+                            append-icon="mdi-magnify"
+                            hide-details
+                            dense
+                        />
+                    </form>
+                </template>
+            </Banner>
+        </div>
+    </body>
 </template>
 
 <script>
+import Banner from "@/components/Banner.vue";
+
 export default {
     name: "Search",
 
-    components: {},
+    components: {
+        Banner,
+    },
 
     props: {
         propName: {
@@ -54,7 +84,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+#searchApp.search-view {
+    display: flex;
+    flex-direction: column;
 
+    .search-form {
+        .search-field {
+            height: 40px;
+            margin-bottom: 50px;
+            .v-input__icon.v-input__icon--append button {
+                color: $mid_blue;
+            }
+        }
+    }
+}
 </style>
 
