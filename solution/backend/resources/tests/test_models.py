6b3a2eab32f3bda74f7d18442047d62bc2cb9808
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2022-11-17 13:16:29 -0500

    EREGCSC-1632 Guarantee uniqueness of resource sections and subparts (#610)
    * Override validate_unique
    
    * Location save method works for updating
    
    Before this commit, save was raising ValidationError simply *if*
    the location already exists without regard to whether we're creating
    or updating the location.
    
    Now it will work as intended. Cannot create duplicate sections but
    can update them.
    
    * Add unit tests
    
    * Rename tests

---
+++ solution/backend/resources/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from resources.models import Section, Subpart
+
+
+class TestLocationUniqueness(TestCase):
+    def setUp(self):
+        Section.objects.create(title=42, part=433, section_id=1)
+        Subpart.objects.create(title=42, part=433, subpart_id="A")
+
+    def test_create_dupe_section(self):
+        with self.assertRaises(ValidationError):
+            Section.objects.create(title=42, part=433, section_id=1)
+
+    def test_create_dupe_subpart(self):
+        with self.assertRaises(ValidationError):
+            Subpart.objects.create(title=42, part=433, subpart_id="A")
+
+    def test_overwrite(self):
+        section = Section.objects.create(title=42, part=433, section_id=2)
+        with self.assertRaises(ValidationError):
+            section.section_id = 1
+            section.save()
+        subpart = Subpart.objects.create(title=42, part=433, subpart_id="B")
+        with self.assertRaises(ValidationError):
+            subpart.subpart_id = "A"
+            subpart.save()
+
+    def test_update_or_create(self):
+        Section.objects.update_or_create(title=42, part=433, section_id=1, defaults={})
+        Subpart.objects.update_or_create(title=42, part=433, subpart_id="A", defaults={})
