3fd52e27080bfae09577c67bf01ee5c7148e77be
Caleb Godwin <cgodwin@fearless.tech>
2022-11-28 14:54:12 -0500

    Add Swagger docs for search endpoint

--- solution/backend/regcore/search/v3views.py
+++ solution/backend/regcore/search/v3views.py
@@ -1,12 +1,18 @@
 from datetime import date
 from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema
+
+from common.mixins import OptionalPaginationMixin
+from common.api import OpenApiQueryParameter
 
 from .models import SearchIndex
 from .serializers import SearchResultSerializer
 
-from common.mixins import OptionalPaginationMixin
 
-
+@extend_schema(
+    description="Search the regulation text. This endpoint is incomplete and may change.",
+    parameters=[OpenApiQueryParameter("q", "The word or phrase to search for.", str, True)],
+)
 class V3SearchView(OptionalPaginationMixin, viewsets.ReadOnlyModelViewSet):
     serializer_class = SearchResultSerializer
 
