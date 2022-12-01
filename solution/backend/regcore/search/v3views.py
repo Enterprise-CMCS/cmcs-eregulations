e48adcd59a3f946e64d2876253b899424e917245
Caleb Godwin <cgodwin@fearless.tech>
2022-12-01 14:18:19 -0500

    Add needed fields

--- solution/backend/regcore/search/v3views.py
+++ solution/backend/regcore/search/v3views.py
@@ -1,6 +1,7 @@
 from datetime import date
 from rest_framework import viewsets
 from drf_spectacular.utils import extend_schema
+from django.db.models import F
 
 from common.mixins import OptionalPaginationMixin
 from common.api import OpenApiQueryParameter
@@ -18,4 +19,8 @@ class V3SearchView(OptionalPaginationMixin, viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         query = self.request.GET.get("q")
-        return SearchIndex.objects.effective(date.today()).search(query)
+        return SearchIndex.objects.effective(date.today()).search(query).annotate(
+            part_title=F("part__title"),
+            part_document_title=F("part__document__title"),
+            date=F("part__date"),
+        )
