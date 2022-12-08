9dd70f85fcb8a824ae3593c63f7396f9a0774be7
Caleb Godwin <cgodwin@fearless.tech>
2022-12-08 15:17:32 -0500

    allow exception

--- solution/backend/createdb.py
+++ solution/backend/createdb.py
@@ -19,14 +19,14 @@ def handler(event, context):
     if not connection.is_usable():
         raise Exception("database is unreachable")
 
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'main'"
-            )
-            cursor.execute(
-                f"CREATE DATABASE {os.environ.get('STAGE')} OWNER {os.environ.get('DB_USER')}"
-            )
-    except ProgrammingError:
-        # The next step will tell us if this is a problem.
-        pass
+    #try:
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'main'"
+        )
+        cursor.execute(
+            f"CREATE DATABASE {os.environ.get('STAGE')} OWNER {os.environ.get('DB_USER')}"
+        )
+    # except ProgrammingError:
+    #     # The next step will tell us if this is a problem.
+    #     pass
