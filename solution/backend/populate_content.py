338afcb8b362d9acf0d319160b56f7ca5e9459a6
Caleb Godwin <cgodwin@fearless.tech>
2022-12-08 18:16:15 -0500

    Add fixtures for singleton config items

--- solution/backend/populate_content.py
+++ solution/backend/populate_content.py
@@ -3,37 +3,25 @@ import os
 from django.core.management import call_command
 
 
-def load_data(fixture, model):
-    if not model.objects.count():
-        call_command("loaddata", fixture)
-
-
 def handler(event, context):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmcs_regulations.settings")
     import django
     django.setup()
 
-    from resources.models import (
-        Category,
-        SubCategory,
-        Subpart,
-        Section,
-        SupplementalContent,
-        FederalRegisterDocumentGroup,
-        FederalRegisterDocument,
-        ResourcesConfiguration,
-    )
-
-    from regcore.search.models import Synonym
+    fixtures = [
+        "regulations.siteconfiguration.json",
+        "regcore.parserconfiguration.json",
+        "resources.category.json",
+        "resources.subcategory.json",
+        "resources.subpart.json",
+        "resources.section.json",
+        "resources.supplementalcontent.json",
+        "resources.federalregisterdocumentgroup.json",
+        "resources.federalregisterdocument.json",
+        "resources.resourcesconfiguration.json",
+        "search.synonym.json",
+    ]
 
     call_command("flush", interactive=False)  # Reset the database before loading any fixtures
-
-    load_data("resources.category.json", Category)
-    load_data("resources.subcategory.json", SubCategory)
-    load_data("resources.subpart.json", Subpart)
-    load_data("resources.section.json", Section)
-    load_data("resources.supplementalcontent.json", SupplementalContent)
-    load_data("resources.federalregisterdocumentgroup.json", FederalRegisterDocumentGroup)
-    load_data("resources.federalregisterdocument.json", FederalRegisterDocument)
-    load_data("resources.resourcesconfiguration.json", ResourcesConfiguration)
-    load_data("search.synonym.json", Synonym)
+    for fixture in fixtures:
+        call_command("loaddata", fixture)
