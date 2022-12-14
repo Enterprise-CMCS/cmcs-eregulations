6a682a1a85f88673f9fc40f4ee62d4258daae008
Phil Reese <preese@fearless.tech>
2022-12-14 13:32:56 -0500

    refactor: clean up django search view and remove vestigial template

--- solution/backend/regulations/views/search.py
+++ solution/backend/regulations/views/search.py
@@ -1,42 +1,15 @@
-from datetime import date
-
 from django.views.generic.base import TemplateView
-from django.http import Http404
-
-from regcore.models import Part
-from regcore.search.models import SearchIndex, Synonym
-from .utils import get_structure
 
 
 class SearchView(TemplateView):
-
-    template_name = 'regulations/search_new.html'
+    template_name = 'regulations/search.html'
 
     def get_context_data(self, **kwargs):
-        query = self.request.GET.get("q")
         context = super().get_context_data(**kwargs)
-        today = date.today()
-        results = SearchIndex.objects.effective(today).search(query)
-        parts = Part.objects.effective(today)
-        if not parts:
-            raise Http404
-        structure = get_structure(parts)
-        synonym = None
-        synonym_list = []
-        if query:
-            synonym = Synonym.objects.filter(isActive=True, baseWord__iexact=query.strip('\"')).first()
-            if synonym:
-                for syn in synonym.filtered_synonyms:
-                    synonym_list.append(str(syn))
+        host = self.request.get_host()
 
         c = {
-            'parts': parts,
-            'toc': structure,
-            'results': results,
-            'synonym': synonym,
-            'synonym_list': synonym_list,
-            'unquoted_search': query and not query.startswith('"') and not query.endswith('"') and len(query.split(" ")) > 1,
-            'query': query,
+            'host': host
          }
 
         return {**context, **c, **self.request.GET.dict()}
