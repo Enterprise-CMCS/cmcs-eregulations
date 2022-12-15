24f5abcdb19cfc05b2431b1edaa324261a82b741
thwalker6 <thomas.e.walker88@gmail.com>
2022-12-15 11:17:40 -0500

    EREGSCSC-1635 + EREGCSC-1655 - FR display issues (#623)
    * EREGSCSC-1635 Front page content missing
    
    * merge issue
    
    * changes for sidebar to work and linter
    
    * lint issues
    
    * parser issue
    
    * Move grouping behavior to mixin
    
    * removed par for fr_doc from resourcexp mixin
    
    Co-authored-by: Caleb Godwin <cgodwin@fearless.tech>

--- solution/backend/resources/v3views/mixins.py
+++ solution/backend/resources/v3views/mixins.py
@@ -1,10 +1,16 @@
-from django.db.models import Q, F, Prefetch
+from django.db.models import Q, F, Prefetch, OuterRef, Subquery
 from django.contrib.postgres.search import SearchHeadline, SearchQuery, SearchVector, SearchRank
 from django.core.exceptions import BadRequest
 
-from resources.models import AbstractLocation, AbstractResource, AbstractCategory
-
 from common.api import OpenApiQueryParameter
+from resources.models import (
+    AbstractLocation,
+    AbstractResource,
+    AbstractCategory,
+    FederalRegisterDocument,
+    FederalRegisterDocumentGroup
+)
+
 from common.mixins import OptionalPaginationMixin
 
 
@@ -80,6 +86,49 @@ class LocationExplorerViewSetMixin(OptionalPaginationMixin, LocationFiltererMixi
         return query.distinct()
 
 
+# In order for the order of fed reg documents to be in the correct order, the latest document needs to be extracted.
+# IF a search is done by location and fr_grouping is there results could appear out of order if the most recent document
+# is not associated to the location it makes the results look weird.  This fixes it.
+# TODO: Make this mixin handle everything about the "fr_grouping" parameter too, not passed in as a method parameter.
+class FRDocGroupingMixin:
+    PARAMETERS = [
+        OpenApiQueryParameter("fr_grouping", "Determines if FR Documents should be grouped or not"
+                              "Default is true", bool, False),
+    ]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["fr_grouping"] = self.request.GET.get("fr_grouping", "true").lower() == "true"
+        return context
+
+    def get_final_ids(self, id_query):
+        fr_grouping = self.request.GET.get("fr_grouping", "true").lower() == "true"
+        if fr_grouping:
+            fr_groups = []
+            ids = []
+
+            for i in id_query:
+                if i.group_annotated not in fr_groups:
+                    fr_groups.append(i.group_annotated)
+                # Supplemental content either is none or negative on group annotated. Those must be on the list if requested for
+                # abstract resource
+                if i.group_annotated is None or i.group_annotated < 0:
+                    ids.append(i.id)
+
+            #  This uses a subquery  to get the latest fr_document ID for the fr groups and uses that in place of the original
+            #  ID found.
+            if fr_groups:
+                newest = FederalRegisterDocument.objects.filter(group_id=OuterRef('pk')).order_by('-date')
+                groups = FederalRegisterDocumentGroup.objects\
+                    .annotate(newest_doc=Subquery(newest.values('id')[:1]))\
+                    .filter(id__in=fr_groups)\
+                    .values_list('newest_doc', flat=True)
+                ids = list(groups) + ids
+        else:
+            ids = super().get_final_ids(id_query)
+        return ids
+
+
 # Provides a filterable and searchable viewset for any type of resource
 # Must implement get_search_fields() as a list of fields or a map of model names to lists of fields
 # Must provide "model" as the resource model type to display
@@ -96,8 +145,6 @@ class ResourceExplorerViewSetMixin(OptionalPaginationMixin, LocationFiltererMixi
                               "\"&categories=X&categories=Y\" for multiple.", int, False),
         OpenApiQueryParameter("sort", "Sort results by this field. Valid values are \"newest\", \"oldest\", and \"relevance\". "
                               "Newest is the default, and relevance requires a search query.", str, False),
-        OpenApiQueryParameter("fr_grouping", "Determines if FR Documents should be grouped or not"
-                              "Default is true", bool, False),
     ] + OptionalPaginationMixin.PARAMETERS + LocationFiltererMixin.PARAMETERS
 
     location_filter_prefix = "locations__"
@@ -115,7 +162,6 @@ class ResourceExplorerViewSetMixin(OptionalPaginationMixin, LocationFiltererMixi
         context = super().get_serializer_context()
         context["category_details"] = self.request.GET.get("category_details", "true").lower() == "true"
         context["location_details"] = self.request.GET.get("location_details", "true").lower() == "true"
-        context["fr_grouping"] = self.request.GET.get("fr_grouping", "true").lower() == "true"
         return context
 
     def get_search_vectors(self):
@@ -154,12 +200,14 @@ class ResourceExplorerViewSetMixin(OptionalPaginationMixin, LocationFiltererMixi
                     annotations[f"{model}_{field}_headline"] = self.make_headline(f"{model}__{field}", search_query, search_type)
         return annotations
 
+    def get_final_ids(self, id_query):
+        return [i[0] for i in id_query.values_list("id", "group_annotated")]
+
     def get_queryset(self):
         locations = self.request.GET.getlist("locations")
         categories = self.request.GET.getlist("categories")
         search_query = self.request.GET.get("q")
         sort_method = self.request.GET.get("sort")
-        fr_grouping = self.request.GET.get("fr_grouping", "true").lower() == "true"
 
         id_query = self.model.objects\
                        .filter(approved=True)\
@@ -172,11 +220,9 @@ class ResourceExplorerViewSetMixin(OptionalPaginationMixin, LocationFiltererMixi
         if categories:
             id_query = id_query.filter(category__id__in=categories)
 
-        if fr_grouping:
-            id_query = id_query.order_by("group_annotated").distinct("group_annotated")
+        ids = self.get_final_ids(id_query)
 
         annotations = {}
-        ids = [i[0] for i in id_query.values_list("id", "group_annotated")]
         locations_prefetch = AbstractLocation.objects.all().select_subclasses()
         category_prefetch = AbstractCategory.objects.all().select_subclasses().select_related("subcategory__parent")\
                                             .contains_fr_docs()
