9fbd2effd44bd2566caa8d8bd5889b7d9798f0a7
Thomas Walker <thomas.e.walker88@gmail.com>
2022-12-19 13:25:54 -0500

    lint issues

--- solution/backend/regulations/views/search.py
+++ solution/backend/regulations/views/search.py
@@ -1,9 +1,5 @@
 from django.views.generic.base import TemplateView
-from django.http import Http404
-
-from regcore.models import Part
-from regcore.search.models import SearchIndex, Synonym, SearchConfiguration
-from .utils import get_structure, get_tag_contents
+from regcore.search.models import SearchConfiguration
 
 
 class SearchView(TemplateView):
