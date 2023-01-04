c7786c0eed4eb53aadf4badb5ca46a617166e35d
Phil Reese <preese@fearless.tech>
2023-01-04 09:47:24 -0500

    feat: add basic blocking modal

---
+++ solution/ui/regulations/js/src/components/BlockingModal.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="blocking-modal">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BlockingModal",
+
+    components: {},
+
+    props: {
+        propName: {
+            type: String,
+            required: true,
+        },
+    },
+
+    beforeCreate() {},
+
+    created() {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    beforeUpdate() {},
+
+    updated() {},
+
+    beforeDestroy() {},
+
+    destroyed() {},
+
+    data() {
+        return {
+            dataProp: "value",
+        }
+    },
+
+    computed: {
+        computedProp() {
+            return this.dataProp.toUpperCase();
+        },
+    },
+
+    methods: {
+        methodName() {
+            console.log("method has been invoked");
+        },
+    },
+
+}
+</script>
+
+<style>
+
+</style>
+
