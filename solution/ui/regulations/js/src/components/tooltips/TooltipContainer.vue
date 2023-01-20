75bd00004c0bc23cb8d0a17069ed221d6373197d
Phil Reese <preese@fearless.tech>
2023-01-20 09:39:27 -0500

    chore: skeletons for new components

---
+++ solution/ui/regulations/js/src/components/tooltips/TooltipContainer.vue
@@ -0,0 +1,68 @@
+<template>
+    <div
+        v-show="entered && btn_type === 'icon'"
+        class="copy-tooltip hovered"
+        :class="tooltipClasses"
+        :style="tooltipStyles"
+    >
+        <slot name="tooltip-content"
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TooltipContainer",
+
+    components: {},
+
+    props: {
+        propName: {
+            type: String,
+            required: true,
+        },
+    },
+
+    beforeCreate() {},
+
+    created() {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    beforeUpdate() {},
+
+    updated() {},
+
+    beforeDestroy() {},
+
+    destroyed() {},
+
+    data() {
+        return {
+            dataProp: "value",
+        }
+    },
+
+    computed: {
+        tooltipClasses() {
+            return {
+                "tooltip-caret": this.leftSafe,
+                "tooltip-caret-left": !this.leftSafe,
+            };
+        },
+    },
+
+    methods: {
+        methodName() {
+            console.log("method has been invoked");
+        },
+    },
+
+}
+</script>
+
+<style>
+
+</style>
+
