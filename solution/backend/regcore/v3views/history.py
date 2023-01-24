9fe0befc0f2815479bbec4b1e89a0a7b21e61da0
Caleb Godwin <cgodwin@fearless.tech>
2023-01-24 13:24:06 -0500

    Linting

--- solution/backend/regcore/v3views/history.py
+++ solution/backend/regcore/v3views/history.py
@@ -7,14 +7,20 @@ from rest_framework.response import Response
 
 from regcore.serializers.history import HistorySerializer
 
-async def compute_object(section, year, client):
-    data = await client.head(f"https://www.govinfo.gov/link/cfr/{section['title']}/{section['part']}?sectionnum={section['section']}&year={year}&link-type=pdf")
+
+GOVINFO_YEAR_MIN = 1996
+GOVINFO_LINK = "https://www.govinfo.gov/link/cfr/{}/{}?sectionnum={}&year={}&link-type=pdf"
+
+
+async def check_year(section, year, client):
+    data = await client.head(GOVINFO_LINK.format(section["title"], section["part"], section["section"], year))
     if data.status_code != 302:
         return None
     return {
         "year": str(year),
         "link": data.headers["location"],
     }
+
 
 def year_generator(title, part, section):
     loop = asyncio.new_event_loop()
@@ -25,11 +31,13 @@ def year_generator(title, part, section):
         "part": part,
         "section": section,
     }
-    for future in asyncio.as_completed([compute_object(section_data, year, client) for year in range(1996, datetime.date.today().year + 1)]):
+    max_year = datetime.date.today().year + 1
+    for future in asyncio.as_completed([check_year(section_data, year, client) for year in range(GOVINFO_YEAR_MIN, max_year)]):
         yield loop.run_until_complete(future)
+
 
 class SectionHistoryViewSet(viewsets.ViewSet):
     def list(self, request, *args, **kwargs):
         title, part, section = [self.kwargs.get(i) for i in ["title", "part", "section"]]
-        years = sorted([year for year in year_generator(title, part, section) if year != None], key=lambda year: year["year"])
+        years = sorted([year for year in year_generator(title, part, section) if year is not None], key=lambda year: year["year"])
         return Response(HistorySerializer(instance=years, many=True).data)
