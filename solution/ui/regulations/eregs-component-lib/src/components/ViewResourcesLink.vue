beef042ef09d69322051f8de251b177fec40760b
Phil Reese <preese@fearless.tech>
2023-02-24 10:43:43 -0500

    refactor: more reg text style tweaks

--- solution/ui/regulations/eregs-component-lib/src/components/ViewResourcesLink.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/ViewResourcesLink.vue
@@ -8,12 +8,11 @@
     <div v-else class="view-resources-link" style="padding-left: 5px">
         <button v-if="count !== '0'" class="btn" @click="clickHandler">
             <span class="bold">View {{ section }} resources</span>
-            <span class="count">({{ count }})</span>
+            <span class="count"> ({{ count }})</span>
         </button>
         <button v-else class="btn disabled">
             <span class="bold">{{ section }} Resources</span>
-            <span class="count">(0)</span>
-
+            <span class="count"> (0)</span>
         </button>
     </div>
 </template>
