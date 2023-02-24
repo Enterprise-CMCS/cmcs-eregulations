3a3c18323de8118a8ff84c3ee88cbf32157adb17
Phil Reese <preese@fearless.tech>
2023-02-24 17:08:16 -0500

    refactor: add watcher for when main.js is edited

--- solution/ui/regulations/eregs-component-lib/watch-dist.js
+++ solution/ui/regulations/eregs-component-lib/watch-dist.js
@@ -4,6 +4,7 @@ import shell from "shelljs";
 
 // Initialize watchers.
 const distWatcher = chokidar.watch("./dist");
+const mainWatcher = chokidar.watch("./src/main.js");
 
 // Something to use when events are received.
 const log = console.log.bind(console);
@@ -15,6 +16,19 @@ distWatcher
     })
     .on("change", (path) => {
         log(`File ${path} has been changed`);
+    })
+    .on("unlink", (path) => {
+        log(`File ${path} has been unlinked`);
+    })
+
+mainWatcher
+    .on("add", (path) => {
+        log(`File ${path} has been added`);
+        shell.exec("npm run build:main");
+    })
+    .on("change", (path) => {
+        log(`File ${path} has been changed`);
+        shell.exec("npm run build:main");
     })
     .on("unlink", (path) => {
         log(`File ${path} has been unlinked`);
