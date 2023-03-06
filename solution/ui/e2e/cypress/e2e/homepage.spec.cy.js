68c0fb3c710809dfc17e3df43832a4792e27f3f7
thwalker6 <thomas.e.walker88@gmail.com>
2023-03-06 13:47:32 -0500

    EREGCSC-1564 Revised Jump-to (#725)
    * EREGCSC-1564 Revised Jump-to
    
    * set check of titles to 1
    
    * base url updated
    
    * Hardcode title 42 as only title. Update cypress
    
    * cypress update
    
    * formatting changes
    
    * formatting changes
    
    * formatting fix
    
    * added id's to dropdowns
    
    * ids added to dropdowns
    
    * changed to type for vue, fixed version url
    
    * css changes

--- solution/ui/e2e/cypress/e2e/homepage.spec.cy.js
+++ solution/ui/e2e/cypress/e2e/homepage.spec.cy.js
@@ -140,8 +140,8 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
     it("jumps to a regulation Part using the jump-to select", () => {
         cy.viewport("macbook-15");
         cy.visit("/");
-        cy.get(".jump-to-input select").select("433");
-        cy.get(".jump-to > form").submit();
+        cy.get("#jumpToPart").select("433");
+        cy.get("#jumpBtn").click({ force: true });
 
         cy.url().should("eq", Cypress.config().baseUrl + "/42/433/#433");
     });
@@ -149,9 +149,9 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
     it("jumps to a regulation Part section using the section number text input", () => {
         cy.viewport("macbook-15");
         cy.visit("/");
-        cy.get(".jump-to-input select").should("be.visible").select("433");
-        cy.get(".jump-to-input input.number-box").type("40");
-        cy.get(".jump-to > form").submit();
+        cy.get("#jumpToPart").should("be.visible").select("433");
+        cy.get("#jumpToSection").type("40");
+        cy.get("#jumpBtn").click({ force: true });
 
         cy.url().should(
             "eq",
---
+++ solution/ui/regulations/eregs-component-lib/src/components/JumpTo.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+        <div class="jump-to-label">Jump to Regulation Section</div>
+        <form @submit.prevent="getLink">
+            <div class="jump-to-input">
+                <select
+                    v-if="defaultTitle === ''"
+                    id="jumpToTitle"
+                    v-model="selectedTitle"
+                    name="title"
+                    aria-label="Regulation title number"
+                    class="ds-c-field"
+                    required
+                >
+                    <option value="" disabled selected>Title</option>
+                    <option
+                        v-for="listedTitle in titles"
+                        :key="listedTitle"
+                        :value="listedTitle"
+                    >
+                        {{ listedTitle }} CFR
+                    </option>
+                </select>
+                §
+                <select
+                    id="jumpToPart"
+                    v-model="selectedPart"
+                    name="part"
+                    class="ds-c-field"
+                    aria-label="Regulation part number"
+                    required
+                    :disabled="!selectedTitle"
+                >
+                    <option value="" disable selected>Part</option>
+                    <option
+                        v-for="listedPart in filteredParts"
+                        :key="listedPart"
+                        :value="listedPart"
+                    >
+                        {{ listedPart }}
+                    </option>
+                </select>
+                <span class="dot">.</span>
+                <input
+                    id="jumpToSection"
+                    v-model="selectedSection"
+                    class="number-box ds-c-field"
+                    name="section"
+                    placeholder=""
+                    type="text"
+                    pattern="\d+"
+                    title="Regulation section number, i.e. 111"
+                    aria-label="Regulation section number, i.e. 111"
+                />
+                <input
+                    id="jumpBtn"
+                    class="submit"
+                    :class="{ active: isActive }"
+                    type="submit"
+                    value="Go"
+                />
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+import { getTitles, getParts } from "../api";
+
+export default {
+    name: "JumpTo",
+
+    props: {
+        apiurl: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: false,
+            default: "",
+        },
+        part: {
+            type: String,
+            required: false,
+            default: "",
+        },
+    },
+    data() {
+        return {
+            titles: [],
+            selectedPart: "",
+            defaultTitle: "",
+            selectedTitle: "",
+            selectedSection: "",
+            hideTitle: false,
+            filteredParts: [],
+            isActive: false,
+            parts: [],
+            link: "",
+            base:
+                import.meta.env.VITE_ENV && import.meta.env.VITE_ENV !== "prod"
+                    ? `/${import.meta.env.VITE_ENV}`
+                    : "",
+        };
+    },
+
+    async created() {
+        // When title 45 or another title is added uncomment line below, and remove the hardcoded
+        // this.titles = await getTitles(this.apiurl);
+        this.titles = ["42"];
+
+        if (this.titles.length === 1) {
+            this.selectedTitle = this.titles[0];
+            this.defaultTitle = this.selectedTitle;
+            this.hideTitle = true;
+        }
+        if (this.title !== "") {
+            this.selectedTitle = this.title;
+            this.hideTitle = true;
+        }
+        if (this.part !== "") {
+            this.selectedPart = this.part;
+            this.isActive = true;
+        }
+    },
+
+    watch: {
+        selectedTitle(title) {
+            if (title === "") {
+                this.selectedPart = "";
+                this.isActive = false;
+            } else {
+                this.getParts(title);
+            }
+        },
+
+        selectedPart(part) {
+            if (part !== "") {
+                this.isActive = true;
+            } else {
+                this.isActive = false;
+            }
+        },
+    },
+
+    methods: {
+        async getParts(title) {
+            const partsList = await getParts(this.apiurl, title);
+            this.filteredParts = partsList.map((part) => part.name);
+        },
+        getLink(e) {
+            let link = `${this.base}/goto/?title=${this.selectedTitle}&part=${this.selectedPart}`;
+            if (this.selectedSection !== "") {
+                link += `&section=${this.selectedSection}&-version='latest'`;
+            }
+
+            window.location.href = link;
+        },
+    },
+};
+</script>
