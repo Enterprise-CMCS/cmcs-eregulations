f6fe4a213b10ecc0a3689de8713cca0d264f2038
Caleb Godwin <cgodwin@fearless.tech>
2023-03-13 13:56:54 -0400

    Resolve todos

--- solution/backend/common/auth.py
+++ solution/backend/common/auth.py
@@ -3,11 +3,11 @@ from rest_framework import authentication
 from rest_framework import exceptions
 
 
-class SettingsUser:  # TODO: keep this on v3 move
+class SettingsUser:
     is_authenticated = False
 
 
-class SettingsAuthentication(authentication.BasicAuthentication):  # TODO: keep this on v3 move
+class SettingsAuthentication(authentication.BasicAuthentication):
     def authenticate_credentials(self, userid, password, request=None):
         if userid == settings.HTTP_AUTH_USER and password == settings.HTTP_AUTH_PASSWORD:
             user = SettingsUser()
