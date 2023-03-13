12d8af3c4c3bec550c0f98f9876ba66e36d41149
Phil Reese <preese@fearless.tech>
2023-03-13 16:11:57 -0400

    feat: header search styling, scaffolding for future els

--- solution/ui/regulations/eregs-vite/src/components/HeaderComponent.vue
+++ solution/ui/regulations/eregs-vite/src/components/HeaderComponent.vue
@@ -13,8 +13,24 @@ onMounted(() => {});
 </script>
 
 <template>
-    <div class="header--brand">
-        <a href="/">Medicaid &amp; CHIP eRegulations</a>
+    <div class="header--content-container">
+        <div class="header--brand">
+            <a href="/">Medicaid &amp; CHIP eRegulations</a>
+        </div>
+        <div class="header--jump-to">
+        </div>
+        <div class="header--links-search">
+            <div class="header--links">
+            </div>
+            <div class="header--search">
+                <form class="search__form" action="">
+                    <input type="search" name="q" placeholder="Search" />
+                    <button type="submit" class="search__button--submit" aria-label="Search">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
