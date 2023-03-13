481a4b7bcf9fdf1f80b574a08002e1b4eb2ffedb
Caleb Godwin <cgodwin@fearless.tech>
2023-03-13 11:08:28 -0400

    Move search urls

--- solution/backend/regcore/search/urls.py
+++ solution/backend/regcore/search/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import SearchView
 
 urlpatterns = [
-    path("search", SearchView.as_view({
+    path("", SearchView.as_view({
         "get": "list",
     })),
 ]
