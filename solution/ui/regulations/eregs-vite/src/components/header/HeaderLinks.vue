25478b4aca9e716b9ffcd6ac91a20b8f2d198d3c
Phil Reese <preese@fearless.tech>
2023-03-15 12:54:15 -0400

    feat: first pass at header links style

--- solution/ui/regulations/eregs-vite/src/components/header/HeaderLinks.vue
+++ solution/ui/regulations/eregs-vite/src/components/header/HeaderLinks.vue
@@ -1,7 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref, onBeforeMount, onMounted, onUnmounted, onUpdated } from "vue";
+
+const defaultLinks = [
+    {
+        name: "resources",
+        label: "Resources",
+        active: true,
+        link: "link/to/page",
+    },
+    {
+        name: "about",
+        label: "About",
+        active: false,
+        link: "link/to/page",
+    },
+];
+
+const links = ref(defaultLinks);
+
+onMounted(() => {});
+</script>
 
 <template>
-    <div>
-        Links
+    <div class="links--container">
+        <ul class="links__list links__list--wide">
+            <li v-for="(link, index) in links" :key="index">
+                <span
+                    class="header--links__anchor"
+                    :class="{ active: link.active }"
+                >{{ link.label }}</span>
+            </li>
+        </ul>
+        <div class="links__list--trigger-btn">
+            More
+        </div>
     </div>
 </template>
