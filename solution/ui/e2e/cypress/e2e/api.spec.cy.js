38eb45298b6f16e3821e1f6c04d7ac47f5cf4020
Caleb Godwin <cgodwin@fearless.tech>
2023-03-28 17:00:44 -0400

    Fix test again

--- solution/ui/e2e/cypress/e2e/api.spec.cy.js
+++ solution/ui/e2e/cypress/e2e/api.spec.cy.js
@@ -42,7 +42,7 @@ const API_ENDPOINTS_V3 = [
 describe("API testing", () => {
     API_ENDPOINTS_V3.forEach(endpoint => {
         it(`sends GET request to ${endpoint} and checks for a 200 response`, () => {
-            cy.request(encodeURIComponent(endpoint)).as("request");
+            cy.request(endpoint).as("request");
             cy.get("@request").then((response) => {
                 cy.log(`${endpoint} - ${response.status}`);
                 expect(response.status).to.eq(200);
@@ -53,7 +53,7 @@ describe("API testing", () => {
 
 describe("Synonyms endpoint special character testing", () => {
     SPECIAL_CHARACTERS.forEach(character => {
-        const endpoint = `${SYNONYMS_ENDPOINT}${character}`;
+        const endpoint = SYNONYMS_ENDPOINT + encodeURIComponent(character);
         it(`sends GET request to ${endpoint} and checks for a 200 response`, () => {
             cy.request(endpoint).as("request");
             cy.get("@request").then((response) => {
