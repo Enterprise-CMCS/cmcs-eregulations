dee10978c8d6e3351e74b5452bd85ea1daa143e1
Thomas Walker <thomas.e.walker88@gmail.com>
2023-03-30 13:57:23 -0400

    Unit test for creating search object

--- solution/backend/regcore/tests/test_models.py
+++ solution/backend/regcore/tests/test_models.py
@@ -1,14 +1,71 @@
 from django.test import TestCase
 from regcore.models import ParserConfiguration, Part, TitleConfiguration
 from regcore.search.models import Synonym
+from regcore.search.models import create_search
 
 
 class TestRegoreModels(TestCase):
     @classmethod
     def setUpTestData(cls):
+        doc = {
+            "label": [
+                "400"
+            ],
+            "title": "PART 400 - PAYMENTS FOR SERVICES ",
+            "source": {
+                "header": "Source:",
+                "content": "fizz buzz ",
+                "node_type": ""
+            },
+            "children": [
+                {
+                    "label": [
+                        "400",
+                        "1"
+                    ],
+                    "title": "§ 400.1 Purpose.",
+                    "children": [
+                        {
+                            "text": "fizz buzz",
+                            "label": [
+                                "400",
+                                "1"
+                            ],
+                            "node_type": "Paragraph"
+                        }
+                    ],
+                    "node_type": "SECTION"
+                },
+                {
+                    "label": [
+                        "400",
+                        "10"
+                    ],
+                    "title": "§ 400.10 stuff.",
+                    "children": [
+                        {
+                            "text": "some stuff",
+                            "node_type": "Paragraph"
+                        }
+                    ],
+                    "node_type": "SECTION"
+                }
+            ],
+            "authority": {
+                "header": "Authority:",
+                "content": "42 U.S.C. 1302 and 1396r-8. ",
+                "node_type": ""
+            },
+            "node_type": "PART",
+            "editorial_note": {
+                "header": "",
+                "content": "",
+                "node_type": ""
+            }
+        }
         structure = {"type": "title", "children": [{"type:": "part", "label": 400, "children": ["part content"]}]}
         Part.objects.create(title='42', date="2020-06-30", last_updated="2022-09-21 08:36:45.735759",
-                            depth=2, structure=structure, name=400, document={"document": "test"},
+                            depth=2, structure=structure, name=400, document=doc,
                             depth_stack={"depthstack": "stuff"})
         TitleConfiguration.objects.create(title=50, subchapters="c, d", parts="40, 50",
                                           parser_config=ParserConfiguration.objects.get(id=1))
@@ -29,3 +86,11 @@ class TestRegoreModels(TestCase):
     def test_Synonym_create(self):
         s1 = Synonym.objects.get(baseWord="Syn1")
         self.assertTrue(s1.synonyms.exists())
+
+    def test_create_search(self):
+        part = Part.objects.get(name=400)
+        searchIndexes = create_search(part, part.document, [], parent=None)
+        self.assertEqual(searchIndexes[0].part_number, "400")
+        self.assertEqual(len(searchIndexes),2)
+        self.assertEqual(searchIndexes[1].section_number, "10")
+        self.assertEqual(searchIndexes[0].section_string, "400.1")
