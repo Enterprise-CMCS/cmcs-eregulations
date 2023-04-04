0c719b8ffa7224955caa1dda20f59095386b0421
thwalker6 <thomas.e.walker88@gmail.com>
2023-04-04 11:16:40 -0400

    EREGCSC-1825: Unit test for creating search object (#757)
    * Unit test for creating search object
    
    * linter issue
    
    * cleaned up some unittest with fixtures
    
    * linter fix

--- solution/backend/regcore/tests/test_models.py
+++ solution/backend/regcore/tests/test_models.py
@@ -1,14 +1,18 @@
 from django.test import TestCase
 from regcore.models import ParserConfiguration, Part, TitleConfiguration
 from regcore.search.models import Synonym
+from regcore.search.models import create_search
+import json
 
 
 class TestRegoreModels(TestCase):
     @classmethod
     def setUpTestData(cls):
+        with open("regcore/tests/fixtures/part.json") as f:
+            doc = json.load(f)
         structure = {"type": "title", "children": [{"type:": "part", "label": 400, "children": ["part content"]}]}
         Part.objects.create(title='42', date="2020-06-30", last_updated="2022-09-21 08:36:45.735759",
-                            depth=2, structure=structure, name=400, document={"document": "test"},
+                            depth=2, structure=structure, name=400, document=doc,
                             depth_stack={"depthstack": "stuff"})
         TitleConfiguration.objects.create(title=50, subchapters="c, d", parts="40, 50",
                                           parser_config=ParserConfiguration.objects.get(id=1))
@@ -29,3 +33,11 @@ class TestRegoreModels(TestCase):
     def test_Synonym_create(self):
         s1 = Synonym.objects.get(baseWord="Syn1")
         self.assertTrue(s1.synonyms.exists())
+
+    def test_create_search(self):
+        part = Part.objects.get(name=400)
+        searchIndexes = create_search(part, part.document, [], parent=None)
+        self.assertEqual(searchIndexes[0].part_number, "400")
+        self.assertEqual(len(searchIndexes), 2)
+        self.assertEqual(searchIndexes[1].section_number, "10")
+        self.assertEqual(searchIndexes[0].section_string, "400.1")
