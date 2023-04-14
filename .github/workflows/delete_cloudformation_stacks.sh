f2d79c2622566df6c38c14996dcaae7564abd286
Parissa <parissa.eggleston@gmail.com>
2023-04-14 16:23:07 -0400

    move code to its own script

---
+++ .github/workflows/delete_cloudformation_stacks.sh
@@ -0,0 +1,36 @@
+ #!/bin/bash
+ set -e
+
+
+# Function to get the stack status
+STACK_NAME=$1
+PR=$2
+CONFIG=$3
+
+echo $STACK_NAME
+
+# Function to get the stack status
+get_stack_status() {
+  aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query 'Stacks[0].StackStatus' --output text 2> /dev/null
+}
+
+# Check if the stack exists
+echo "check if stack exists"
+STACK_EXISTS=$(get_stack_status)
+
+echo ${STACK_EXISTS} 
+
+while [[ "${STACK_EXISTS}" == "CREATE_IN_PROGRESS" || "${STACK_EXISTS}" == "UPDATE_IN_PROGRESS" || "${STACK_EXISTS}" == "DELETE_IN_PROGRESS" ]]; do
+  echo "Waiting for the stack '${STACK_NAME}' to finish its ongoing operation..."
+  sleep 10
+  STACK_EXISTS=$(get_stack_status)
+done
+
+if [ -z "${STACK_EXISTS}" ]; then
+  echo "Stack '${STACK_NAME}' does not exist. Nothing to delete."
+else
+  # Stack exists and is not in an "in progress" state, delete the stack
+  echo "Deleting the stack '${STACK_NAME}'..."
+  serverless remove --stage dev${PR} --config "${CONFIG}"
+  echo "Stack '${STACK_NAME}' deleted successfully."
+fi
