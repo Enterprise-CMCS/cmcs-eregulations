4d2490ab8bfc54d2f2e42f97019c07480f8a5994
Parissa <parissa.eggleston@gmail.com>
2023-04-17 12:28:46 -0400

    refactor code

--- .github/workflows/delete_cloudformation_stacks.sh
+++ .github/workflows/delete_cloudformation_stacks.sh
@@ -16,24 +16,21 @@ get_stack_status() {
 }
 
 # Check if the stack exists
-STACK_EXISTS=$(get_stack_status)
 
-echo "before while for ${STACK_EXISTS}"
-
-if [ -z "${STACK_EXISTS}" ]; then
-   echo "within stack does not exist"
-   echo "Stack '${STACK_NAME}' does not exist. Nothing to delete."
-else
-  echo "stack exists"
-  while [[ "${STACK_EXISTS}" == "CREATE_IN_PROGRESS" || "${STACK_EXISTS}" == "UPDATE_IN_PROGRESS" || "${STACK_EXISTS}" == "DELETE_IN_PROGRESS" ]]; do
-    echo "Waiting for the stack '${STACK_NAME}' to finish its ongoing operation..."
-    sleep 10
+if aws cloudformation describe-stacks --stack-name "$STACK_NAME" > /dev/null 2>&1; then
     STACK_EXISTS=$(get_stack_status)
-  done
+    echo "Stack status: $stack_status"
+    while [[ "${STACK_EXISTS}" == "CREATE_IN_PROGRESS" || "${STACK_EXISTS}" == "UPDATE_IN_PROGRESS" || "${STACK_EXISTS}" == "DELETE_IN_PROGRESS" ]]; do
+      echo "Waiting for the stack '${STACK_NAME}' to finish its ongoing operation..."
+      sleep 10
+      STACK_EXISTS=$(get_stack_status)
+    done
 
    # Stack exists and is not in an "in progress" state, delete the stack
    echo "Deleting the stack '${STACK_NAME}'..."
    serverless remove --stage dev${PR} --config ./serverless-ecfr.yml
    aws --debug cloudformation wait stack-delete-complete --stack-name "${STACK_NAME}"
    echo "Stack '${STACK_NAME}' deleted successfully."
+else
+   echo "Stack '${STACK_NAME}' does not exist. Nothing to delete."
 fi
