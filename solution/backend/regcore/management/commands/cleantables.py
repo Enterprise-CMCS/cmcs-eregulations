9c616d2fb08769ceb9b0a60c47a05a59c4a9854d
Thomas <thomas.e.walker88@gmail.com>
2023-05-01 14:06:30 -0400

    add command to delete certian models objects

---
+++ solution/backend/regcore/management/commands/cleantables.py
@@ -0,0 +1,22 @@
+
+from django.core.management.base import BaseCommand
+
+from resources.models import (AbstractCategory,
+                              AbstractResource,
+                              AbstractLocation,
+                              FederalRegisterDocumentGroup,
+                              ResourcesConfiguration
+                              )
+
+from regcore.search.models import Synonym
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        AbstractResource.objects.all().delete()
+        AbstractCategory.objects.all().delete()
+        AbstractLocation.objects.all().delete()
+        ResourcesConfiguration.objects.all().delete()
+        FederalRegisterDocumentGroup.objects.all().delete()
+        Synonym.objects.all().delete()
