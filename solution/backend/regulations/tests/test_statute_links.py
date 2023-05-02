cfa5548b9fb2ec629d814fd13c5723cf48ded59b
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2023-05-02 13:06:46 -0400

    EREGCSC-1840 Test out parsing for SSA-USC numbering (#800)
    * Add admin page for importing
    
    * Add regex to detect and add conversions
    
    * Rename to Statute Link Converter, add Act field
    
    * Linting
    
    * Add unit test for try_import
    
    * Remove unused test
    
    * Clarify assertCountEqual
    
    * Implement suggested changes (see commit message)
    
    * 'Section X' changed to 'Title X section Y'
    * Reorder some imports
    * Add source_url field (readonly) for future reference
    * Update tests to account for new field
    
    * More requested changes
    
    * Add help text
    
    * Update regex to support more sections
    
    * Allow letters in section number
    
    * Include sections with dashes
    
    * Change section to charfield
    
    * Change ordering and verbose name of fields

---
+++ solution/backend/regulations/tests/test_statute_links.py
@@ -0,0 +1,85 @@
+import json
+from unittest import mock
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from requests.exceptions import HTTPError
+
+from regulations.admin import StatuteLinkConverterAdmin
+from regulations.models import StatuteLinkConverter
+
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, status_code, text):
+            self.status_code = status_code
+            self.text = text
+
+        def raise_for_status(self):
+            if self.status_code != 200:
+                raise HTTPError(f"status code is {self.status_code}: {self.text}")
+
+    if args[0] == "http://test.com/test.xml":
+        try:
+            data = ""
+            with open("regulations/tests/fixtures/statute_link_data.xml", "r") as f:
+                for line in f:
+                    data += line
+            return MockResponse(200, data)
+        except Exception as e:
+            return MockResponse(400, str(e))
+    elif args[0] == "http://test.com/invalid.xml":
+        return MockResponse(200, "Hello World")
+    return MockResponse(404, "invalid URL")
+
+
+class TestStatuteLinkImport(TestCase):
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_try_import(self, mocked_get):
+        with open("regulations/tests/fixtures/statute_link_golden.json", "r") as f:
+            golden = json.load(f)
+        admin = StatuteLinkConverterAdmin(model=StatuteLinkConverter, admin_site=None)
+        conversions = admin.try_import("http://test.com/test.xml", "SSA")
+        # Despite the name, assertCountEqual does make sure all items in the list are the same, not just the count.
+        # It just supports lists that are out of order, unlike assertEqual.
+        self.assertCountEqual(conversions, golden)
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_try_import_404(self, mocked_get):
+        admin = StatuteLinkConverterAdmin(model=StatuteLinkConverter, admin_site=None)
+        with self.assertRaises(HTTPError):
+            _ = admin.try_import("http://test.com/bad-link.xml", "SSA")
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_try_import_conversions_exist(self, mocked_get):
+        with open("regulations/tests/fixtures/statute_link_golden.json", "r") as f:
+            golden = json.load(f)
+        for i in golden:
+            StatuteLinkConverter.objects.create(
+                title=i["title"],
+                section=i["section"],
+                usc=i["usc"],
+                act=i["act"],
+                source_url=i["source_url"]
+            )
+        admin = StatuteLinkConverterAdmin(model=StatuteLinkConverter, admin_site=None)
+        with self.assertRaises(ValidationError):
+            _ = admin.try_import("http://test.com/test.xml", "SSA")
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_try_import_no_conversions(self, mocked_get):
+        admin = StatuteLinkConverterAdmin(model=StatuteLinkConverter, admin_site=None)
+        with self.assertRaises(ValidationError):
+            _ = admin.try_import("http://test.com/invalid.xml", "SSA")
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_try_import_bad_params(self, mocked_get):
+        admin = StatuteLinkConverterAdmin(model=StatuteLinkConverter, admin_site=None)
+        bad_params = [
+            ("", "SSA"),
+            ("  ", "SSA"),
+            ("http://test.com/test.xml", ""),
+        ]
+        for i in bad_params:
+            self.assertRaises(ValidationError, admin.try_import, *i)
