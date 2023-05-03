801785714a9b5f5a454e0060fc48bd8d9791e4da
Thomas <thomas.e.walker88@gmail.com>
2023-05-03 12:21:33 -0400

    Add lines for linter

--- solution/backend/resources/models.py
+++ solution/backend/resources/models.py
@@ -240,7 +240,10 @@ class SupplementalContent(AbstractResource, TypicalResourceFieldsMixin):
         verbose_name = "Supplemental Content"
         verbose_name_plural = "Supplemental Content"
 
+
 doc_types = [('RFI', 'RFI'), ('NPRM', 'NPRM'), ("Final", 'Final')]
+
+
 class FederalRegisterDocument(AbstractResource, TypicalResourceFieldsMixin):
     docket_numbers = ArrayField(models.CharField(max_length=255, blank=True, null=True), default=list, blank=True)
     document_number = models.CharField(max_length=255, blank=True, null=True)
