df5eed2752b22e4de65c3cd8fae62177268f6b75
Caleb Godwin <cgodwin@fearless.tech>
2023-05-09 12:37:35 -0400

    Improve regex

--- solution/backend/regulations/templatetags/link_statutes.py
+++ solution/backend/regulations/templatetags/link_statutes.py
@@ -9,22 +9,25 @@ register = template.Library()
 LINK_FORMAT = '<a target="_blank" class="external" href="https://uscode.house.gov/view.xhtml'\
               '?req=granuleid:USC-prelim-title{}-section{}&num=0&edition=prelim">{}</a>'
 
-# Matches entire statute references, including one or more sections and an optional Act.
-# For example, "Sections 1902(a)(2) and (b)(1) and 1903(b) of the Social Security Act" and its variations.
-# Will also match "Section 1902" if the section is contained within the DEFAULT_ACT. See tests for more complete examples.
-STATUTE_REF_PATTERN = r"\bsect(?:ion[s]?|s?).?\s*((?:\d+[a-z]?(?:-+[a-z0-9]+)?(?:(?:,?\s*(?:and|or|\&)?\s*)?\([a-z0-9]+\))*"\
-                      r"(?:,?\s*(?:and|or|\&)?\s*)?)+)(?:\s*of\s*the\s*([a-z0-9\s]*?(?=\bact\b)))?"
-
-# Matches individual sections, for example "Section 1902(a)(2) and (b)(1)" and its variations.
-SECTION_PATTERN = r"(\d+[a-z]?(?:-+[a-z0-9]+)?(?:(?:,?\s*(?:and|or|\&)?\s*)?\([a-z0-9]+\))*)"
-
 # Extracts the section ID only, for example "1902-1G" and its variations.
 SECTION_ID_PATTERN = r"(\d+[a-z]?(?:-+[a-z0-9]+)?)"
 
+# Matches ", and", ", or", "and", "or", "&", and more variations.
+AND_OR_PATTERN = r"(?:,?\s*(?:and|or|\&)?\s*)?"
+
+# Matches individual sections, for example "Section 1902(a)(2) and (b)(1)" and its variations.
+SECTION_PATTERN = rf"({SECTION_ID_PATTERN}(?:{AND_OR_PATTERN}\([a-z0-9]+\))*)"
+
+# Matches entire statute references, including one or more sections and an optional Act.
+# For example, "Sections 1902(a)(2) and (b)(1) and 1903(b) of the Social Security Act" and its variations.
+# Will also match "Section 1902" if the section is contained within the DEFAULT_ACT. See tests for more complete examples.
+STATUTE_REF_PATTERN = rf"\bsect(?:ion[s]?|s?).?\s*((?:{SECTION_PATTERN}{AND_OR_PATTERN})+)"\
+                      r"(?:\s*of\s*the\s*([a-z0-9\s]*?(?=\bact\b)))?"
+
 # Regex's are precompiled to improve page load time.
-STATUTE_REF_REGEX = re.compile(STATUTE_REF_PATTERN, re.IGNORECASE)
-SECTION_REGEX = re.compile(SECTION_PATTERN, re.IGNORECASE)
 SECTION_ID_REGEX = re.compile(SECTION_ID_PATTERN, re.IGNORECASE)
+SECTION_REGEX = re.compile(SECTION_PATTERN, re.IGNORECASE)
+STATUTE_REF_REGEX = re.compile(STATUTE_REF_PATTERN, re.IGNORECASE)
 
 # The act to use if none is specified, for example "section 1902 of the act" defaults to this.
 DEFAULT_ACT = "Social Security Act"
