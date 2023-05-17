706de10a79d44cc6753a057a60edd3be1a3efd66
peggles2 <peggles2@users.noreply.github.com>
2023-05-17 16:58:44 -0400

    EREGCSC-1877-enable-jump-to (#820)
    * EREGCSC-1877
    
    * fix width for title to accomidate length of the title
    
    * update width to 280px

--- solution/ui/e2e/cypress/e2e/error-page.cy.js
+++ solution/ui/e2e/cypress/e2e/error-page.cy.js
@@ -78,24 +78,27 @@ describe("Error page", { scrollBehavior: "center" }, () => {
         cy.get("div.blocking-modal-content").should("not.be.visible");
     });
 
-    it("jumps to a regulation Part using the jump-to select", () => {
+    it("error-page - jumps to a regulation Part using the jump-to select", () => {
         cy.viewport("macbook-15");
         cy.request({ url: "/404", failOnStatusCode: false })
             .its("status")
             .should("equal", 404);
         cy.visit("/404", { failOnStatusCode: false });
-        cy.get("#jumpToPart").select("433");
+        cy.get("#jumpToTitle").select("45", { force: true }).then(() => {
+          cy.get("#jumpToPart").should("be.visible").select("95");
+        });
         cy.get("#jumpBtn").click({ force: true });
 
-        cy.url().should("eq", Cypress.config().baseUrl + "/42/433/#433");
+        cy.url().should("eq", Cypress.config().baseUrl + "/45/95/#95");
     });
 
-    it("NEW -- jumps to a regulation Part section using the section number text input", () => {
+    it("NEW -- error-page - jumps to a regulation Part section using the section number text input", () => {
         cy.viewport("macbook-15");
         cy.request({ url: "/404", failOnStatusCode: false })
             .its("status")
             .should("equal", 404);
         cy.visit("/404", { failOnStatusCode: false });
+        cy.get("#jumpToTitle").select("42")
         cy.get("#jumpToPart").should("be.visible").select("433");
         cy.get("#jumpToSection").type("40");
         cy.get("#jumpBtn").click({ force: true });
