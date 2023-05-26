450288dac88f4f0bf768a6feee953483cf4ad8ac
Parissa <parissa.eggleston@gmail.com>
2023-05-26 15:01:25 -0400

    refactor this code to fail gracefully if database is not there

--- solution/backend/dropdb.py
+++ solution/backend/dropdb.py
@@ -4,18 +4,16 @@ import os
 
 def handler(event, context):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmcs_regulations.settings")
-    import django
     django.setup()
 
-    from django.db import connection
     connection.ensure_connection()
-    try:
-        if not connection.is_usable():
-            raise Exception("database is unreachable")
-    except Exception as e:
-        print(e)
-
-    with connection.cursor() as cursor:
-        cursor.execute(f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{os.environ.get('STAGE', '')}'")
-        cursor.execute(f"DROP DATABASE {os.environ.get('STAGE', '')}")
-    print(f"Database {os.environ.get('STAGE', '')} has been removed")
+    if not connection.is_usable():
+        print("database is unreachable")
+    else:
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{os.environ.get('STAGE', '')}'")
+                cursor.execute(f"DROP DATABASE {os.environ.get('STAGE', '')}")
+                print(f"Database {os.environ.get('STAGE', '')} has been removed")
+        except Exception as e:
+            print(f"An error occurred while deleting the database: {str(e)}")
