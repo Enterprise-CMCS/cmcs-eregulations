60f632dd1fa584bc99cc5a25ba939c0835d467ba
Caleb Godwin <cgodwin@fearless.tech>
2023-05-30 14:14:45 -0400

    Only raise error if db exists but something else goes wrong

--- solution/backend/dropdb.py
+++ solution/backend/dropdb.py
@@ -2,22 +2,25 @@
 import os
 import django
 from django.db import connection
+from django.db.utils import OperationalError
 
 
 def handler(event, context):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmcs_regulations.settings")
     django.setup()
 
-    connection.ensure_connection()
+    try:
+        connection.ensure_connection()
+    except OperationalError as e:
+        print(f"Failed to delete the database: {str(e)}")
+        quit()
+
     if not connection.is_usable():
-        print("database is unreachable")
-    else:
-        try:
-            with connection.cursor() as cursor:
-                db_name = os.environ.get('STAGE', '')
-                query = f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{db_name}'"
-                cursor.execute(query)
-                cursor.execute(f"DROP DATABASE {db_name}")
-                print(f"Database {db_name} has been removed")
-        except Exception as e:
-            print(f"An error occurred while deleting the database: {str(e)}")
+        raise Exception("Database is unreachable")
+
+    with connection.cursor() as cursor:
+        db_name = os.environ.get('STAGE', '')
+        query = f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{db_name}'"
+        cursor.execute(query)
+        cursor.execute(f"DROP DATABASE {db_name}")
+        print(f"Database {db_name} has been removed")
