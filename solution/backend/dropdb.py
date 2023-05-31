dac158adb1d60ee9103e8781493a1aac29393eea
Caleb Godwin <cgodwin@fearless.tech>
2023-05-31 11:35:23 -0400

    Add a connection to the internal postgres database and use that for deletion
    This permits differentiation between ensure_connection raising OperationalError because the database can't
    connect versus the database doesn't exist. ensure_connection should ONLY fail now if the server connection
    fails. Now the error will be raised during cursor.execute(...) if it doesn't exist.

--- solution/backend/dropdb.py
+++ solution/backend/dropdb.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+
 import os
+
 import django
-from django.db import connection
-from django.db.utils import OperationalError
+from django.db import connections
+from django.db.utils import ProgrammingError
 
 
 def handler(event, context):
@@ -10,17 +12,20 @@ def handler(event, context):
     django.setup()
 
     try:
+        connection = connections["postgres"]
+        connection.connect()
         connection.ensure_connection()
-    except OperationalError as e:
-        print(f"Failed to delete the database: {str(e)}")
-        quit()
-
-    if not connection.is_usable():
-        raise Exception("Database is unreachable")
-
-    with connection.cursor() as cursor:
-        db_name = os.environ.get('STAGE', '')
-        query = f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{db_name}'"
-        cursor.execute(query)
-        cursor.execute(f"DROP DATABASE {db_name}")
-        print(f"Database {db_name} has been removed")
+        if not connection.is_usable():
+            raise Exception("database connection is not usable")
+    except Exception as e:
+        raise Exception(f"Failed to connect to the database: {str(e)}")
+    
+    try:
+        with connection.cursor() as cursor:
+            db_name = os.environ.get('STAGE', '')
+            query = f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{db_name}'"
+            cursor.execute(query)
+            cursor.execute(f"DROP DATABASE {db_name}")
+            print(f"Database {db_name} has been removed")
+    except ProgrammingError as e:
+        print(f"Database was not deleted, most likely because it does not exist: {str(e)}")
