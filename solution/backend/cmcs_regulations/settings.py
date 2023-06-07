b72fe9ab6246b96e586b3a58f91471d77c10286f
Thomas Walker <thomas.e.walker88@gmail.com>
2023-06-07 08:08:05 -0400

    Update dropdb to not need a new database

--- solution/backend/cmcs_regulations/settings.py
+++ solution/backend/cmcs_regulations/settings.py
@@ -140,15 +140,10 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'NAME': os.environ.get('DB_NAME', 'eregs'),
-    },
-    'postgres': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': os.environ.get('DB_USER', 'eregs'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'sgere'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-        'NAME': "postgres",
-    },
+        'TEST': {
+            'NAME': 'testing_db'
+        }
+    }
 }
 
 
