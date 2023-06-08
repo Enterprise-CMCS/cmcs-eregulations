4e03884536a26af4d16390687e850e84f940f8b9
Thomas Walker <thomas.e.walker88@gmail.com>
2023-06-08 08:21:16 -0400

    setting django server url

--- solution/backend/cmcs_regulations/settings.py
+++ solution/backend/cmcs_regulations/settings.py
@@ -137,7 +137,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'USER': os.environ.get('DB_USER', 'eregs'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'sgere'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'NAME': os.environ.get('DB_NAME', 'eregs'),
         'TEST': {
