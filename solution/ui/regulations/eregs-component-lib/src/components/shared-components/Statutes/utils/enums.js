f9be3f7c24170743ce9e652c552c77cd364ed71e
Phil Reese <preese@fearless.tech>
2023-07-07 12:41:10 -0400

    EREGCSC-1921 -- Statute SPA/Table (#856)
    * feat: add header/footer to statute page
    
    * feat: make statutes a SPA
    
    * feat: correct header tag
    
    * feat: h2 text change
    
    * feat: get statutes api method
    
    * feat: basic wide table
    
    * feat: desktop cell styling
    
    * feat: table parent, caption styles
    
    * feat: add PDF svg icon; flesh out links
    
    * feat: style desktop table left margin to match banner
    
    * fix: bind key to proper element
    
    * feat: add loading icon when fetching statutes
    
    * chore: comment out caption element for now
    
    * feat: sticky table header
    
    * feat: build table header from array
    
    * feat: add hover color for links
    
    * feat: narrow/desktop rendering; create table from obj
    
    * feat: tweak statute title roman/int label
    
    * feat: very basic narrow styles
    
    * feat: genericize table schemas
    
    * feat: add placeholder aca schema
    
    * feat: better narrow styling
    
    * feat: list/table style tweaks
    
    * chore: formatting
    
    * feat: break table cells out to own components
    
    * chore: stab at better keys
    
    * feat: better narrow styles
    
    * feat: add and style placeholder table caption
    
    * chore: JSDoc
    
    * chore: destructure variables from func parameter object in arguments list
    
    * chore: clean up StatuteTable props
    
    * feat: keep border/padding for extreme narrow views
    
    * chore: update statute template title
    
    * chore: update house.gov effective date month
    
    * feat: right padding to left column cell
    
    * test: add a11y cypress test and fixture for statutes page
    
    * feat: remove spaces in U.S.C. label
    
    * chore: bump to deploy
    
    * chore: add main-content id to statute page; update scroll-margin-top
    
    * feat: add updated caption; update column header label
    
    * feat: update another column label
    
    * docs: update param reference label
    
    * chore: remove empty style tags

---
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/utils/enums.js
@@ -0,0 +1,7 @@
+const DISPLAY_TYPES = ["table", "list"];
+const TABLE_TYPES = ["aca", "ssa"];
+
+export {
+    DISPLAY_TYPES,
+    TABLE_TYPES,
+};
---
+++ solution/ui/regulations/eregs-vite/src/views/Statutes.vue
@@ -0,0 +1,173 @@
+<script setup>
+import { computed, onMounted, onUnmounted, ref } from "vue";
+
+import { getStatutes } from "utilities/api";
+
+import BlockingModal from "eregsComponentLib/src/components/BlockingModal.vue";
+import FlashBanner from "eregsComponentLib/src/components/FlashBanner.vue";
+import IFrameContainer from "eregsComponentLib/src/components/IFrameContainer.vue";
+import SimpleSpinner from "eregsComponentLib/src/components/SimpleSpinner.vue";
+import StatuteTable from "eregsComponentLib/src/components/shared-components/Statutes/StatuteTable.vue";
+
+import Banner from "@/components/Banner.vue";
+import HeaderComponent from "@/components/header/HeaderComponent.vue";
+import HeaderLinks from "@/components/header/HeaderLinks.vue";
+import HeaderSearch from "@/components/header/HeaderSearch.vue";
+import JumpTo from "@/components/JumpTo.vue";
+
+const props = defineProps({
+    aboutUrl: {
+        type: String,
+        default: "/about/",
+    },
+    apiUrl: {
+        type: String,
+        default: "/v3/",
+    },
+    homeUrl: {
+        type: String,
+        default: "/",
+    },
+    resourcesUrl: {
+        type: String,
+        default: "/resources/",
+    },
+    searchUrl: {
+        type: String,
+        default: "/search/",
+    },
+});
+
+// Get statutes
+const statutes = ref({
+    results: [],
+    loading: true,
+});
+const getStatutesArray = async () => {
+    try {
+        const statutesArray = await getStatutes({ apiUrl: props.apiUrl });
+        statutes.value.results = statutesArray;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        statutes.value.loading = false;
+    }
+};
+
+const windowWidth = ref(window.innerWidth);
+const isNarrow = computed(() => windowWidth.value < 1024);
+
+// Watch Banner left margin
+const bannerRef = ref(null);
+const bannerLeftMargin = ref(0);
+
+const getBannerLeftMargin = () => {
+    const bannerContent = bannerRef.value.$el
+        .getElementsByClassName("content")
+        .item(0);
+
+    bannerLeftMargin.value = window.getComputedStyle(bannerContent).marginLeft;
+};
+
+const onWidthChange = () => {
+    windowWidth.value = window.innerWidth;
+    getBannerLeftMargin();
+};
+
+onMounted(() => {
+    window.addEventListener("resize", onWidthChange);
+    getBannerLeftMargin();
+});
+onUnmounted(() => window.removeEventListener("resize", onWidthChange));
+
+// On load
+getStatutesArray();
+</script>
+
+<template>
+    <body class="ds-base statute-page">
+        <BlockingModal>
+            <IFrameContainer
+                src="https://docs.google.com/forms/d/e/1FAIpQLSdcG9mfTz6Kebdni8YSacl27rIwpGy2a7GsMGO0kb_T7FSNxg/viewform?embedded=true"
+                title="Google Forms iframe"
+            />
+        </BlockingModal>
+        <FlashBanner />
+        <header id="header" class="sticky">
+            <HeaderComponent :home-url="homeUrl">
+                <template #jump-to>
+                    <JumpTo :home-url="homeUrl" />
+                </template>
+                <template #links>
+                    <HeaderLinks
+                        :about-url="aboutUrl"
+                        :resources-url="resourcesUrl"
+                    />
+                </template>
+                <template #search>
+                    <HeaderSearch :search-url="searchUrl" />
+                </template>
+            </HeaderComponent>
+        </header>
+        <div id="statuteApp" class="statute-view">
+            <Banner ref="bannerRef" title="Statute Reference">
+                <template #description>
+                    <h2>Look up statute text in online sources</h2>
+                </template>
+            </Banner>
+            <div id="main-content" class="statute__container">
+                <div class="content" :style="{ marginLeft: bannerLeftMargin }">
+                    <div class="table__parent">
+                        <SimpleSpinner
+                            v-if="statutes.loading"
+                            class="table__spinner"
+                        />
+                        <template v-else>
+                            <div class="table__caption">
+                                In selected Social Security Act titles (XI, XVI,
+                                XVIII, XIX, XXI), find equivalent US Code
+                                citations and read the text in your choice of
+                                government website. Coming up soon: more
+                                navigation options and additional statutes.<br />
+                                Learn more about these sources:
+                                <a
+                                    class="external"
+                                    href="https://uscode.house.gov/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >US Code House.gov</a
+                                >,
+                                <a
+                                    class="external"
+                                    href="https://www.govinfo.gov/app/collection/comps/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >Statute Compilation</a
+                                >,
+                                <a
+                                    class="external"
+                                    href="https://www.govinfo.gov/app/collection/uscode"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >US Code Annual</a
+                                >,
+                                <a
+                                    class="external"
+                                    href="https://www.ssa.gov/OP_Home/ssact/ssact.htm"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >SSA.gov Compilation</a
+                                >.
+                            </div>
+                            <StatuteTable
+                                :display-type="isNarrow ? 'list' : 'table'"
+                                :filtered-statutes="statutes.results"
+                                table-type="ssa"
+                            />
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</template>
