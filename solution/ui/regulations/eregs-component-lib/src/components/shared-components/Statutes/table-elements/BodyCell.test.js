3c8f78f73eeca5c81c09faaef74331125c8f9cc9
Phil Reese <preese@fearless.tech>
2023-07-10 16:02:50 -0400

    test: add table cell snapshot tests

---
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/table-elements/BodyCell.test.js
@@ -0,0 +1,25 @@
+import flushPromises from "flush-promises";
+import { render, screen } from "@testing-library/vue";
+import { describe, it, expect } from "vitest";
+
+import { ssaSchema } from "../schemas/tableSchemas";
+import statutesFixture from "cypress/fixtures/statutes.json";
+
+import BodyCell from "./BodyCell.vue";
+
+describe("Statute Table Body Cell", () => {
+    describe("SSA table type", () => {
+        ssaSchema.forEach((column, index) => {
+            it(`Creates a snapshot of a body cell for column ${index + 1}`, async () => {
+                const wrapper = render(BodyCell, {
+                    props: {
+                        cellData: column,
+                        statute: statutesFixture[0],
+                    },
+                });
+                await flushPromises();
+                expect(wrapper).toMatchSnapshot();
+            });
+        });
+    });
+});
