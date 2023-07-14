23144967e075202d1d82ba95de55155ef0b379d1
Parissa <parissa.eggleston@gmail.com>
2023-07-14 18:16:33 -0400

    fix test

--- solution/backend/regulations/tests/test_site_configuration.py
+++ solution/backend/regulations/tests/test_site_configuration.py
@@ -1,17 +1,20 @@
-from django.test import TestCase
+import unittest
+import pytest
+from django.core.exceptions import ValidationError
 from regulations.models import SiteConfiguration
 
-
-class SiteConfigurationTest(TestCase):
+@pytest.mark.django_db
+class SiteConfigurationTest(unittest.TestCase):
     def test_site_configuration_str(self):
-        site_config = SiteConfiguration.objects.create(date_type='effective', date='2023-07-10')
+        SiteConfiguration.objects.all().delete()
+        site_config = SiteConfiguration.objects.create(pk=2, date_type='effective', date='2023-07-10')
         expected_str = 'Effective 2023-07-10'
         self.assertEqual(str(site_config), expected_str)
 
-    def test_site_configuration_reverse(self):
-        expected_date_type = 'effective'
-        expected_date = '2023-07-10'
-        site_config = SiteConfiguration.objects.create(date_type=expected_date_type, date=expected_date)
-        parsed_site_config = SiteConfiguration.objects.get(pk=site_config.pk)
-        self.assertEqual(parsed_site_config.date_type, expected_date_type)
-        self.assertEqual(parsed_site_config.date, expected_date)
+    def test_site_configuration_blank_date_type(self):
+        site_config = SiteConfiguration(date='')
+        site_config.full_clean()  # Should not raise any ValidationError
+
+    def test_site_configuration_valid_date_type(self):
+        site_config = SiteConfiguration(date_type='effective')
+        site_config.full_clean()  # Should not raise any ValidationError
