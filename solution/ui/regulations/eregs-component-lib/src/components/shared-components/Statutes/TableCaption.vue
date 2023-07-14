e4518e9c7b13d9d59e8eebcfdf8d701b3540c96d
Phil Reese <preese@fearless.tech>
2023-07-14 14:37:10 -0400

    feat: better caption

--- solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/TableCaption.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/TableCaption.vue
@@ -1,57 +1,44 @@
-<script setup>
-import { computed } from "vue";
-
-import { romanize } from "utilities/utils";
-
-const props = defineProps({
-    selectedAct: {
-        type: String,
-        required: false,
-        default: "Social Security Act",
-    },
-    selectedTitle: {
-        type: String,
-        required: false,
-        default: "19",
-    },
-});
-
-const romanizedTitle = computed(() =>
-    romanize(parseInt(props.selectedTitle, 10))
-);
-</script>
+<script setup></script>
 
 <template>
     <div class="table__caption">
-        In {{ selectedAct }} Title {{ romanizedTitle }}, find equivalent US Code
-        citations and read the text in your choice of government website. Learn
-        more about these sources:
+        This table shows U.S. Code sections enacted by the Social Security Act.
+        See also:
         <a
+            target="_blank"
+            rel="noopener noreferrer"
             class="external"
+            href="https://uscode.house.gov/view.xhtml?req=granuleid%3AUSC-prelim-title42-chapter7&amp;saved=%7CZ3JhbnVsZWlkOlVTQy1wcmVsaW0tdGl0bGU0Mi1jaGFwdGVyNy1mcm9udA%3D%3D%7C%7C%7C0%7Cfalse%7Cprelim&amp;edition=prelim"
+            >full table of contents and text for 42 U.S.C. Chapter 7 (Social
+            Security)</a
+        >.<br />
+        Learn about these sources:
+        <a
             href="https://uscode.house.gov/"
             target="_blank"
             rel="noopener noreferrer"
+            class="external"
             >US Code House.gov</a
         >,
         <a
-            class="external"
             href="https://www.govinfo.gov/app/collection/comps/"
             target="_blank"
             rel="noopener noreferrer"
+            class="external"
             >Statute Compilation</a
         >,
         <a
-            class="external"
             href="https://www.govinfo.gov/app/collection/uscode"
             target="_blank"
             rel="noopener noreferrer"
+            class="external"
             >US Code Annual</a
         >,
         <a
-            class="external"
             href="https://www.ssa.gov/OP_Home/ssact/ssact.htm"
             target="_blank"
             rel="noopener noreferrer"
+            class="external"
             >SSA.gov Compilation</a
         >.
     </div>
