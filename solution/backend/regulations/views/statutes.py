6bf8379a8b1836e4cd62caa22d6d6f790e20c915
Caleb Godwin <cgodwin@fearless.tech>
2023-07-17 17:22:49 -0400

    Order by U.S.C. update tests

--- solution/backend/regulations/views/statutes.py
+++ solution/backend/regulations/views/statutes.py
@@ -38,7 +38,7 @@ class StatuteLinkConverterViewSet(viewsets.ReadOnlyModelViewSet):
             queryset = queryset.filter(act__iexact=act)
         if title:
             queryset = queryset.filter(statute_title__iexact=title)
-        return queryset.order_by("act", "statute_title")
+        return queryset.order_by("act", "statute_title", "usc")
 
 
 class ActListSerializer(serializers.Serializer):
