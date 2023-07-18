1944ed7af9f211d3928c12445c4258bee9c918eb
Phil Reese <preese@fearless.tech>
2023-07-18 09:12:53 -0400

    test: tweak assertion names

--- solution/ui/regulations/utilities/utils.test.js
+++ solution/ui/regulations/utilities/utils.test.js
@@ -3,7 +3,7 @@ import { getCurrentPageResultsRange, romanize } from "utilities/utils.js";
 import { describe, it, expect } from "vitest";
 
 describe("Utilities.js", () => {
-    it("Gets the current page results range", async () => {
+    it("getCurrentPageResultsRange properly gets the current page results range", async () => {
         let obj = { count: 100, page: 1, pageSize: 25 };
         let results = getCurrentPageResultsRange(obj);
         expect(results).toStrictEqual([1, 25]);
@@ -13,7 +13,7 @@ describe("Utilities.js", () => {
         expect(results).toStrictEqual([26, 50]);
     });
 
-    it("Romanizes numbers", async () => {
+    it("romanize properly converts numbers to roman numerals", async () => {
         expect(romanize(1)).toBe("I");
         expect(romanize(2)).toBe("II");
         expect(romanize(21)).toBe("XXI");
