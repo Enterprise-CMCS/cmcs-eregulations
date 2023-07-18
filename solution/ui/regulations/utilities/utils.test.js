e24f935d94c9c6edb90b9a152883d2da81b553db
Phil Reese <preese@fearless.tech>
2023-07-18 09:02:03 -0400

    test: move utils.test alongside utils; test romanize method

--- solution/ui/regulations/utilities/utils.test.js
+++ solution/ui/regulations/utilities/utils.test.js
@@ -1,4 +1,4 @@
-import { getCurrentPageResultsRange } from "utilities/utils.js";
+import { getCurrentPageResultsRange, romanize } from "utilities/utils.js";
 
 import { describe, it, expect } from "vitest";
 
@@ -7,8 +7,16 @@ describe("Utilities.js", () => {
         let obj = { count: 100, page: 1, pageSize: 25 };
         let results = getCurrentPageResultsRange(obj);
         expect(results).toStrictEqual([1, 25]);
+
         obj = { count: 100, page: 2, pageSize: 25 };
         results = getCurrentPageResultsRange(obj);
         expect(results).toStrictEqual([26, 50]);
+    });
+
+    it("Romanizes numbers", async () => {
+        expect(romanize(1)).toBe("I");
+        expect(romanize(2)).toBe("II");
+        expect(romanize(21)).toBe("XXI");
+        expect(romanize(1936)).toBe("MCMXXXVI");
     });
 });
