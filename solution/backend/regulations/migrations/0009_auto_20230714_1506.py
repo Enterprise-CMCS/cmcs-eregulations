01d530272aeb6be193c7164caf35bfc4007c9844
Parissa <parissa.eggleston@gmail.com>
2023-07-19 16:46:07 -0400

    update migration files

--- solution/backend/regulations/migrations/0009_auto_20230714_1506.py
+++ solution/backend/regulations/migrations/0009_auto_20230714_1506.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('regulations', '0007_statutelinkconverter_usc_sort'),
+        ('regulations', '0008_statutelinkconfiguration'),
     ]
 
     operations = [
--- solution/backend/regulations/migrations/0010_auto_20230719_1157.py
+++ solution/backend/regulations/migrations/0010_auto_20230719_1157.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('regulations', '0008_auto_20230714_1506'),
+        ('regulations', '0009_auto_20230714_1506'),
     ]
 
     operations = [
