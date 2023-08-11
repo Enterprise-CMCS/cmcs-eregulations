6b265f2782527be30cf2aa11f3a75ca84c6cbdc7
Phil Reese <preese@fearless.tech>
2023-08-11 15:16:33 -0400

    feat: handle null date

--- solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/utils/dateMethods.js
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/utils/dateMethods.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 /* eslint-disable eqeqeq */
 
+import _isNull from "lodash/isNull";
 import _isUndefined from "lodash/isUndefined";
 
 // SSA table column date methods
@@ -9,10 +10,16 @@ import _isUndefined from "lodash/isUndefined";
  * @param {string} type - Label for the date
  * @param {string} date - Date to be formatted
  *
- * @returns {string} - Formatted date label
+ * @returns {string} - Formatted date label or empty string
  */
 const getDateLabel = ({ type, date }) => {
-    if (_isUndefined(type) || _isUndefined(date)) return "";
+    if (
+        _isNull(type) ||
+        _isNull(date) ||
+        _isUndefined(type) ||
+        _isUndefined(date)
+    )
+        return "";
 
     const rawDate = new Date(date);
     const formattedDate = rawDate.toLocaleString("default", {
@@ -20,7 +27,7 @@ const getDateLabel = ({ type, date }) => {
         year: "numeric",
     });
 
-    if (formattedDate == "Invalid Date") return "";
+    if (formattedDate === "Invalid Date") return "";
 
     return `${type} ${formattedDate}`;
 };
