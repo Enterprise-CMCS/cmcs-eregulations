8e7cb21dc12a88e4242c9106536375e7030e9fa9
Phil Reese <preese@fearless.tech>
2023-08-14 13:25:57 -0400

    test: update headerCell snapshot tests

--- solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/table-elements/HeaderCell.test.js
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/table-elements/HeaderCell.test.js
@@ -2,6 +2,8 @@ import flushPromises from "flush-promises";
 import { render } from "@testing-library/vue";
 import { describe, it, expect } from "vitest";
 
+import statuteDatesFixture from "cypress/fixtures/statute-dates.json";
+
 import { ssaSchema } from "../schemas/tableSchemas";
 
 import HeaderCell from "./HeaderCell.vue";
@@ -9,11 +11,23 @@ import HeaderCell from "./HeaderCell.vue";
 describe("Statute Table Header Cell", () => {
     describe("SSA table header", () => {
         ssaSchema.forEach((column, index) => {
-            it(`Creates a snapshot of header cell for column ${index + 1}`, async () => {
+            it(`Creates a snapshot of header cell for column ${index + 1} without dates`, async () => {
                 const wrapper = render(HeaderCell, {
                     props: {
                         cellData: column.header,
                         displayType: "table",
+                    },
+                });
+                await flushPromises();
+                expect(wrapper).toMatchSnapshot();
+            });
+
+            it(`Creates a snapshot of header cell for column ${index + 1} with dates`, async () => {
+                const wrapper = render(HeaderCell, {
+                    props: {
+                        cellData: column.header,
+                        displayType: "table",
+                        columnDates: statuteDatesFixture,
                     },
                 });
                 await flushPromises();
