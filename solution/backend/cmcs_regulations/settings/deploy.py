c0db7ba8f878d96a29b9d4499aff50e82eeee5e4
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-16 10:52:40 -0400

    seting up medi storage

--- solution/backend/cmcs_regulations/settings/deploy.py
+++ solution/backend/cmcs_regulations/settings/deploy.py
@@ -23,3 +23,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("bucket_name", '')
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
