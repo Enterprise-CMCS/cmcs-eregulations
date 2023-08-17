dcc0ee07486cb5e829d8c822c4393daa5c2dccef
Caleb Godwin <cgodwin@fearless.tech>
2023-08-17 16:42:24 -0400

    Add config test

--- solution/backend/regulations/tests/test_reg_ref_links.py
+++ solution/backend/regulations/tests/test_reg_ref_links.py
@@ -9,7 +9,10 @@ class LinkRegRefsTestCase(SimpleTestCase):
         with open("regulations/tests/fixtures/reg_ref_link_tests.json", "r") as f:
             test_values = json.load(f)
 
-        config = {}
+        config = {
+            "cfr_ref_exceptions": {},
+            "link_cfr_refs": True,
+        }
 
         for test in test_values:
             template = Template("{% load link_reg_refs %}{% link_reg_refs paragraph link_config as text %}{{ text | safe }}")
@@ -18,3 +21,15 @@ class LinkRegRefsTestCase(SimpleTestCase):
                 "link_config": config,
             })
             self.assertEqual(template.render(context), test["expected"])
+
+    def test_reg_link_config(self):
+        with open("regulations/tests/fixtures/reg_ref_config_tests.json", "r") as f:
+            test_values = json.load(f)
+
+        for test in test_values:
+            template = Template("{% load link_reg_refs %}{% link_reg_refs paragraph link_config as text %}{{ text | safe }}")
+            context = Context({
+                "paragraph": test["input"],
+                "link_config": test["config"],
+            })
+            self.assertEqual(template.render(context), test["expected"], f"Failed while testing {test['testing']}.")
