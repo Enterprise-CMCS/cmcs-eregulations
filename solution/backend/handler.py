95603f173053040c40f5f5a5505912c445ec0648
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-17 15:45:18 -0400

    removing unnecessary vriable

--- solution/backend/handler.py
+++ solution/backend/handler.py
@@ -4,7 +4,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
-
+try:
+  import unzip_requirements
+except ImportError:
+  pass
 import os
 
 from django.core.wsgi import get_wsgi_application
