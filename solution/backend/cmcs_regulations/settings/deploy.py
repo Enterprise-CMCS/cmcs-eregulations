eb44379c9da7998df1f5de14ec37f726bcec116c
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-18 12:23:29 -0400

    adding serverles variables

--- solution/backend/cmcs_regulations/settings/deploy.py
+++ solution/backend/cmcs_regulations/settings/deploy.py
@@ -20,8 +20,8 @@ DATABASES = {
     },
 }
 AWS_STORAGE_BUCKET_NAME = os.environ.get("bucket_name", '')
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_S3_BUCKET_ACCESS_KEY_ID", '')
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_BUCKET_SECRET_ACCESS_KEY", '')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
