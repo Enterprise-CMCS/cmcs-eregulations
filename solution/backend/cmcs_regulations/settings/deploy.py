8f5317967e0bc04eca26406a228211df6e4d0b55
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-21 07:44:30 -0400

    set django storages to older version

--- solution/backend/cmcs_regulations/settings/deploy.py
+++ solution/backend/cmcs_regulations/settings/deploy.py
@@ -26,6 +26,7 @@ AWS_S3_REGION_NAME = "us-east-1"
 AWS_S3_CUSTOM_DOMAIN = (
     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 )
+
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = "private"
