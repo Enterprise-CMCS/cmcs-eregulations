ba0dc9b98daf806d63743c97e24ef5a66a05a3ad
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-21 10:52:21 -0400

    s

--- solution/backend/cmcs_regulations/settings/deploy.py
+++ solution/backend/cmcs_regulations/settings/deploy.py
@@ -19,9 +19,7 @@ DATABASES = {
         'NAME': "postgres",
     },
 }
-AWS_STORAGE_BUCKET_NAME = os.environ.get("BUCKET_NAME", '')
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_S3_BUCKET_ACCESS_KEY_ID", '')
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_BUCKET_SECRET_ACCESS_KEY", '')
+AWS_STORAGE_BUCKET_NAME = "wagtail-bucket"
 AWS_S3_REGION_NAME = "us-east-1"
 AWS_S3_CUSTOM_DOMAIN = (
     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
