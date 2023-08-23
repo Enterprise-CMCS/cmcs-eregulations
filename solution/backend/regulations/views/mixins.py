271a7775fa41e4de0de969b86a67f72c74952de7
Phil Reese <preese@fearless.tech>
2023-08-23 09:07:05 -0400

    feat: add login mixin to tame after login redirect

--- solution/backend/regulations/views/mixins.py
+++ solution/backend/regulations/views/mixins.py
@@ -1,3 +1,5 @@
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 def build_citation(context):
@@ -16,3 +18,11 @@ class CitationContextMixin:
         context = super(CitationContextMixin, self).get_context_data(**kwargs)
         context['citation'] = build_citation(context)
         return context
+
+class IsAuthenticatedMixin():
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(reverse('login') + "?next=%s" % request.path)
+
+
+        return super().dispatch(request, *args, **kwargs)
--- solution/backend/regulations/views/policy_repository.py
+++ solution/backend/regulations/views/policy_repository.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
+from regulations.views.mixins import IsAuthenticatedMixin
 
-class PolicyRepositoryView(LoginRequiredMixin, TemplateView):
-    login_url = "/admin/login/?next=/policy-repository/"
+
+class PolicyRepositoryView(IsAuthenticatedMixin, TemplateView):
+
     template_name = 'regulations/policy_repository.html'
 
     def get_context_data(self, **kwargs):
