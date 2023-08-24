1e0b21842f97af9dcdf6105ba03c273021e209c0
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-24 11:02:07 -0400

    lets see if this works

--- solution/backend/file_manager/admin.py
+++ solution/backend/file_manager/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+import boto3
 from .models import UploadedFile
-
+from django.conf import settings
 
 @admin.register(UploadedFile)
 class UploadedFileAdmin(admin.ModelAdmin):
+    
     list_display = ("name",)
     search_fields = ["name"]
     ordering = ("name",)
     fields = ("name", "file",)
+
+
+    change_form_template = "admin/edit_file.html"
+    def response_change(self, request, obj):
+        if "download-file" in request.POST:
+            s3 = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+            s3.download_file(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Filename=obj.file.name.split('/')[1], Key=obj.file.name)
 
--- solution/backend/file_manager/models.py
+++ solution/backend/file_manager/models.py
@@ -1,7 +1,10 @@
 
 from django.db import models
-
+import os
 
 class UploadedFile(models.Model):
     name = models.CharField(max_length=512, null=True, blank=True)
     file = models.FileField(upload_to='uploaded_files/')
+    
+    def filename(self):
+        return os.path.basename(self.file.name)
