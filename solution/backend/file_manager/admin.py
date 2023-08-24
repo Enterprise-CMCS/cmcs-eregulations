dedf27711cd2f20e84cff4cd7f9e0d6a831271f8
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-24 15:49:42 -0400

    get this

--- solution/backend/file_manager/admin.py
+++ solution/backend/file_manager/admin.py
@@ -17,8 +17,9 @@ class UploadedFileAdmin(admin.ModelAdmin):
 
     def download(self, obj):
         s3_client = boto3.client('s3',
-                                 aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+                                 aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+                                 aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+                                 aws_session_token=os.environ["AWS_ACCESS_KEY_ID"])
         try:
             response = s3_client.generate_presigned_url('get_object',
                                                         Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
