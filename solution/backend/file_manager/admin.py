1f2b2fc78603685c0d5bab1000fa8267bbc815c3
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-24 15:04:50 -0400

    adding session token

--- solution/backend/file_manager/admin.py
+++ solution/backend/file_manager/admin.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 from django.conf import settings
 from django.contrib import admin
@@ -18,7 +20,8 @@ class UploadedFileAdmin(admin.ModelAdmin):
     def download(self, obj):
         s3_client = boto3.client('s3',
                                  aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+                                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                                 aws_session_token=os.environ["AWS_SESSION_TOKEN"])
         try:
             response = s3_client.generate_presigned_url('get_object',
                                                         Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
