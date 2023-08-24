494de0c91b89458187191c1caa41fba3fa1ce308
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-24 13:34:53 -0400

    add action button for downloading file in admin

--- solution/backend/file_manager/admin.py
+++ solution/backend/file_manager/admin.py
@@ -1,22 +1,34 @@
 import boto3
 from django.conf import settings
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import UploadedFile
 
 
 @admin.register(UploadedFile)
 class UploadedFileAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("name", 'delete')
     search_fields = ["name"]
     ordering = ("name",)
     fields = ("name", "file",)
 
     change_form_template = "admin/edit_file.html"
 
-    def response_change(self, request, obj):
-        if "download-file" in request.POST:
-            s3 = boto3.client('s3',
-                              aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                              aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-            s3.download_file(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Filename=obj.file.name.split('/')[1], Key=obj.file.name)
+    def download(self, obj):
+        s3_client = boto3.client('s3',
+                                 aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+        try:
+            response = s3_client.generate_presigned_url('get_object',
+                                                        Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                                                                'Key': obj.file.name},
+                                                        ExpiresIn=600)
+        except Exception as e:
+            print(e)
+        return response
+
+    def delete(self, obj):
+        link = self.download(obj)
+        html = '<input type="button" onclick="location.href=\'{}\'" value="download" />'.format(link)
+        return format_html(html)
