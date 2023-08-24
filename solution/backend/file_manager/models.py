253daa820844876b0378fb47349e7c107a7bf5b4
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-24 11:17:36 -0400

    linter corrections

--- solution/backend/file_manager/models.py
+++ solution/backend/file_manager/models.py
@@ -1,10 +1,11 @@
+import os
 
 from django.db import models
-import os
+
 
 class UploadedFile(models.Model):
     name = models.CharField(max_length=512, null=True, blank=True)
     file = models.FileField(upload_to='uploaded_files/')
-    
+
     def filename(self):
         return os.path.basename(self.file.name)
