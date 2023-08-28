b549d890faf40b33d395d833c407af687ed1615e
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-28 09:05:00 -0400

    cleaned up code

--- solution/backend/file_manager/admin.py
+++ solution/backend/file_manager/admin.py
@@ -15,41 +15,37 @@ class UploadedFileAdmin(admin.ModelAdmin):
     readonly_fields = ('download_file',)
     fields = ("name", "file", 'download_file',)
 
+    def establish_client(self):
+        if settings.DEBUG:
+            return boto3.client('s3',
+                                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+        else:
+            return boto3.client('s3', config=boto3.session.Config(signature_version='s3v4',))
+
     # this function will delete the object.  Without overwriting the whole form
     def del_file(self, obj):
-        if settings.DEBUG:
-            s3 = boto3.client('s3',
-                              aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                              aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-            s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f"/{obj.file.name}")
-        else:
-            s3 = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4',))
-            s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f"/{obj.file.name}")
+        s3_client = self.establish_client()
+        s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f"{obj.file.name}")
 
     def delete_model(self, request, obj):
-        print('Deleting from s3')
         try:
             self.del_file(obj)
+            obj.delete()
+            print('File Deleted')
         except Exception:
-            print('Could not delte from bucket')
-        print('Finished deleting file.')
+            print('Could not delete from server.')
 
     def download(self, obj):
-        # for localized testing
-        if settings.DEBUG:
-            s3_client = boto3.client('s3',
-                                     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                                     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-        else:
-            s3_client = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4',))
+        s3_client = self.establish_client()
         try:
-            response = s3_client.generate_presigned_url('get_object',
-                                                        Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
-                                                                'Key': obj.file.name},
-                                                        ExpiresIn=600)
-        except Exception as e:
-            print(e)
-        return response
+            return s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                                                            'Key': obj.file.name},
+                                                    ExpiresIn=600)
+        except Exception:
+            print('Could not set up download url.')
+            return 'Not available for download.'
 
     def download_file(self, obj):
         if obj.id:
