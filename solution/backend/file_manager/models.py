6ddc660956d3974fd11d46ba02b79164235e2d12
Thomas Walker <thomas.e.walker88@gmail.com>
2023-08-31 15:27:43 -0400

    EREGCSC-2148 - Internal document set up (#941)
    * initial commit
    
    * Adding serverless bucket creation and use
    
    * lets see if this works
    
    * linter corrections
    
    * add action button for downloading file in admin
    
    * addd deploy configs
    
    * try this
    
    * dfd
    
    * redo
    
    * adding session token
    
    * see if this works
    
    * get this
    
    * working version of downloads
    
    * Add download button to upload file
    
    * Adding bucket name to environment variables
    
    * fhdfh
    
    * Add logic to check if file exist
    
    * logic for deleting added
    
    * cleaned up code
    
    * dfd
    
    * Changes to the model for subject and category
    
    * Adding some basic api calls
    
    * Set up generate files through api calls
    
    * Update admin to use api for download
    
    * feat: bring in header/modal to policy-repository
    
    * feat: bring in header link urls
    
    * feat: pass params to fetchJson method as single object
    
    * feat: do not set/get localstorage if authenticated is true
    
    * chore: remove unnecessary param
    
    * chore: pass authenticated through CRUD helper methods
    
    * Finalizing api calls
    
    * feat: snake isAuthenticated into vite spa
    
    * feat: auth api example using statute endpoint
    
    * Added descriptions for swagger
    
    * refactor: genericize should/should not cache flag in api methods
    
    * removing unneceessary template
    
    * Add authentication to endpoints
    
    * fix error on download
    
    * made category one per item, added abbreviation
    
    * added abbreviation to category serializer
    
    * Updated string methods
    
    * Model changes
    
    * admin chanes
    
    * feat: very basic download proof of concept
    
    * lets try this
    
    * feat: basic, crude styling
    
    * style: minor flex direction change
    
    * feat: add empty state
    
    * Upload change
    
    * linter corrections
    
    * add content type to file upload
    
    * Fixed issue where youc ant download
    
    * linter fix
    
    * Updating serverless
    
    ---------
    
    Co-authored-by: Phil Reese <preese@fearless.tech>

---
+++ solution/backend/file_manager/models.py
@@ -0,0 +1,48 @@
+import os
+import uuid
+
+from django.db import models
+
+from common.fields import VariableDateField
+from resources.models import AbstractLocation
+
+
+class DocumentType(models.Model):
+    class Meta:
+        verbose_name_plural = "Document Types"
+    name = models.CharField(max_length=512, null=False, blank=False)
+    description = models.CharField(max_length=512, null=False, blank=False)
+    order = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Subject(models.Model):
+    full_name = models.CharField(max_length=512, null=False, blank=False)
+    short_name = models.CharField(max_length=50, null=True, blank=True)
+    abbreviation = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+class UploadedFile(models.Model):
+    name = models.CharField(max_length=512, null=True, blank=True)
+    file = models.FileField(upload_to='uploaded_files/')
+    date = VariableDateField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    internal_notes = models.TextField(null=True, blank=True)
+    subject = models.ManyToManyField(Subject, blank=True, related_name="uploads")
+    document_type = models.ForeignKey(DocumentType, blank=True, null=True, related_name="uploads", on_delete=models.SET_NULL)
+    locations = models.ManyToManyField(AbstractLocation, blank=True, related_name="uploads")
+    uid = models.UUIDField(
+         primary_key=False,
+         default=uuid.uuid4,
+         editable=False)
+
+    def filename(self):
+        return os.path.basename(self.file.name)
+
+    def __str__(self) -> str:
+        return self.name + ' ' + self.description
---
+++ solution/backend/file_manager/serializers.py
@@ -0,0 +1,41 @@
+from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
+
+from resources.serializers.locations import AbstractLocationPolymorphicSerializer, MetaLocationSerializer
+
+from .models import DocumentType, Subject
+
+
+class DocumentTypeSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    description = serializers.CharField()
+
+    class Meta:
+        model = DocumentType
+
+
+class SubjectSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    full_name = serializers.CharField()
+    short_name = serializers.CharField()
+    abbreviation = serializers.CharField()
+
+    class Meta:
+        model = Subject
+
+
+class UploadedFileSerializer(serializers.Serializer, ):
+    name = serializers.CharField()
+    date = serializers.DateField()
+    description = serializers.CharField()
+    locations = serializers.SerializerMethodField()
+    document_type = DocumentTypeSerializer(many=False, read_only=True)
+    subject = SubjectSerializer(many=True, read_only=True)
+    uid = serializers.CharField()
+
+    @extend_schema_field(MetaLocationSerializer.many(True))
+    def get_locations(self, obj):
+        if self.context.get("location_details", True):
+            return AbstractLocationPolymorphicSerializer(obj.locations, many=True).data
+        return serializers.PrimaryKeyRelatedField(read_only=True, many=True).to_representation(obj.locations.all())
