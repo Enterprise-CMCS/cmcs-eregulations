8b35194a5799bf774b6a52d3ae8a66fc5db94513
Parissa <parissa.eggleston@gmail.com>
2023-09-11 08:25:33 -0400

    reset deploy.py to see if that fixes the issue

--- solution/backend/cmcs_regulations/settings/deploy.py
+++ solution/backend/cmcs_regulations/settings/deploy.py
@@ -1,26 +1,24 @@
 from .base import * # noqa
 import os
 
-default_database_values = {
-    'ENGINE': 'django.db.backends.postgresql',
-    'USER': os.environ.get('DB_USER', 'eregs'),
-    'HOST': os.environ.get('DB_HOST', 'db'),
-    'PORT': os.environ.get('DB_PORT', '5432'),
-}
-
 DATABASES = {
     'default': {
-        **default_database_values,
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': os.environ.get('DB_USER', 'eregs'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'sgere'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
         'NAME': os.environ.get('DB_NAME', 'eregs'),
     },
     'postgres': {
-        **default_database_values,
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': os.environ.get('DB_USER', 'eregs'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'sgere'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
         'NAME': "postgres",
     },
 }
-
 S3_AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", None)
 S3_AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
