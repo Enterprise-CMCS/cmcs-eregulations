1c23c20e45f632c6ca88f29882252ec941a91f82
Parissa <parissa.eggleston@gmail.com>
2023-09-12 12:08:18 -0400

    update html

--- solution/backend/regulations/context_processors.py
+++ solution/backend/regulations/context_processors.py
@@ -1,5 +1,8 @@
-from .models import SiteConfiguration
 import os
+
+from .models import SiteConfiguration
+
+
 def site_config(request):
     config = SiteConfiguration.objects.first()
     return {
