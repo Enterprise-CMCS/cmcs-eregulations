246c5a8d366e8077c2cab4b2247f0370a54f9f0b
Parissa <parissa.eggleston@gmail.com>
2023-09-12 21:31:43 -0400

    see if this will fix our redirect issue

--- solution/backend/cmcs_regulations/settings/euasettings.py
+++ solution/backend/cmcs_regulations/settings/euasettings.py
@@ -1,5 +1,6 @@
 from .deploy import * # noqa
 import os
+import re
 
 INSTALLED_APPS += [ # noqa
     'mozilla_django_oidc',
@@ -12,6 +13,9 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'regulations.admin.OidcAdminAuthenticationBackend',
 )
+
+STAGE_ENV = os.environ.get("STAGE_ENV", "")
+BASE_URL = os.environ.get("BASE_URL", "")
 OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID", None)
 OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET", None)
 OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get("OIDC_OP_AUTHORIZATION_ENDPOINT", None)
@@ -19,6 +23,11 @@ OIDC_OP_TOKEN_ENDPOINT = os.environ.get("OIDC_OP_TOKEN_ENDPOINT", None)
 OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", None)
 OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT", None)
 OIDC_RP_SIGN_ALGO = 'RS256'
-LOGIN_REDIRECT_URL = '/admin/'
+LOGIN_REDIRECT_URL = BASE_URL + '/admin/'
 LOGOUT_REDIRECT_URL = '/'
 EUA_FEATUREFLAG = os.environ.get('EUA_FEATUREFLAG', 'false')
+if re.match(r'^dev\d*$', STAGE_ENV):
+    numeric_part = re.sub(r'^dev', '', STAGE_ENV)
+    LOGIN_REDIRECT_URL = BASE_URL.replace('/dev', f'/{numeric_part}') + '/admin/'
+else:
+    LOGIN_REDIRECT_URL = BASE_URL + '/admin/'
