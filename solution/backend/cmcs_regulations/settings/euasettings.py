912ff03e927f73ac389731a7b8e84fdf03b4997e
Parissa <parissa.eggleston@gmail.com>
2023-09-12 22:25:42 -0400

    update code to include the logout url

--- solution/backend/cmcs_regulations/settings/euasettings.py
+++ solution/backend/cmcs_regulations/settings/euasettings.py
@@ -24,11 +24,10 @@ OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", None)
 OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT", None)
 OIDC_RP_SIGN_ALGO = 'RS256'
 EUA_FEATUREFLAG = os.environ.get('EUA_FEATUREFLAG', 'false')
-BASE_PATH = ""
 if re.match(r'^dev\d*$', STAGE_ENV):
     numeric_part = re.sub(r'^dev', '', STAGE_ENV)
-    BASE_PATH = BASE_URL.replace('/dev', f'/{numeric_part}')
+    LOGIN_REDIRECT_URL = BASE_URL.replace('/dev', f'/{numeric_part}') + '/admin/'
+    LOGOUT_REDIRECT_URL = BASE_URL.replace('/dev', f'/{numeric_part}') + '/'
 else:
-    BASE_PATH = BASE_URL
-LOGIN_REDIRECT_URL = BASE_PATH + '/admin/'
-LOGOUT_REDIRECT_URL = BASE_PATH + '/'
+    LOGIN_REDIRECT_URL = BASE_URL + '/admin/'
+    LOGOUT_REDIRECT_URL = BASE_URL + '/'
