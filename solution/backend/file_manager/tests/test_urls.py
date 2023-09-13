5894973f343827b2cd096158cf3904d07247cf80
Thomas Walker <thomas.e.walker88@gmail.com>
2023-09-13 10:49:34 -0400

    add test for checking auth for endpoint

--- solution/backend/file_manager/tests/test_urls.py
+++ solution/backend/file_manager/tests/test_urls.py
@@ -11,10 +11,18 @@ auth_headers = {
 
 
 class FileUploadTestCase(TestCase):
-    def test_get_upload_link(self):
+    def login(self) -> None:
         self.client = APIClient()
         self.user = User.objects.create_superuser(username='test_user', password='test')  # noqa: S106
 
+    def test_get_upload_link(self):
+        self.login()
         self.client.force_authenticate(self.user)
         response = self.client.post(reverse('file-upload-new', kwargs={'file_name': 'test.doc'}))
         self.assertEqual(response.data['url'], 'https://s3.amazonaws.com/test_bucket')
+
+    def test_get_upload_link_failure(self):
+        self.assertRaises(Exception, self.client.post(reverse('file-upload-new', kwargs={'file_name': 'test.doc'})))
+
+    def tearDown(self) -> None:
+        self.user = None
