3de057a0f0e9104ddfb45d7c714dc1aaaf682a16
Phil Reese <preese@fearless.tech>
2023-10-02 14:51:30 -0400

    test: basic e2e tests for policy repo search page

--- solution/ui/e2e/cypress/support/common-commands/login.js
+++ solution/ui/e2e/cypress/support/common-commands/login.js
@@ -1,7 +1,12 @@
 // login via policy repository page for now
-export const eregsLogin = ({ username, password }) => {
-    cy.session([username, password], () => {
-        cy.visit("/policy-repository/");
+export const eregsLogin = ({
+    username,
+    password,
+    landingPage = "/policy-repository/",
+}) => {
+    cy.session([username, password, landingPage], () => {
+        cy.visit(landingPage);
+        cy.wait(1000);
         cy.get("#id_username").type(username);
         cy.get("#id_password").type(password);
         cy.get("#login-form").submit();
