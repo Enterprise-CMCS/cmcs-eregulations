325b0ae96b105714cc9c03ea84b96b62611b7abf
Caleb Godwin <cgodwin@fearless.tech>
2023-10-05 17:39:03 -0400

    Successfully deploy test code

--- solution/text-extractor/text_extractor.py
+++ solution/text-extractor/text_extractor.py
@@ -1,4 +1,22 @@
 import sys
+import json
 
 def handler(event, context):
-    return "This is a test using Python " + sys.version
+    if event["rawPath"] == "/a":
+        return None
+    if event["rawPath"] == "/b":
+        return 123
+    if event["rawPath"] == "/c":
+        return {
+            "statusCode": 200,
+            'headers': {'Content-Type': 'application/json'},
+            "body": json.dumps({"message": "This is a test using Python " + sys.version + " in a Docker container"}),
+        }
+    if event["rawPath"] == "/d":
+        return {}
+    if event["rawPath"] == "/e":
+        return
+    if event["rawPath"] == "/f":
+        return {
+            "statusCode": 404,
+        }
