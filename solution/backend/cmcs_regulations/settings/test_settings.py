17aa7ae984d7478b9395588907d39e5d2237b68a
Parissa <parissa.eggleston@gmail.com>
2023-10-12 14:34:05 -0400

    add the eua environments to test_settings.py

--- solution/backend/cmcs_regulations/settings/test_settings.py
+++ solution/backend/cmcs_regulations/settings/test_settings.py
@@ -1,5 +1,4 @@
 from .base import * # noqa
-from .euasettings import * # noqa
 
 import os
 USE_AWS_TOKEN = True
@@ -13,6 +12,28 @@ AWS_S3_CUSTOM_DOMAIN = (
 AWS_QUERYSTRING_AUTH = False
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+## TODO - this should be removed after we merge euasettings.py with base.py in teh future
+STAGE_ENV = os.environ.get("STAGE_ENV", "")
+BASE_URL = os.environ.get("BASE_URL", "")
+OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID", None)
+OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET", None)
+OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get("OIDC_OP_AUTHORIZATION_ENDPOINT", None)
+OIDC_OP_TOKEN_ENDPOINT = os.environ.get("OIDC_OP_TOKEN_ENDPOINT", None)
+OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", None)
+OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT", None)
+OIDC_REDIRECT_URL = "/admin/oidc/callback/"
+OIDC_RP_SIGN_ALGO = 'RS256'
+LOGIN_REDIRECT_URL = '/admin/'
+LOGOUT_REDIRECT_URL = '/'
+EUA_FEATUREFLAG = bool(os.getenv('EUA_FEATUREFLAG', 'False').lower() == 'true')
+
+if re.match(r'^dev\d*$', STAGE_ENV):
+    LOGIN_REDIRECT_URL = f"/{STAGE_ENV}/admin/"
+    LOGOUT_REDIRECT_URL = f"/{STAGE_ENV}/"
+elif STAGE_ENV == 'dev' or STAGE_ENV == 'val':
+    LOGIN_REDIRECT_URL = f"/{STAGE_ENV}/admin/"
+    LOGOUT_REDIRECT_URL = f"/{STAGE_ENV}/"
 
 DATABASES = {
     'default': {
