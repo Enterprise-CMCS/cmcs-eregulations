eef48d750af35e08b6713b1579d45047e5407034
Parissa <parissa.eggleston@gmail.com>
2023-10-16 09:27:52 -0400

    add test

--- solution/backend/regulations/tests/test_dynamic_urls.py
+++ solution/backend/regulations/tests/test_dynamic_urls.py
@@ -2,13 +2,14 @@ import unittest
 from django.template import Template, Context
 
 class CustomTemplateTagUnitTest(unittest.TestCase):
-    def test_get_domain_tag(self):
+    def test_get_domain_request_match(self):
         # Create a template with the custom template tag
         template_code = '{% load dynamic_urls %}{% get_domain request CUSTOM_URL %}'
         template = Template(template_code)
 
         # Create a context with the necessary variables
-        context = Context({'request': self.mock_request(), 'CUSTOM_URL': 'https://custom-url.com'})
+        context = Context(
+            {'request': self.mock_request('eregulations.cms.gov'), 'CUSTOM_URL': 'https://custom-url.com'})
 
         # Render the template with the context
         rendered_template = template.render(context)
@@ -19,9 +20,27 @@ class CustomTemplateTagUnitTest(unittest.TestCase):
         # Assert that the rendered template contains the expected output
         self.assertIn(expected_output, rendered_template)
 
-    def mock_request(self):
+    def test_get_domain_request_no_match(self):
+        # Create a template with the custom template tag
+        template_code = '{% load dynamic_urls %}{% get_domain request CUSTOM_URL %}'
+        template = Template(template_code)
+
+        # Create a context with the necessary variables
+        context = Context(
+            {'request': self.mock_request('regulations-pilot.cms.gov'), 'CUSTOM_URL': 'https://custom-url.com'})
+
+        # Render the template with the context
+        rendered_template = template.render(context)
+
+        # Define the expected output based on the custom template tag's behavior
+        expected_output = 'https://custom-url.com'  # Should not change for 'regulations-pilot.cms.gov'
+
+        # Assert that the rendered template contains the expected output
+        self.assertIn(expected_output, rendered_template)
+
+    def mock_request(self, host):
         class MockRequest:
             def get_host(self):
-                return 'eregulations.cms.gov'
+                return host
 
         return MockRequest()
