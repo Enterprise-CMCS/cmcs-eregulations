79973cc91f9f29b117bba561aa30df20a352e956
Thomas Walker <thomas.e.walker88@gmail.com>
2023-10-17 09:22:35 -0400

    add to index used instead of seed data

--- solution/backend/common/functions.py
+++ solution/backend/common/functions.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
 
+from content_search.functions import add_to_index
 from content_search.models import ContentIndex
-from file_manager.models import DocumentType, Subject
+from file_manager.models import DocumentType, Subject, UploadedFile
 from regcore.search.models import Synonym
 from regulations.models import (
     RegulationLinkConfiguration,
@@ -46,15 +47,12 @@ def loadSeedData():
         ("regulations.statutelinkconfiguration.json", StatuteLinkConfiguration),
         ("file_manager.documenttype.json", DocumentType),
         ("file_manager.subject.json", Subject),
-        ("content_search.contentindex.json", ContentIndex),
         ("cmcs_regulations/fixtures/contenttypes.contenttype.json", ContentType),
         ("cmcs_regulations/fixtures/auth.permission.json", Permission),
         ("cmcs_regulations/fixtures/auth.group.json", Group),
     ]
     # The vector column for the search index has issues with using seed data.  As in it takes forever.
     # We remove the column first, then we add it back after fixtures are done.
-
-    # cursor = connections['default'].cursor()
 
     file_index = ContentIndex.objects.all()
     for con in file_index:
@@ -67,14 +65,12 @@ def loadSeedData():
 
     for fixture in fixtures:
         call_command("loaddata", fixture[0])
-    # cursor = connections['default'].cursor()
-    # cursor.execute('''
-    #           ALTER TABLE public.content_search_contentindex ADD COLUMN vector_column tsvector GENERATED ALWAYS AS (
-    #             setweight(to_tsvector('english', coalesce(doc_name_string, '')), 'A') ||
-    #             setweight(to_tsvector('english', coalesce(summary_string,'')), 'A') ||
-    #             setweight(to_tsvector('english', coalesce(file_name_string,'')), 'C') ||
-    #             setweight(to_tsvector('english', coalesce(date_string,'')), 'C') ||
-    #             setweight(to_tsvector('english', coalesce(content,'')), 'D')
-    #           ) STORED;
-    #           CREATE INDEX public.content_search_index_vec ON content_search_contentindex USING GIN (vector_column);
-    #         ''')
+
+    for sup in SupplementalContent.objects.all():
+        add_to_index(sup)
+
+    for fr_doc in FederalRegisterDocument.objects.all():
+        add_to_index(fr_doc)
+
+    for up_file in UploadedFile.objects.all():
+        add_to_index(up_file)
