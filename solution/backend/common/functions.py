98d9aa77b4165d3813fd44cf4e1b5d6a2f77b053
Thomas Walker <thomas.e.walker88@gmail.com>
2023-10-17 08:05:52 -0400

    test for post save

--- solution/backend/common/functions.py
+++ solution/backend/common/functions.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
-from django.db import connections
 
 from content_search.models import ContentIndex
 from file_manager.models import DocumentType, Subject
@@ -47,15 +46,10 @@ def loadSeedData():
         ("regulations.statutelinkconfiguration.json", StatuteLinkConfiguration),
         ("file_manager.documenttype.json", DocumentType),
         ("file_manager.subject.json", Subject),
-        ("content_search.contentindex.json", ContentIndex),
         ("cmcs_regulations/fixtures/contenttypes.contenttype.json", ContentType),
         ("cmcs_regulations/fixtures/auth.permission.json", Permission),
         ("cmcs_regulations/fixtures/auth.group.json", Group),
     ]
-    # The vector column for the search index has issues with using seed data.  As in it takes forever.
-    # We remove the column first, then we add it back after fixtures are done.
-
-    # cursor = connections['default'].cursor()
 
     file_index = ContentIndex.objects.all()
     for con in file_index:
@@ -68,14 +62,3 @@ def loadSeedData():
 
     for fixture in fixtures:
         call_command("loaddata", fixture[0])
-    # cursor = connections['default'].cursor()
-    # cursor.execute('''
-    #           ALTER TABLE public.content_search_contentindex ADD COLUMN vector_column tsvector GENERATED ALWAYS AS (
-    #             setweight(to_tsvector('english', coalesce(doc_name_string, '')), 'A') ||
-    #             setweight(to_tsvector('english', coalesce(summary_string,'')), 'A') ||
-    #             setweight(to_tsvector('english', coalesce(file_name_string,'')), 'C') ||
-    #             setweight(to_tsvector('english', coalesce(date_string,'')), 'C') ||
-    #             setweight(to_tsvector('english', coalesce(content,'')), 'D')
-    #           ) STORED;
-    #           CREATE INDEX public.content_search_index_vec ON content_search_contentindex USING GIN (vector_column);
-    #         ''')
--- solution/backend/content_search/tests/test_search.py
+++ solution/backend/content_search/tests/test_search.py
@@ -5,7 +5,6 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from content_search.functions import add_to_index
 from content_search.models import ContentIndex
 from file_manager.models import Subject, UploadedFile
 from resources.models import FederalRegisterDocument, Section, SupplementalContent
@@ -60,12 +59,7 @@ class SearchTest(TestCase):
                     file.locations.set([self.location2])
                     file.subjects.set([self.subject2])
                     file.save()
-        for sup in SupplementalContent.objects.all():
-            add_to_index(sup)
-        for fr in FederalRegisterDocument.objects.all():
-            add_to_index(fr)
-        for file in UploadedFile.objects.all():
-            add_to_index(file)
+
 
     def test_no_query_not_logged_in(self):
         response = self.client.get("/v3/content-search/?resource-type=external")
