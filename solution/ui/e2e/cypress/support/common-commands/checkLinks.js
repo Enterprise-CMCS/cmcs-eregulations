cdc8f2743d7152f740ed39ee2df2bd60ed66e007
Phil Reese <preese@fearless.tech>
2023-10-27 12:26:38 -0400

    feat: add rel where needed; add testing method to more test suites

--- solution/ui/e2e/cypress/support/common-commands/checkLinks.js
+++ solution/ui/e2e/cypress/support/common-commands/checkLinks.js
@@ -1,5 +1,4 @@
 export const checkLinkRel = () => {
-    let checkedLink;
     cy.log("Checking all links to sites");
     cy.get("a:not([href*='mailto:']).external").each((link) => {
         expect(link).to.have.attr("rel", "noopener noreferrer");
