40edca7dcdeeb8ca3ae39cc3d7cfc5afd02461aa
Phil Reese <preese@fearless.tech>
2023-10-30 09:36:33 -0400

    test: remove new condition

--- solution/ui/e2e/cypress/support/common-commands/checkLinks.js
+++ solution/ui/e2e/cypress/support/common-commands/checkLinks.js
@@ -1,12 +1,6 @@
 export const checkLinkRel = () => {
     cy.log("Checking all links to sites");
-
     cy.get("a[target=_blank]:not([href*='mailto:'])").each((link) => {
-        expect(link).to.have.attr("rel", "noopener noreferrer");
-    });
-
-    // there are some links that have target=blank instead of target=_blank
-    cy.get("a[target=blank]:not([href*='mailto:'])").each((link) => {
         expect(link).to.have.attr("rel", "noopener noreferrer");
     });
 };
