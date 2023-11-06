6979ef9cd42d401e435ae983c4a1caae5ebf39da
Thomas Walker <thomas.e.walker88@gmail.com>
2023-11-06 11:05:53 -0500

    fix spelling on bucket

--- solution/backend/empty_bucket.py
+++ solution/backend/empty_bucket.py
@@ -6,5 +6,5 @@ import boto3
 def handler(event, context):
     bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
     s3 = boto3.resource('s3')
-    bucket = s3.bucket(bucket_name)
+    bucket = s3.Bucket(bucket_name)
     bucket.objects.all().delete()
