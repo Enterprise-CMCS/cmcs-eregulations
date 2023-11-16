23de485b4599b4468cde9e21151f53591c51076c
Parissa <parissa.eggleston@gmail.com>
2023-11-16 14:47:31 -0500

    ensure that the user is added to the group even if it was already created

--- solution/backend/createsu.py
+++ solution/backend/createsu.py
@@ -13,13 +13,13 @@ def handler(self, *args, **options):
         admin_user = User.objects.create_superuser(os.environ.get('DJANGO_ADMIN_USERNAME'),
                                                    'admin_user@email.com',
                                                    os.environ.get('DJANGO_ADMIN_PASSWORD'))
-        e_regs_admin_group, created = Group.objects.get_or_create(name='e-Regs-Admin')
-        if created:
-            admin_user.groups.add(e_regs_admin_group)
+        e_regs_admin_group, _ = Group.objects.get_or_create(name='e-Regs-Admin')
+        admin_user.groups.add(e_regs_admin_group)
+
     if not User.objects.filter(username=os.environ.get('DJANGO_USERNAME')).exists():
         user = User.objects.create_superuser(os.environ.get('DJANGO_USERNAME'),
                                              'user@email.com',
                                              os.environ.get('DJANGO_PASSWORD'))
-        e_regs_reader_group, created = Group.objects.get_or_create(name='e-Regs-Reader')
-        if created:
-            user.groups.add(e_regs_reader_group)
+        e_regs_reader_group, _ = Group.objects.get_or_create(name='e-Regs-Reader')
+        user.groups.add(e_regs_reader_group)
+
