1cfe023b88d412fce87a00c1aeafddb1b4309401
Phil Reese <preese@fearless.tech>
2023-11-27 14:32:40 -0500

    feat: move label into input like vuetify's text input

--- solution/ui/regulations/eregs-vite/src/components/policy-repository/SubjectSelector.vue
+++ solution/ui/regulations/eregs-vite/src/components/policy-repository/SubjectSelector.vue
@@ -133,7 +133,17 @@ const filterResetClick = () => {
             </template>
             <template v-else>
                 <form @submit.prevent>
-                    <label for="subjectReduce">Filter the subject list</label>
+                    <label
+                        for="subjectReduce"
+                        :style="{
+                            display: `${
+                                state.filter?.length > 0
+                                    ? 'none'
+                                    : 'inline-block'
+                            }`,
+                        }"
+                        >Filter the subject list</label
+                    >
                     <input
                         id="subjectReduce"
                         v-model="state.filter"
