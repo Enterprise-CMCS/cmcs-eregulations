d1451127539f38c53f2aac5b48b7f0aa6cb07b15
Thomas Walker <thomas.e.walker88@gmail.com>
2023-11-28 12:54:46 -0500

    linter fix

--- solution/text-extractor/extractors/binary.py
+++ solution/text-extractor/extractors/binary.py
@@ -19,9 +19,9 @@ class BinaryExtractor(Extractor):
             data = f.openstream('WordDocument').read()
             data = data.decode('latin-1', errors='ignore')
             data = (re.sub(r'[^\x0A,\u00c0-\u00d6,\u00d8-\u00f6,\u00f8-\u02af,\u1d00-\u1d25,\u1d62-\u1d65,\u1d6b-\u1d77,' +
-                            r'\u1d79-\u1d9a,\u1e00-\u1eff,\u2090-\u2094,\u2184-\u2184,\u2488-\u2490,\u271d-\u271d,' +
-                            r'\u2c60-\u2c7c,\u2c7e-\u2c7f,\ua722-\ua76f,\ua771-\ua787,\ua78b-\ua78c,\ua7fb-\ua7ff,' +
-                            r'\ufb00-\ufb06,\x20-\x7E]', r'*', data))
+                           r'\u1d79-\u1d9a,\u1e00-\u1eff,\u2090-\u2094,\u2184-\u2184,\u2488-\u2490,\u271d-\u271d,' +
+                           r'\u2c60-\u2c7c,\u2c7e-\u2c7f,\ua722-\ua76f,\ua771-\ua787,\ua78b-\ua78c,\ua7fb-\ua7ff,' +
+                           r'\ufb00-\ufb06,\x20-\x7E]', r'*', data))
             p = re.compile(r'\*{300,433}((?:[^*]|\*(?!\*{14}))+?)\*{15,}')
             result = (re.findall(p, data))
             result = result[0].replace('*', '')
