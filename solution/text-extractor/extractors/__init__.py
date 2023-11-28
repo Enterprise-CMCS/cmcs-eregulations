834a67ce0050c95cb70c7822877ed1f7591ffe31
Thomas Walker <thomas.e.walker88@gmail.com>
2023-11-28 15:28:35 -0500

    Eregcsc 2264 -DOC, Docx added to text extractor (#1057)
    * Initial commit
    
    * update to get web working again
    
    * Use temporarydirectory for docker
    
    * Some clean up
    
    * clean up docker template
    
    * update object for config
    
    * deploy text extractor
    
    * update serverless-experimental
    
    * remove npm install
    
    * add libreoffice
    
    * use textract before import test
    
    * fake resposne
    
    * fix
    
    * clena up
    
    * remove bad line of code
    
    * get doc files working
    
    * fix issue on parsing file types
    
    * remove exception handling
    
    * exception handling
    
    * cx
    
    * remove exception handling
    
    * move return statement
    
    * linter fix
    
    * clean up for review
    
    * close the file on web
    
    * remove text extractor from serverless-exp

--- solution/text-extractor/extractors/__init__.py
+++ solution/text-extractor/extractors/__init__.py
@@ -1,3 +1,4 @@
+from .binary import BinaryExtractor as BinaryExtractor
 from .exceptions import ExtractorException as ExtractorException
 from .exceptions import ExtractorInitException as ExtractorInitException
 from .extractor import Extractor as Extractor
@@ -5,3 +6,4 @@ from .extractor import Extractor as Extractor
 # Add your file extractors here to initialize them
 from .pdf import PdfExtractor as PdfExtractor
 from .text import TextExtractor as TextExtractor
+from .word import WordExractor as WordExractor
--- solution/text-extractor/extractors/text.py
+++ solution/text-extractor/extractors/text.py
@@ -1,8 +1,11 @@
+import textract
+
 from .extractor import Extractor
 
 
 class TextExtractor(Extractor):
     file_types = ("text/plain",)
 
-    def extract(self, file: bytes) -> str:
-        return file.decode()
+    def extract(self, file_path: str) -> str:
+        text = textract.process(file_path)
+        return text
