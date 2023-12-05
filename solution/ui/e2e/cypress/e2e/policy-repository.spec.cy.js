c59a5049f7c61a3c25abfe24559a79a89ccd1513
Phil Reese <preese@fearless.tech>
2023-12-05 14:31:14 -0500

    feat: add e2e assertions on order of subject chips

--- solution/ui/e2e/cypress/e2e/policy-repository.spec.cy.js
+++ solution/ui/e2e/cypress/e2e/policy-repository.spec.cy.js
@@ -242,6 +242,15 @@ describe("Policy Repository", () => {
             "include",
             "/policy-repository?type=internal&subjects=3&q=test"
         );
+        cy.get(".document__subjects a")
+            .eq(0)
+            .should("have.text", " Access to Services ");
+        cy.get(".document__subjects a")
+            .eq(1)
+            .should("have.text", " Adult Day Health ");
+        cy.get(".document__subjects a")
+            .eq(2)
+            .should("have.text", " Ambulatory Prenatal Care ");
         cy.get(`a[data-testid=add-subject-chip-4]`)
             .should("have.attr", "title")
             .and("include", "Adult Day Health");
