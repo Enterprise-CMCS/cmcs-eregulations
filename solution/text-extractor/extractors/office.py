e71d11455d0e7de4ffd398af04bd1e585fbf6f19
Caleb Godwin <cgodwin@fearless.tech>
2023-12-07 14:08:44 -0500

    Add xlsm file type

--- solution/text-extractor/extractors/office.py
+++ solution/text-extractor/extractors/office.py
@@ -8,14 +8,18 @@ from .extractor import Extractor
 class OfficeExtractor(Extractor):
     file_types = (
         'docx',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
         'xls',
         'xlsx',
+        'xlsm',
         'pptx',
     )
 
     def extract(self, file_path: str) -> str:
-        text = textract.process(file_path)
+        # Treat xlsm files as xlsx for textract
+        if self.file_type == "xlsm":
+            self.file_type = "xlsx"
+
+        text = textract.process(file_path, extension=self.file_type)
 
         # cleans up weird characters
         line = re.sub(r'(\n)+', ' ', text.decode('utf-8').encode('ascii', 'ignore').decode('utf-8'))
