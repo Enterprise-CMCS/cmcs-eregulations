171442f91b7302426dcece10a1c55729d5825fb6
peggles2 <peggles2@users.noreply.github.com>
2023-12-11 14:43:13 -0500

    Eregcsc 2016 deploy eua changes prod (#1102)
    * integrate eua changes to deploy.py
    
    * fix ruff errors
    
    * forgot to add STAGE_ENV
    
    * code review changes
    
    * update local as well

--- solution/backend/cmcs_regulations/settings/deploy.py
+++ solution/backend/cmcs_regulations/settings/deploy.py
@@ -1,5 +1,6 @@
 from .base import * # noqa
 import os
+import re
 
 default_database_values = {
     'ENGINE': 'django.db.backends.postgresql',
@@ -30,3 +31,30 @@ AWS_QUERYSTRING_AUTH = False
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 USE_AWS_TOKEN = False
+
+AUTHENTICATION_BACKENDS = (
+    'regulations.admin.OidcAdminAuthenticationBackend',
+    'mozilla_django_oidc.auth.OIDCAuthenticationBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+STAGE_ENV = os.environ.get("STAGE_ENV", "")
+BASE_URL = os.environ.get("BASE_URL", "")
+
+# EUA settings
+OIDC_RP_IDP_SIGN_KEY = os.environ.get("OIDC_RP_IDP_SIGN_KEY", None)
+OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID", None)
+OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET", None)
+OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get("OIDC_OP_AUTHORIZATION_ENDPOINT", None)
+OIDC_OP_TOKEN_ENDPOINT = os.environ.get("OIDC_OP_TOKEN_ENDPOINT", None)
+OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", None)
+OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT", None)
+OIDC_REDIRECT_URL = "/admin/oidc/callback/"
+OIDC_RP_SIGN_ALGO = 'RS256'
+LOGIN_REDIRECT_URL = '/admin/'
+LOGOUT_REDIRECT_URL = '/'
+EUA_FEATUREFLAG = os.getenv('EUA_FEATUREFLAG', 'False').lower() == 'true'
+
+if re.match(r'^dev\d*$', STAGE_ENV) or STAGE_ENV == 'dev' or STAGE_ENV == 'val':
+    LOGIN_REDIRECT_URL = f"/{STAGE_ENV}/admin/"
+    LOGOUT_REDIRECT_URL = f"/{STAGE_ENV}/"
