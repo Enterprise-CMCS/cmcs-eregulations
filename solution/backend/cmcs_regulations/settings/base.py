289dae102b7b0a9a996241090b0c7b5c4006a2df
Thomas Walker <thomas.e.walker88@gmail.com>
2023-12-11 07:59:02 -0500

    Textract ARN hotfix (#1103)

--- solution/backend/cmcs_regulations/settings/base.py
+++ solution/backend/cmcs_regulations/settings/base.py
@@ -229,7 +229,8 @@ USE_LOCAL_TEXTRACT = False
 # The first text extractor is if it was created by serverless.  If it wasnt then it will use the
 # text extractor who's arn you provide in the docker file.
 
-TEXTRACT_ARN = os.environ.get("TEXT_EXTRACTOR_ARN", os.environ.get('TEXTRACT_ARN', '')) # noqa
+TEXT_EXTRACTOR_ARN = os.environ.get("TEXT_EXTRACTOR_ARN", '')
+TEXTRACT_ARN = os.environ.get('TEXTRACT_ARN', '')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8081",
