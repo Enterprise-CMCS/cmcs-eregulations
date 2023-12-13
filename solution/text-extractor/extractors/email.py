3f17949d8676cd8cb27a22e301f4c114058be865
Caleb Godwin <cgodwin@fearless.tech>
2023-12-13 13:38:17 -0500

    Fix eml recursive payload extractor

--- solution/text-extractor/extractors/email.py
+++ solution/text-extractor/extractors/email.py
@@ -11,35 +11,39 @@ from .extractor import Extractor
 class EmailExtractor(Extractor):
     file_types = ("eml",)
 
-    def _extract_payload(self, payload):
+    def _extract_payload(self, message):
+        payload = message.get_payload()
+
         if isinstance(payload, list):
             text = ""
             for i in payload:
-                text += f" {self._extract_payload(i)} "
+                text += f" {self._extract_payload(i)}"
             return text
 
-        if not payload.get_filename() and payload.get_content_type() == "text/plain":
-            return f" {payload.get_payload()} "
+        if not message.get_filename() and message.get_content_type() == "text/plain":
+            return f" {payload}"
 
-        if not payload.get_filename():
+        if not message.get_filename():
             return ""
 
-        file_name = payload.get_filename()
+        file_name = message.get_filename()
         file_type = file_name.lower().split('.')[-1]
 
         with NamedTemporaryFile(delete=False) as file:
-            file.write(payload.get_payload(decode=True))
+            file.write(message.get_payload(decode=True))
             file.close()
 
             try:
                 extractor = Extractor.get_extractor(file_type, self.config)
-                return extractor.extract(file.name)
+                text = extractor.extract(file.name)
             except ExtractorInitException as e:
                 raise ExtractorException(f"failed to initialize extractor for attachment \"{file_name}\": {str(e)}")
             except ExtractorException as e:
                 raise ExtractorException(f"failed to extract text for attachment \"{file_name}\": {str(e)}")
             except Exception as e:
                 raise ExtractorException(f"extracting text for attachment \"{file_name}\" failed unexpectedly: {str(e)}")
+            
+            return f" {file_name} {text}"
 
     def extract(self, file_path: str) -> str:
         try:
@@ -48,4 +52,4 @@ class EmailExtractor(Extractor):
         except Exception as e:
             raise ExtractorException(f"eml file failed to extract: {str(e)}")
 
-        return self._extract_payload(msg.get_payload())
+        return self._extract_payload(msg)
