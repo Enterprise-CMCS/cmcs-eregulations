97f6dc3b3f4809e8f99205a032b831b2ccda75b2
Caleb Godwin <cgodwin@fearless.tech>
2024-01-08 14:27:38 -0500

    Merge cleanup

--- solution/text-extractor/extractors/markup.py
+++ solution/text-extractor/extractors/markup.py
@@ -8,11 +8,9 @@ from .extractor import Extractor
 class MarkupExtractor(Extractor):
     file_types = ("html", "htm", "xhtml", "xml")
 
-    def extract(self, file_path: str) -> str:
+    def extract(self, file: bytes) -> str:
         warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
-        with open(file_path, "rb") as f:
-            data = f.read()
-        extractor = BeautifulSoup(data, "html.parser")  # Use html.parser to avoid lxml dependency (has M1 issues)
+        extractor = BeautifulSoup(file, "html.parser")  # Use html.parser to avoid lxml dependency (has M1 issues)
         for i in extractor(["script", "style"]):
             i.decompose()  # Remove <script> and <style> inline tags
         return extractor.get_text(" ")
