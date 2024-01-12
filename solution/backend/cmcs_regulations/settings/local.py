f10d8802679b111f09eef4e6a72c05082eeb6964
peggles2 <peggles2@users.noreply.github.com>
2024-01-12 12:43:48 -0500

    EREGCSC-2429-cleanup-eua (#1130)
    * EREGCSC-2429-cleanup-eua

--- solution/backend/cmcs_regulations/settings/local.py
+++ solution/backend/cmcs_regulations/settings/local.py
@@ -1,7 +1,6 @@
 from .base import * # noqa
 import os
 import socket
-import re
 
 DEBUG = os.environ.get("DEBUG", False)
 
@@ -29,12 +28,6 @@ DATABASES = {
     },
 }
 
-AUTHENTICATION_BACKENDS = (
-    'regulations.admin.OidcAdminAuthenticationBackend',
-    'mozilla_django_oidc.auth.OIDCAuthenticationBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
 S3_AWS_ACCESS_KEY_ID = os.environ.get("FILE_MANAGER_AWS_ACCESS_KEY_ID", None)
 S3_AWS_SECRET_ACCESS_KEY = os.environ.get("FILE_MANAGER_AWS_SECRET_ACCESS_KEY", None)
 AWS_STORAGE_BUCKET_NAME = os.environ.get("FILE_MANAGER_AWS_BUCKET_NAME", None)
@@ -49,27 +42,7 @@ USE_AWS_TOKEN = True
 TEXTRACT_KEY_ID = os.environ.get("TEXTRACT_KEY_ID", None)
 TEXTRACT_SECRET_KEY = os.environ.get("TEXTRACT_SECRET_KEY", None)
 USE_LOCAL_TEXTRACT = True
-STAGE_ENV = os.environ.get("STAGE_ENV", "")
 BASE_URL = os.environ.get("BASE_URL", "")
 
 # EUA settings
-OIDC_RP_IDP_SIGN_KEY = os.environ.get("OIDC_RP_IDP_SIGN_KEY", None)
-OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID", None)
-OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET", None)
-OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get("OIDC_OP_AUTHORIZATION_ENDPOINT", None)
-OIDC_OP_TOKEN_ENDPOINT = os.environ.get("OIDC_OP_TOKEN_ENDPOINT", None)
-OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", None)
-OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT", None)
-OIDC_REDIRECT_URL = "/admin/oidc/callback/"
-OIDC_RP_SIGN_ALGO = 'RS256'
-LOGIN_REDIRECT_URL = '/admin/'
-
-LOGOUT_REDIRECT_URL = 'http://localhost:8000/admin'
-EUA_FEATUREFLAG = os.getenv('EUA_FEATUREFLAG', 'False').lower() == 'true'
-OIDC_END_EUA_SESSION = os.environ.get("OIDC_END_EUA_SESSION", None)
-OIDC_OP_LOGOUT_URL_METHOD = 'regulations.logout.eua_logout'
-OIDC_STORE_ID_TOKEN = True
-
-if re.match(r'^dev\d*$', STAGE_ENV) or STAGE_ENV == 'dev' or STAGE_ENV == 'val':
-    LOGIN_REDIRECT_URL = f"/{STAGE_ENV}/admin/"
-    LOGOUT_REDIRECT_URL = f"/{STAGE_ENV}/admin/"
+from .euasettings import * # noqa
--- solution/backend/cmcs_regulations/settings/test_settings.py
+++ solution/backend/cmcs_regulations/settings/test_settings.py
@@ -1,5 +1,4 @@
 from .base import * # noqa
-import re
 import os
 
 USE_AWS_TOKEN = True
@@ -18,24 +17,14 @@ SERVER_USER = os.environ.get("SERVER_USER", '')
 SERVER_PASSWORD = os.environ.get("SERVER_PASSWORD", '')
 # TODO - this should be removed after we merge euasettings.py with base.py in teh future
 
-STAGE_ENV = os.environ.get("STAGE_ENV", "")
 BASE_URL = os.environ.get("BASE_URL", "")
-OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID", None)
-OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET", None)
-OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get("OIDC_OP_AUTHORIZATION_ENDPOINT", None)
-OIDC_OP_TOKEN_ENDPOINT = os.environ.get("OIDC_OP_TOKEN_ENDPOINT", None)
-OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", None)
+from .euasettings import * # noqa
+
 OIDC_OP_JWKS_ENDPOINT = "/example/jwks/endpoint/"
 OIDC_REDIRECT_URL = "/admin/oidc/callback/"
 OIDC_RP_SIGN_ALGO = 'RS256'
 LOGIN_REDIRECT_URL = '/admin/'
 LOGOUT_REDIRECT_URL = '/logout'
-
-EUA_FEATUREFLAG = bool(os.getenv('EUA_FEATUREFLAG', 'False').lower() == 'true')
-
-if re.match(r'^dev\d*$', STAGE_ENV) or STAGE_ENV == 'dev' or STAGE_ENV == 'val':
-    LOGIN_REDIRECT_URL = f"/{STAGE_ENV}/admin/"
-    LOGOUT_REDIRECT_URL = f"/{STAGE_ENV}/logout"
 
 DATABASES = {
     'default': {
