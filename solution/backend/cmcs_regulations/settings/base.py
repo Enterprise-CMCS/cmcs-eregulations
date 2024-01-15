09dc969bc103711150caea35efe201679c29b742
Parissa <parissa.eggleston@gmail.com>
2024-01-15 14:57:13 -0500

    fix login to redirect correctly

--- solution/backend/cmcs_regulations/settings/base.py
+++ solution/backend/cmcs_regulations/settings/base.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mozilla_django_oidc.middleware.SessionRefresh',
     'csp.middleware.CSPMiddleware',
     'regulations.middleware.JsonErrors',
     'regulations.middleware.NoIndex',
