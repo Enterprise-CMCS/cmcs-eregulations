bd1e528c4aaf897e02b80c86d1ed3c5698a73467
Caleb Godwin <cgodwin@fearless.tech>
2024-01-18 12:40:14 -0500

    Separate format conversions and extractions using Textract

--- solution/text-extractor/extractors/pdf.py
+++ solution/text-extractor/extractors/pdf.py
@@ -1,40 +1,20 @@
 import logging
 from tempfile import TemporaryDirectory
 
-import boto3
 from pdf2image import convert_from_bytes
 
-from .exceptions import ExtractorException, ExtractorInitException
+from .exceptions import ExtractorException
 from .extractor import Extractor
 
 logger = logging.getLogger(__name__)
 
 
 class PdfExtractor(Extractor):
-    file_types = ("application/pdf", 'pdf')
+    file_types = ("pdf",)
 
     def __init__(self, file_type: str, config: dict):
         super().__init__(file_type, config)
-        try:
-            logger.debug("Initializing boto3 client.")
-            self.client = boto3.client(
-                "textract",
-                **self._get_aws_arguments(config),
-            )
-        except Exception as e:
-            raise ExtractorInitException(f"failed to initialize Textract client: {str(e)}")
-
-    def _get_aws_arguments(self, config: dict) -> dict:
-        try:
-            return {
-                "aws_access_key_id": config["aws"]["aws_access_key_id"],
-                "aws_secret_access_key": config["aws"]["aws_secret_access_key"],
-                "region_name": config["aws"]["aws_region"],
-            }
-        except KeyError:
-            return {
-                "config": boto3.session.Config(signature_version='s3v4',),
-            }
+        self.extractor = Extractor.get_extractor("jpeg", config)
 
     def _convert_to_images(self, file: bytes, temp_dir: str) -> [str]:
         logger.debug("Converting PDF file to images stored in a temporary directory.")
@@ -61,16 +41,9 @@ class PdfExtractor(Extractor):
                 except Exception as e:
                     raise ExtractorException(f"failed to read temporary image file: {str(e)}")
                 try:
-                    logger.debug("Sending page bytes to AWS Textract.")
-                    response = self.client.detect_document_text(Document={'Bytes': content})
+                    text += self.extractor.extract(content)
+                except ExtractorException as e:
+                    logger.warning("Page %i failed to extract: %s", i, str(e))
                 except Exception as e:
-                    raise ExtractorException(f"AWS Textract client failed: {str(e)}")
-                try:
-                    logger.debug("Parsing AWS Textract response.")
-                    for item in response["Blocks"]:
-                        if item["BlockType"] == "LINE":
-                            text += item["Text"] + " "
-                except KeyError as e:
-                    raise ExtractorException(f"AWS response formatted improperly: {str(e)}")
-
+                    logger.warning("Page %i unexpectedly failed to extract: %s", i, str(e))
         return text
