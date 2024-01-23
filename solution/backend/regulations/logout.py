7302a335b27628cc6b2b6487622050b0f08365ce
Parissa <parissa.eggleston@gmail.com>
2024-01-23 15:24:46 -0500

    update the url to use the one from settings

--- solution/backend/regulations/logout.py
+++ solution/backend/regulations/logout.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 
-
 def eua_logout(request):
     id_token = request.session.get('oidc_id_token')
     # get the domain url from the request and add /login to the end
-    logout_redirect_url = request.build_absolute_uri('/') + settings.STAGE_ENV + '/login'
+    logout_redirect_url = request.build_absolute_uri('/') + settings.LOGIN_REDIRECT_URL
 
     # In the local environment where there is no STAGE_ENV, handle the possibility of //logout
     logout_redirect_url = logout_redirect_url.replace('//login', '/login').replace('/prod', '')
