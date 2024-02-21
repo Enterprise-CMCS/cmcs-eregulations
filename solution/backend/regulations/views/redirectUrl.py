a100d0d293753bc76c881e9662a59b3b53d5b402
Parissa <parissa.eggleston@gmail.com>
2024-02-21 17:18:52 -0500

    remove redirectUrl.py

--- solution/backend/regulations/views/redirectUrl.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import redirect
-
-def redirect_view(request):
-    # Replace 'https://newdomain.com' with the actual URL where you want to redirect
-    redirect_url = 'https://newdomain.com'
-    return redirect(redirect_url)
