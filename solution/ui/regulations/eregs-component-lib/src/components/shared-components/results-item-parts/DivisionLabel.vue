84f2dbb02b90f1c22b0918fca63421f701549c67
Phil Reese <preese@fearless.tech>
2024-03-07 12:44:32 -0500

    feat: first pass at group/divison label

---
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/results-item-parts/DivisionLabel.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    division: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <span
+        class="result__context--date--bar result__context--division"
+        :title="`${division.group.name} — ${division.name}`"
+        >Uploaded by {{ division.group.abbreviation }} —
+        {{ division.abbreviation }}</span
+    >
+</template>
