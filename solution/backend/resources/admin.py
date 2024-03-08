7dce3f7f1dc18393d3f6188333db029a0005b303
Caleb Godwin <cgodwin@fearless.tech>
2024-03-08 14:04:03 -0500

    Use get content to fetch extract url for FR docs

--- solution/backend/resources/admin.py
+++ solution/backend/resources/admin.py
@@ -623,7 +623,10 @@ class FederalRegisterDocumentAdmin(AbstractResourceAdmin):
     def get_content(self, obj):
         if obj.id:
             index = ContentIndex.objects.get(fr_doc=obj)
-            link = reverse("call-extractor", kwargs={"content_id": index.uid})
+            link = reverse("call-extractor", kwargs={
+                "content_id": index.uid,
+                "fr_doc_id": obj.id,
+            })
             html = '<a class="button" href="{}">Get content</a>'.format(link)
             return format_html(html)
         return "N/A"
