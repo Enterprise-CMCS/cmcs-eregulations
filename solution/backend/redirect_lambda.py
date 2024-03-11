a9d0faa1024c80a6148db98407d9cda8aeaab11b
Parissa <parissa.eggleston@gmail.com>
2024-03-11 16:59:11 -0400

    try this and cleanup

--- solution/backend/redirect_lambda.py
+++ solution/backend/redirect_lambda.py
@@ -1,16 +1,15 @@
-import json
-
 def lambda_handler(event, context):
     original_path = event['path']
     new_domain = 'https://eregulations.cms.gov'
     new_url = f'{new_domain}{original_path}'
 
     response = {
-        'statusCode': 302,  # HTTP status code for redirection
+        'statusCode': 302,
         'headers': {
-            'Location': new_url,  # Set the new URL in the Location header
+            'Location': new_url,
         },
-        'body': '',  # Empty body for a redirect response
+        'body': f'Redirecting to: {new_url}',
+
     }
 
     return response
