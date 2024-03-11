4fbc9da2d0cd97f33b301fed8d525b8fc9e0631e
Parissa <parissa.eggleston@gmail.com>
2024-03-11 16:26:48 -0400

    update code to redirect dynamically

--- solution/backend/redirect_lambda.py
+++ solution/backend/redirect_lambda.py
@@ -1,9 +1,16 @@
-def handler(event, context):
-    redirect_url = "https://eregulations.cms.gov"
+import json
+
+def lambda_handler(event, context):
+    original_path = event['path']
+    new_domain = 'https://eregulations.cms.gov'
+    new_url = f'{new_domain}{original_path}'
+
     response = {
-        "statusCode": 302,
-        "headers": {
-            "Location": redirect_url,
+        'statusCode': 302,  # HTTP status code for redirection
+        'headers': {
+            'Location': new_url,  # Set the new URL in the Location header
         },
+        'body': '',  # Empty body for a redirect response
     }
+
     return response
