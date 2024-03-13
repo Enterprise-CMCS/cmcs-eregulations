d83824603a9f8dead9d97fa0511ada81d65cf576
Parissa <parissa.eggleston@gmail.com>
2024-03-13 13:30:55 -0400

    update code for redirection

--- solution/backend/redirect_lambda.py
+++ solution/backend/redirect_lambda.py
@@ -1,15 +1,16 @@
+from urllib.parse import urljoin
+
+
 def handler(event, context):
     original_path = event['path']
     new_domain = 'https://eregulations.cms.gov'
-    new_url = f'{new_domain}{original_path}'
+    new_url = urljoin(new_domain, original_path)
 
     response = {
         'statusCode': 302,
         'headers': {
             'Location': new_url,
         },
-        'body': f'Redirecting to: {new_url}',
-
     }
 
     return response
