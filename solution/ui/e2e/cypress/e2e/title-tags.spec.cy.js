21d5c04bd22a0e99feaf2c36c0b8cfaff4fa4dcc
Phil Reese <preese@fearless.tech>
2024-03-13 15:54:05 -0400

    test: various subjects page states should have expected title

--- solution/ui/e2e/cypress/e2e/title-tags.spec.cy.js
+++ solution/ui/e2e/cypress/e2e/title-tags.spec.cy.js
@@ -4,6 +4,10 @@ describe("Updated HTML Title Tags", { scrollBehavior: "center" }, () => {
         cy.intercept("/**", (req) => {
             req.headers["x-automated-test"] = Cypress.env("DEPLOYING");
         });
+
+        cy.intercept("**/v3/file-manager/subjects", {
+            fixture: "subjects.json",
+        }).as("subjects");
     });
 
     it("Homepage title tags", () => {
@@ -18,6 +22,38 @@ describe("Updated HTML Title Tags", { scrollBehavior: "center" }, () => {
         cy.title().should(
             "eq",
             "Statute Reference | Medicaid & CHIP eRegulations"
+        );
+    });
+
+    it("Subjects page without a selected subject", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/subjects/");
+        cy.title().should(
+            "eq",
+            "Find by Subject | Medicaid & CHIP eRegulations"
+        );
+
+        cy.get(".subj-toc__list li[data-testid=subject-toc-li-63] a")
+            .should("have.text", " Managed Care ")
+            .click({ force: true });
+        cy.title().should(
+            "eq",
+            "Managed Care | Find by Subject | Medicaid & CHIP eRegulations"
+        );
+
+        cy.go("back");
+        cy.title().should(
+            "eq",
+            "Find by Subject | Medicaid & CHIP eRegulations"
+        );
+    });
+
+    it("Subjects page with a selected subject already in URL", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/subjects/");
+        cy.title().should(
+            "eq",
+            "Find by Subject | Medicaid & CHIP eRegulations"
         );
     });
 });
