bed93350b51ffe03ae69f538bc12832855f4253d
Parissa <parissa.eggleston@gmail.com>
2024-03-28 13:33:40 -0400

    update script

--- solution/backend/scripts/backup_db.sh
+++ solution/backend/scripts/backup_db.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
+# This script will dump the given database data to a file.
 # Prompt for user input
 read -p "Enter the database user: " DB_USER
 read -p "Enter the database host: " DB_HOST
 read -p "Enter the database port: " DB_PORT
 read -p "Enter the database name: " DB_NAME
-read -p "Enter the path to the backup file: " BACKUP_FILE
 read -sp "Enter the database password: " DB_PASSWORD
 
 CURRENT_DATE=$(date +"%Y%m%d%H%M%S")
