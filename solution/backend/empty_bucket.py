193dabc1cd2bfbc441c088c4811347d2739cf1e5
Parissa <parissa.eggleston@gmail.com>
2024-04-10 17:15:35 -0400

    delete versions

--- solution/backend/empty_bucket.py
+++ solution/backend/empty_bucket.py
@@ -3,25 +3,10 @@ import os
 import boto3
 
 
-def delete_versions_and_markers(bucket_name):
-    s3 = boto3.client('s3')
-    # Get all versions and delete markers
-    versions = s3.list_object_versions(Bucket=bucket_name)
-    delete_markers = versions.get('DeleteMarkers', [])
-    versions = versions.get('Versions', [])
-
-    # Prepare objects for deletion
-    objects_to_delete = {'Objects': []}
-    objects_to_delete['Objects'].extend([
-        {'Key': version['Key'], 'VersionId': version['VersionId']} for version in versions
-    ])
-    objects_to_delete['Objects'].extend([
-        {'Key': marker['Key'], 'VersionId': marker['VersionId']} for marker in delete_markers
-    ])
-
-    # Delete versions and delete markers if any
-    if objects_to_delete['Objects']:
-        s3.delete_objects(Bucket=bucket_name, Delete=objects_to_delete)
+def delete_versions(bucket_name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    bucket.object_versions.delete()
 
 
 def empty_bucket(bucket_name):
@@ -38,7 +23,7 @@ def handler(event, context):
         raise ValueError("Missing environment variable: STAGE_ENV")
 
     if not storage_bucket_name:
-        raise ValueError("Missing environment variable: AWS_STORAGE_BUCKET_NAME", None)
+        raise ValueError("Missing environment variable: AWS_STORAGE_BUCKET_NAME")
 
     # Dynamically construct the CloudFront logs bucket name based on the stage
     cloudfront_logs_bucket_name = f"eregs-{stage}-cloudfront-logs"
@@ -48,7 +33,7 @@ def handler(event, context):
 
     # Empty the CloudFront logs bucket and delete all versions and markers
     empty_bucket(cloudfront_logs_bucket_name)
-    delete_versions_and_markers(cloudfront_logs_bucket_name)
+    delete_versions(cloudfront_logs_bucket_name)
 
     return {
         "message": f"Emptied {storage_bucket_name} and processed versions in {cloudfront_logs_bucket_name}"
