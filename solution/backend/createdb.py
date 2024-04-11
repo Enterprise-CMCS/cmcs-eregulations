2ce24e1b6478db0dec9231f20631b7c629d09e8a
Caleb Godwin <cgodwin@fearless.tech>
2024-04-11 15:31:46 -0400

    Linting

--- solution/backend/createdb.py
+++ solution/backend/createdb.py
@@ -23,7 +23,8 @@ def handler(event, context):
                 "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'eregs'"
             )
             cursor.execute(
-                f"CREATE DATABASE {os.environ.get('STAGE')} WITH TEMPLATE eregs STRATEGY FILE_COPY OWNER {os.environ.get('DB_USER')}"
+                f"CREATE DATABASE {os.environ.get('STAGE')} WITH TEMPLATE eregs "\
+                f"STRATEGY FILE_COPY OWNER {os.environ.get('DB_USER')}"
             )
     except ProgrammingError:
         # This is raised if the database already exists
