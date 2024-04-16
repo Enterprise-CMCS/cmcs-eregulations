076c2e53a8780e927a70c546a6712b15257e7d29
Parissa <parissa.eggleston@gmail.com>
2024-04-16 12:10:12 -0400

    remove the versioning configuration for cloudfront logs bucket

--- solution/backend/empty_bucket.py
+++ solution/backend/empty_bucket.py
@@ -2,9 +2,17 @@ import os
 
 import boto3
 
-
-def handler(event, context):
-    bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
+def empty_bucket(bucket_name):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
     bucket.objects.all().delete()
+
+
+def handler(event, context):
+    stage = os.environ.get("STAGE_ENV")
+
+    # empty storage bucket
+    empty_bucket(os.environ.get("AWS_STORAGE_BUCKET_NAME", None))
+
+    # empty the cloudfront logs bucket.
+    empty_bucket(f"eregs-{stage}-cloudfront-logs")
