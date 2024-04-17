83edcfc942342672e3f982e3426d6600ba12cc9a
peggles2 <peggles2@users.noreply.github.com>
2024-04-17 14:52:51 -0400

    EREGCSC-25595-auto-remove-s3-log-buckets (#1260)
    * remove the versioning configuration for cloudfront logs bucket
    
    * fix ruff errors
    
    * update code to pass the bucket name
    
    * add permissions to the bucket for this lambda function
    
    * Add listobjects

--- solution/backend/empty_bucket.py
+++ solution/backend/empty_bucket.py
@@ -3,8 +3,19 @@ import os
 import boto3
 
 
-def handler(event, context):
-    bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
+def empty_bucket(bucket_name):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
     bucket.objects.all().delete()
+
+
+def handler(event, context):
+    stage = os.environ.get("STAGE_ENV")
+    storage_bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    cloudfront_logs_bucket_name = f"eregs-{stage}-cloudfront-logs"
+
+    # empty storage bucket
+    empty_bucket(storage_bucket_name)
+
+    # empty the cloudfront logs bucket.
+    empty_bucket(cloudfront_logs_bucket_name)
