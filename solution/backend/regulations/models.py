01b3b35a9d170fcfbb03ede780e70f609de72286
Parissa <parissa.eggleston@gmail.com>
2024-05-13 14:32:29 -0400

    update changes

--- solution/backend/regulations/models.py
+++ solution/backend/regulations/models.py
@@ -30,6 +30,25 @@ ROMAN_TABLE = [
 class CustomUser(AbstractUser):
     department = models.CharField(max_length=100, blank=True, null=True)
 
+    # Change related_name for groups and user_permissions to avoid conflict
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="customuser_set",  # unique related_name
+        related_query_name="customuser",
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="customuser_set",  # unique related_name
+        related_query_name="customuser",
+    )
+
 
 class SiteConfiguration(SingletonModel):
     allow_indexing = models.BooleanField(default=False, help_text="Should robots be allowed to index this website?")
