7c27ce668e54993cfea308dfd788475c70ea7c70
Parissa <parissa.eggleston@gmail.com>
2024-05-20 17:10:41 -0400

    fix ruff errors

--- solution/backend/user/tests/test_user.py
+++ solution/backend/user/tests/test_user.py
@@ -1,13 +1,15 @@
 import pytest
-from django.contrib.auth.models import User
 
 from user.models import Profile
 
 
 @pytest.mark.django_db
-def test_user_profile_creation():
-    # Create a user
-    user = User.objects.create_user(username='testuser', password='password123', email='testuser@example.com')
+def test_user_profile_creation(create_user, capsys):
+    user = create_user
+
+    # Capture the output to check signal
+    captured = capsys.readouterr()
+    print(captured.out)
 
     # Check if the profile is created
     assert Profile.objects.filter(user=user).exists()
@@ -18,15 +20,19 @@ def test_user_profile_creation():
 
 
 @pytest.mark.django_db
-def test_user_profile_update():
-    # Create a user
-    user = User.objects.create_user(username='testuser', password='password123', email='testuser@example.com')
+def test_user_profile_update(create_user, capsys):
+    user = create_user
+
+    # Capture the output to check signal
+    captured = capsys.readouterr()
+    print(captured.out)
+
     profile = Profile.objects.get(user=user)
 
     # Update the department field
-    profile.department = '/DHHS/CMS/OA/CMCS/DEHPG'
+    profile.department = '/DHHS/CMS/OA/CMCS/FMG/DFOE/FOEBB'
     profile.save()
 
     # Fetch the profile again and check the department field
     profile = Profile.objects.get(user=user)
-    assert profile.department == '/DHHS/CMS/OA/CMCS/DEHPG'
+    assert profile.department == '/DHHS/CMS/OA/CMCS/FMG/DFOE/FOEBB'
