b764bd83963c896b0d9ab7cbafcc21c0530269b9
Parissa <parissa.eggleston@gmail.com>
2024-05-20 13:53:55 -0400

    fix ruff errors

--- solution/backend/user/admin.py
+++ solution/backend/user/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+
 from .models import Profile
+
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
     verbose_name_plural = 'Profiles'
     fields = ('department',)
+
 
 class CustomUserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
@@ -22,6 +25,7 @@ class CustomUserAdmin(BaseUserAdmin):
         if not obj:
             return []
         return super().get_inline_instances(request, obj)
+
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
