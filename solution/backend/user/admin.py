e203967ea650d394cc96ae16c7491a962e2058cc
Parissa <parissa.eggleston@gmail.com>
2024-05-20 09:59:17 -0400

    add department to admin

--- solution/backend/user/admin.py
+++ solution/backend/user/admin.py
@@ -1,18 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-
 from .models import Profile
-
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
+    verbose_name_plural = 'Profiles'
+    fields = ('department',)
 
-
-class UserAdmin(BaseUserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'get_department')
+    list_select_related = ('profile',)
 
+    def get_department(self, instance):
+        return instance.profile.department
+    get_department.short_description = 'Department'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
