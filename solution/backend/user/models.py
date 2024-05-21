aad84abd9c3ece18336a54ba1ff2b0de26e321f1
Parissa <parissa.eggleston@gmail.com>
2024-05-21 12:48:12 -0400

    update code

--- solution/backend/user/models.py
+++ solution/backend/user/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -8,3 +10,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        print(f"Creating profile for user: {instance.username}")
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    print(f"Saving profile for user: {instance.username}")
+    instance.profile.save()
