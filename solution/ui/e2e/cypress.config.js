1534da21b054b0534bc0607c93eea3d625c9ac4e
Phil Reese <preese@fearless.tech>
2024-05-31 09:49:37 -0400

    test: prettier formatting; remove videoUploadOnPasses config entry as it has been removed

--- solution/ui/e2e/cypress.config.js
+++ solution/ui/e2e/cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8000",
-    videoUploadOnPasses: false,
     defaultCommandTimeout: 10000,
     retries: 2,
     chromeWebSecurity: false,
