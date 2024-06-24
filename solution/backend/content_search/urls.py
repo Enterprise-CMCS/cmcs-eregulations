cd08b9695e146c9df93705b658286e8fcad24026
Caleb Godwin <cgodwin@fearless.tech>
2024-06-24 11:33:56 -0400

    Fix urls.py

--- solution/backend/content_search/urls.py
+++ solution/backend/content_search/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ContentSearchViewSet
+from .views import ContentSearchViewSet, PostContentTextViewset
 
 urlpatterns = [
     path("", ContentSearchViewSet.as_view({
         "get": "list",
-        "post": "post",
     })),
+    path("upload", PostContentTextViewset.as_view(), name='post-content'),
 ]
