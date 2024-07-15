4fa9f5a682f83d3f4bd1b7c9c7f7acef7008e1c8
Caleb Godwin <cgodwin@fearless.tech>
2024-07-15 14:28:50 -0400

    Remove doc id sort field from file list display

--- solution/backend/resources/admin/internal_resources.py
+++ solution/backend/resources/admin/internal_resources.py
@@ -61,8 +61,8 @@ class InternalFileForm(AbstractInternalResourceForm):
 @admin.register(InternalFile)
 class InternalFileAdmin(AbstractInternalResourceAdmin):
     form = InternalFileForm
-    list_display = ["date", "document_id", "title", "category", "updated_at", "approved", "document_id_sort"]
-    list_display_links = ["date", "document_id", "title", "category", "updated_at", "approved", "document_id_sort"]
+    list_display = ["date", "document_id", "title", "category", "updated_at", "approved"]
+    list_display_links = ["date", "document_id", "title", "category", "updated_at", "approved"]
     search_fields = ["date", "document_id", "title", "summary"]
     ordering = ["date", "document_id", "category", "updated_at"]
     readonly_fields = ["download_file", "file_name", "file_type"]
