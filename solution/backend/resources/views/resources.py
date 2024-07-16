da6eb288c216399b94ef1c7574fe48a6e5b2713f
Caleb Godwin <cgodwin@fearless.tech>
2024-07-16 17:30:53 -0400

    Fix group_resources

--- solution/backend/resources/views/resources.py
+++ solution/backend/resources/views/resources.py
@@ -35,13 +35,13 @@ class ResourceViewSet(viewsets.ReadOnlyModelViewSet):
     model = AbstractResource
 
     def get_serializer_context(self):
-        return {"show_related": self.group_resources}
+        return {"show_related": string_to_bool(self.request.GET.get("group_resources"), True)}
 
     def get_queryset(self):
         citations = self.request.GET.getlist("citations")
         categories = self.request.GET.getlist("categories")
         subjects = self.request.GET.getlist("subjects")
-        self.group_resources = string_to_bool(self.request.GET.get("group_resources"), True)
+        group_resources = string_to_bool(self.request.GET.get("group_resources"), True)
 
         category_prefetch = AbstractCategory.objects.select_subclasses()
         citation_prefetch = AbstractCitation.objects.select_subclasses()
@@ -58,7 +58,7 @@ class ResourceViewSet(viewsets.ReadOnlyModelViewSet):
             prefix = "" if self.model == AbstractResource else "abstractresource_ptr__"
             query = query.filter(**{f"{prefix}abstractinternalresource__isnull": True})
 
-        if self.group_resources:
+        if group_resources:
             # Prefetch related_resources and filter out non-parent group members
             query = query.prefetch_related(
                 Prefetch("related_resources", AbstractResource.objects.filter(approved=True).prefetch_related(
