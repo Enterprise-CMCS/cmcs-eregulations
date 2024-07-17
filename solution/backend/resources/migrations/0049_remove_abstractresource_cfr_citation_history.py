7a71735daa3130986f73cce40681eca1eb8f8023
Caleb Godwin <cgodwin@fearless.tech>
2024-07-17 11:16:16 -0400

    Fix migration dependencies

--- solution/backend/resources/migrations/0049_remove_abstractresource_cfr_citation_history.py
+++ solution/backend/resources/migrations/0049_remove_abstractresource_cfr_citation_history.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('resources', '0047_recompute_related_fields'),
+        ('resources', '0048_remove_abstractresource_document_id_sort_and_more'),
     ]
 
     operations = [
