9d74e96d58913c5479370014b5cf4341079cf319
Caleb Godwin <cgodwin@fearless.tech>
2024-07-24 11:12:59 -0400

    Remove old management commands causing ruff errors

--- solution/backend/regcore/management/commands/populateseeddata.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from common.populate_seed import loadSeedData
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **kwargs):
-        loadSeedData()
