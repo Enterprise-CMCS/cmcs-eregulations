2ebcad001a93e296540d6aaaa34cada8ad626e39
Phil Reese <philr8@gmail.com>
2024-07-25 16:00:41 -0400

    feat: better implementation in more appropriate place

--- solution/backend/common/admin.py
+++ solution/backend/common/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import ModelChoiceField
+
+
+class CustomCategoryChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        needs_indent = "Subcategory" in obj._meta.verbose_name
+        name_string = f"{obj.name} ({obj._meta.verbose_name})"
+
+        return f"⠀⠀{name_string}" if needs_indent else name_string
 
 
 class AbstractAdmin(admin.ModelAdmin):
     list_per_page = 200
     admin_priority = 20
+    formfield_overrides = {
+        models.ForeignKey: {"form_class": CustomCategoryChoiceField},
+    }
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         lookups = getattr(self, "foreignkey_lookups", {})
