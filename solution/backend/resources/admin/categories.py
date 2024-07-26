60d24c55f6f3ccc8dc85219e5dc34457f14511f0
Phil Reese <philr8@gmail.com>
2024-07-26 13:44:44 -0400

    feat: update names

--- solution/backend/resources/admin/categories.py
+++ solution/backend/resources/admin/categories.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Prefetch
 
 from common.admin import AbstractAdmin
 from resources.models import (
@@ -16,6 +17,7 @@ class AbstractCategoryAdmin(AbstractAdmin):
 @admin.register(PublicCategory)
 class PublicCategoryAdmin(AbstractCategoryAdmin):
     admin_priority = 30
+    exclude = ["parent"]
     list_display = ["name", "description", "order", "show_if_empty"]
     search_fields = ["name", "description"]
     ordering = ["name", "description", "order"]
@@ -28,10 +30,24 @@ class PublicSubCategoryAdmin(AbstractCategoryAdmin):
     search_fields = ["name", "description"]
     ordering = ["name", "description", "order", "parent"]
 
+    foreignkey_lookups = {
+        "parent": lambda: PublicCategory.objects.all(),
+    }
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related(
+                Prefetch("parent", PublicCategory.objects.all()),
+            )
+        )
+
 
 @admin.register(InternalCategory)
 class InternalCategoryAdmin(AbstractCategoryAdmin):
     admin_priority = 32
+    exclude = ["parent"]
     list_display = ["name", "description", "order", "show_if_empty"]
     search_fields = ["name", "description"]
     ordering = ["name", "description", "order"]
@@ -43,3 +59,16 @@ class InternalSubCategoryAdmin(AbstractCategoryAdmin):
     list_display = ["name", "description", "order", "show_if_empty", "parent"]
     search_fields = ["name", "description"]
     ordering = ["name", "description", "order", "parent"]
+
+    foreignkey_lookups = {
+        "parent": lambda: InternalCategory.objects.all(),
+    }
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related(
+                Prefetch("parent", InternalCategory.objects.all()),
+            )
+        )
