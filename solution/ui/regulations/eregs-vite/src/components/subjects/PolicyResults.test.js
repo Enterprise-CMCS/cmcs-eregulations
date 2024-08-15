c96bbba0f2bb1e45dad660c71055e692f3b975cd
Phil Reese <philr8@gmail.com>
2024-08-15 13:31:34 -0400

    test: all tests pass

--- solution/ui/regulations/eregs-vite/src/components/subjects/PolicyResults.test.js
+++ solution/ui/regulations/eregs-vite/src/components/subjects/PolicyResults.test.js
@@ -18,18 +18,56 @@ const MOCK_RESULTS = [
         url: "url",
     },
     {
-            type: "internal_file",
+        type: "internal_file",
+        cfr_citations: [],
+        subjects: [],
+        document_id: "docID",
+        title: "this is a document name string",
+        date: "2024-10-10",
+        url: "",
+        related_resources: null,
+        summary: "This is the summary for the test text file",
+        file_name: "test.txt",
+        file_type: "",
+        uid: "a4e00982-4944-4a8d-8dd9-e5d2caa11f51",
+    },
+    {
+        name_headline: "",
+        summary_headline:
+            "this <span class='search-highlight'>is</span> a summary headline",
+        content_headline: "",
+        resource: {
+            type: "public_link",
+            category: {},
             cfr_citations: [],
             subjects: [],
-            document_id: "docID",
-            title: "this is a document name string",
-            date: "2024-10-10",
-            url: "",
+            document_id: "",
+            title: "",
+            date: "2021-06-28",
+            url:
+                "https://innovation.cms.gov/data-and-reports/2021/sim-rd2-test-final-appendix",
             related_resources: null,
-            summary: "This is the summary for the test text file",
-            file_name: "test.txt",
-            file_type: "",
-            uid: "a4e00982-4944-4a8d-8dd9-e5d2caa11f51",
+        },
+        reg_text: null,
+    },
+    {
+        name_headline: "",
+        summary_headline:
+            "this <span class='search-highlight'>is</span> a summary headline",
+        content_headline: "this <span class='search-highlight'>is</span> a content headline",
+        resource: {
+            type: "public_link",
+            category: {},
+            cfr_citations: [],
+            subjects: [],
+            document_id: "",
+            title: "",
+            date: "2021-06-28",
+            url:
+                "https://innovation.cms.gov/data-and-reports/2021/sim-rd2-test-final-appendix",
+            related_resources: null,
+        },
+        reg_text: null,
     },
 ];
 
@@ -61,26 +99,7 @@ describe("getResultLinkText", () => {
         );
     });
     it("/content-search external result", async () => {
-        const result = {
-            name_headline: "",
-            summary_headline:
-                "this <span class='search-highlight'>is</span> a summary headline",
-            content_headline: "",
-            resource: {
-                type: "public_link",
-                category: {},
-                cfr_citations: [],
-                subjects: [],
-                document_id: "",
-                title: "",
-                date: "2021-06-28",
-                url:
-                    "https://innovation.cms.gov/data-and-reports/2021/sim-rd2-test-final-appendix",
-                related_resources: null,
-            },
-            reg_text: null,
-        };
-        expect(PolicyResults.getResultLinkText(result)).toBe(
+        expect(PolicyResults.getResultLinkText(MOCK_RESULTS[2])).toBe(
             "<span class='result__link--label'>this <span class='search-highlight'>is</span> a summary headline</span>"
         );
     });
@@ -122,16 +141,33 @@ describe("getResultSnippet", () => {
 });
 
 describe("showResultSnippet", () => {
-    it("is internal and has a summary_headline", async () => {
+    it("/content-search internal result and has a summary_headline", async () => {
+        expect(PolicyResults.showResultSnippet(MOCK_RESULTS[0])).toBe(true);
+    });
+    it("/resources/internal result and has a summary_headline", async () => {
         expect(PolicyResults.showResultSnippet(MOCK_RESULTS[1])).toBe(true);
     });
-    it("is internal and does NOT have a summary_headline or summary_string or content_headline", async () => {
-        expect(PolicyResults.showResultSnippet(MOCK_RESULTS[2])).toBe(false);
+    it("/resources/internal and does NOT have a summary_headline or summary_string or content_headline", async () => {
+        const result = {
+            type: "internal_file",
+            cfr_citations: [],
+            subjects: [],
+            document_id: "docID",
+            title: "this is a document name string",
+            date: "2024-10-10",
+            url: "",
+            related_resources: null,
+            summary: "",
+            file_name: "test.txt",
+            file_type: "",
+            uid: "a4e00982-4944-4a8d-8dd9-e5d2caa11f51",
+        };
+        expect(PolicyResults.showResultSnippet(result)).toBe(false);
     });
-    it("is external and has a content_headline", async () => {
+    it("/content-search external and has a content_headline", async () => {
         expect(PolicyResults.showResultSnippet(MOCK_RESULTS[3])).toBe(true);
     });
-    it("is external and does NOT have a content_headline or content_string", async () => {
-        expect(PolicyResults.showResultSnippet(MOCK_RESULTS[5])).toBe(false);
+    it("/content-search external and does NOT have a content_headline", async () => {
+        expect(PolicyResults.showResultSnippet(MOCK_RESULTS[2])).toBe(false);
     });
 });
