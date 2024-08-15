45cf2f7f2750f4098817effa8a63d687638236bc
Parissa <parissa.eggleston@gmail.com>
2024-08-15 16:35:18 -0400

    fix ruff error

--- solution/backend/resources/serializers/groups.py
+++ solution/backend/resources/serializers/groups.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
 from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
 
 
 class ResourceSerializer(serializers.Serializer):
