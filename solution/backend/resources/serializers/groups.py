bcdc988b247aab4e13f431dabcb909952330235a
Parissa <parissa.eggleston@gmail.com>
2024-08-15 16:30:09 -0400

    update code to work with swagger

--- solution/backend/resources/serializers/groups.py
+++ solution/backend/resources/serializers/groups.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
+
+
+class ResourceSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
 
 
 class ResourceGroupSerializer(serializers.Serializer):
     name = serializers.CharField()
     common_identifiers = serializers.ListField(child=serializers.CharField())
-    resources = serializers.PrimaryKeyRelatedField(many=True, read_only=True)  # TODO: serialize the whole resource
+
+    @extend_schema_field(serializers.ListField(child=serializers.IntegerField()))
+    def get_resources(self, obj):
+        # Default behavior: return primary keys
+        return [resource.id for resource in obj.resources.all()]
+
+    resources = serializers.SerializerMethodField()
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get('request', None)
+
+        # Serialize resources by full representation if requested
+        if request and request.query_params.get('serialize_by') == 'full':
+            resource_serializer = ResourceSerializer(instance.resources.all(), many=True)
+            representation['resources'] = resource_serializer.data
+
+        return representation
