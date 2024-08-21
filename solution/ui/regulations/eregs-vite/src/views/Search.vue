008d21b8f72f7d0b1a1408e52b40ae9f58f0c847
Phil Reese <philr8@gmail.com>
2024-08-21 14:24:26 -0400

    feat: crudely copy over logic from subjects page

--- solution/ui/regulations/eregs-vite/src/views/Search.vue
+++ solution/ui/regulations/eregs-vite/src/views/Search.vue
@@ -113,11 +113,336 @@ provide("FilterTypesDict", FilterTypesDict);
 provide("homeUrl", props.homeUrl);
 provide("isAuthenticated", props.isAuthenticated);
 
+// provide router query params to remove on child component change
+const commonRemoveList = ["page", "categories", "intcategories"];
+const policySelectionsRemoveList = ["subjects"];
+const searchInputRemoveList = commonRemoveList.concat(["q"]);
+
+provide("commonRemoveList", commonRemoveList);
+provide("policySelectionsRemoveList", policySelectionsRemoveList);
+
+const categoriesRef = ref([]);
+const setCategories = (categories) => {
+    categoriesRef.value = categories;
+};
+
+/**
+ * @param {Object} queryParams - $route.query
+ * @returns {Boolean} - true if all doc types are selected and nothing else
+ */
+const allDocTypesOnly = (queryParams) => {
+    const { type, ...rest } = queryParams;
+    if (
+        (type && type.includes("all") && _isEmpty(rest)) ||
+        (!type && _isEmpty(rest))
+    ) {
+        return true;
+    }
+
+    return false;
+};
+
 // search query refs and methods
 const searchQuery = ref($route.query.q || "");
 const clearSearchQuery = () => {
     searchQuery.value = "";
 };
+
+const executeSearch = (payload) => {
+    const routeClone = { ...$route.query };
+
+    const cleanedRoute = useRemoveList({
+        route: routeClone,
+        removeList: searchInputRemoveList,
+    });
+
+    $router.push({
+        name: "search",
+        query: {
+            ...cleanedRoute,
+            q: payload.query,
+        },
+    });
+};
+
+const clearSearchInput = () => {
+    const routeClone = { ...$route.query };
+
+    const cleanedRoute = useRemoveList({
+        route: routeClone,
+        removeList: searchInputRemoveList,
+    });
+
+    $router.push({
+        name: "search",
+        query: {
+            ...cleanedRoute,
+        },
+    });
+};
+
+// partsLastUpdated fetch for related regulations citations filtering
+const partsLastUpdated = ref({
+    results: {},
+    loading: true,
+});
+
+const getPartsLastUpdated = async () => {
+    try {
+        const titles = await getTitles({ apiUrl: props.apiUrl });
+        partsLastUpdated.value.results = await getLastUpdatedDates({
+            apiUrl: props.apiUrl,
+            titles,
+        });
+    } catch (error) {
+        console.error(error);
+    } finally {
+        partsLastUpdated.value.loading = false;
+    }
+};
+
+// policyDocList fetch for policy document list
+const policyDocList = ref({
+    count: 0,
+    results: [],
+    loading: true,
+    error: false,
+});
+
+const getDocList = async ({ requestParamString = "", query, type }) => {
+    policyDocList.value.loading = true;
+    policyDocList.value.error = false;
+
+    const requestParams = `${requestParamString}&page_size=${pageSize}&group_resources=false`;
+    const docType = type ? DOCUMENT_TYPES_MAP[type] : undefined;
+
+    let contentList;
+
+    try {
+        if (query) {
+            contentList = await getCombinedContent({
+                apiUrl: props.apiUrl,
+                requestParams,
+                docType,
+            });
+        } else {
+            contentList = await getContentWithoutQuery({
+                apiUrl: props.apiUrl,
+                requestParams,
+                docType,
+            });
+        }
+
+        policyDocList.value.results = contentList.results;
+        policyDocList.value.count = contentList.count;
+    } catch (error) {
+        console.error(error);
+        policyDocList.value.results = [];
+        policyDocList.value.count = 0;
+        policyDocList.value.error = true;
+    } finally {
+        policyDocList.value.loading = false;
+    }
+};
+
+// use "reactive" method to make urlParams reactive when provided/injected
+// selectedParams.paramString is used as the reactive prop
+const selectedParams = reactive({
+    paramString: "",
+    paramsArray: [],
+});
+
+// method to add selected params
+const addSelectedParams = (paramArgs) => {
+    const { id, name, type } = paramArgs;
+
+    // update paramString that is used as reactive prop for watch
+    if (selectedParams.paramString) {
+        selectedParams.paramString += `&${type}=${id}`;
+    } else {
+        selectedParams.paramString = `?${type}=${id}`;
+    }
+
+    // create new selectedParams key for array of objects for selections
+    selectedParams.paramsArray.push({ id, name, type });
+};
+
+const clearSelectedParams = () => {
+    selectedParams.paramString = "";
+    selectedParams.paramsArray = [];
+};
+
+provide("selectedParams", selectedParams);
+
+const setSelectedParams = (subjectsListRef) => (param) => {
+    const [paramType, paramValue] = param;
+
+    if (commonRemoveList.includes(paramType)) {
+        return;
+    }
+
+    if (paramType === "q") {
+        searchQuery.value = paramValue;
+        return;
+    }
+
+    const paramList = !_isArray(paramValue) ? [paramValue] : paramValue;
+    paramList.forEach((paramId) => {
+        const subject = subjectsListRef.value.results.filter(
+            (subjectObj) => paramId === subjectObj.id.toString()
+        )[0];
+
+        if (subject) {
+            addSelectedParams({
+                type: paramType,
+                id: paramId,
+                name: getSubjectName(subject),
+            });
+        }
+    });
+};
+
+// policyDocSubjects fetch for subject selector
+// fetch here so we have it in context; pass down to selector via props
+const policyDocSubjects = ref({
+    results: [],
+    loading: true,
+});
+
+const setDocumentTitle = (subjectId, subjectList) => {
+    const subjectToSelect = subjectList.filter(
+        (subjectObj) => subjectObj.id.toString() === subjectId
+    )[0];
+    const subjName = getSubjectName(subjectToSelect);
+    document.title = `${subjName} | ${document.title}`;
+};
+
+// called on load
+const getDocSubjects = async () => {
+    try {
+        const subjectsResponse = await getInternalSubjects({
+            apiUrl: props.apiUrl,
+        });
+
+        policyDocSubjects.value.results = subjectsResponse.results;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        policyDocSubjects.value.loading = false;
+
+        // set title if needed
+        if (policyDocSubjects.value.results.length && $route.query.subjects) {
+            setDocumentTitle(
+                $route.query.subjects,
+                policyDocSubjects.value.results
+            );
+        }
+
+        // if there's a $route, call addSelectedParams
+        if (!allDocTypesOnly($route.query)) {
+            // wipe everything clean to start
+            clearSelectedParams();
+            clearSearchQuery();
+
+            // now that everything is cleaned, iterate over new query params
+            Object.entries($route.query).forEach((param) => {
+                setSelectedParams(policyDocSubjects)(param);
+            });
+
+            getDocList({
+                requestParamString: getRequestParams($route.query),
+                query: $route.query.q,
+                type: $route.query.type,
+            });
+        }
+    }
+};
+
+const selectedSubjectParts = ref([]);
+
+const setSelectedSubjectParts = () => {
+    if (selectedParams.paramsArray.length) {
+        if (selectedParams.paramsArray[0].id) {
+            const selectedSubject = policyDocSubjects.value.results.filter(
+                (subjectObj) =>
+                    subjectObj.id.toString() ===
+                    selectedParams.paramsArray[0].id
+            )[0];
+            selectedSubjectParts.value = getSubjectNameParts(selectedSubject);
+        }
+    } else {
+        selectedSubjectParts.value = [];
+    }
+};
+
+watch(
+    () => policyDocSubjects.value.loading,
+    async (newLoading) => {
+        if (!newLoading) {
+            setSelectedSubjectParts();
+        }
+    }
+);
+
+watch(
+    () => selectedParams.paramString,
+    async () => {
+        setSelectedSubjectParts();
+    }
+);
+
+watch(
+    () => $route.query,
+    async (newQueryParams) => {
+        // set title on subject selection
+        const { subjects } = newQueryParams;
+        if (subjects) {
+            const subjectTitleToSet = _isArray(subjects)
+                ? subjects[0]
+                : subjects;
+            setDocumentTitle(
+                subjectTitleToSet,
+                policyDocSubjects.value.results
+            );
+        }
+
+        // wipe everything clean to start
+        clearSelectedParams();
+        clearSearchQuery();
+
+        const sanitizedQueryParams = Object.entries(newQueryParams).filter(
+            ([key]) => PARAM_VALIDATION_DICT[key]
+        );
+
+        // if all params are removed, return
+        if (_isEmpty(sanitizedQueryParams)) {
+            return;
+        }
+
+        // if both internal and external checkboxes are selected and nothing else, return
+        if (allDocTypesOnly($route.query)) {
+            return;
+        }
+
+        // now that everything is cleaned, iterate over new query params
+        Object.entries(newQueryParams).forEach(
+            setSelectedParams(policyDocSubjects)
+        );
+
+        // parse $route.query to return `${key}=${value}` string
+        // and provide to getDocList
+        const newRequestParams = getRequestParams(newQueryParams);
+        await getDocList({
+            requestParamString: newRequestParams,
+            query: $route.query.q,
+            type: $route.query.type,
+        });
+    }
+);
+
+// fetches on page load
+getPartsLastUpdated();
+getDocSubjects();
 </script>
 
 <template>
@@ -159,8 +484,10 @@ const clearSearchQuery = () => {
                 <SearchInput
                     form-class="search-form"
                     label="Search for a document"
-                    page="subjects"
+                    page="search"
                     :search-query="searchQuery"
+                    @execute-search="executeSearch"
+                    @clear-form="clearSearchInput"
                 />
                 Filters will also be here
             </section>
@@ -168,6 +495,51 @@ const clearSearchQuery = () => {
             <section class="search-results">
                 Count and sort options here
                 <h3>Search results here</h3>
+                <template
+                    v-if="policyDocList.loading || partsLastUpdated.loading"
+                >
+                    <span class="loading__span">Loading...</span>
+                </template>
+                <template v-else-if="policyDocList.error">
+                    <div class="doc__list">
+                        <h2
+                            v-if="!selectedSubjectParts.length || searchQuery"
+                            class="search-results__heading"
+                        >
+                            Search Results
+                        </h2>
+                        <SearchErrorMsg
+                            :search-query="searchQuery"
+                            show-apology
+                            :survey-url="surveyUrl"
+                        />
+                    </div>
+                </template>
+                <template v-else>
+                    <PolicyResults
+                        :base="homeUrl"
+                        :categories="categoriesRef"
+                        :results="policyDocList.results"
+                        :results-count="policyDocList.count"
+                        :page="parseInt($route.query.page, 10) || 1"
+                        :page-size="pageSize"
+                        :parts-last-updated="partsLastUpdated.results"
+                        :has-editable-job-code="hasEditableJobCode"
+                        :search-query="searchQuery"
+                        :selected-subject-parts="selectedSubjectParts"
+                    />
+                    <div class="pagination-expand-row">
+                        <div class="pagination-expand-container">
+                            <PaginationController
+                                v-if="policyDocList.count > 0"
+                                :count="policyDocList.count"
+                                :page="parseInt($route.query.page, 10) || 1"
+                                :page-size="pageSize"
+                                view="subjects"
+                            />
+                        </div>
+                    </div>
+                </template>
             </section>
         </main>
     </body>
