c573e9ae9db7d9274fce17ac6207ecf521d09e19
Caleb Godwin <cgodwin@fearless.tech>
2024-09-10 17:28:19 -0400

    Update ViewSetPagination to handle additional attributes and custom counts
    Can override get_count_map() and get_additional_attributes() to add counts
    and data to the top level of the pagination response.

--- solution/backend/regcore/search/views.py
+++ solution/backend/regcore/search/views.py
@@ -5,7 +5,6 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
 from common.api import OpenApiQueryParameter
-from common.mixins import OptionalPaginationMixin
 
 from .models import SearchIndexV2
 from .serializers import SearchResultSerializer
@@ -13,9 +12,9 @@ from .serializers import SearchResultSerializer
 
 @extend_schema(
     description="Search the regulation text. This endpoint is incomplete and may change. Results are paginated by default.",
-    parameters=[OpenApiQueryParameter("q", "The word or phrase to search for.", str, True)] + OptionalPaginationMixin.PARAMETERS,
+    parameters=[OpenApiQueryParameter("q", "The word or phrase to search for.", str, True)]
 )
-class SearchView(OptionalPaginationMixin, viewsets.ReadOnlyModelViewSet):
+class SearchView(viewsets.ReadOnlyModelViewSet):
     serializer_class = SearchResultSerializer
 
     def get_queryset(self):
