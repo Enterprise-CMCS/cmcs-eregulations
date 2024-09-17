495f97b3bbe33c1672f7f59c601b2031eb43a8b1
Phil Reese <philr8@gmail.com>
2024-09-17 13:26:07 -0400

    chore: refactor SearchEmtpyState to use Composition API

--- solution/ui/regulations/eregs-vite/src/components/SearchEmptyState.vue
+++ solution/ui/regulations/eregs-vite/src/components/SearchEmptyState.vue
@@ -1,3 +1,54 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    eregs_url: {
+        type: String,
+        default: "",
+    },
+    eregs_url_label: {
+        type: String,
+        default: "",
+    },
+    eregs_sublabel: {
+        type: String,
+        default: "",
+    },
+    query: {
+        type: String,
+        required: false,
+    },
+    showInternalLink: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const eregsLink = computed(() =>
+    props.query ? `${this.eregs_url}?q=${this.query}` : this.eregs_url
+);
+
+const ecfrLink = computed(
+    () =>
+        `https://www.ecfr.gov/search?search[hierarchy][title]=42&search[query]=${this.query}`
+);
+
+const federalRegisterLink = computed(
+    () =>
+        `https://www.federalregister.gov/documents/search?conditions[agencies][]=centers-for-medicare-medicaid-services&conditions[term]=${this.query}`
+);
+
+const medicaidGovLink = computed(
+    () =>
+        `https://www.medicaid.gov/search-gsc?&gsc.sort=#gsc.tab=0&gsc.q=${this.query}&gsc.sort=`
+);
+
+const unitedStatesCodeLink = computed(
+    () =>
+        `https://uscode.house.gov/search.xhtml?edition=prelim&searchString=%28${this.query}+%28title%3A42+chapter%3A7+subchapter%3A19%29+OR+%28title%3A42+chapter%3A7+subchapter%3A21%29+%29&pageNumber=1&itemsPerPage=100&sortField=RELEVANCE&displayType=CONTEXT&action=search&q=dGVzdCAodGl0bGU6NDIgY2hhcHRlcjo3IHN1YmNoYXB0ZXI6MTkpIE9SICh0aXRsZTo0MiBjaGFwdGVyOjcgc3ViY2hhcHRlcjoyMSkg%7C%3A%3A%3A%3A%3A%3A%3A%3Afalse%3A%7C%3A%3A%3A%3A%3A%3A%3A%3Afalse%3A%7Ctrue%7C%5B%3A%3A%3A%3A%3A%3A%3A%3Afalse%3A%5D%7C%5BQWxsIEZpZWxkcw%3D%3D%3A%3BQWxsIEZpZWxkcw%3D%3D%3A%5D`
+);
+</script>
+
 <template>
     <div class="empty-state-container">
         <div class="empty-state-container-title">Expand your search</div>
@@ -50,70 +101,3 @@
         </div>
     </div>
 </template>
-
-<script>
-export default {
-    name: "SearchEmptyState",
-
-    components: {},
-
-    props: {
-        eregs_url: {
-            type: String,
-            default: "",
-        },
-        eregs_url_label: {
-            type: String,
-            default: "",
-        },
-        eregs_sublabel: {
-            type: String,
-            default: "",
-        },
-        query: {
-            type: String,
-            required: false,
-        },
-        showInternalLink: {
-            type: Boolean,
-            default: true,
-        },
-    },
-
-    beforeCreate() {},
-
-    created() {},
-
-    beforeMount() {},
-
-    mounted() {},
-
-    beforeUpdate() {},
-
-    updated() {},
-
-    beforeDestroy() {},
-
-    destroyed() {},
-
-    computed: {
-        eregsLink() {
-            return this.query
-                ? `${this.eregs_url}?q=${this.query}`
-                : this.eregs_url;
-        },
-        ecfrLink() {
-            return `https://www.ecfr.gov/search?search[hierarchy][title]=42&search[query]=${this.query}`;
-        },
-        federalRegisterLink() {
-            return `https://www.federalregister.gov/documents/search?conditions[agencies][]=centers-for-medicare-medicaid-services&conditions[term]=${this.query}`;
-        },
-        medicaidGovLink() {
-            return `https://www.medicaid.gov/search-gsc?&gsc.sort=#gsc.tab=0&gsc.q=${this.query}&gsc.sort=`;
-        },
-        unitedStatesCodeLink() {
-            return `https://uscode.house.gov/search.xhtml?edition=prelim&searchString=%28${this.query}+%28title%3A42+chapter%3A7+subchapter%3A19%29+OR+%28title%3A42+chapter%3A7+subchapter%3A21%29+%29&pageNumber=1&itemsPerPage=100&sortField=RELEVANCE&displayType=CONTEXT&action=search&q=dGVzdCAodGl0bGU6NDIgY2hhcHRlcjo3IHN1YmNoYXB0ZXI6MTkpIE9SICh0aXRsZTo0MiBjaGFwdGVyOjcgc3ViY2hhcHRlcjoyMSkg%7C%3A%3A%3A%3A%3A%3A%3A%3Afalse%3A%7C%3A%3A%3A%3A%3A%3A%3A%3Afalse%3A%7Ctrue%7C%5B%3A%3A%3A%3A%3A%3A%3A%3Afalse%3A%5D%7C%5BQWxsIEZpZWxkcw%3D%3D%3A%3BQWxsIEZpZWxkcw%3D%3D%3A%5D`;
-        },
-    },
-};
-</script>
