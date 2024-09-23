2c57a55ad5197aaad51b0ae91ba467161e01212e
Phil Reese <philr8@gmail.com>
2024-09-23 13:35:06 -0400

    chore: use ESLint 9.0 flat config

---
+++ solution/ui/regulations/eslint.config.js
@@ -0,0 +1,32 @@
+// eslint.config.js
+import pluginVue from "eslint-plugin-vue";
+
+export default [
+    ...pluginVue.configs["flat/recommended"],
+    {
+        rules: {
+            eqeqeq: "off",
+            "import/no-unresolved": "off",
+            "import/first": "off",
+            "no-console": ["error", { allow: ["warn", "error"] }],
+            "no-nested-ternary": "off",
+            "prefer-template": "off",
+            "vue/html-indent": ["error", 4],
+            "vue/order-in-components": "off",
+            "vue/max-attributes-per-line": [
+                "error",
+                {
+                    singleline: {
+                        max: 2,
+                    },
+                    multiline: {
+                        max: 1,
+                    },
+                },
+            ],
+            "vue/multi-word-component-names": "off",
+            "vue/no-multiple-template-root": "off",
+            "vue/no-v-html": "off",
+        },
+    },
+];
