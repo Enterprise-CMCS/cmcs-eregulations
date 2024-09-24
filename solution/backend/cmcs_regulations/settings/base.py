d029e31de16a76ae3f0b90cfe3693f6c399c6368
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2024-09-24 15:49:41 -0400

    fix: only allow SessionAuthentication by default, must specify other schemes as needed (#1428)

--- solution/backend/cmcs_regulations/settings/base.py
+++ solution/backend/cmcs_regulations/settings/base.py
@@ -83,6 +83,9 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 TEMPLATE_CONTEXT_PROCESSORS = (
