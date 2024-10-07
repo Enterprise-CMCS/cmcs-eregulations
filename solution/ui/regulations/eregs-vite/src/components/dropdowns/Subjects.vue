a62c02ed9792906f452a692088d894ccfb92ec55
Phil Reese <philr8@gmail.com>
2024-10-07 14:47:15 -0400

    feat: allow menu close on list item click

--- solution/ui/regulations/eregs-vite/src/components/dropdowns/Subjects.vue
+++ solution/ui/regulations/eregs-vite/src/components/dropdowns/Subjects.vue
@@ -1,8 +1,4 @@
 <script setup>
-import { computed, ref } from "vue";
-
-import { getSubjectName } from "utilities/filters";
-
 import SubjectSelector from "@/components/subjects/SubjectSelector.vue";
 
 const props = defineProps({
@@ -11,13 +7,33 @@ const props = defineProps({
         required: true,
     },
 });
+
+// override v-menu model to programmatically close the menu
+const menuToggleModel = defineModel({
+    type: Boolean,
+    default: false,
+});
+
+const clickHandler = (event) => {
+    const menuItemClicked =
+        event.target.className.includes("sidebar-li__button");
+
+    if (menuItemClicked) {
+        menuToggleModel.value = !menuToggleModel.value;
+    }
+};
 </script>
 
 <template>
     <v-btn id="subjects-activator" variant="outlined" density="compact" flat
-        ><span>Test</span></v-btn
+        ><span>Test {{ model }}</span></v-btn
     >
-    <v-menu activator="#subjects-activator">
+    <v-menu
+        v-model="menuToggleModel"
+        activator="#subjects-activator"
+        :close-on-content-click="false"
+        @click="clickHandler"
+    >
         <SubjectSelector
             :policy-doc-subjects="list"
             class="subjects__select-container--menu"
