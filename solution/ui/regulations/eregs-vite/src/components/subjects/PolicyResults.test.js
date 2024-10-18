e9aeefd75fa00d347863c9e9ae90ba0fc9d1f8b4
Phil Reese <philr8@gmail.com>
2024-10-18 12:44:58 -0400

    test: update assertion to actually test what we need to test

--- solution/ui/regulations/eregs-vite/src/components/subjects/PolicyResults.test.js
+++ solution/ui/regulations/eregs-vite/src/components/subjects/PolicyResults.test.js
@@ -352,23 +352,23 @@ describe("showResultSnippet", () => {
 
 describe("getCollapseName", () => {
     const doc1 = {
-        node_id: "doc1",
-        id: "doc1",
-    };
-
-    const doc2 = {
-        node_id: "doc2",
+        node_id: "doc1-node_id",
+        id: "doc1-id",
     };
 
     it("returns the id if id exists", async () => {
         expect(PolicyResults.getCollapseName(doc1)).toBe(
-            "related citations collapsible doc1"
+            "related citations collapsible doc1-id"
         );
     });
 
+    const doc2 = {
+        node_id: "doc2-node_id",
+    };
+
     it("returns the node_id if no id", async () => {
         expect(PolicyResults.getCollapseName(doc2)).toBe(
-            "related citations collapsible doc2"
+            "related citations collapsible doc2-node_id"
         );
     });
 });
