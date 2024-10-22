4e0f1d12a62242e7d4e9d9abb0c6bff5c0cc54b7
addis-samtek <155009976+addis-samtek@users.noreply.github.com>
2024-10-21 22:01:53 -0400

    verion 1 working

---
+++ cdk-eregs/lib/stacks/ssm-parameter-stack.ts
@@ -0,0 +1,27 @@
+
+import * as cdk from 'aws-cdk-lib';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import { Construct } from 'constructs';
+
+export interface SsmParameterStackProps extends cdk.StackProps {
+  stage: string;
+}
+
+export class SsmParameterStack extends cdk.Stack {
+  public readonly ssmParameters: Record<string, string>;
+
+  constructor(scope: Construct, id: string, props: SsmParameterStackProps) {
+    super(scope, id, props);
+
+    this.ssmParameters = {
+      iamPath: ssm.StringParameter.valueForStringParameter(this, '/account_vars/iam/path'),
+      permissionsBoundaryPolicy: ssm.StringParameter.valueForStringParameter(
+        this,
+        '/account_vars/iam/permissions_boundary_policy'
+      ),
+      logLevel: ssm.StringParameter.valueForStringParameter(this, '/eregulations/text_extractor/log_level'),
+      httpUser: ssm.StringParameter.valueForStringParameter(this, '/eregulations/http/user'),
+      httpPassword: ssm.StringParameter.valueForStringParameter(this, '/eregulations/http/password'),
+    };
+  }
+}
