9dc8701bbbf9f803917da5000cf4c1ccafbf56d5
Caleb Godwin <cgodwin@fearless.tech>
2024-10-28 15:36:22 -0400

    feat: implement hello world test lambda deployed via cdk

---
+++ cdk-test/lib/hello-world-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+// Lambda L2 construct
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+// API Gateway L2 construct
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+export class HelloWorldStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, stage: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const helloWorldLambda = new lambda.Function(this, `cmcs-eregs-${stage}-HelloWorldLambda`, {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    });
+
+    const apiGateway = new apigateway.LambdaRestApi(this, `cmcs-eregs-${stage}-HelloWorldAPI`, {
+      handler: helloWorldLambda,
+      proxy: false,
+    });
+
+    const helloResource = apiGateway.root.addResource('hello');
+    helloResource.addMethod('GET');
+  }
+}
