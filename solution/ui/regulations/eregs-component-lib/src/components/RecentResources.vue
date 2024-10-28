0c69c470de798e33b1cca6cee6f1602db721b32c
Phil Reese <philr8@gmail.com>
2024-10-28 14:25:16 -0400

    feat: remove or update descriptive text where needed

--- solution/ui/regulations/eregs-component-lib/src/components/RecentResources.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/RecentResources.vue
@@ -52,18 +52,16 @@ export default {
         </v-tabs>
         <v-window v-model="tab">
             <v-window-item>
-                <p class="recent-rules-descriptive-text">
-                    Includes 42 CFR 400, 430-460, 600, and 45 CFR 95
-                </p>
                 <RecentChangesContainer
                     :api-url="apiUrl"
                     :categories="categories"
                     type="supplemental"
+                    class="recent-supplemental-content"
                 ></RecentChangesContainer>
             </v-window-item>
             <v-window-item>
                 <p class="recent-rules-descriptive-text">
-                    Includes 42 CFR 400, 430-460, 600, and 45 CFR 95
+                    Includes 42 CFR 400, 430-460, 483, 600; 45 CFR 95, 155-156
                 </p>
                 <RecentChangesContainer
                     :api-url="apiUrl"
