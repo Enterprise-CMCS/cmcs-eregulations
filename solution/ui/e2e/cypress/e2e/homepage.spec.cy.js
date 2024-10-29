e048d092c76a70daa1f6bdd4d56a1a31701e6a54
Phil Reese <philr8@gmail.com>
2024-10-29 13:44:15 -0400

    test: fix failing tests

--- solution/ui/e2e/cypress/e2e/homepage.spec.cy.js
+++ solution/ui/e2e/cypress/e2e/homepage.spec.cy.js
@@ -10,8 +10,8 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
             fixture: "categories.json",
         }).as("categories");
         cy.intercept(
-            "**/v3/resources/public/federal_register_links?page=1&page_size=3**",
-            { fixture: "frdocs.json" }
+            "**/v3/resources/public/federal_register_links?page=1&page_size=5**",
+            { fixture: "frdocs.json" },
         ).as("frdocs");
     });
 
@@ -59,6 +59,14 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
         cy.get(".resources__container .v-tabs")
             .contains("Recent Rules")
             .click({ force: true });
+
+        cy.get(".recent-rules-descriptive-text")
+            .first()
+            .should(($el) => {
+                expect($el.text().trim()).to.equal(
+                    "Includes 42 CFR 400, 430-460, 483, 600; 45 CFR 95, 155-156",
+                );
+            });
 
         cy.get(".related-rule").should("have.length", 7);
         cy.get(".related-rule.ungrouped").then(($els) => {
@@ -235,7 +243,7 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
 
         cy.url().should("eq", Cypress.config().baseUrl + "/45/95/");
         cy.contains(
-            "General Administration—Grant Programs (Public Assistance, Medical Assistance and State Children's Health Insurance Programs)"
+            "General Administration—Grant Programs (Public Assistance, Medical Assistance and State Children's Health Insurance Programs)",
         );
     });
 
@@ -250,7 +258,7 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
         cy.visit("/");
         cy.get(".cta .about-text__container p").should(($el) => {
             expect($el.text().replace(/\s+/g, " ").trim()).to.equal(
-                "eRegulations organizes together regulations, subregulatory guidance, and other related policy materials."
+                "eRegulations organizes together regulations, subregulatory guidance, and other related policy materials.",
             );
         });
     });
@@ -273,7 +281,7 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
         cy.url().should(
             "eq",
             Cypress.config().baseUrl +
-                `/search/?q=%22public%20health%20emergency%22`
+                `/search/?q=%22public%20health%20emergency%22`,
         );
     });
 
@@ -282,13 +290,7 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
         cy.visit("/");
         cy.get(".resources__container").should("exist");
         cy.wait("@categories");
-        cy.get(".recent-rules-descriptive-text")
-            .first()
-            .should(($el) => {
-                expect($el.text().trim()).to.equal(
-                    "Includes 42 CFR 400, 430-460, 600, and 45 CFR 95"
-                );
-            });
+        cy.get(".recent-rules-descriptive-text").should("not.exist");
         cy.get(".resources__container")
             .contains("View More Guidance")
             .should("not.exist");
