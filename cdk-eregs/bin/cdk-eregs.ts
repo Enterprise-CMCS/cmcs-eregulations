9e623ef8f9e95b037595f16bd5611ca1fb52ca49
addis-samtek <155009976+addis-samtek@users.noreply.github.com>
2024-11-19 00:16:55 -0500

    Add text extract stack cdk

--- cdk-eregs/bin/cdk-eregs.ts
+++ cdk-eregs/bin/cdk-eregs.ts
@@ -9,7 +9,7 @@ import { IamPermissionsBoundaryAspect } from '../lib/aspects/iam-permissions-bou
 import { EphemeralRemovalPolicyAspect } from '../lib/aspects/removal-policy-aspect';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { MaintenanceApiStack } from '../lib/stacks/maintainance-stack';
-
+import { TextExtractorStack } from '../lib/stacks/text-extract-stack';
 // import { StackFactory } from '../lib/factories/stack-factory';
 // import { getStackConfigs } from '../config/stack-definition';
 async function main() {
@@ -17,6 +17,17 @@ async function main() {
     // const synthesizerConfigJson = await getParameterValue('/cms/cloud/cdkSynthesizerConfig');
     const synthesizerConfigJson = await getParameterValue('/eregulations/cdk_config');
     const synthesizerConfig = JSON.parse(synthesizerConfigJson);
+    const [vpcId, logLevel, httpUser, httpPassword] = await Promise.all([
+      getParameterValue('/account_vars/vpc/id'),
+      getParameterValue('/eregulations/text_extractor/log_level'),
+      getParameterValue('/eregulations/http/user'),
+      getParameterValue('/eregulations/http/password'),
+    ]);
+     // Get environment configuration
+     const env = { 
+      account: process.env.CDK_DEFAULT_ACCOUNT || process.env.AWS_ACCOUNT_ID, 
+      region: process.env.CDK_DEFAULT_REGION || 'us-east-1'
+    };
     const app = new cdk.App({
       defaultStackSynthesizer: new cdk.DefaultStackSynthesizer(synthesizerConfig),
     });
@@ -99,6 +110,20 @@ async function main() {
     apiConfig: {
       loggingLevel: cdk.aws_apigateway.MethodLoggingLevel.INFO,
     },
+  }, stageConfig);
+  new TextExtractorStack(app, stageConfig.getResourceName('text-extractor'), {
+    env,
+    lambdaConfig: {
+      memorySize: 1024,
+      timeout: 900,
+      reservedConcurrentExecutions: 10,
+    },
+    environmentConfig: {
+      vpcId,
+      logLevel,
+      httpUser,
+      httpPassword,
+    }
   }, stageConfig);
   // Example deployment in app.ts
 
