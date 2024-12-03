2b52ab849eeddf349dfeaa7fbc974a89abd5935e
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2024-12-03 10:02:57 -0500

    fix: only count approved resources in subjects endpoint (#1493)

--- solution/backend/resources/views/subjects.py
+++ solution/backend/resources/views/subjects.py
@@ -9,9 +9,9 @@ from resources.serializers import SubjectWithCountsSerializer
 
 @extend_schema(
     tags=["resources/metadata"],
-    description="Retrieve a list of subjects, each annotated with the count of associated public and internal resources. "
-                "Authenticated users can see the count of both public and internal resources, while unauthenticated users "
-                "only see the count of public resources. The subjects are ordered by a predefined sort order.",
+    description="Retrieve a list of subjects, each annotated with the count of associated approved public and internal "
+                "resources. Authenticated users can see the count of both public and internal resources, while unauthenticated "
+                "users only see the count of public resources. The subjects are ordered by a predefined sort order.",
     responses={200: SubjectWithCountsSerializer(many=True)},
 )
 class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,10 +19,12 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = ViewSetPagination
 
     def get_queryset(self):
+        public_filter = Q(resources__abstractpublicresource__isnull=False) & Q(resources__approved=True)
+        internal_filter = Q(resources__abstractinternalresource__isnull=False) & Q(resources__approved=True)
         return Subject.objects.annotate(**{
-            "public_resources": Count("resources", filter=Q(resources__abstractpublicresource__isnull=False)),
+            "public_resources": Count("resources", filter=public_filter),
             "internal_resources": (
-                Count("resources", filter=Q(resources__abstractinternalresource__isnull=False))
+                Count("resources", filter=internal_filter)
                 if self.request.user.is_authenticated else
                 Value(0)
             ),
