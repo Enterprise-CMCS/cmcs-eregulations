0fcb619658dfc786c159252caf74bed59e3b42c6
Phil Reese <philr8@gmail.com>
2024-12-19 12:59:20 -0500

    test: get utils.js to 80% branch and line coverage

--- solution/ui/regulations/utilities/utils.js
+++ solution/ui/regulations/utilities/utils.js
@@ -286,7 +286,10 @@ const createOneIndexedArray = (length) => {
  * @returns {string} - string with surrounding quotes removed
  */
 const stripQuotes = (string) => {
-    return string.replace(/(^")|("$)/g, "");
+    if (!string || typeof string !== "string") {
+        return string;
+    }
+    return string.replace(/^['"]|['"]$/g, "");
 };
 
 /**
@@ -435,7 +438,16 @@ const formatResourceCategories = ({
 };
 
 const formatDate = (value) => {
+    if (!value) {
+        return "Invalid Date";
+    }
+
     const date = new Date(value);
+
+    if (date.toString() === "Invalid Date") {
+        return date.toString();
+    }
+
     const options = {
         year: "numeric",
         month: "long",
@@ -630,7 +642,7 @@ const getFieldVal = ({ item, fieldName }) => {
     } else if (item?.reg_text) {
         return item.reg_text[fieldName];
     } else {
-        return item[fieldName];
+        return item?.[fieldName];
     }
 };
 
@@ -647,7 +659,10 @@ const getFrDocType = (doc) => {
 const deserializeResult = (obj) => {
     const returnObj = {};
 
-    returnObj.action_type = getFieldVal({ item: obj, fieldName: "action_type" });
+    returnObj.action_type = getFieldVal({
+        item: obj,
+        fieldName: "action_type",
+    });
     returnObj.category = getFieldVal({ item: obj, fieldName: "category" });
     returnObj.cfr_citations = getFieldVal({
         item: obj,
