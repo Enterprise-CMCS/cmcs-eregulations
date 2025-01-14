463c02026fce6b7a6a6f0bd562249aa740621f16
Phil Reese <preese@fearless.tech>
2025-01-14 11:01:30 -0500

    EREGCSC-2004 -- Implement Vitest into our build pipeline (#1513)
    * chore: first pass at adding Vitest tests to exp gh action
    
    * chore: remove prerequisites for test-javascript
    
    * chore: remove npm cache
    
    * chore: add optional dependency to package.json for ci/cd
    
    * chore: build-and-deploy-vue required before notify job
    
    * chore: add updated package-lock to source control
    
    * test: mock vue-router in SubjectSelector test suite
    
    * chore: run build pipeline for all Vue components before testing
    
    * chore: install npm deps without building
    
    * chore: remove superfluous job deps
    
    * chore: install FE deps in separate step before Vitest step
    
    * chore: don't block any jobs with Vitest job
    
    * fix: fix YAML formatting issue
    
    * fix: restore deploy-django to needs matrix for required output
    
    * chore: notify after only build-and-deploy-vue
    
    * chore: remove deploy-go requirement for cypress job
    
    * test: add leeway to flaky JS unit test
    
    * chore: make JS unit testing its own workflow
    
    * chore: remove environment from vitest workflow
    
    * chore: add some badges to README
    
    * chore: update branch name in Build Status badge table
    
    * chore: rearrange badges
    
    * chore: badge tweaks

--- solution/ui/regulations/vitest.config.ts
+++ solution/ui/regulations/vitest.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
                 "**/test/**/*",
                 "**/utilities/**/*",
             ],
+            reportOnFailure: true,
         },
     },
     resolve: {
