78d54c8b5a3a9da7594e835844ab3480bb392eaa
Phil Reese <preese@fearless.tech>
2025-01-22 11:05:18 -0500

    EREGCSC-2739 -- Link sign-in page to account access instructions (#1519)
    * fix: align login page gutters/container with access and about pg
    
    * feat: update sign in link help text
    
    * test: update login page cypress e2e test suite

--- solution/ui/e2e/cypress/e2e/custom-login-page.cy.js
+++ solution/ui/e2e/cypress/e2e/custom-login-page.cy.js
@@ -17,6 +17,28 @@ describe("custom login page", { scrollBehavior: "center" }, () => {
         cy.get(".header--sign-in span.disabled").should("exist");
     });
 
+    it("custom-login-page - renders a custom login page", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/login");
+        cy.get(".login h1").should(
+            "have.text",
+            "Sign in with your CMS account",
+        );
+        cy.get(".login form")
+            .should("exist")
+            .and("have.text", "Continue to CMS IDM");
+        cy.get("p[data-testid='new-user-msg']").should(
+            "have.text",
+            "New to signing in to eRegulations? Get account access.",
+        );
+        cy.get("a[data-testid='new-user-link']")
+            .should("have.text", "Get account access")
+            .and("have.attr", "href")
+            .and("include", "/get-account-access");
+        cy.get("a[data-testid='new-user-link']").click();
+        cy.url().should("include", "/get-account-access");
+    });
+
     it("custom-login-page - jumps to a regulation Part using the jump-to select", () => {
         cy.viewport("macbook-15");
         cy.visit("/login");
