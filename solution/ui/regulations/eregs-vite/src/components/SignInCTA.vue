b5cd9d294db9f6d9a2fa489cb78e7b8b0823c6e0
Phil Reese <philr8@gmail.com>
2025-01-23 13:22:37 -0500

    feat: create basic Sign In CTA component

---
+++ solution/ui/regulations/eregs-vite/src/components/SignInCTA.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    accessUrl: {
+        type: String,
+        required: true,
+    },
+    isAuthenticated: {
+        type: Boolean,
+        required: true,
+    },
+    testId: {
+        type: String,
+        default: "loginSpa",
+    },
+});
+</script>
+
+<template>
+    <div class="div__login-cta">
+        <span :data-testid="testId"
+            >To see internal documents,
+            <slot name="sign-in-link">sign in</slot></span
+        >
+        or
+        <a :href="accessUrl">learn how to get account access</a>.
+    </div>
+</template>
