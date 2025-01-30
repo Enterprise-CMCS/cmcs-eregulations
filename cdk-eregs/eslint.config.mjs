f8edb688f11faa85a6461a734e9f9ff01d2e1211
Phil Reese <philr8@gmail.com>
2025-01-30 15:03:11 -0500

    chore: add basic eslint config to CDK directory

---
+++ cdk-eregs/eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globalConfig from "../eslint-global-rules.mjs";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            ...globalConfig,
+        },
+    },
+];
