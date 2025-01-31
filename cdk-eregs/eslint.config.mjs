dc020a5bdd102e04395628a1238b2799559a97c7
Phil Reese <philr8@gmail.com>
2025-01-31 13:14:17 -0500

    chore: update cdk-eregs ESLint to handle JS better

--- cdk-eregs/eslint.config.mjs
+++ cdk-eregs/eslint.config.mjs
@@ -6,8 +6,14 @@ import globalConfig from "../eslint-global-rules.mjs";
 export default [
     { files: ["**/*.{js,mjs,cjs,ts}"] },
     { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    {
+        files: ["**/*.js"],
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            "indent": ["error", 4, { SwitchCase: 1 }],
+        } 
+    },
     {
         rules: {
             ...globalConfig,
