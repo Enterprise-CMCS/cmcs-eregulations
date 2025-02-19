378ab1a6786e0743e76342cf15d9ca9297a28e1b
Phil Reese <philr8@gmail.com>
2025-02-19 13:50:16 -0500

    feat: acct dropdown menu positioning

--- solution/ui/regulations/eregs-vite/src/components/header/HeaderDropdownMenu.vue
+++ solution/ui/regulations/eregs-vite/src/components/header/HeaderDropdownMenu.vue
@@ -1,13 +1,35 @@
 <script setup>
+import { computed, onMounted, onUnmounted, ref } from "vue";
 const emit = defineEmits(["closeMenu"]);
 
 const outsideClick = () => {
     emit("closeMenu");
 };
+
+const windowWidth = ref(window.innerWidth);
+const appendPxSuffix = (int) => `${int}px`;
+const positionStyle = computed(() => {
+    return windowWidth.value > 1504
+        ? { right: appendPxSuffix((windowWidth.value - 1440) / 2)}
+        : {}
+});
+
+const onWidthChange = () => {
+    windowWidth.value = window.innerWidth;
+};
+
+onMounted(() => {
+    window.addEventListener("resize", onWidthChange);
+});
+onUnmounted(() => window.removeEventListener("resize", onWidthChange));
 </script>
 
 <template>
-    <div v-clickaway="outsideClick" class="dropdown-menu__container">
+    <div
+        v-clickaway="outsideClick"
+        class="dropdown-menu__container"
+        :style="positionStyle"
+    >
         <slot name="dropdown-menu-content" />
     </div>
 </template>
