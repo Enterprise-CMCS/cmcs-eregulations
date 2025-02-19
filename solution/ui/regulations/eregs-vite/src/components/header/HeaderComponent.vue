68d835d7bd818937cd00a42a92c956d0688083c1
Phil Reese <philr8@gmail.com>
2025-02-19 13:18:24 -0500

    feat: header rows centered with auto margin

--- solution/ui/regulations/eregs-vite/src/components/header/HeaderComponent.vue
+++ solution/ui/regulations/eregs-vite/src/components/header/HeaderComponent.vue
@@ -12,23 +12,29 @@ const isLoginPage = window.location.pathname.includes("login");
 <template>
     <div class="header--content-container">
         <div class="header__row header__row--first">
-            <div class="header--brand">
-                <a :href="homeUrl">Medicaid &amp; <br>CHIP eRegulations</a>
+            <div class="row--content__wrapper">
+                <div class="header--brand">
+                    <a :href="homeUrl">Medicaid &amp; <br>CHIP eRegulations</a>
+                </div>
+                <div class="header--search">
+                    <slot name="search" />
+                </div>
+                <div class="header--account">
+                    <div class="header--sign-in" :class="{ active: isLoginPage }">
+                        <slot name="sign-in" />
+                    </div>
+                    <slot name="get-access" />
+                </div>
             </div>
-            <div class="header--search">
-                <slot name="search" />
-            </div>
-            <div class="header--sign-in" :class="{ active: isLoginPage }">
-                <slot name="sign-in" />
-            </div>
-            <slot name="get-access" />
         </div>
         <div class="header__row header__row--second">
-            <div class="header--jump-to">
-                <slot name="jump-to" />
-            </div>
-            <div class="header--links">
-                <slot name="links" />
+            <div class="row--content__wrapper">
+                <div class="header--jump-to">
+                    <slot name="jump-to" />
+                </div>
+                <div class="header--links">
+                    <slot name="links" />
+                </div>
             </div>
         </div>
     </div>
