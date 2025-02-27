2e490b0a39de63d4dbe68560a6566ae3cb7b81cf
Phil Reese <philr8@gmail.com>
2025-02-27 09:44:44 -0500

    chore: restore original vers of files
    based on validation feedback

--- solution/ui/regulations/eregs-vite/src/components/header/HeaderDropdownMenu.vue
+++ solution/ui/regulations/eregs-vite/src/components/header/HeaderDropdownMenu.vue
@@ -1,34 +1,13 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref } from "vue";
 const emit = defineEmits(["closeMenu"]);
 
 const outsideClick = () => {
     emit("closeMenu");
 };
-
-const windowWidth = ref(window.innerWidth);
-const positionStyle = computed(() => {
-    return windowWidth.value > 1504
-        ? { right: `${(windowWidth.value - 1440) / 2}px`}
-        : {}
-});
-
-const onWidthChange = () => {
-    windowWidth.value = window.innerWidth;
-};
-
-onMounted(() => {
-    window.addEventListener("resize", onWidthChange);
-});
-onUnmounted(() => window.removeEventListener("resize", onWidthChange));
 </script>
 
 <template>
-    <div
-        v-clickaway="outsideClick"
-        class="dropdown-menu__container"
-        :style="positionStyle"
-    >
+    <div v-clickaway="outsideClick" class="dropdown-menu__container">
         <slot name="dropdown-menu-content" />
     </div>
 </template>
