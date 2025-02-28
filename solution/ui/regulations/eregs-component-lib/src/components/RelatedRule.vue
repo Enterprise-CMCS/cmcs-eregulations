d3219753700163a96d4bcec4c4366562f7190017
Phil Reese <philr8@gmail.com>
2025-02-28 11:35:49 -0500

    feat: refactor Related Rule

--- solution/ui/regulations/eregs-component-lib/src/components/RelatedRule.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/RelatedRule.vue
@@ -1,3 +1,72 @@
+<script setup>
+/* eslint-disable vue/prop-name-casing */
+import { computed, inject, ref } from "vue";
+
+import { formatDate } from "utilities/utils";
+
+import IndicatorLabel from "./shared-components/results-item-parts/IndicatorLabel.vue";
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    grouped: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    citation: {
+        type: String,
+        required: true,
+    },
+    publication_date: {
+        type: String,
+        default: undefined,
+    },
+    document_number: {
+        type: String,
+        required: true,
+    },
+    html_url: {
+        type: String,
+        required: true,
+    },
+    action: {
+        type: String,
+        default: undefined,
+    },
+});
+
+const itemTitleLineLimit = inject("itemTitleLineLimit", { default: 9 });
+
+const formatPubDate = (value) => {
+    return formatDate(value);
+};
+
+const ruleClasses = computed(() => {
+    return {
+        grouped: props.grouped,
+        ungrouped: !props.grouped,
+    };
+});
+
+const citationClasses = computed(() => {
+    return {
+        grouped: props.grouped,
+    };
+});
+
+const recentTitleClass = computed(() => {
+    return `line-clamp-${itemTitleLineLimit}`;
+});
+
+</script>
+
 <template>
     <div class="related-rule recent-change" :class="ruleClasses">
         <a
@@ -26,81 +95,3 @@
         </a>
     </div>
 </template>
-
-<script>
-/* eslint-disable vue/prop-name-casing */
-import { formatDate } from "utilities/utils";
-
-import IndicatorLabel from "./shared-components/results-item-parts/IndicatorLabel.vue";
-
-export default {
-    name: "RelatedRule",
-
-    components: {
-        IndicatorLabel,
-    },
-
-    inject: {
-        itemTitleLineLimit: { default: 9 },
-    },
-
-    methods: {
-        formatPubDate(value) {
-            return formatDate(value);
-        },
-    },
-
-    props: {
-        title: {
-            type: String,
-            required: true,
-        },
-        type: {
-            type: String,
-            required: true,
-        },
-        grouped: {
-            type: Boolean,
-            required: false,
-            default: false,
-        },
-        citation: {
-            type: String,
-            required: true,
-        },
-        publication_date: {
-            type: String,
-            default: undefined,
-        },
-        document_number: {
-            type: String,
-            required: true,
-        },
-        html_url: {
-            type: String,
-            required: true,
-        },
-        action: {
-            type: String,
-            default: undefined,
-        },
-    },
-
-    computed: {
-        ruleClasses() {
-            return {
-                grouped: this.grouped,
-                ungrouped: !this.grouped,
-            };
-        },
-        citationClasses() {
-            return {
-                grouped: this.grouped,
-            };
-        },
-        recentTitleClass() {
-            return `line-clamp-${this.itemTitleLineLimit}`;
-        },
-    },
-};
-</script>
