9536cf7b5339135edd2021a1e2ab910977128535
Caleb Godwin <cgodwin@fearless.tech>
2025-03-20 20:25:45 -0400

    Rename handler

--- solution/parser/launcher/parser_launcher.py
+++ solution/parser/launcher/parser_launcher.py
@@ -5,7 +5,7 @@ import boto3
 logger = logging.getLogger(__name__)
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     logger.info("Retrieving secrets from AWS Secrets Manager.")
 
     try:
