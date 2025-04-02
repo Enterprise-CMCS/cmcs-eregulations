68d57fc20c2f6ab70806c1ff69fee275b3fb5619
Phil Reese <philr8@gmail.com>
2025-04-02 15:26:25 -0400

    test: add Regulations to sorting python unit tests

--- solution/backend/content_search/tests/test_search.py
+++ solution/backend/content_search/tests/test_search.py
@@ -321,18 +321,35 @@ class SearchTest(TestCase):
         self.assertEqual(results[1]['resource']["date"], "2019-05-12")
 
     # This test ensures that sorted search results put undated resources after dated resources
+    # and Regulations at the end of the list.
     def test_sorting_newest_undated(self):
         self.login()
+
+        # Newest first
         response = self.client.get("/v3/content-search/?q=sort&sort=-date")
         data = get_paginated_data(response)
         results = data["results"]
-        self.assertEqual(len(results), 3)
+        self.assertEqual(len(results), 4)
         self.assertEqual(results[0]['resource']["document_id"], "Test Supplemental 1")
         self.assertEqual(results[0]['resource']["date"], "2023-02-01")
         self.assertEqual(results[1]['resource']["title"], "Internal notes")
         self.assertEqual(results[1]['resource']["date"], "2019-05-12")
         self.assertEqual(results[2]['resource']["document_id"], "Into the unknown")
         self.assertEqual(results[2]['resource']["date"], "")
+        self.assertEqual(results[3]['reg_text']["date"], "2023-01-01")
+
+        # Oldest first
+        response = self.client.get("/v3/content-search/?q=sort&sort=date")
+        data = get_paginated_data(response)
+        results = data["results"]
+        self.assertEqual(len(results), 4)
+        self.assertEqual(results[0]['resource']["title"], "Internal notes")
+        self.assertEqual(results[0]['resource']["date"], "2019-05-12")
+        self.assertEqual(results[1]['resource']["document_id"], "Test Supplemental 1")
+        self.assertEqual(results[1]['resource']["date"], "2023-02-01")
+        self.assertEqual(results[2]['resource']["document_id"], "Into the unknown")
+        self.assertEqual(results[2]['resource']["date"], "")
+        self.assertEqual(results[3]['reg_text']["date"], "2023-01-01")
 
     # This test ensures that reg text is included in the search results, and only if show_regulations is set to true or not set.
     def test_reg_text_search(self):
