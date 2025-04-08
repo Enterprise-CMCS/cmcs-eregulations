856e891fe863fa0bd32e54d0dc18b63ed4f90632
Caleb Godwin <cgodwin@fearless.tech>
2025-04-08 14:30:13 -0400

    fix: date field populate migration to run in atomic blocks of 100 at a time

---
+++ solution/backend/content_search/migrations/0006_populate_date_field.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models, transaction
+from django.db.models import Q
+
+
+def populate_date(apps, schema_editor):
+    ContentIndex = apps.get_model('content_search', 'ContentIndex')
+    q_filter = Q(resource__isnull=False) & ~Q(resource__date="") & Q(date__isnull=True)
+    while ContentIndex.objects.filter(q_filter).exists():
+        with transaction.atomic():
+            print("Updating 100 indices", flush=True)
+            for index in ContentIndex.objects.filter(q_filter)[:100]:
+                index.date = index.resource.date or None
+                index.save()
+
+
+class Migration(migrations.Migration):
+    atomic = False
+
+    dependencies = [
+        ('content_search', '0005_contentindex_date'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_date, reverse_code=migrations.RunPython.noop),
+    ]
