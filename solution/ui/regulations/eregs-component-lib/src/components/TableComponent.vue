19cf2dd0e13315e36b7411afddcdf90449821600
Phil Reese <philr8@gmail.com>
2025-04-29 09:47:57 -0400

    chore: refactor TableComponent.vue with Copilot

--- solution/ui/regulations/eregs-component-lib/src/components/TableComponent.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/TableComponent.vue
@@ -1,23 +1,16 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    table_markup: {
+        type: String,
+        required: true,
+    },
+});
+
+const table = computed(() => props.table_markup);
+</script>
+
 <template>
     <section v-sanitize-html="table" class="table-section" />
 </template>
-
-<script>
-/* eslint-disable vue/prop-name-casing */
-export default {
-    name: "TableComponent",
-
-    props: {
-        table_markup: {
-            type: String,
-            required: true,
-        },
-    },
-
-    computed: {
-        table() {
-            return this.table_markup;
-        },
-    },
-};
-</script>
