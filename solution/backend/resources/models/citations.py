b40da8679aca8e8466d70105db8caf5f0a6b65db
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2025-05-06 14:56:54 -0400

    EREGCSC-2828 Fix for "Lists of CFR citations are out of order" bug (#1658)
    * Use Coalesce(Case(...)) in AbstractCitation to fix ordering
    
    * Create child_id field for proper string-based sorting
    
    child_id populated on save for subpart and section models,
    child_id = subpart_id for subparts,
    child_id = section_id padded to 12 chars (e.g. '000000000123' for section '123').
    
    AbstractCitation ordered by title, then part, then child_id.
    
    Non-atomic migration utilizes multiple transactions each processing 100 citations.
    
    * Linting
    
    * Fix tests, add comment

--- solution/backend/resources/models/citations.py
+++ solution/backend/resources/models/citations.py
@@ -8,6 +8,7 @@ from common.mixins import DisplayNameFieldMixin
 class AbstractCitation(models.Model, DisplayNameFieldMixin):
     title = models.IntegerField()
     part = models.IntegerField()
+    child_id = models.CharField(max_length=12)
 
     objects = InheritanceManager()
 
@@ -15,12 +16,19 @@ class AbstractCitation(models.Model, DisplayNameFieldMixin):
         self.full_clean()
         super().save(*args, **kwargs)
 
+    class Meta:
+        ordering = ["title", "part", "child_id"]
+
 
 class Subpart(AbstractCitation):
     subpart_id = models.CharField(max_length=12)
 
     def __str__(self):
         return f"{self.title} CFR {self.part} Subpart {self.subpart_id}"
+
+    def save(self, *args, **kwargs):
+        self.child_id = self.subpart_id
+        super().save(*args, **kwargs)
 
     def validate_unique(self, exclude=None):
         super().validate_unique(exclude=exclude)
@@ -39,6 +47,13 @@ class Section(AbstractCitation):
 
     def __str__(self):
         return f"{self.title} CFR {self.part}.{self.section_id}"
+
+    def save(self, *args, **kwargs):
+        # Ensure child_id is zero-padded to 12 digits. (12 is arbitrary, but it is the max length of the field.)
+        # int() ensures that the value is an integer, because even though the field is an IntegerField,
+        # it can be passed as a string to the model during create or update.
+        self.child_id = f"{int(self.section_id):012d}"
+        super().save(*args, **kwargs)
 
     def validate_unique(self, exclude=None):
         super().validate_unique(exclude=exclude)
