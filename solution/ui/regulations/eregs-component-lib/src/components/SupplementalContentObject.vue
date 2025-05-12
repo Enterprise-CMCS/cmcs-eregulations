e39b0df1c51d80859cfdc779b69be9ac5e37be7f
Phil Reese <philr8@gmail.com>
2025-05-12 15:55:21 -0400

    feat: add filetype chip to reader/homepage public links

--- solution/ui/regulations/eregs-component-lib/src/components/SupplementalContentObject.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/SupplementalContentObject.vue
@@ -47,15 +47,22 @@ const isBlank = (str) => {
     return !str || /^\s*$/.test(str);
 };
 
-const addFileTypeButton = ({ fileName, uid, docType }) => {
-    if (DOCUMENT_TYPES_MAP[docType] !== "Internal") {
-        return "";
+const addFileTypeButton = ({ fileName, uid, url, docType }) => {
+    if (DOCUMENT_TYPES_MAP[docType] == "Public") {
+        return getFileTypeButton({
+            fileName: url,
+            uid,
+        });
     }
 
-    return getFileTypeButton({
-        fileName,
-        uid,
-    });
+    if (DOCUMENT_TYPES_MAP[docType] == "Internal") {
+        return getFileTypeButton({
+            fileName: url,
+            uid,
+        });
+    }
+
+    return "";
 };
 
 const formatDate = (value) => {
@@ -113,6 +120,7 @@ const getLinkClasses = (docType, description) => {
                             addFileTypeButton({
                                 fileName,
                                 uid,
+                                url,
                                 docType,
                             })
                     "
