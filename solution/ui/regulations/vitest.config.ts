2d8cf43f760bad395c3ca9b095352102bb7c48c3
Phil Reese <philr8@gmail.com>
2025-05-13 09:49:27 -0400

    fix: import defineConfig from vitest to fix exception

--- solution/ui/regulations/vitest.config.ts
+++ solution/ui/regulations/vitest.config.ts
@@ -1,5 +1,5 @@
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import aliases from "./alias.js";
 
 export default defineConfig({
