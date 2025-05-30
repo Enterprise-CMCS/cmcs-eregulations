9812a167deba1dd214d9e6afdd9566a77f7071be
Phil Reese <philr8@gmail.com>
2025-05-30 09:13:36 -0400

    test: add act exception unit test

--- solution/backend/regulations/tests/test_link_statutes.py
+++ solution/backend/regulations/tests/test_link_statutes.py
@@ -10,43 +10,37 @@ class ReplaceCitationTests(TestCase):
     def setUp(self):
         # Common setup for tests
         self.maxDiff = None  # To see full diff in case of failure
-        self.exceptions = {"42": ["1901"]}
         self.link_conversions = {
             "Social Security Act": {
-                "1902": {"title": "42", "usc": "1902"}
+                "1102": {"title": "42", "usc": "1302"}
             }
         }
-
-    def test_replace_usc_citation_generate_url_only_false(self):
-        match = MagicMock()
-        match.group.side_effect = ["6409"]
-        result = replace_usc_citation(match, title="42", exceptions=self.exceptions, generate_url_only=False)
-        expected_link = (
-            '<a target="_blank" rel="noopener noreferrer" class="external" '
-            'href="https://uscode.house.gov/view.xhtml?req=granuleid:USC-prelim-title42-section6409&num=0&edition=prelim">'
-            '6409</a>'
-        )
-        self.assertEqual(result, expected_link)
 
     def test_replace_usc_citation_generate_url_only_true(self):
         match = MagicMock()
         match.group.side_effect = ["6409"]
-        result = replace_usc_citation(match, title="42", exceptions=self.exceptions, generate_url_only=True)
+        result = replace_usc_citation(match, title="42", exceptions={}, generate_url_only=True)
         expected_link = (
             "https://uscode.house.gov/view.xhtml?req=granuleid:USC-prelim-title42-section6409&num=0&edition=prelim"
         )
         self.assertEqual(result, expected_link)
 
+    def test_replace_section_exception(self):
+        match = MagicMock()
+        match.group.side_effect = ["1102"]
+        mock_act = "Social Security Act"
+        mock_statute_ref_exceptions = {"Social Security Act": ["1102"]}
+        mock_exceptions = mock_statute_ref_exceptions.get(mock_act, [])
+        result = replace_section(
+            match, act=mock_act, link_conversions=self.link_conversions, exceptions=mock_exceptions, generate_url_only=True
+        )
+        self.assertEqual(result, "")
+
     def test_replace_section_generate_url_only_false(self):
         match = MagicMock()
         match.group.side_effect = ["1102"]
-        link_conversions_mock = {
-            "Social Security Act": {
-                "1102": {"title": "42", "usc": "1302"}
-            }
-        }
         result = replace_section(
-            match, act="Social Security Act", link_conversions=link_conversions_mock, exceptions=[], generate_url_only=False
+            match, act="Social Security Act", link_conversions=self.link_conversions, exceptions={}, generate_url_only=False
         )
         expected_link = (
             '<a target="_blank" rel="noopener noreferrer" class="external" '
@@ -58,14 +52,9 @@ class ReplaceCitationTests(TestCase):
     def test_replace_section_generate_url_only_true(self):
         match = MagicMock()
         match.group.side_effect = ["1102"]
-        link_conversions_mock = {
-            "Social Security Act": {
-                "1102": {"title": "42", "usc": "1302"}
-            }
-        }
         result = replace_section(
             match,
-            act="Social Security Act", link_conversions=link_conversions_mock, exceptions=self.exceptions, generate_url_only=True
+            act="Social Security Act", link_conversions=self.link_conversions, exceptions={}, generate_url_only=True
         )
         expected_link = (
             "https://uscode.house.gov/view.xhtml?req=granuleid:USC-prelim-title42-section1302&num=0&edition=prelim"
