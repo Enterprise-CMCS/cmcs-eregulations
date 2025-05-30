f97bb1a319497e33939504333ede1c331d1078df
Phil Reese <philr8@gmail.com>
2025-05-30 09:27:33 -0400

    test: add usc exception unit tests; restore removed unit test

--- solution/backend/regulations/tests/test_link_statutes.py
+++ solution/backend/regulations/tests/test_link_statutes.py
@@ -16,6 +16,39 @@ class ReplaceCitationTests(TestCase):
             }
         }
 
+    def test_replace_usc_citation_exception_url_only_false(self):
+        match = MagicMock()
+        match.group.side_effect = ["6409"]
+        mock_usc_ref_exceptions = {"26": ["6409"]}
+        mock_title = "26"
+        mock_exceptions = mock_usc_ref_exceptions.get(mock_title, [])
+        result = replace_usc_citation(
+            match, title=mock_title, exceptions=mock_exceptions, generate_url_only=False
+        )
+        self.assertEqual(result, "6409")
+
+    def test_replace_usc_citation_exception_url_only_true(self):
+        match = MagicMock()
+        match.group.side_effect = ["6409"]
+        mock_usc_ref_exceptions = {"26": ["6409"]}
+        mock_title = "26"
+        mock_exceptions = mock_usc_ref_exceptions.get(mock_title, [])
+        result = replace_usc_citation(
+            match, title=mock_title, exceptions=mock_exceptions, generate_url_only=True
+        )
+        self.assertEqual(result, "")
+
+    def test_replace_usc_citation_generate_url_only_false(self):
+        match = MagicMock()
+        match.group.side_effect = ["6409"]
+        result = replace_usc_citation(match, title="42", exceptions={}, generate_url_only=False)
+        expected_link = (
+            '<a target="_blank" rel="noopener noreferrer" class="external" '
+            'href="https://uscode.house.gov/view.xhtml?req=granuleid:USC-prelim-title42-section6409&num=0&edition=prelim">'
+            '6409</a>'
+        )
+        self.assertEqual(result, expected_link)
+
     def test_replace_usc_citation_generate_url_only_true(self):
         match = MagicMock()
         match.group.side_effect = ["6409"]
@@ -25,7 +58,18 @@ class ReplaceCitationTests(TestCase):
         )
         self.assertEqual(result, expected_link)
 
-    def test_replace_section_exception(self):
+    def test_replace_section_exception_url_only_false(self):
+        match = MagicMock()
+        match.group.side_effect = ["1102"]
+        mock_act = "Social Security Act"
+        mock_statute_ref_exceptions = {"Social Security Act": ["1102"]}
+        mock_exceptions = mock_statute_ref_exceptions.get(mock_act, [])
+        result = replace_section(
+            match, act=mock_act, link_conversions=self.link_conversions, exceptions=mock_exceptions, generate_url_only=False
+        )
+        self.assertEqual(result, "1102")
+
+    def test_replace_section_exception_url_only_true(self):
         match = MagicMock()
         match.group.side_effect = ["1102"]
         mock_act = "Social Security Act"
