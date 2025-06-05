e8f7014ba4b42f60e8073da9a71bec1756891e8e
Caleb Godwin <cgodwin@fearless.tech>
2025-06-05 14:28:44 -0400

    Fix vulnerability in redirect lambda
    Remove ability to redirect to non-root paths which introduced an open redirect vulnerability.
    Now the lambda will _always_ redirect to the homepage. This will force users to update bookmarks, but the pilot domain has been deprecated for some time already.

--- solution/backend/redirect_lambda.py
+++ solution/backend/redirect_lambda.py
@@ -1,13 +1,10 @@
 import os
-from urllib.parse import urljoin
 
 
 def handler(event, context):
-    original_path = event['path']
-
     # Not using "get" because we want to fail if the environment variable is not set
     new_domain = os.environ["CUSTOM_URL"]
-    redirect_url = urljoin(f"https://{new_domain}", original_path)
+    redirect_url = f"https://{new_domain}"
 
     response = {
         'statusCode': 302,
