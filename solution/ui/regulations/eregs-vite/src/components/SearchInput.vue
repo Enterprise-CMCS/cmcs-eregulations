a623d8bc90724197aa262a8176d0215bcf63fadc
Phil Reese <philr8@gmail.com>
2025-06-16 16:08:53 -0400

    chore: tweak aria-label for search input submit btn

--- solution/ui/regulations/eregs-vite/src/components/SearchInput.vue
+++ solution/ui/regulations/eregs-vite/src/components/SearchInput.vue
@@ -129,7 +129,7 @@ watch(
             <template #append-inner>
                 <v-icon
                     icon="mdi-magnify"
-                    :aria-label="`Search for ${searchInputValue}`"
+                    :aria-label="`Search${searchInputValue ? ' for ' + searchInputValue : ''}`"
                     data-testid="search-form-submit"
                     @click="submitForm"
                     @keydown.enter.space.prevent="submitForm"
