b5194a8fd0b9507497c33e658ddf802c9dd82c94
Phil Reese <preese@fearless.tech>
2025-06-17 13:55:51 -0400

    EREGCSC-2968 Enable search from SMM page (#1710)
    * feat: add SignInCTA to Manual page
    
    * feat: very basic search input functionality
    
    * feat: fetch SMM cat ID for use in search redirect
    
    * feat: use internal type for search string
    
    * style: search input width
    
    * chore: move SMM table data to its own file
    
    * feat: disable search input while fetching catId
    
    * chore: tweak aria-label for search input submit btn
    
    * test: add very basic cypress test suite
    
    * test: further table stakes manual page e2e tests
    
    * test: linting
    
    * feat: set SMM category boolean to remove magic number/string on fe
    
    * test: skip search redirect test until shipping story

---
+++ solution/ui/e2e/cypress/e2e/manual.spec.cy.js
@@ -0,0 +1,79 @@
+const username = Cypress.env("TEST_USERNAME");
+const password = Cypress.env("TEST_PASSWORD");
+
+describe("State Medicaid Manual page", { scrollBehavior: "center" }, () => {
+    beforeEach(() => {
+        cy.clearIndexedDB();
+        cy.intercept("/**", (req) => {
+            req.headers["x-automated-test"] = Cypress.env("DEPLOYING");
+        });
+    });
+
+    it("should have rel='noopener noreferrer' on all external links", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/manual/");
+        cy.checkLinkRel();
+    });
+
+    // remove skip after addressing a11y issues
+    it.skip("checks a11y for Get Account Access page", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/manual", { timeout: 60000 });
+        cy.checkLinkRel();
+        cy.injectAxe();
+        cy.checkAccessibility();
+    });
+
+    it("goes to the Subjects page using header link", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/manual/");
+        cy.clickHeaderLink({
+            page: "subjects",
+            label: "Research a Subject",
+            screen: "wide",
+        });
+        cy.url().should("include", "/subjects");
+    });
+
+    it("shows the sign in Call to Action on landing page when not logged in", () => {
+        cy.viewport("macbook-15");
+        cy.visit("/manual", { timeout: 60000 });
+        cy.get(".search__container .login-cta__div").contains(
+            "To search within the manual, sign in or learn how to get account access.",
+        );
+        cy.get("span[data-testid=loginManual] a")
+            .should("have.attr", "href")
+            .and("include", "/login/?next=")
+            .and("include", "/manual/");
+        cy.get("a.access__anchor")
+            .should("have.attr", "href")
+            .and("include", "/get-account-access/");
+
+        cy.eregsLogin({
+            username,
+            password,
+            landingPage: "/",
+        });
+        cy.visit("/manual");
+        cy.get(".subj-landing__container .login-cta__div").should("not.exist");
+    });
+
+    // remove skip in final cleanup story
+    it.skip("should redirect to the Search page with the correct selected subject and filters when a search term is entered", () => {
+        cy.viewport("macbook-15");
+        cy.eregsLogin({ username, password });
+        cy.visit("/manual");
+
+        // Search for a term
+        cy.get("input#main-content").type("mock", { force: true });
+        cy.get('[data-testid="search-form-submit"]').click({
+            force: true,
+        });
+
+        // Assert URL
+        cy.url()
+            .should("include", "/search")
+            .and("include", "type=internal")
+            .and("include", "intcategories=60");
+    });
+});
