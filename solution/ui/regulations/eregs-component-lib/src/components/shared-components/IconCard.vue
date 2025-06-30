90d86da1714f35ebc59d5e8bccd487be93793aa1
Phil Reese <philr8@gmail.com>
2025-06-30 15:01:51 -0400

    feat: first pass at crude cards

---
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/IconCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed, ref } from "vue";
+
+import HomeBook from "spaComponents/svgs/home-book.vue";
+import HomeClipboard from "spaComponents/svgs/home-clipboard.vue";
+import HomeSearch from "spaComponents/svgs/home-search.vue";
+
+const props = defineProps({
+    iconType: {
+        type: String,
+        required: true,
+    },
+});
+
+const iconComponents = {
+    book: HomeBook,
+    clipboard: HomeClipboard,
+    search: HomeSearch,
+};
+
+</script>
+
+<template>
+    <v-card
+        class="icon-card"
+        variant="tonal"
+    >
+        <v-card-title>
+            <div class="icon-card__title-container">
+                <div>
+                    <slot name="card-title">
+                        Default Title
+                    </slot>
+                </div>
+                <component
+                    :is="iconComponents[props.iconType]"
+                    class="icon-card__icon"
+                />
+            </div>
+        </v-card-title>
+        <v-card-text>
+            <slot name="card-content">
+                Default Content
+            </slot>
+        </v-card-text>
+    </v-card>
+</template>
