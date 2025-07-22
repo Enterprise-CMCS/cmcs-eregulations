d66bd4aa8326273f84bdb359eb4aee5042b3de25
Phil Reese <philr8@gmail.com>
2025-07-22 15:08:32 -0400

    chore: add link to SSA page caption

--- solution/ui/regulations/eregs-vite/src/views/Statutes.vue
+++ solution/ui/regulations/eregs-vite/src/views/Statutes.vue
@@ -166,7 +166,14 @@ getStatutesArray();
                 Social Security Act Table of Contents
             </h2>
             <p class="p__description">
-                List the sections of 42 U.S.C. Chapter 7 enacted by the Social Security Act.
+                List the sections of
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="external"
+                    href="https://uscode.house.gov/view.xhtml?req=granuleid%3AUSC-prelim-title42-chapter7&amp;saved=%7CZ3JhbnVsZWlkOlVTQy1wcmVsaW0tdGl0bGU0Mi1jaGFwdGVyNy1mcm9udA%3D%3D%7C%7C%7C0%7Cfalse%7Cprelim&amp;edition=prelim"
+                >42 U.S.C. Chapter 7</a>
+                enacted by the Social Security Act.
             </p>
             <div id="main-content" class="statute__container">
                 <div class="content">
