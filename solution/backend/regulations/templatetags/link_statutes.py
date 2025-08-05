a2d7b4a737e810a723aed72d9ee923d32d3d3702
Phil Reese <philr8@gmail.com>
2025-08-05 14:48:55 -0400

    chore: re-enable other linking types

--- solution/backend/regulations/templatetags/link_statutes.py
+++ solution/backend/regulations/templatetags/link_statutes.py
@@ -21,15 +21,15 @@ def link_statutes(paragraph, link_conversions, link_config):
         paragraph,
     )
 
-    # if link_config["link_statute_refs"]:
-    #     paragraph = STATUTE_REF_REGEX.sub(
-    #         partial(replace_sections, link_conversions=link_conversions, exceptions=link_config["statute_ref_exceptions"]),
-    #         paragraph,
-    #     )
-    # if link_config["link_usc_refs"]:
-    #     paragraph = USC_REF_REGEX.sub(
-    #         partial(replace_usc_citations, exceptions=link_config["usc_ref_exceptions"]),
-    #         paragraph
-    #     )
+    if link_config["link_statute_refs"]:
+        paragraph = STATUTE_REF_REGEX.sub(
+            partial(replace_sections, link_conversions=link_conversions, exceptions=link_config["statute_ref_exceptions"]),
+            paragraph,
+        )
+    if link_config["link_usc_refs"]:
+        paragraph = USC_REF_REGEX.sub(
+            partial(replace_usc_citations, exceptions=link_config["usc_ref_exceptions"]),
+            paragraph
+        )
 
     return paragraph
