8af2d2f93504f04331bd43dfd551ebea2529dff8
Phil Reese <philr8@gmail.com>
2025-08-06 10:35:14 -0400

    feat: match on preceding section symbol

--- solution/backend/regulations/templatetags/link_statutes.py
+++ solution/backend/regulations/templatetags/link_statutes.py
@@ -16,11 +16,6 @@ register = template.Library()
 
 @register.simple_tag
 def link_statutes(paragraph, link_conversions, link_config):
-    paragraph = REGULATION_REF_REGEX.sub(
-        partial(replace_regulation_ref, link_conversions=link_conversions, exceptions=link_config["statute_ref_exceptions"]),
-        paragraph,
-    )
-
     if link_config["link_statute_refs"]:
         paragraph = STATUTE_REF_REGEX.sub(
             partial(replace_sections, link_conversions=link_conversions, exceptions=link_config["statute_ref_exceptions"]),
@@ -31,5 +26,10 @@ def link_statutes(paragraph, link_conversions, link_config):
             partial(replace_usc_citations, exceptions=link_config["usc_ref_exceptions"]),
             paragraph
         )
+
+    paragraph = REGULATION_REF_REGEX.sub(
+        partial(replace_regulation_ref),
+        paragraph,
+    )
 
     return paragraph
