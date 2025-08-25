559c1aef7d91e51a69cadddde5f462fc112f9ac9
Phil Reese <philr8@gmail.com>
2025-08-25 13:43:05 -0400

    feat: first pass at using copy buttons

--- solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/StatuteCitationTable.vue
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/StatuteCitationTable.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CopyCitation from "eregsComponentLib/src/components/tooltips/CopyCitation.vue";
 import { computed } from "vue";
 
 const props = defineProps({
@@ -8,7 +9,10 @@ const props = defineProps({
     },
 });
 
-const citationArr = computed(() => Object.entries(props.citationObj));
+const citationArr = computed(() => {
+    return Object.entries(props.citationObj)
+        .filter(([key, _value]) => key !== "input")
+});
 </script>
 
 <template>
@@ -25,7 +29,11 @@ const citationArr = computed(() => Object.entries(props.citationObj));
             class="more-info__row"
         >
             <div>
-                {{ item[0] }}
+                <CopyCitation
+                    :formatted-citation="item[0] !== 'link' ? item[1] : null"
+                    :link="item[0] === 'link' ? item[1] : null"
+                    :action-type="item[0] === 'link' ? 'link' : 'citation'"
+                />
             </div>
             <div>
                 {{ item[1] }}
