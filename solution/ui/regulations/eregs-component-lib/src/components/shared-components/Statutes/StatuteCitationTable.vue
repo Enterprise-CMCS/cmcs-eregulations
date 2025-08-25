1c6dd6d8b5cb2193f6467d5ad9f359f010231f8b
Phil Reese <philr8@gmail.com>
2025-08-25 10:53:13 -0400

    chore: create statuteCitationTable component

---
+++ solution/ui/regulations/eregs-component-lib/src/components/shared-components/Statutes/StatuteCitationTable.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    citationObj: {
+        type: Object,
+        required: true,
+    },
+});
+
+const citationArr = computed(() => Object.entries(props.citationObj));
+</script>
+
+<template>
+    <div
+        v-if="citationArr.length"
+        class="research__container"
+    >
+        <h3 class="research__title">
+            Citation Link
+        </h3>
+        <div
+            v-for="item in citationArr"
+            :key="item[0]"
+            class="research__row"
+        >
+            {{ item[1] }}
+        </div>
+    </div>
+</template>
