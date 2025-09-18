52eeeae967d8fb178832d3b51210d759b559c102
Phil Reese <philr8@gmail.com>
2025-09-18 14:12:03 -0400

    test: fix failing tests

--- solution/ui/e2e/cypress/e2e/homepage.spec.cy.js
+++ solution/ui/e2e/cypress/e2e/homepage.spec.cy.js
@@ -461,43 +461,43 @@ describe("Homepage", { scrollBehavior: "center" }, () => {
     it("should have an open left nav on load on desktop", () => {
         cy.viewport("macbook-15");
         cy.visit("/");
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
     });
 
     it("should have a closed left nav on load on tablet", () => {
         cy.viewport(800, 1024);
         cy.visit("/");
-        cy.get("nav#leftNav").should("have.attr", "class", "closed");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav closed");
     });
 
     it("should have a closed left nav on mobile", () => {
         cy.viewport("iphone-x");
         cy.visit("/");
-        cy.get("nav#leftNav").should("have.attr", "class", "closed");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav closed");
     });
 
     it("should responsively open and close the left nav if user does not click open/close button", () => {
         cy.viewport("macbook-15");
         cy.visit("/");
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
         cy.viewport(800, 1024);
-        cy.get("nav#leftNav").should("have.attr", "class", "closed");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav closed");
         cy.viewport("macbook-15");
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
     });
 
     it("should keep left nav open if user explicitly expands it, even if screen width changes", () => {
         cy.viewport(800, 1024);
         cy.visit("/");
-        cy.get("nav#leftNav").should("have.attr", "class", "closed");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav closed");
         cy.get("button.nav-toggle__button").click({ force: true });
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
         cy.viewport("macbook-15");
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
         cy.viewport(800, 1024);
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
         cy.viewport("iphone-x");
-        cy.get("nav#leftNav").should("have.attr", "class", "open");
+        cy.get("nav#leftNav").should("have.attr", "class", "toc__nav open");
     });
 
     it("takes you to a google doc when you click the 'Contact the Team' link", () => {
