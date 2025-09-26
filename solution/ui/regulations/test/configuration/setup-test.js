65d4559db5e646f8365f3f8490d77536cc3784f8
Phil Reese <philr8@gmail.com>
2025-09-26 08:19:51 -0400

    chore: remove whatwg-fetch from project

--- solution/ui/regulations/test/configuration/setup-test.js
+++ solution/ui/regulations/test/configuration/setup-test.js
@@ -1,7 +1,7 @@
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { setupServer } from "msw/node";
 import handlers from "../../msw/mocks/handlers";
-import "whatwg-fetch";
+// import "whatwg-fetch";
 
 const server = setupServer(...handlers);
 
