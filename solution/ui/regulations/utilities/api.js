49fbddcf02c2df40d42a057970e57ee84e564890
Phil Reese <philr8@gmail.com>
2025-11-07 14:21:16 -0500

    chore: get csrf token on front end and tweak POST API method

--- solution/ui/regulations/utilities/api.js
+++ solution/ui/regulations/utilities/api.js
@@ -14,6 +14,21 @@ let config = {
     maxRetryCount: 2,
 };
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 const fetchJson = ({
     url,
     options = {},
@@ -180,7 +195,11 @@ function httpApiPost(
         options: {
             method: "POST",
             params,
-            body: JSON.stringify(data),
+            body: data,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-CSRFToken": getCookie("csrftoken"),
+            },
         },
     });
 }
@@ -522,7 +541,6 @@ const getCombinedContent = async ({
 
 const getSemanticSearchResults = async ({ apiUrl, data }) =>
     httpApiPost(`${apiUrl}content-search/`, { data });
-
 
 const getGranularCounts = async ({
     apiUrl,
