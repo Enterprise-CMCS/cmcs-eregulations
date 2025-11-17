ffbd187ddd5bec9fe14ae41aa053b6cd59eda9f9
Phil Reese <philr8@gmail.com>
2025-11-17 16:09:00 -0500

    feat: add section to order_by for v3/statutes

--- solution/backend/regulations/views/statutes.py
+++ solution/backend/regulations/views/statutes.py
@@ -54,7 +54,7 @@ class StatuteLinkConverterViewSet(viewsets.ReadOnlyModelViewSet):
             queryset = queryset.filter(act__iexact=act)
         if title:
             queryset = queryset.filter(statute_title__iexact=title)
-        return queryset.order_by("act", "statute_title", "usc_sort")
+        return queryset.order_by("act", "statute_title", "section", "usc_sort")
 
 
 class ActListSerializer(serializers.Serializer):
