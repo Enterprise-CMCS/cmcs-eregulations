47c59dbc28485812015e84be12c928f2af0c6606
Caleb Godwin <cgodwin@fearless.tech>
2026-01-28 11:00:58 -0500

    linting

--- solution/backend/content_search/models.py
+++ solution/backend/content_search/models.py
@@ -1,5 +1,5 @@
 
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
