b130db1776e5abb60f50d38a7460a2de0a882450
Phil Reese <philr8@gmail.com>
2026-01-30 11:46:34 -0500

    feat: securely restore target=_blank to sanitized links via official DOMPurify lifecycle hook

--- solution/ui/regulations/directives/sanitizeHtml.js
+++ solution/ui/regulations/directives/sanitizeHtml.js
@@ -1,5 +1,13 @@
 import DOMPurify from "dompurify";
 
+DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+    // set all elments owning target to target="_blank"
+    if ("target" in node) {
+        node.setAttribute("target", "_blank");
+        node.setAttribute("rel", "noopener noreferrer");
+    }
+});
+
 // https://vuejs.org/guide/reusability/custom-directives#function-shorthand
 // It's common for a custom directive to have the same behavior for mounted and updated,
 // with no need for the other hooks. In such cases we can define the directive as a function:
