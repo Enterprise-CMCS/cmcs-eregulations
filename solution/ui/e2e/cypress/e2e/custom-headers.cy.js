996e369faa244dd25bdebf434efe3f0448db0a26
Phil Reese <philr8@gmail.com>
2026-02-06 10:18:52 -0500

    test: migrate from Cypress.env to cy.env per 15.10.0 migration guide

--- solution/ui/e2e/cypress/e2e/custom-headers.cy.js
+++ solution/ui/e2e/cypress/e2e/custom-headers.cy.js
@@ -1,32 +1,35 @@
 describe("Custom Request Headers", () => {
     beforeEach(() => {
-        cy.intercept("/**", (req) => {
-            req.headers["x-automated-test"] =
-                Cypress.env("DEPLOYING");
-        }).as("headers");
+        cy.env(["DEPLOYING"]).then(({ DEPLOYING }) => {
+            cy.intercept("/**", (req) => {
+                req.headers["x-automated-test"] = DEPLOYING;
+            }).as("headers");
+        });
     });
 
     it("has custom testing header x-automated-test added to every request", () => {
-        cy.viewport("macbook-15");
+        cy.env(["DEPLOYING"]).then(({ DEPLOYING }) => {
+            cy.viewport("macbook-15");
 
-        // test custom header on explicit URL visit
-        cy.visit("/42/433/");
-        cy.wait("@headers")
-            .its("request.headers")
-            .should(
-                "have.property",
-                "x-automated-test",
-                Cypress.env("DEPLOYING")
-            );
+            // test custom header on explicit URL visit
+            cy.visit("/42/433/");
+            cy.wait("@headers")
+                .its("request.headers")
+                .should(
+                    "have.property",
+                    "x-automated-test",
+                    DEPLOYING
+                );
 
-        // test custom header after form submit directs to new URL
-        cy.get(".toc-section-number").contains("433.50").click({ force: true });
-        cy.wait("@headers")
-            .its("request.headers")
-            .should(
-                "have.property",
-                "x-automated-test",
-                Cypress.env("DEPLOYING")
-            );
+            // test custom header after form submit directs to new URL
+            cy.get(".toc-section-number").contains("433.50").click({ force: true });
+            cy.wait("@headers")
+                .its("request.headers")
+                .should(
+                    "have.property",
+                    "x-automated-test",
+                    DEPLOYING
+                );
+        });
     });
 });
