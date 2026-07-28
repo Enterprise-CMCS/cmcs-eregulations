47f0d0312e78404bd76e1845806db042283cb192
Caleb Godwin <75450124+cgodwin1@users.noreply.github.com>
2026-07-28 10:41:57 -0400

    New parser initial infrastructure (#2208)
    Co-authored-by: Caleb Godwin <cgodwin@fearless.tech>

---
+++ cdk-eregs/lib/stacks/ecfr-parser-stack.ts
@@ -0,0 +1,126 @@
+import * as cdk from 'aws-cdk-lib';
+import {
+    aws_lambda as lambda,
+    aws_logs as logs,
+    aws_sqs as sqs,
+    aws_events as events,
+    aws_events_targets as targets,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { StageConfig } from '../../config/stage-config';
+import * as path from 'path';
+
+interface LambdaConfig {
+    memorySize: number;
+    timeout: number;
+}
+
+interface EnvironmentConfig {
+    logLevel: string;
+}
+
+export interface EcfrParserStackProps extends cdk.StackProps {
+    lambdaConfig: LambdaConfig;
+    environmentConfig: EnvironmentConfig;
+}
+
+export class EcfrParserStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: EcfrParserStackProps, stageConfig: StageConfig) {
+        super(scope, id, props);
+
+        const siteEndpoint = cdk.Fn.importValue(stageConfig.getResourceName('api-endpoint'));
+
+        const deadLetterQueue = new sqs.Queue(this, 'EcfrParserDeadLetterQueue', {
+            queueName: stageConfig.getResourceName('ecfr-parser-dlq'),
+            retentionPeriod: cdk.Duration.days(14),
+        });
+
+        const queue = new sqs.Queue(this, 'EcfrParserQueue', {
+            queueName: stageConfig.getResourceName('ecfr-parser-queue'),
+            visibilityTimeout: cdk.Duration.seconds(900),
+            retentionPeriod: cdk.Duration.days(14),
+            deadLetterQueue: {
+                queue: deadLetterQueue,
+                maxReceiveCount: 5,
+            },
+        });
+
+        new logs.LogGroup(this, 'EcfrParserWorkerLogGroup', {
+            logGroupName: stageConfig.aws.lambda('ecfr-parser-worker'),
+            retention: logs.RetentionDays.INFINITE,
+        });
+
+        new logs.LogGroup(this, 'EcfrParserLauncherLogGroup', {
+            logGroupName: stageConfig.aws.lambda('ecfr-parser-launcher'),
+            retention: logs.RetentionDays.INFINITE,
+        });
+
+        const worker = new lambda.DockerImageFunction(this, 'EcfrParserWorkerFunction', {
+            functionName: stageConfig.getResourceName('ecfr-parser-worker'),
+            code: lambda.DockerImageCode.fromImageAsset(path.resolve(__dirname, '../../../solution/'), {
+                file: 'parsers/ecfr-worker/Dockerfile',
+            }),
+            memorySize: props.lambdaConfig.memorySize,
+            timeout: cdk.Duration.seconds(props.lambdaConfig.timeout),
+            environment: {
+                LOG_LEVEL: props.environmentConfig.logLevel,
+                EREGS_API_URL_V3: `${siteEndpoint}v3/`,
+            },
+        });
+
+        const launcher = new lambda.DockerImageFunction(this, 'EcfrParserLauncherFunction', {
+            functionName: stageConfig.getResourceName('ecfr-parser-launcher'),
+            code: lambda.DockerImageCode.fromImageAsset(path.resolve(__dirname, '../../../solution/'), {
+                file: 'parsers/ecfr-launcher/Dockerfile',
+            }),
+            memorySize: props.lambdaConfig.memorySize,
+            timeout: cdk.Duration.seconds(props.lambdaConfig.timeout),
+            environment: {
+                LOG_LEVEL: props.environmentConfig.logLevel,
+                EREGS_API_URL_V3: `${siteEndpoint}v3/`,
+                PARSER_QUEUE_URL: queue.queueUrl,
+            },
+        });
+
+        queue.grantConsumeMessages(worker);
+        queue.grantSendMessages(launcher);
+
+        new lambda.EventSourceMapping(this, 'EcfrParserWorkerEventSource', {
+            target: worker,
+            batchSize: 1,
+            eventSourceArn: queue.queueArn,
+            enabled: true,
+        });
+
+        const rule = new events.Rule(this, 'EcfrParserLauncherSchedule', {
+            schedule: events.Schedule.expression('cron(0 0 * * ? *)'),
+            enabled: true,
+        });
+        rule.addTarget(new targets.LambdaFunction(launcher));
+
+        const outputs: Record<string, cdk.CfnOutputProps> = {
+            EcfrParserQueueArn: {
+                value: queue.queueArn,
+                exportName: stageConfig.getResourceName('ecfr-parser-queue-arn'),
+            },
+            EcfrParserQueueUrl: {
+                value: queue.queueUrl,
+                exportName: stageConfig.getResourceName('ecfr-parser-queue-url'),
+            },
+            EcfrParserDlqArn: {
+                value: deadLetterQueue.queueArn,
+                exportName: stageConfig.getResourceName('ecfr-parser-dlq-arn'),
+            },
+            EcfrParserWorkerLambdaArn: {
+                value: worker.functionArn,
+                exportName: stageConfig.getResourceName('ecfr-parser-worker-lambda-arn'),
+            },
+            EcfrParserLauncherLambdaArn: {
+                value: launcher.functionArn,
+                exportName: stageConfig.getResourceName('ecfr-parser-launcher-lambda-arn'),
+            },
+        };
+
+        Object.entries(outputs).forEach(([name, cfg]) => new cdk.CfnOutput(this, name, cfg));
+    }
+}
---
+++ cdk-eregs/lib/stacks/fr-parser-stack.ts
@@ -0,0 +1,126 @@
+import * as cdk from 'aws-cdk-lib';
+import {
+    aws_lambda as lambda,
+    aws_logs as logs,
+    aws_sqs as sqs,
+    aws_events as events,
+    aws_events_targets as targets,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { StageConfig } from '../../config/stage-config';
+import * as path from 'path';
+
+interface LambdaConfig {
+    memorySize: number;
+    timeout: number;
+}
+
+interface EnvironmentConfig {
+    logLevel: string;
+}
+
+export interface FrParserStackProps extends cdk.StackProps {
+    lambdaConfig: LambdaConfig;
+    environmentConfig: EnvironmentConfig;
+}
+
+export class FrParserStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: FrParserStackProps, stageConfig: StageConfig) {
+        super(scope, id, props);
+
+        const siteEndpoint = cdk.Fn.importValue(stageConfig.getResourceName('api-endpoint'));
+
+        const deadLetterQueue = new sqs.Queue(this, 'FrParserDeadLetterQueue', {
+            queueName: stageConfig.getResourceName('fr-parser-dlq'),
+            retentionPeriod: cdk.Duration.days(14),
+        });
+
+        const queue = new sqs.Queue(this, 'FrParserQueue', {
+            queueName: stageConfig.getResourceName('fr-parser-queue'),
+            visibilityTimeout: cdk.Duration.seconds(900),
+            retentionPeriod: cdk.Duration.days(14),
+            deadLetterQueue: {
+                queue: deadLetterQueue,
+                maxReceiveCount: 5,
+            },
+        });
+
+        new logs.LogGroup(this, 'FrParserWorkerLogGroup', {
+            logGroupName: stageConfig.aws.lambda('fr-parser-worker'),
+            retention: logs.RetentionDays.INFINITE,
+        });
+
+        new logs.LogGroup(this, 'FrParserLauncherLogGroup', {
+            logGroupName: stageConfig.aws.lambda('fr-parser-launcher'),
+            retention: logs.RetentionDays.INFINITE,
+        });
+
+        const worker = new lambda.DockerImageFunction(this, 'FrParserWorkerFunction', {
+            functionName: stageConfig.getResourceName('fr-parser-worker'),
+            code: lambda.DockerImageCode.fromImageAsset(path.resolve(__dirname, '../../../solution/'), {
+                file: 'parsers/fr-worker/Dockerfile',
+            }),
+            memorySize: props.lambdaConfig.memorySize,
+            timeout: cdk.Duration.seconds(props.lambdaConfig.timeout),
+            environment: {
+                LOG_LEVEL: props.environmentConfig.logLevel,
+                EREGS_API_URL_V3: `${siteEndpoint}v3/`,
+            },
+        });
+
+        const launcher = new lambda.DockerImageFunction(this, 'FrParserLauncherFunction', {
+            functionName: stageConfig.getResourceName('fr-parser-launcher'),
+            code: lambda.DockerImageCode.fromImageAsset(path.resolve(__dirname, '../../../solution/'), {
+                file: 'parsers/fr-launcher/Dockerfile',
+            }),
+            memorySize: props.lambdaConfig.memorySize,
+            timeout: cdk.Duration.seconds(props.lambdaConfig.timeout),
+            environment: {
+                LOG_LEVEL: props.environmentConfig.logLevel,
+                EREGS_API_URL_V3: `${siteEndpoint}v3/`,
+                PARSER_QUEUE_URL: queue.queueUrl,
+            },
+        });
+
+        queue.grantConsumeMessages(worker);
+        queue.grantSendMessages(launcher);
+
+        new lambda.EventSourceMapping(this, 'FrParserWorkerEventSource', {
+            target: worker,
+            batchSize: 1,
+            eventSourceArn: queue.queueArn,
+            enabled: true,
+        });
+
+        const rule = new events.Rule(this, 'FrParserLauncherSchedule', {
+            schedule: events.Schedule.expression('cron(5 0 * * ? *)'),
+            enabled: true,
+        });
+        rule.addTarget(new targets.LambdaFunction(launcher));
+
+        const outputs: Record<string, cdk.CfnOutputProps> = {
+            FrParserQueueArn: {
+                value: queue.queueArn,
+                exportName: stageConfig.getResourceName('fr-parser-queue-arn'),
+            },
+            FrParserQueueUrl: {
+                value: queue.queueUrl,
+                exportName: stageConfig.getResourceName('fr-parser-queue-url'),
+            },
+            FrParserDlqArn: {
+                value: deadLetterQueue.queueArn,
+                exportName: stageConfig.getResourceName('fr-parser-dlq-arn'),
+            },
+            FrParserWorkerLambdaArn: {
+                value: worker.functionArn,
+                exportName: stageConfig.getResourceName('fr-parser-worker-lambda-arn'),
+            },
+            FrParserLauncherLambdaArn: {
+                value: launcher.functionArn,
+                exportName: stageConfig.getResourceName('fr-parser-launcher-lambda-arn'),
+            },
+        };
+
+        Object.entries(outputs).forEach(([name, cfg]) => new cdk.CfnOutput(this, name, cfg));
+    }
+}
