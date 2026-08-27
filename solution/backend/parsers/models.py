36882a8e4cd201090c5ba01aec200b9ab61c4d97
Caleb Godwin <cgodwin@fearless.com>
2026-08-27 13:06:32 -0400

    Linting

--- solution/backend/parsers/models.py
+++ solution/backend/parsers/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator, RegexValidator
+from django.core.validators import RegexValidator
 from django.db import models
 from solo.models import SingletonModel
 
