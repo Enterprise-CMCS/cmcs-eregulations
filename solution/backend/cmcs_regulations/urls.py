0649a250ddeca9eef3065d9f1f08515b1f7f5e51
Caleb Godwin <cgodwin@fearless.com>
2026-09-24 15:26:35 -0400

    Remove obsolete JWT token endpoints

--- solution/backend/cmcs_regulations/urls.py
+++ solution/backend/cmcs_regulations/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 from django.views.generic.base import RedirectView, TemplateView
-from rest_framework_simplejwt import views as jwt_views
 
 from regulations.sitemap import PartSitemap
 from regulations.views.login import AdminLoginView
@@ -37,8 +36,6 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('oidc/', include('mozilla_django_oidc.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 admin.site.site_header = "eRegs"
 admin.site.site_title = 'eRegs Admin Panel'
